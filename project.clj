265011c257c9634f2daab9669d27d55364d5c02f
Jon Pither <jon.pither@gmail.com>
2018-03-19 12:10:01 +0000

    Lein project for now, and initial test

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ding "0.1.0-SNAPSHOT"
+  :description "A Clojure library that gives graph query over RocksDB"
+  :url "https://github.com/juxt/ding"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.taoensso/nippy "2.13.0"]
+                 [org.rocksdb/rocksdbjni "5.11.3"]])
---
+++ test/juxt/ding/rocks_test.clj
@@ -0,0 +1,5 @@
+(ns juxt.rocks-test
+  (:require [clojure.test :as t]))
+
+(t/deftest test-hello-world
+  (t/is true))
