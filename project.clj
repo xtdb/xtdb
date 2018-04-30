581caa4efeb9ff8eef224722cc57af7ff69fd521
Jon Pither <jon.pither@gmail.com>
2018-04-30 16:44:00 +0100

    Warn on reflection, add bench to dev src paths

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [com.taoensso/nippy "2.13.0"]
                  [org.rocksdb/rocksdbjni "5.11.3"]
                  [gloss "0.2.6"]]
-  :profiles {:dev {:dependencies [[clj-time "0.14.2"]]}}
+  :profiles {:dev {:dependencies [[clj-time "0.14.2"]]
+                   :source-paths ["bench"]}}
+  :global-vars {*warn-on-reflection* true}
   :jvm-opts ~(vec (remove nil?
                           [(when (System/getenv "USE_YOURKIT_AGENT")
                              (when-let [path (first (filter #(.exists (clojure.java.io/as-file %))
