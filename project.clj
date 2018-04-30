53576175f1ec2e10ed0e9e75e5cf218d2066c55c
Jon Pither <jon.pither@gmail.com>
2018-04-30 16:15:18 +0100

    YourKit profiling

--- project.clj
+++ project.clj
@@ -7,4 +7,9 @@
                  [com.taoensso/nippy "2.13.0"]
                  [org.rocksdb/rocksdbjni "5.11.3"]
                  [gloss "0.2.6"]]
-  :profiles {:dev {:dependencies [[clj-time "0.14.2"]]}})
+  :profiles {:dev {:dependencies [[clj-time "0.14.2"]]}}
+  :jvm-opts ~(vec (remove nil?
+                          [(when (System/getenv "USE_YOURKIT_AGENT")
+                             (when-let [path (first (filter #(.exists (clojure.java.io/as-file %))
+                                                            ["/home/jon/dev/yourkit/bin/linux-x86-64/libyjpagent.so"]))]
+                               (str "-agentpath:" path)))])))
