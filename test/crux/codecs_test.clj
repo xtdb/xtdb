77d1ea5c754fd31edc96bf2915e77e548e6aff65
Jon Pither <jon.pither@gmail.com>
2018-05-08 08:26:36 +0100

    test clean up

--- test/crux/codecs_test.clj
+++ test/crux/codecs_test.clj
@@ -1,7 +1,7 @@
 (ns crux.codecs-test
-  (:require [crux.codecs :refer :all]
-            [clojure.test :as t])
-  (:import [java.nio ByteBuffer]))
+  (:require [clojure.test :as t]
+            [crux.codecs :refer :all])
+  (:import java.nio.ByteBuffer))
 
 (defn- round-trip [f m]
   (decode f #^bytes (.array ^ByteBuffer (encode f m))))
@@ -12,11 +12,11 @@
       (encode f {:a 1 :b 2}))
 
     (t/testing "Can encode/decode vanilla frame"
-      (t/is (= {:a 1 :b 2} (decode f (.array ^ByteBuffer (encode f {:a 1 :b 2})))))))
+      (t/is (= {:a 1 :b 2} (round-trip f {:a 1 :b 2})))))
 
   (t/testing "Can encode/decode exotic frame"
     (let [f (compile-frame :a :int32 :b :md5)]
-      (t/is (= 1 (:a (decode f (.array ^ByteBuffer (encode f {:a 1 :b "sad"})))))))))
+      (t/is (= 1 (:a (round-trip f {:a 1 :b "sad"})))))))
 
 (t/deftest test-prefix-codecs
   (let [f1 (compile-frame :a :int32 :b :int32)
@@ -24,13 +24,13 @@
         f (compile-header-frame [:a :int32] {1 f1 2 f2})]
 
     (t/testing "Can encode/decode prefixed frame"
-      (t/is (= {:a 1 :b 2} #^bytes (decode f #^bytes (.array ^ByteBuffer (encode f {:a 1 :b 2})))))
-      (t/is (= {:a 2 :c 2} #^bytes (decode f #^bytes (.array ^ByteBuffer (encode f {:a 2 :c 2}))))))))
+      (t/is (= {:a 1 :b 2} (round-trip f {:a 1 :b 2})))
+      (t/is (= {:a 2 :c 2} (round-trip f {:a 2 :c 2}))))))
 
 (t/deftest test-enums
   (let [e (compile-enum :foo :tar)
         f (compile-frame :a :int32 :b e)]
-    (t/is (= {:a 1, :b :foo} (decode f #^bytes (.array ^ByteBuffer (encode f {:a 1 :b :foo})))))))
+    (t/is (= {:a 1, :b :foo} (round-trip f {:a 1 :b :foo})))))
 
 (t/deftest test-prefix-uses-enum
   (let [e (compile-enum :foo :tar)
@@ -44,13 +44,9 @@
 
 (t/deftest test-various-datatypes
   (t/testing "String datatype"
-    (let [f (compile-frame  :a :string)
-          m {:a "hello"}
-          encoded (encode f m)]
-      (t/is (= {:a "hello"} (decode f #^bytes (.array ^ByteBuffer encoded))))))
+    (let [f (compile-frame  :a :string)]
+      (t/is (= {:a "hello"} (round-trip f {:a "hello"})))))
 
   (t/testing "Keyword datatype"
-    (let [f (compile-frame  :a :keyword)
-          m {:a :bob}
-          encoded (encode f m)]
-      (t/is (= {:a :bob} (decode f #^bytes (.array ^ByteBuffer encoded)))))))
+    (let [f (compile-frame  :a :keyword)]
+      (t/is (= {:a :bob} (round-trip f {:a :bob}))))))
