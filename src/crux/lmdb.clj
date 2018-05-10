fb5b79576d718793b82e207a14ebd591572c0613
Håkan Råberg <hakan.raberg@gmail.com>
2018-05-10 16:57:07 +0100

    Handling LMDB byte array key values, not necessarily in an efficient way.

--- src/crux/lmdb.clj
+++ src/crux/lmdb.clj
@@ -1,47 +1,24 @@
 (ns crux.lmdb
-  (:require [crux.byte-utils :as bu]
+  (:require [clojure.java.io :as io]
+            [crux.byte-utils :as bu]
             [crux.kv-store :as ks])
   (:import [java.io File]
+           [java.nio ByteBuffer]
            [org.lwjgl.system MemoryStack MemoryUtil]
            [org.lwjgl.util.lmdb LMDB MDBVal]))
-
-(defrecord CruxLMDBKv [db-name db]
-  ks/CruxKvStore
-  (open [this]
-    )
-
-  (seek [{:keys [db]} k]
-    )
-
-  (seek-and-iterate [{:keys [db]} k upper-bound]
-    )
-
-  (seek-and-iterate-bounded [{:keys [db]} k]
-    )
-
-  (store [{:keys [db]} k v])
-
-  (merge! [{:keys [db]} k v])
-
-  (close [_])
-
-  (destroy [this]))
-
-(defn crux-lmdb-kv [db-name]
-  (map->CruxLMDBKv {:db-name db-name}))
 
 (defn success? [rc]
   (when-not (= LMDB/MDB_SUCCESS rc)
     (throw (IllegalStateException. (LMDB/mdb_strerror rc)))))
 
 (defn transaction [env f]
-  (with-open [s (MemoryStack/stackPush)]
-    (let [pp (.mallocPointer s 1)]
+  (with-open [stack (MemoryStack/stackPush)]
+    (let [pp (.mallocPointer stack 1)]
       (success? (LMDB/mdb_txn_begin env MemoryUtil/NULL 0 pp))
       (let [txn (.get pp)
             [result
              error] (try
-                      [(f s txn)
+                      [(f stack txn)
                        (LMDB/mdb_txn_commit txn)]
                       (catch Throwable t
                         (LMDB/mdb_txn_abort txn)
@@ -50,8 +27,8 @@
         result))))
 
 (defn env-create []
-  (with-open [s (MemoryStack/stackPush)]
-    (let [pp (.mallocPointer s 1)]
+  (with-open [stack (MemoryStack/stackPush)]
+    (let [pp (.mallocPointer stack 1)]
       (success? (LMDB/mdb_env_create pp))
       (.get pp))))
 
@@ -64,29 +41,82 @@
 (defn dbi-open [env]
   (transaction
    env
-   (fn [^MemoryStack s ^long txn]
-     (let [ip (.mallocInt s 1)
+   (fn [^MemoryStack stack ^long txn]
+     (let [ip (.mallocInt stack 1)
            ^CharSequence name nil]
-       (success? (LMDB/mdb_dbi_open txn name LMDB/MDB_INTEGERKEY ip))
+       (success? (LMDB/mdb_dbi_open txn name 0 ip))
        (.get ip 0)))))
 
-(defn put-int->string [env dbi k v]
-  (transaction
-   env
-   (fn [^MemoryStack s ^long txn]
-     (let [kv (.mv_data (MDBVal/callocStack s)
-                        (.putInt (.malloc s 4) 0 k))
-           dv (.mv_data (MDBVal/callocStack s)
-                        (.UTF8 s v false))]
-       (success? (LMDB/mdb_put txn dbi kv dv 0))))))
+(defn put-bytes->bytes-in-txn [^MemoryStack stack txn dbi ^bytes k ^bytes v]
+  (let [kb (.flip (.put (.malloc stack (alength k)) k))
+        kv (.mv_data (MDBVal/callocStack stack) kb)
+        vb (.flip (.put (.malloc stack (alength v)) v))
+        dv (.mv_data (MDBVal/callocStack stack) vb)]
+    (success? (LMDB/mdb_put txn dbi kv dv 0))))
 
-(defn get-int->string [env dbi k]
+(defn put-bytes->bytes [env dbi ^bytes k ^bytes v]
   (transaction
    env
-   (fn [^MemoryStack s ^long txn]
-     (let [kv (.mv_data (MDBVal/callocStack s)
-                        (.putInt (.malloc s 4) 0 k))
-           dv (MDBVal/callocStack s)]
-       (success? (LMDB/mdb_get txn dbi kv dv))
-       (some-> (.mv_data dv)
-               (MemoryUtil/memUTF8))))))
+   (fn [^MemoryStack stack ^long txn]
+     (put-bytes->bytes-in-txn stack txn dbi k v))))
+
+(defn get-bytes->bytes-in-txn ^bytes [^MemoryStack stack txn dbi ^bytes k]
+  (let [kb (.flip (.put (.malloc stack (alength k)) k))
+        kv (.mv_data (MDBVal/callocStack stack) kb)
+        dv (MDBVal/callocStack stack)]
+    (success? (LMDB/mdb_get txn dbi kv dv))
+    (let [bb (.mv_data dv)]
+      (doto (byte-array (.remaining bb))
+        (->> (.get bb))))))
+
+(defn get-bytes->bytes ^bytes [env dbi ^bytes k]
+  (transaction
+   env
+   (fn [^MemoryStack stack ^long txn]
+     (get-bytes->bytes-in-txn stack txn dbi k))))
+
+;; TODO: this should be configurable.
+(defn- db-path [db-name]
+  (str "/tmp/" (name db-name) ".db"))
+
+(defrecord CruxLMDBKv [db-name env dbi]
+  ks/CruxKvStore
+  (open [this]
+    (let [env (env-create)]
+      (env-open env (io/file (db-path db-name)))
+      (assoc this :env env :dbi (dbi-open env))))
+
+  (seek [_ k]
+    (get-bytes->bytes env dbi k))
+
+  (seek-and-iterate [_ k upper-bound]
+    )
+
+  (seek-and-iterate-bounded [_ k]
+    )
+
+  (store [_ k v]
+    (put-bytes->bytes env dbi k v))
+
+  (merge! [_ k v]
+    (transaction
+     env
+     (fn [^MemoryStack stack ^long txn]
+       (put-bytes->bytes-in-txn
+        stack
+        txn
+        dbi
+        (+ (bu/bytes->long (get-bytes->bytes-in-txn stack txn dbi k))
+           (bu/bytes->long v))))))
+
+  (close [_]
+    (env-close env))
+
+  (destroy [this]
+    (transaction
+     env
+     (fn [_ txn]
+       (success? (LMDB/mdb_drop txn dbi true))))))
+
+(defn crux-lmdb-kv [db-name]
+  (map->CruxLMDBKv {:db-name db-name}))
--- test/crux/lmdb_test.clj
+++ test/crux/lmdb_test.clj
@@ -1,7 +1,9 @@
 (ns crux.lmdb-test
   (:require [clojure.test :as t]
+            [crux.byte-utils :as bu]
             [crux.test-utils :as tu]
             [crux.lmdb :as lmdb]))
+
 ;; Based on
 ;; https://github.com/LWJGL/lwjgl3/blob/master/modules/samples/src/test/java/org/lwjgl/demo/util/lmdb/LMDBDemo.java
 (t/deftest test-lmdb-demo []
@@ -10,8 +12,11 @@
     (try
       (lmdb/env-open env db-dir)
       (let [dbi (lmdb/dbi-open env)]
-        (lmdb/put-int->string env dbi 1 "LMDB")
-        (t/is (= "LMDB" (lmdb/get-int->string env dbi 1))))
+        (lmdb/put-bytes->bytes env
+                               dbi
+                               (bu/long->bytes 1)
+                               (.getBytes "LMDB"))
+        (t/is (= "LMDB" (String. (lmdb/get-bytes->bytes env dbi (bu/long->bytes 1))))))
       (finally
         (tu/delete-dir db-dir)
         (lmdb/env-close env)))))
