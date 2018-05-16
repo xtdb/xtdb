36cadb97bcbb14bcc780d25e623035cde4c64a0e
Håkan Råberg <hakan.raberg@gmail.com>
2018-05-16 11:21:52 +0100

    Growing LMDB map on demand.

--- src/crux/lmdb.clj
+++ src/crux/lmdb.clj
@@ -1,37 +1,59 @@
 (ns crux.lmdb
   (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
             [crux.byte-utils :as bu]
             [crux.kv-store :as ks]
             [crux.io :as cio])
   (:import [java.io Closeable]
            [java.nio ByteBuffer]
+           [clojure.lang ExceptionInfo]
            [org.lwjgl.system MemoryStack MemoryUtil]
-           [org.lwjgl.util.lmdb LMDB MDBVal]))
+           [org.lwjgl.util.lmdb LMDB MDBVal MDBEnvInfo]))
 
 ;; Based on
 ;; https://github.com/LWJGL/lwjgl3/blob/master/modules/samples/src/test/java/org/lwjgl/demo/util/lmdb/LMDBDemo.java
 
 (defn success? [rc]
   (when-not (= LMDB/MDB_SUCCESS rc)
-    (throw (IllegalStateException. (LMDB/mdb_strerror rc)))))
+    (throw (ex-info (LMDB/mdb_strerror rc) {:error rc}))))
+
+(defn env-set-mapsize [^long env ^long size]
+  (success? (LMDB/mdb_env_set_mapsize env size)))
+
+(defn increase-mapsize [env ^long factor]
+  (with-open [stack (MemoryStack/stackPush)]
+    (let [info (MDBEnvInfo/callocStack stack)]
+      (success? (LMDB/mdb_env_info env info))
+      (let [new-mapsize (* factor (.me_mapsize info))]
+        (log/warn "Increasing mapsize to:" new-mapsize)
+        (env-set-mapsize env new-mapsize)))))
 
 (defn transaction
   ([env f]
    (transaction env f LMDB/MDB_RDONLY))
   ([env f flags]
-   (with-open [stack (MemoryStack/stackPush)]
-     (let [pp (.mallocPointer stack 1)]
-       (success? (LMDB/mdb_txn_begin env MemoryUtil/NULL flags pp))
-       (let [txn (.get pp)
-             [result
-              error] (try
-                       [(f stack txn)
-                        (LMDB/mdb_txn_commit txn)]
-                       (catch Throwable t
-                         (LMDB/mdb_txn_abort txn)
-                         (throw t)))]
-         (success? error)
-         result)))))
+   (try
+     (with-open [stack (MemoryStack/stackPush)]
+       (let [pp (.mallocPointer stack 1)
+             rc (LMDB/mdb_txn_begin env MemoryUtil/NULL flags pp)]
+         (if (= LMDB/MDB_MAP_RESIZED rc)
+           (env-set-mapsize env 0)
+           (success? rc))
+         (let [txn (.get pp)
+               [result
+                commit-rc] (try
+                             [(f stack txn)
+                              (LMDB/mdb_txn_commit txn)]
+                             (catch Throwable t
+                               (LMDB/mdb_txn_abort txn)
+                               (throw t)))]
+           (success? commit-rc)
+           result)))
+     (catch ExceptionInfo e
+       (if (= LMDB/MDB_MAP_FULL (:error (ex-data e)))
+         (do (increase-mapsize env 2)
+             (transaction env f flags))
+         (throw e))))))
 
 (defn env-create []
   (with-open [stack (MemoryStack/stackPush)]
@@ -45,9 +67,6 @@
                                                    .mkdirs))
                                flags
                                0664)))
-
-(defn env-set-mapsize [^long env ^long size]
-  (success? (LMDB/mdb_env_set_mapsize env size)))
 
 (defn env-close [^long env]
   (LMDB/mdb_env_close env))
@@ -103,16 +122,13 @@
              (LMDB/mdb_cursor_close cursor))))))))
 
 (def default-env-flags (bit-or LMDB/MDB_WRITEMAP LMDB/MDB_MAPASYNC))
-(def default-max-size (* 10 1024 1024 1024))
 
 (defrecord CruxLMDBKv [db-dir env env-flags dbi max-size]
   ks/CruxKvStore
   (open [this]
     (let [env-flags (or env-flags default-env-flags)
-          env (env-create)
-          max-size (or max-size default-max-size)]
+          env (env-create)]
       (try
-        (env-set-mapsize env max-size)
         (env-open env db-dir env-flags)
         (assoc this
                :env env
