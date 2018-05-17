217cf42e3efee765d687369ac2a69b850a1d908c
Håkan Råberg <hakan.raberg@gmail.com>
2018-05-17 12:14:51 +0100

    No need to provide read options, but can keep and reuse write options.

--- src/crux/rocksdb.clj
+++ src/crux/rocksdb.clj
@@ -2,14 +2,14 @@
   (:require [clojure.java.io :as io]
             [crux.kv-store :refer :all])
   (:import java.io.Closeable
-           [org.rocksdb Checkpoint Options ReadOptions RocksDB RocksIterator WriteBatch WriteOptions]))
+           [org.rocksdb Checkpoint Options RocksDB RocksIterator WriteBatch WriteOptions]))
 
 (defn- iterator->kv [^RocksIterator i]
   (when (.isValid i)
     [(.key i) (.value i)]))
 
-(defn- ^Closeable rocks-iterator [{:keys [^RocksDB db ^ReadOptions vanilla-read-options]}]
-  (let [i (.newIterator db vanilla-read-options)]
+(defn- ^Closeable rocks-iterator [{:keys [^RocksDB db]}]
+  (let [i (.newIterator db)]
     (reify
       KvIterator
       (-seek [this k]
@@ -33,24 +33,24 @@
                (catch Throwable t
                  (.close opts)
                  (throw t)))]
-      (assoc this :db db :options opts :vanilla-read-options (ReadOptions.))))
+      (assoc this :db db :options opts :write-options (doto (WriteOptions.)
+                                                        (.setDisableWAL true)))))
 
   (iterate-with [this f]
     (with-open [i (rocks-iterator this)]
       (f i)))
 
-  (store [{:keys [^RocksDB db]} kvs]
-    (with-open [wb (WriteBatch.)
-                wo (WriteOptions.)]
+  (store [{:keys [^RocksDB db ^WriteOptions write-options]} kvs]
+    (with-open [wb (WriteBatch.)]
       (doseq [[k v] kvs]
         (.put wb k v))
-      (.write db wo wb)))
+      (.write db write-options wb)))
 
   (backup [{:keys [^RocksDB db]} dir]
     (.createCheckpoint (Checkpoint/create db) (.getAbsolutePath (io/file dir))))
 
   Closeable
-  (close [{:keys [^RocksDB db ^Options options ^ReadOptions vanilla-read-options]}]
+  (close [{:keys [^RocksDB db ^Options options ^WriteOptions write-options]}]
     (.close db)
     (.close options)
-    (.close vanilla-read-options)))
+    (.close write-options)))
