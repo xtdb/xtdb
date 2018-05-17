d11d24c9d2ff58996086351ab4ceb0ce49a14d3e
Håkan Råberg <hakan.raberg@gmail.com>
2018-05-17 17:25:44 +0100

    Breaking out fns so its easier to launch Kafka and ZK locally outside tests.

--- test/crux/embedded_kafka.clj
+++ test/crux/embedded_kafka.clj
@@ -11,7 +11,7 @@
            [org.apache.kafka.clients.consumer
             KafkaConsumer]
            [org.apache.zookeeper.server
-            NIOServerCnxnFactory ZooKeeperServer]
+             ServerCnxnFactory NIOServerCnxnFactory ZooKeeperServer]
            [java.net InetSocketAddress]
            [java.util Properties]))
 
@@ -22,36 +22,44 @@
 (def ^:dynamic ^String *host* "localhost")
 (def ^:dynamic ^String *broker-id* "0")
 
-(def ^:dynamic *zookeeper-connect* (str *host* ":" 2181))
-(def ^:dynamic *kafka-bootstrap-servers* (str *host* ":" 9092))
+(def default-zookeeper-port 2182)
+(def default-kafka-port 9092)
 
-(def ^:dynamic *kafka-broker-config*
-  {"offsets.topic.replication.factor" "1"
+(def ^:dynamic *zookeeper-connect* (str *host* ":" default-zookeeper-port))
+(def ^:dynamic *kafka-bootstrap-servers* (str *host* ":" default-kafka-port))
+
+(def default-kafka-broker-config
+  {"host" *host*
+   "port" default-kafka-port
+   "broker.id" *broker-id*
+   "zookeeper.connect" *zookeeper-connect*
+   "offsets.topic.replication.factor" "1"
    "transaction.state.log.replication.factor" "1"
    "transaction.state.log.min.isr" "1"})
 
 (defn write-meta-properties [log-dir broker-id]
   (let [meta-properties (io/file log-dir "meta.properties")]
     (when-not (.exists meta-properties)
+      (io/make-parents meta-properties)
       (with-open [out (io/output-stream meta-properties)]
         (doto (Properties.)
           (.setProperty "version" "0")
           (.setProperty "broker.id" (str broker-id))
           (.store out ""))))))
 
+(defn start-kafka-broker ^KafkaServerStartable [config]
+  (doto (KafkaServerStartable. (KafkaConfig. (merge default-kafka-broker-config config)))
+    (.startup)))
+
 (defn with-embedded-kafka [f]
-  (let [log-dir (cio/create-tmpdir "kafka-log")
+  (let [log-dir (doto (cio/create-tmpdir "kafka-log")
+                  (write-meta-properties *broker-id*))
         port (cio/free-port)
-        config (KafkaConfig.
-                (merge *kafka-broker-config*
-                       {"host.name" *host*
-                        "port" (str port)
-                        "broker.id" *broker-id*
-                        "zookeeper.connect" *zookeeper-connect*
-                        "log.dir" (.getAbsolutePath log-dir)}))
-        broker (KafkaServerStartable. config)]
-    (write-meta-properties log-dir *broker-id*)
-    (.startup broker)
+        broker (start-kafka-broker {"host.name" *host*
+                                    "port" (str port)
+                                    "broker.id" *broker-id*
+                                    "zookeeper.connect" *zookeeper-connect*
+                                    "log.dir" (.getAbsolutePath log-dir)})]
     (try
       (binding [*kafka-bootstrap-servers* (str *host* ":" port)]
         (f))
@@ -60,22 +68,29 @@
         (some-> broker .awaitShutdown)
         (cio/delete-dir log-dir)))))
 
+(defn start-zookeeper ^ServerCnxnFactory
+  ([snapshot-dir log-dir]
+   (start-zookeeper snapshot-dir log-dir *host* default-zookeeper-port))
+  ([snapshot-dir log-dir ^String host ^long port]
+   (let [tick-time 500
+         max-connections 16
+         server (ZooKeeperServer. (io/file snapshot-dir) (io/file log-dir) tick-time)
+         server-cnxn-factory (doto (NIOServerCnxnFactory/createFactory)
+                               (.configure (InetSocketAddress. host port)
+                                           max-connections)
+                               (.startup server))]
+     server-cnxn-factory)))
+
 (defn with-embedded-zookeeper [f]
-  (let [tick-time 500
-        max-connections 16
-        snapshot-dir (cio/create-tmpdir "zk-snapshot")
+  (let [snapshot-dir (cio/create-tmpdir "zk-snapshot")
         log-dir (cio/create-tmpdir "zk-log")
         port (cio/free-port)
-        server (ZooKeeperServer. snapshot-dir log-dir tick-time)
-        server-cnxn-factory (doto (NIOServerCnxnFactory/createFactory)
-                              (.configure (InetSocketAddress. *host* port)
-                                          max-connections)
-                              (.startup server))]
+        server-cnxn-factory (start-zookeeper snapshot-dir log-dir *host* port)]
     (try
       (binding [*zookeeper-connect* (str  *host* ":" port)]
         (f))
       (finally
-        (some-> server-cnxn-factory .shutdown)
+        (some-> ^ServerCnxnFactory server-cnxn-factory .shutdown)
         (doseq [dir [snapshot-dir log-dir]]
           (cio/delete-dir dir))))))
 
