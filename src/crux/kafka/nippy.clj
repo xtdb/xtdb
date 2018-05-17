4455333984e7ac3cf0e12fd3136722e489466efb
Håkan Råberg <hakan.raberg@gmail.com>
2018-05-17 15:31:57 +0100

    Don't AOT compile nippy.

--- src/crux/kafka/nippy.clj
+++ src/crux/kafka/nippy.clj
@@ -1,18 +1,25 @@
 (ns crux.kafka.nippy
-  (:require [taoensso.nippy :as nippy])
   (:import [org.apache.kafka.common.serialization
             Deserializer Serializer]))
+
+(def freeze)
+(def thaw)
+
+(when-not *compile-files*
+  (require 'taoensso.nippy)
+  (alter-var-root #'freeze (constantly (resolve 'taoensso.nippy/freeze)))
+  (alter-var-root #'thaw (constantly (resolve 'taoensso.nippy/thaw))))
 
 (deftype NippySerializer []
   Serializer
   (close [_])
   (configure [_ _ _])
   (serialize [_ _ data]
-    (nippy/freeze data)))
+    (freeze data)))
 
 (deftype NippyDeserializer []
   Deserializer
   (close [_])
   (configure [_ _ _])
   (deserialize [_ _ data]
-    (nippy/thaw data)))
+    (thaw data)))
