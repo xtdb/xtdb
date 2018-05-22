4f7b3fb057554c0705f3eb099c9da84fd8cde727
Håkan Råberg <hakan.raberg@gmail.com>
2018-05-22 14:32:35 +0100

    Mini profiler.

---
+++ dev/prof.clj
@@ -0,0 +1,31 @@
+(ns prof)
+
+(set! *unchecked-math* :warn-on-boxed)
+
+(def profile-state (atom {}))
+
+(defn profile-fn [f & args]
+  (let [start-time (System/currentTimeMillis)]
+    (try
+      (apply f args)
+      (finally
+        (swap! profile-state update-in [f]
+               (fn [{:keys [^long n ^long t]
+                     :or {n 0 t 0}}]
+                 (let [t (+ t (- (System/currentTimeMillis) start-time))
+                       n (inc n)]
+                   {:t t
+                    :n n
+                    :a (double (/ t n))})))))))
+
+(defn primitive-fn? [f]
+  (->>  (:members (clojure.reflect/reflect f))
+        (some (comp #{'invokePrim} :name))))
+
+(defn profile! [& ns]
+  (doseq [ns ns
+          [_ v] (ns-interns ns)]
+    (alter-var-root v #(if (and (fn? %)
+                                (not (primitive-fn? %)))
+                         (partial profile-fn %)
+                         %))))
