5d76643dbbea6dd58839387a8878e2d3645be8d6
Håkan Råberg <hakan.raberg@gmail.com>
2018-06-08 16:02:55 +0200

    Refactoring, more Java idiomatic.

--- src/crux/ByteUtils.java
+++ src/crux/ByteUtils.java
@@ -6,16 +6,16 @@ import java.nio.ByteOrder;
 
 @SuppressWarnings("deprecation")
 public class ByteUtils {
-    public static Comparator UNSIGNED_BYTES_COMPARATOR = new UnsignedBytesComparator();
+    public static final Comparator UNSIGNED_BYTES_COMPARATOR = new UnsignedBytesComparator();
 
-    private static sun.misc.Unsafe theUnsafe;
-    private static boolean isLittleEndian = ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN;
+    private static final boolean IS_LITTLE_ENDIAN = ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN;
+    private static final sun.misc.Unsafe UNSAFE;
 
     static {
         try {
             Field f = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
             f.setAccessible(true);
-            theUnsafe = (sun.misc.Unsafe) f.get(null);
+            UNSAFE = (sun.misc.Unsafe) f.get(null);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -23,14 +23,14 @@ public class ByteUtils {
 
     public static int compareBytes(byte[] a, byte[] b, int maxLength) {
         int maxCompareLength = Math.min(Math.min(a.length, b.length), maxLength);
-        int maxStrideLength = maxCompareLength - 7;
+        int maxStrideLength = maxCompareLength & ~(Long.BYTES - 1);
         int i = 0;
         for (; i < maxStrideLength; i += Long.BYTES) {
             int arrayIndex = i + sun.misc.Unsafe.ARRAY_BYTE_BASE_OFFSET;
-            long aLong = theUnsafe.getLong(a, arrayIndex);
-            long bLong = theUnsafe.getLong(b, arrayIndex);
+            long aLong = UNSAFE.getLong(a, arrayIndex);
+            long bLong = UNSAFE.getLong(b, arrayIndex);
             if (aLong != bLong) {
-                if (isLittleEndian) {
+                if (IS_LITTLE_ENDIAN) {
                     return Long.compareUnsigned(Long.reverseBytes(aLong),
                                                 Long.reverseBytes(bLong));
                 } else {
@@ -40,8 +40,8 @@ public class ByteUtils {
         }
         for (; i < maxCompareLength; i++) {
             int arrayIndex = i + sun.misc.Unsafe.ARRAY_BYTE_BASE_OFFSET;
-            int aByte = theUnsafe.getByte(a, arrayIndex) & 0xff;
-            int bByte = theUnsafe.getByte(b, arrayIndex) & 0xff;
+            int aByte = UNSAFE.getByte(a, arrayIndex) & 0xff;
+            int bByte = UNSAFE.getByte(b, arrayIndex) & 0xff;
             if (aByte != bByte) {
                 return aByte - bByte;
             }
@@ -58,7 +58,7 @@ public class ByteUtils {
 
     public static class UnsignedBytesComparator implements Comparator<byte[]> {
         public int compare(byte[] a, byte[] b) {
-            return ByteUtils.compareBytes(a, b);
+            return ByteUtils.compareBytes(a, b, Integer.MAX_VALUE);
         }
     }
 }
