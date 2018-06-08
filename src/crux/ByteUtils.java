ef08d257d8ff3ee7d6c2281dd955fa00d3d3ea22
Håkan Råberg <hakan.raberg@gmail.com>
2018-06-08 15:21:45 +0200

    Attempt to get rid of deprecation warnings on CI.

--- src/crux/ByteUtils.java
+++ src/crux/ByteUtils.java
@@ -3,19 +3,19 @@ package crux;
 import java.lang.reflect.Field;
 import java.util.Comparator;
 import java.nio.ByteOrder;
-import sun.misc.Unsafe;
 
+@SuppressWarnings("deprecation")
 public class ByteUtils {
     public static Comparator UNSIGNED_BYTES_COMPARATOR = new UnsignedBytesComparator();
 
-    private static Unsafe theUnsafe;
+    private static sun.misc.Unsafe theUnsafe;
     private static boolean isLittleEndian = ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN;
 
     static {
         try {
-            Field f = Unsafe.class.getDeclaredField("theUnsafe");
+            Field f = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
             f.setAccessible(true);
-            theUnsafe = (Unsafe) f.get(null);
+            theUnsafe = (sun.misc.Unsafe) f.get(null);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -26,7 +26,7 @@ public class ByteUtils {
         int maxStrideLength = maxCompareLength - 7;
         int i = 0;
         for (; i < maxStrideLength; i += Long.BYTES) {
-            int arrayIndex = i + Unsafe.ARRAY_BYTE_BASE_OFFSET;
+            int arrayIndex = i + sun.misc.Unsafe.ARRAY_BYTE_BASE_OFFSET;
             long aLong = theUnsafe.getLong(a, arrayIndex);
             long bLong = theUnsafe.getLong(b, arrayIndex);
             if (aLong != bLong) {
@@ -39,7 +39,7 @@ public class ByteUtils {
             }
         }
         for (; i < maxCompareLength; i++) {
-            int arrayIndex = i + Unsafe.ARRAY_BYTE_BASE_OFFSET;
+            int arrayIndex = i + sun.misc.Unsafe.ARRAY_BYTE_BASE_OFFSET;
             int aByte = theUnsafe.getByte(a, arrayIndex) & 0xff;
             int bByte = theUnsafe.getByte(b, arrayIndex) & 0xff;
             if (aByte != bByte) {
