949a5145f340fde89bf5527170462f8323f8b8e4
Håkan Råberg <hakan.raberg@gmail.com>
2018-06-09 23:20:28 +0200

    Redoing the Datalog spec based on Racket's for a firmer base.

--- dev/hakan.clj
+++ dev/hakan.clj
@@ -1,62 +1,41 @@
 (ns hakan
-  (:require [clojure.spec.alpha :as s]
-            [clojure.set :as set]))
+  (:require [clojure.spec.alpha :as s]))
 
 ;;; Experiment implementing a parser for a subset of Prolog using spec.
 
-;; See Racket for the Datalog syntax in EBNF (slightly different from this one):
+;; See Racket for the Datalog syntax in EBNF.
 ;; https://docs.racket-lang.org/datalog/datalog.html
 
 (defn- prolog-var? [s]
   (and (symbol? s)
        (Character/isUpperCase (char (first (name s))))))
 
-(defn- safe-clause? [{:keys [head body]
-                     :as clause}]
-  (let [head-vars (->> head
-                       :args
-                       (filter (comp #{:var} first))
-                       (set))
-        body-vars (->> body
-                       (filter (comp #{:predicate} first))
-                       (mapcat (comp :args second))
-                       (filter (comp #{:var} first))
-                       (set))]
-    (empty? (set/difference head-vars body-vars))))
+(s/def ::program (s/* ::statement))
+(s/def ::statement (s/alt :assertion ::assertion
+                          :retraction ::retraction
+                          :query ::query))
+(s/def ::assertion (s/cat :clause ::clause
+                          :dot #{'.}))
+(s/def ::retraction (s/cat :clause ::clause
+                           :tilde #{'-}))
+(s/def ::query (s/cat :literal ::literal
+                      :question-mark #{'?}))
+(s/def ::clause (s/alt :rule (s/cat :literal ::literal
+                                    :comma-hypen #{:-}
+                                    :body ::body)
+                       :fact ::literal))
+(s/def ::body (s/+ ::literal))
+(s/def ::literal (s/alt :predicate (s/cat :symbol ::identifier
+                                          :terms (s/? (s/coll-of ::term :kind list?)))
+                        :equality-predicate ::equality-predicate))
+(s/def ::equality-predicate (s/and list? (s/cat :op '#{= !=}
+                                                :terms (s/+ ::term))))
+(s/def ::term (s/or :variable ::variable
+                    :constant ::constant))
 
-(def ^:private separator? '#{:- <- . - ?})
-
-(s/def ::dot #{'.})
-(s/def ::var prolog-var?)
-(s/def ::literal (complement prolog-var?))
-(s/def ::atom (s/or :var ::var :literal ::literal))
-(s/def ::predicate (s/cat :name (every-pred symbol? (complement separator?))
-                          :args (s/? (s/coll-of ::atom :kind list?))))
-(s/def ::fact (s/& (s/cat :name symbol?
-                          :args (s/? (s/coll-of ::literal :kind list?))
-                          :op (s/or :assert ::dot
-                                    :retact #{'-}))
-                   (s/conformer #(update % :op first))))
-(s/def ::safe-clause safe-clause?)
-(s/def ::rule (s/& (s/cat :head ::predicate
-                          :comma-hyphen #{:- '<-}
-                          :body (s/* (s/alt :predicate ::predicate
-                                            :expression list?))
-                          :dot ::dot)
-                   safe-clause?
-                   (s/conformer #(dissoc % :comma-hyphen :dot))))
-(s/def ::query (s/& (s/cat :query ::predicate
-                           :question-mark #{'?})
-                    (s/conformer #(dissoc % :question-mark))))
-(s/def ::comment (s/& (s/cat :percent #{'%}
-                             :comment (s/* (every-pred any? (complement separator?)))
-                             :dot ::dot)
-                      (s/conformer #(dissoc % :percent :dot))))
-(s/def ::clause (s/alt :fact ::fact
-                       :rule ::rule
-                       :query ::query
-                       :comment ::comment))
-(s/def ::program (s/* ::clause))
+(s/def ::constant (complement (some-fn list? prolog-var?)))
+(s/def ::identifier (s/and symbol? (complement (some-fn prolog-var? '#{. - ? = %}))))
+(s/def ::variable prolog-var?)
 
 (comment
   (s/conform
@@ -77,41 +56,42 @@
    ::program
    '[parent(john,douglas).
      parent(john,douglas)?
-     % parent(john, douglas).
+     ;; % parent(john, douglas).
 
      parent(john,ebbon)?
 
      parent(bob,john).
      parent(ebbon,bob).
      parent(A,B)?
-     % parent(john, douglas).
-     % parent(bob, john).
-     % parent(ebbon, bob).
+     ;; % parent(john, douglas).
+     ;; % parent(bob, john).
+     ;; % parent(ebbon, bob).
 
      parent(john,B)?
-     % parent(john, douglas).
+     ;; % parent(john, douglas).
 
      parent(A,A)?
 
      ancestor(A,B) :- parent(A,B).
      ancestor(A,B) :- parent(A,C), ancestor(C, B).
      ancestor(A, B)?
-     % ancestor(ebbon, bob).
-     % ancestor(bob, john).
-     % ancestor(john, douglas).
-     % ancestor(bob, douglas).
-     % ancestor(ebbon, john).
-     % ancestor(ebbon, douglas).
+     ;; % ancestor(ebbon, bob).
+     ;; % ancestor(bob, john).
+     ;; % ancestor(john, douglas).
+     ;; % ancestor(bob, douglas).
+     ;; % ancestor(ebbon, john).
+     ;; % ancestor(ebbon, douglas).
 
      ancestor(X,john)?
-     % ancestor(bob, john).
-     % ancestor(ebbon, john).
+     ;; % ancestor(bob, john).
+     ;; % ancestor(ebbon, john).
 
      parent(bob, john)-
      parent(A,B)?
-     % parent(john, douglas).
-     % parent(ebbon, bob).
+     ;; % parent(john, douglas).
+     ;; % parent(ebbon, bob).
 
      ancestor(A,B)?
-     % ancestor(john, douglas).
-     % ancestor(ebbon, bob).]))
+     ;; % ancestor(john, douglas).
+     ;; % ancestor(ebbon, bob).
+     ]))
