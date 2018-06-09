877bf4586c116391a38e3d507785208388a95915
Håkan Råberg <hakan.raberg@gmail.com>
2018-06-09 23:48:39 +0200

    Example of how Racket's paren mode looks.

--- dev/hakan.clj
+++ dev/hakan.clj
@@ -95,3 +95,32 @@
      ;; % ancestor(john, douglas).
      ;; % ancestor(ebbon, bob).
      ]))
+
+;;; https://docs.racket-lang.org/datalog/Parenthetical_Datalog_Module_Language.html
+;; (! (parent john douglas))
+;; (? (parent john douglas))
+
+;; (? (parent john ebbon))
+
+;; (! (parent bob john))
+;; (! (parent ebbon bob))
+;; (? (parent A B))
+
+;; (? (parent john B))
+
+;; (? (parent A A))
+
+;; (! (:- (ancestor A B)
+;;        (parent A B)))
+;; (! (:- (ancestor A B)
+;;        (parent A C)
+;;        (ancestor C B)))
+;; (? (ancestor A B))
+
+;; (? (ancestor X john))
+
+;; (~ (parent bob john))
+
+;; (? (parent A B))
+
+;; (? (ancestor A B))
