93e64b612f6a64d8bbb0b0b632b569e678906660
Håkan Råberg <hakan.raberg@gmail.com>
2018-06-14 16:47:31 +0200

    Renamed fn issue.

--- src/crux/doc.clj
+++ src/crux/doc.clj
@@ -311,7 +311,7 @@
             (swap! trie-state update :result-stack (if (zero? depth)
                                                      empty
                                                      pop))
-            (let [[max-ks result] (flatten-triejoin-result result-stack)
+            (let [[max-ks result] (flatten-triejoin-result-stack result-stack)
                   result (constrain-triejoin-result result shared-attrs)]
               (log/debug :leaf-match-constrained (mapv bu/bytes->hex max-ks) result)
               [max-ks result]))
