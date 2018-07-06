f075e0a70601f5b6a044745f4a0c7adc872aa12e
Ben Gerard <bng@juxt.pro>
2018-07-06 16:01:11 +0100

    Add sass to deploy script

--- .circleci/aerobatic_deploy.sh
+++ .circleci/aerobatic_deploy.sh
@@ -2,5 +2,7 @@
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install npm
 sudo npm install aerobatic-cli -g
+sudo npm instal sass
 cd ~/crux/website
+sass assets/scss/style.css assets/css/style.css
 aero deploy
