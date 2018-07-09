6cfbf0ff013b767a9c3faae1494798b2f87fba45
Ben Gerard <bng@juxt.pro>
2018-07-09 10:19:51 +0100

    Fix aerobatic deploy script

--- .circleci/aerobatic_deploy.sh
+++ .circleci/aerobatic_deploy.sh
@@ -2,7 +2,7 @@
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install npm
 sudo npm install aerobatic-cli -g
-sudo npm instal sass
+sudo npm install sass -g
 cd ~/crux/website
 sass assets/scss/style.css assets/css/style.css
 aero deploy
