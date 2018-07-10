1b57b02685c60f3baa14da2f831eaca5dbec9fc7
Roxolan0 <hugo@juxt.pro>
2018-07-10 14:56:32 +0100

    Refactoring

--- src/crux/http_server.clj
+++ src/crux/http_server.clj
@@ -1,5 +1,6 @@
 (ns crux.http-server
   (:require [clojure.edn :as edn]
+            [clojure.string :as st]
             [crux.db :as db]
             [crux.doc :as doc]
             [crux.io :as cio]
@@ -11,55 +12,8 @@
             [ring.util.request :as req])
   (:import [java.io Closeable]))
 
-(defn exception-response [^Exception e status]
-  {:status status
-   :headers {"Content-Type" "text/plain"}
-   :body (str
-          (.getMessage e) "\n"
-          (ex-data e))})
-
-(defn status [kvs db-dir]
-  {:status 200
-   :headers {"Content-Type" "application/edn"}
-   :body (pr-str
-          {:crux.kv-store/kv-backend (.getName (class kvs))
-           :crux.kv-store/estimate-num-keys (kvs/count-keys kvs)
-           :crux.kv-store/size (cio/folder-human-size db-dir)
-           :crux.tx-log/tx-time (doc/read-meta kvs :crux.tx-log/tx-time)})})
-
-(defn do-query [kvs request]
-  (try
-    (let [query (case (:request-method request)
-                  :get (-> request
-                           :query-params
-                           (find "q")
-                           last
-                           edn/read-string)
-                  :post (edn/read-string (req/body-string request)))]
-      (try
-        {:status 200
-         :headers {"Content-Type" "application/edn"}
-         :body (pr-str (q/q (q/db kvs) query))}
-        (catch Exception e
-          (if (= "Invalid input" (.getMessage e))
-            (exception-response e 400) ;; Valid edn, invalid query
-            (exception-response e 500))))) ;; Valid query; something internal failed
-    (catch Exception e
-      (exception-response e 400)))) ;; Invalid edn
-
-(defn transact [tx-log request]
-  (try
-    (let [tx-op (edn/read-string (req/body-string request))]
-      (try
-        {:status 200
-         :headers {"Content-Type" "application/edn"}
-         :body (pr-str @(db/submit-tx tx-log [tx-op]))}
-        (catch Exception e
-          (if (= "Invalid input" (.getMessage e))
-            (exception-response e 400) ;; Valid edn, invalid tx-op
-            (exception-response e 500))))) ;; Valid tx-op; something internal failed
-    (catch Exception e
-      (exception-response e 400)))) ;; Invalid edn
+;; ---------------------------------------------------
+;; Utils
 
 (defn check-path [request valid-paths valid-methods]
   (let [path (req/path-info request)
@@ -67,13 +21,74 @@
     (and (some #{path} valid-paths)
          (some #{method} valid-methods))))
 
+(defn param
+  ([request]
+   (param request nil))
+  ([request param-name]
+   (-> (case (:request-method request)
+         :get (-> request
+                  :query-params
+                  (find param-name)
+                  last)
+         :post (req/body-string request))
+       edn/read-string)))
+
+(defn success-response [m]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (pr-str m)})
+
+
+(defn exception-response [^Exception e status]
+  {:status status
+   :headers {"Content-Type" "text/plain"}
+   :body (str
+          (.getMessage e) "\n"
+          (ex-data e))})
+
+(defmacro let-valid [bindings expr]
+  `(try
+     (let ~bindings
+       (try
+         ~expr
+         (catch Exception e#
+           (if (st/starts-with? (.getMessage e#) "Invalid input")
+             (exception-response e# 400) ;; Valid edn, invalid content
+             (exception-response e# 500))))) ;; Valid content; something internal failed, or content validity is not properly checked
+     (catch Exception e#
+       (exception-response e# 400)))) ;;Invalid edn
+
+;; ---------------------------------------------------
+;; Services
+
+(defn status [kvs db-dir]
+  (success-response
+   {:crux.kv-store/kv-backend (.getName (class kvs))
+    :crux.kv-store/estimate-num-keys (kvs/count-keys kvs)
+    :crux.kv-store/size (cio/folder-human-size db-dir)
+    :crux.tx-log/tx-time (doc/read-meta kvs :crux.tx-log/tx-time)}))
+
+(defn query [kvs request]
+  (let-valid [query-map (param request "q")]
+    (success-response
+     (q/q (q/db kvs) query-map))))
+
+(defn transact [tx-log request]
+  (let-valid [tx-op (param request)]
+    (success-response
+     @(db/submit-tx tx-log [tx-op]))))
+
+;; ---------------------------------------------------
+;; Jetty server
+
 (defn handler [kvs tx-log db-dir request]
   (cond
     (check-path request ["/"] [:get])
     (status kvs db-dir)
 
+
     (check-path request ["/q" "/query"] [:get :post])
-    (do-query kvs request)
+    (query kvs request)
 
     (check-path request ["/tx-log"] [:post])
     (transact tx-log request)
