4f04e1fadc2555561eef8e0d699711e03b19128d
Patrik Karlin <patrik.karlin@gmail.com>
2018-09-04 16:49:42 +0100

    api: block until system is started

--- src/crux/api.clj
+++ src/crux/api.clj
@@ -5,19 +5,22 @@
 
 (defn ^Closeable start-system
   [options]
+  (log/info "running crux in library mode")
   (let [close-promise (promise)
-        options (merge bootstrap/default-options options)]
-    (log/info "running crux in library mode")
-    (doto (Thread.
-            (fn library-crux-thread []
-              (log/info "crux thread intialized")
-              (bootstrap/start-system
-                options
-                (fn with-system-callback [system]
-                  (log/info "crux system start completed")
-                  @close-promise
-                  (log/info "starting teardown of crux system")))
-              (log/info "crux system completed teardown")))
-      (.start))
+        started-promise (promise)
+        options (merge bootstrap/default-options options)
+        running-future
+        (future
+          (log/info "crux thread intialized")
+          (bootstrap/start-system
+            options
+            (fn with-system-callback [system]
+              (deliver started-promise true)
+              (log/info "crux system start completed")
+              @close-promise
+              (log/info "starting teardown of crux system")))
+          (log/info "crux system completed teardown"))]
+    (while (not (or (deref started-promise 100 false)
+                    (deref running-future 100 false))))
     (reify Closeable
       (close [_] (deliver close-promise true)))))
