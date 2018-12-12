045591dac8553d9db57700b599c003732c089a15
Håkan Råberg <hakan.raberg@gmail.com>
2018-12-12 13:29:32 +0100

    Preliminary WatDiv schema for Datomic, not verified. #94.

--- test/crux/watdiv_test.clj
+++ test/crux/watdiv_test.clj
@@ -314,3 +314,94 @@ WHERE
    ?s a ex:Artist;
      foaf:firstName ?n.
 }"))))))))
+
+;; TODO: Not used or verified yet. See:
+;; https://dsg.uwaterloo.ca/watdiv/watdiv-data-model.txt
+;; [com.datomic/datomic-free "0.9.5697"]
+(def datomic-schema
+  [#:db{:valueType :db.type/string, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/composer")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/follows")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/friendOf")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/gender")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/hasGenre")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/hits")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/likes")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/makesPurchase")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/purchaseDate")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/purchaseFor")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/subscribes")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://db.uwaterloo.ca/~galuc/wsdbm/userId")}
+   #:db{:valueType :db.type/ref :ident (keyword "http://ogp.me/ns#tag")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://ogp.me/ns#title")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/dc/terms/Location")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://purl.org/goodrelations/description")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/goodrelations/includes")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://purl.org/goodrelations/name")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/goodrelations/offers")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://purl.org/goodrelations/price")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://purl.org/goodrelations/serialNumber")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://purl.org/goodrelations/validFrom")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://purl.org/goodrelations/validThrough")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/ontology/mo/artist")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/ontology/mo/conductor")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://purl.org/ontology/mo/movement")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://purl.org/ontology/mo/opus")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://purl.org/ontology/mo/performed_in")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/ontology/mo/performer")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://purl.org/ontology/mo/producer")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://purl.org/ontology/mo/record_number")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://purl.org/ontology/mo/release")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/stuff/rev#hasReview")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://purl.org/stuff/rev#rating")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://purl.org/stuff/rev#reviewer")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://purl.org/stuff/rev#text")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://purl.org/stuff/rev#title")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://purl.org/stuff/rev#totalVotes")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/actor")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/aggregateRating")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/author")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/award")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://schema.org/birthDate")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/bookEdition")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/caption")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/contactPoint")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/contentRating")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/contentSize")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://schema.org/datePublished")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/description")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/director")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/duration")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/editor")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/eligibleQuantity")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/eligibleRegion")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/email")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/employee")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://schema.org/expires")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/faxNumber")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/isbn")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/jobTitle")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/keywords")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/language")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/legalName")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/nationality")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/numberOfPages")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/openingHours")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/paymentAccepted")}
+   #:db{:valueType :db.type/instant, :ident (keyword "http://schema.org/priceValidUntil")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/printColumn")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/printEdition")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/printPage")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/printSection")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/producer")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/publisher")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/telephone")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/text")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://schema.org/trailer")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://schema.org/url")}
+   #:db{:valueType :db.type/long, :ident (keyword "http://schema.org/wordCount")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://www.geonames.org/ontology#parentCountry")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://www.w3.org/1999/02/22-rdf-syntax-ns#type")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://xmlns.com/foaf/age")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://xmlns.com/foaf/familyName")}
+   #:db{:valueType :db.type/string, :ident (keyword "http://xmlns.com/foaf/givenName")}
+   #:db{:valueType :db.type/ref, :ident (keyword "http://xmlns.com/foaf/homepage")}])
