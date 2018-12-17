69fbd2ae85c54868aa66d0f3f5fb19cc0a0659b7
Jon Pither <jon.pither@gmail.com>
2018-12-17 18:56:07 +0000

    Deleting aerobatic stuff for website front page

--- .circleci/aerobatic_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install npm
-sudo npm install aerobatic-cli -g
-sudo npm install sass -g
-cd ~/crux/website
-sass assets/scss/style.scss assets/css/style.css
-aero deploy
