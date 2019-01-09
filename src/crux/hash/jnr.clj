0a52b9274b798eec91854dd5ec8b73c0c4875a98
Håkan Råberg <hakan.raberg@gmail.com>
2019-01-09 17:28:30 +0100

    This buffer isn't the digest, so should have default size. #110.

--- src/crux/hash/jnr.clj
+++ src/crux/hash/jnr.clj
@@ -46,7 +46,7 @@
   (ThreadLocal/withInitial
    (reify Supplier
      (get [_]
-       (ExpandableDirectByteBuffer. hash/id-hash-size)))))
+       (ExpandableDirectByteBuffer.)))))
 
 (defn gcrypt-id-hash-buffer ^org.agrona.DirectBuffer [^MutableDirectBuffer to ^DirectBuffer buffer]
   (let [^DirectBuffer buffer (if (mem/off-heap? buffer)
