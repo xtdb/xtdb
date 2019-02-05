a8609c1afb4ef40552612a21e323bd087c6b6ae0
Patrik Karlin <patrik.karlin@gmail.com>
2019-02-05 14:36:56 +0100

    run bench scripts from project root
    Helps with the docker setup

--- bench/bin/run_watdiv.sh
+++ bench/bin/run_watdiv.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-cd ..
-
 (
     CRUX_WATDIV_NUM_QUERIES=1 \
     CRUX_WATDIV=true \
     CRUX_WATDIV_RUN_CRUX=true \
     lein test :only crux.watdiv-test
 )
+
+echo $(cat target/watdiv_*)
