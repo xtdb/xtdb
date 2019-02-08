debb75fc8f1669a78e8c984382a93d07904ea6ad
Håkan Råberg <hakan.raberg@gmail.com>
2019-02-08 14:53:42 +0100

    Updating agrona.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/tools.logging "0.4.1"]
                  [com.stuartsierra/dependency "0.2.0"]
                  [com.taoensso/nippy "2.14.0"]
-                 [org.agrona/agrona "0.9.31"]
+                 [org.agrona/agrona "0.9.32"]
                  ;; Provided dependencies included in uberjar.
                  [org.apache.kafka/kafka-clients "2.1.0" :scope "provided"]
                  [com.github.jnr/jnr-ffi "2.1.9" :scope "provided"]
