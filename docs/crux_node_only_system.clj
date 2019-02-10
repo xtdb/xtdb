d6d2025b94759dd3175f60017822ac35aae9d5c6
Jon Pither <jon.pither@gmail.com>
2019-02-10 14:13:25 +0000

    Still trying to connect to dockerised kafka

--- docs/crux_node_only_system.clj
+++ docs/crux_node_only_system.clj
@@ -1,18 +1,33 @@
 (ns crux-node-only-system
   (:require [clojure.tools.logging :as log]
-            [crux.api :as api]
+            [crux.bootstrap.local-node :as local-node]
             [crux.http-server :as srv]
             [crux.kafka.embedded :as ek]
-            [integrant.core :as ig]))
+            [integrant.core :as ig]
+            [crux.kafka :as k]))
 
- ;; (def s (api/start-local-node {:db-dir "dev-storage/data"
- ;;                              :bootstrap-servers "localhost:9092"}))
+(def tx-topic-config
+  {"retention.ms" (str Long/MAX_VALUE)})
+
+(comment
+  ;; Follow https://docs.confluent.io/current/installation/docker/docs/installation/single-node-client.html, then try:
+
+  (let [admin-client (k/create-admin-client {"bootstrap.servers" "localhost:9092"})]
+    (println "Creating Topic")
+    (k/create-topic admin-client "foo-topic" 1 1 tx-topic-config)
+    (println "Topic Created"))
+
+  ;; Theory, the port is not exposed in the confluent setup?
+
+  ;; This validates my issue:
+  ;; https://www.e4developer.com/2018/05/20/how-to-easily-run-kafka-with-docker-for-development/
+)
 
 (def config {:crux/local-node {:db-dir "dev-storage/data"
                                :bootstrap-servers "localhost:9092"}})
 
 (defmethod ig/init-key :crux/local-node [_ opts]
-  (api/start-local-node opts))
+  (local-node/start-local-node opts))
 
 (defmethod ig/halt-key! :crux/local-node [_ ^java.io.Closeable closeable]
   (.close closeable))
