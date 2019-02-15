3b8d9f8954cb4f6ce521a1517603c6d1867f7f6d
patrik <patrik.karlin@gmail.com>
2019-02-15 14:24:22 +0100

    standalone-example: don't fail restore script when there is no backup

--- example/standalone_webservice/bin/restore.sh
+++ example/standalone_webservice/bin/restore.sh
@@ -4,6 +4,10 @@ set -e
 
 TEMP_DIR=$(mktemp -d)
 
-aws s3 cp s3://crux-bench-results/backup.tar.gz "${TEMP_DIR}/backup.tar.gz"
-echo tar -zvzf "${TEMP_DIR}/backup.tar.gz" --directory "${CHECKPOINT_DIR}"
-tar -zvxf "${TEMP_DIR}/backup.tar.gz" --directory "${CHECKPOINT_DIR}"
+aws s3 cp s3://crux-bench-results/backup.tar.gz "${TEMP_DIR}/backup.tar.gz" \
+    || true # do not fail if there is no backup
+
+if [ -f "${TEMP_DIR}/backup.tar.gz" ]; then
+    echo tar -zvzf "${TEMP_DIR}/backup.tar.gz" --directory "${CHECKPOINT_DIR}"
+    tar -zvxf "${TEMP_DIR}/backup.tar.gz" --directory "${CHECKPOINT_DIR}"
+fi
