ebd5830c29b3ca18b443207eb1bd26ccc6039051
Håkan Råberg <hakan.raberg@gmail.com>
2019-02-15 16:35:05 +0100

    Bunch of CSS. #145.

--- example/standalone_webservice/src/example_standalone_webservice/main.clj
+++ example/standalone_webservice/src/example_standalone_webservice/main.clj
@@ -8,6 +8,8 @@
             [yada.yada :refer [handler listener]]
             [hiccup2.core :refer [html]]
             [yada.resource :refer [resource]]
+            [yada.resources.classpath-resource]
+            [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]
             [example-standalone-webservice.backup :as backup])
   (:import [crux.api IndexVersionOutOfSyncException]
@@ -36,86 +38,87 @@
         [:html
          [:head
           [:meta {:charset "utf-8"}]
+          [:link {:rel "stylesheet" :type "text/css" :href "/static/styles/normalize.css"}]
+          [:link {:rel "stylesheet" :type "text/css" :href "/static/styles/main.css"}]
           [:title "Message Board"]]
          [:body
-          [:div
-           [:h1 [:a {:href "/"} "Message Board"]]
+          [:header
+           [:h2 [:a {:href "/"} "Message Board"]]]
+          [:div.timetravel
            [:form
             {:action "/" :method "GET"}
-            [:label {:for "valid-time"} "Valid time:"]
-            [:br]
-            [:input {:type "datetime-local" :value (str/replace (format-date vt) #"-00:00$" "") :name "vt"}]
-            [:br]
-            [:label {:for "tx-time"} "Transaction time:"]
-            [:br]
-            [:select
-             {:id "tx-time" :name "tt"}
-             (for [{:keys [crux.tx/tx-time]} (reverse tx-log)]
-               [:option {:value (format-date tx-time)
-                         :selected (= tt tx-time)} (format-date tx-time)])]
-            [:br]
-            [:input {:type "submit" :value "Go"}]]
-           [:div
-            [:ul
-             (for [[message name created id edited]
-                   (->> (api/q (api/db crux vt tt)
-                               '{:find [m n c e ed]
-                                 :where [[e :message-post/message m]
-                                         [e :message-post/name n]
-                                         [e :message-post/created c]
-                                         [e :message-post/edited ed]]})
-                        (sort-by #(nth % 2)))]
-               [:li
-                [:span "From: " name] [:br]
-                [:span "Created: " (format-date created)]
-                [:br]
+            [:fieldset
+             [:label {:for "valid-time"} "Valid time:"]
+             [:input {:type "datetime-local" :value (str/replace (format-date vt) #"-00:00$" "") :name "vt"}]
+             [:label {:for "tx-time"} "Transaction time:"]
+             [:select
+              {:id "tx-time" :name "tt"}
+              (for [{:keys [crux.tx/tx-time]} (reverse tx-log)]
+                [:option {:value (format-date tx-time)
+                          :selected (= tt tx-time)} (format-date tx-time)])]]
+            [:input {:type "submit" :value "Go"}]]]
+
+          [:div.comments
+           [:h3 "Comments"]
+           [:ul
+            (for [[message name created id edited]
+                  (->> (api/q (api/db crux vt tt)
+                              '{:find [m n c e ed]
+                                :where [[e :message-post/message m]
+                                        [e :message-post/name n]
+                                        [e :message-post/created c]
+                                        [e :message-post/edited ed]]})
+                       (sort-by #(nth % 2)))]
+              [:li
+               [:dl
+                [:dt "From:"] [:dd name]
+                [:dt "Created:"] [:dd [:a {:href (str "/?vt=" (format-date created))} (format-date created)]]
                 (when (not= created edited)
-                  [:span "Edited: " (format-date edited)])
-                [:pre message]
-                [:form {:action (str "/comment/" id) :method "POST"}
+                  [:dt "Edited:"])
+                (when (not= created edited)
+                  [:dd [:a {:href (str "/?vt=" (format-date edited))} (format-date edited)]])]
+               [:form.edit-comment {:action (str "/comment/" id) :method "POST"}
+                [:fieldset
                  [:input {:type "text" :name "created" :value (format-date created) :hidden true}]
                  [:input {:type "text" :name "name" :value name :hidden true}]
-                 [:label {:for (str "edit-message-" id)} "Message:"]
-                 [:br]
-                 [:textarea {:cols "40" :rows "3" :id (str "edit-message-" id) :name "message"} message]
-                 [:br]
-                 [:input {:type "submit" :name "_action" :value "Edit"}]
-                 [:input {:type "submit" :name "_action" :value "Delete"}]
-                 [:input {:type "submit" :name "_action" :value "Delete History"}]
-                 [:input {:type "submit" :name "_action" :value "Evict"}]]
-                [:br]])]]
-           [:hr]
+                 [:textarea {:id (str "edit-message-" id) :name "message"} message]]
+                [:input {:type "submit" :name "_action" :value "Edit"}]
+                [:input {:type "submit" :name "_action" :value "Delete"}]
+                [:input {:type "submit" :name "_action" :value "Delete History"}]
+                [:input {:type "submit" :name "_action" :value "Evict"}]]])]]
+
+          [:div.add-comment-box
            [:h3 "Add new comment"]
            [:form {:action "/comment" :method "POST"}
-            [:label {:for "name"} "Name:"] [:br]
-            [:input {:type "text" :id "name" :name "name"}]
-            [:br]
-            [:br]
-            [:label {:for "message"} "Message:"]
-            [:br]
-            [:textarea {:cols "40" :rows "10" :id "message" :name "message"}]
-            [:br]
-            [:input {:type "submit" :value "Submit"}]]
-           [:hr]
-           [:div
-            "Transaction History:"
-            [:pre
-             [:table
-              [:thead
-               [:th (str :crux.tx/tx-id)]
-               [:th (str :crux.tx/tx-time)]
-               [:th (str :crux.tx/tx-ops)]]
-              [:tbody
-               (for [{:crux.tx/keys [tx-id tx-time tx-ops]} tx-log]
-                 [:tr
-                  [:td [:a {:href (str "/?tt=" (format-date tx-time))} tx-id]]
-                  [:td (format-date tx-time)]
-                  [:td (with-out-str
-                         (pp/pprint tx-ops))]])]]]]
-           [:div
-            "Status:"
-            [:pre (with-out-str
-                    (pp/pprint (api/status crux)))]]]]])))))
+            [:fieldset
+             [:label {:for "name"} "Name:"]
+             [:input {:type "text" :id "name" :name "name"}]
+             [:label {:for "message"} "Message:"]
+             [:textarea {:cols "40" :rows "10" :id "message" :name "message"}]]
+            [:input {:type "submit" :value "Submit"}]]]
+
+          [:div.transaction-history
+           [:h4 "Transaction History:"
+            [:small "(Earliest first.)"]]
+           [:table
+            [:thead
+             [:th (str :crux.tx/tx-id)]
+             [:th (str :crux.tx/tx-time)]
+             [:th (str :crux.tx/tx-ops)]]
+            [:tbody
+             (for [{:crux.tx/keys [tx-id tx-time tx-ops]} tx-log]
+               [:tr
+                [:td [:a {:href (str "/?tt=" (format-date tx-time))} tx-id]]
+                [:td (format-date tx-time)]
+                [:td (with-out-str
+                       (pp/pprint tx-ops))]])]]]
+          [:div.status
+           [:h4 "Status:"]
+           [:pre (with-out-str
+                   (pp/pprint (api/status crux)))]]
+          [:footer
+           "© 2019 "
+           [:a {:href "https://juxt.pro"} "JUXT"]]]])))))
 
 (defn redirect-with-time [ctx valid-time transaction-time]
   (assoc (:response ctx)
@@ -206,7 +209,9 @@
                                    :message String
                                    :_action String}}
                :produces "text/html"
-               :response #(edit-comment-handler % system)}}})]]])
+               :response #(edit-comment-handler % system)}}})]
+    ["static"
+     (yada.resources.classpath-resource/new-classpath-resource "static")]]])
 
 (def index-dir "data/db-dir-1")
 (def log-dir "data/eventlog-1")
