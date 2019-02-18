89ce79c4a1c9d3b82b35fc43ca6ba7de9c69337c
Håkan Råberg <hakan.raberg@gmail.com>
2019-02-18 22:06:50 +0100

    Test showing need for seeking the left side of the match. All this needs cleanup. Morton dimensions are currently confused in various places, doesn't affect functionality, but understanding and consistency. #144.

--- test/crux/morton_test.clj
+++ test/crux/morton_test.clj
@@ -1,7 +1,19 @@
 (ns crux.morton-test
   (:require [clojure.test :as t]
             [crux.codec :as c]
-            [crux.morton :as morton]))
+            [crux.db :as db]
+            [crux.index :as idx]
+            [crux.kv :as kv]
+            [crux.morton :as morton]
+            [crux.fixtures :as f])
+  (:import org.agrona.ExpandableDirectByteBuffer))
+
+;; TODO: the x and y axis encoding has gone confused again, this is
+;; not necessarily a big issue, but it will confuse when comparing
+;; with various other resources. Most examples assume y dimension is
+;; before x, but our code has been aiming to not do this. But this is
+;; a bit misguided, as one can separate encoding from which value one
+;; puts on which dimension, so plan to fix this.
 
 (t/deftest test-can-encode-and-decode-morton-numbers
   (t/is (= 27 (morton/longs->morton-number 3 5)))
@@ -47,3 +59,73 @@
   (t/is (= [15 36] (morton/zdiv (morton/longs->morton-number 2 2)
                                 (morton/longs->morton-number 6 3)
                                 (morton/longs->morton-number 1 5)))))
+
+;; NOTE: this test is a bit unusual in that it creates the index
+;; without transacting, as we need to test certain relationships
+;; between vt and tt in Z order space. The times here are the raw
+;; reversed times.
+(t/deftest test-can-find-latest-value-on-x-axis
+  (f/with-kv-store
+    (fn []
+      (let [content-hash (c/new-id "0a4d55a8d778e5022fab701977c5d840bbc486d0")
+            eid (c/->id-buffer :foo)
+            valid-time 3
+            tx-time 2
+            z (morton/longs->morton-number valid-time tx-time)
+            tx-id 0
+            eb (ExpandableDirectByteBuffer.)
+            seek-at (fn [i vt tt]
+                      (db/seek-values (idx/->EntityMortonAsOfIndex i
+                                                                   (morton/longs->morton-number vt tt)
+                                                                   eb)
+                                      eid))]
+        (kv/store f/*kv* [[(c/encode-entity+z+tx-id-key-to
+                            nil
+                            eid
+                            z
+                            tx-id)
+                           (c/->id-buffer content-hash)]])
+
+        (with-open [snapshot (kv/new-snapshot f/*kv*)
+                    i (kv/new-iterator snapshot)]
+
+          (t/testing "visible after valid time at transaction time"
+            (let [[_ entity-tx] (seek-at i 2 2)]
+              (t/is (= valid-time (c/date->reverse-time-ms (:vt entity-tx))))
+              (t/is (= tx-time (c/date->reverse-time-ms (:tt entity-tx))))
+              (t/is (= tx-id (:tx-id entity-tx)))
+
+              (t/testing "stays visible"
+                (dotimes [vt 3]
+                  (dotimes [tt 2]
+                    (t/is (seek-at i vt tt)))))))
+
+          (t/testing "not visible before valid time"
+            (t/is (nil? (seek-at i 4 2))))
+
+          (t/testing "not visible before transaction time"
+            (t/is (nil? (seek-at i 2 3))))
+
+          (t/testing "not visible before both times"
+            (t/is (nil? (seek-at i 4 3)))))
+
+        (t/testing "with a value before in valid time from earlier transaction"
+          (let [valid-time 2
+                tx-time 4
+                z (morton/longs->morton-number valid-time tx-time)
+                tx-id 1]
+
+            (kv/store f/*kv* [[(c/encode-entity+z+tx-id-key-to
+                                nil
+                                eid
+                                z
+                                tx-id)
+                               (c/->id-buffer content-hash)]])
+
+            (t/testing "visible after valid time at transaction time"
+              (with-open [snapshot (kv/new-snapshot f/*kv*)
+                          i (kv/new-iterator snapshot)]
+                (let [[_ entity-tx] (seek-at i 2 2)]
+                  (t/is (= valid-time (c/date->reverse-time-ms (:vt entity-tx))))
+                  (t/is (= tx-time (c/date->reverse-time-ms (:tt entity-tx))))
+                  (t/is (= tx-id (:tx-id entity-tx))))))))))))
