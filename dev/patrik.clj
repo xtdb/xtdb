4087c26f3ea0810ebd6720933fb625424e19c17a
patrik <patrik.karlin@gmail.com>
2019-02-26 09:16:08 +0100

    aggrigation decorator: add inputs for aggregation functions

--- dev/patrik.clj
+++ dev/patrik.clj
@@ -19,7 +19,7 @@
            (-> acc :aggr/output vals)))
         ([acc input]
          (reduce
-           (fn [acc [window-name {:keys [partition-by order-by reductions]}]]
+           (fn [acc [window-name {:keys [partition-by order-by reductions] :as all}]]
              (let [partition (when partition-by
                                (reduce
                                  (fn [partition [partition-part-key partition-fn]]
@@ -41,12 +41,15 @@
                      acc
                      [window-name partition]
                      (reduce
-                       (fn [new-state [field {:keys [aggregator]}]]
+                       (fn [new-state [field {:keys [aggregator inputs]}]]
                          (let [p-value (or (get prev-state field)
                                            (aggregator))]
                            (assoc new-state field
-                                  (aggregator p-value input ;; gather the input to the aggrigator
-                                              ))))
+                                  (aggregator p-value
+                                              (merge partition
+                                                     (into {}
+                                                           (for [[k f] inputs]
+                                                             [k (f input)])))))))
                        partition
                        reductions)))
                  (let [acc (if (contains? (get acc window-name) partition)
@@ -70,8 +73,8 @@
   (loop [i 0
          [h & t] coll]
     (if (nil? h)
-      (throw (ex-info "unexpected value not in collection" {:value value
-                                                            :coll coll}))
+      (throw (ex-info "unexpected value not in collection"
+                      {:value value :coll coll}))
       (if (= h value)
         i
         (recur (inc i) t)))))
@@ -83,56 +86,92 @@
 
 (defn aggregate-decorate-datalog
   [{:as datalog
-    {:keys [partition-by project]} :aggrigation}]
-  (let [find-variables (->> (concat (map second partition-by)
-                                    (mapcat
-                                      (fn [[key f inputs]] (vals inputs))
-                                      project))
-                            set vec)]
+    {:keys [partition-by order-by project windows]} :aggrigation}]
+  (let [find-variables
+        (->> (concat (map
+                       (fn [item]
+                         (if (vector? item) (second item) item))
+                       partition-by)
+                     (map second order-by)
+                     (mapcat
+                       (fn [[key f inputs]] (vals inputs))
+                       project))
+             set vec)]
     {:aggregation-spec
      {:windows
-      {:aggr/output
-       {:partition-by (for [[k v] partition-by]
-                        (let [idx (index-of v find-variables)]
-                          [k (fn [val] (get val idx))]))
-        :reductions (into {} (for [[k f inputs] project]
-                               [k {:aggregator (resolve f)}]))}}}
+      (reduce
+        (fn [acc [window-name {:keys [partition-by order-by] :as window}]]
+          (let [{:keys [aggregation-spec datalog]}
+                (aggregate-decorate-datalog {:aggrigation window})]
+            (assoc acc window-name
+                   (merge (-> aggregation-spec :windows :aggr/output)
+                          {:runner-fn (fn [snapshot db] (crux.api/q db snapshot datalog))}))))
+        {:aggr/output
+         {:partition-by (for [item partition-by]
+                          (let [k (if (vector? item) (first item) (keyword item))
+                                v (if (vector? item) (second item) item)
+                                idx (index-of v find-variables)]
+                            [k (fn [val] (get val idx))]))
+          :reductions (into {} (for [[k f inputs] project]
+                                 [k {:aggregator (resolve f)
+                                     :inputs (into {}
+                                                   (for [[k v] inputs]
+                                                     (let [idx (index-of v find-variables)]
+                                                       [k (fn [val] (get val idx))])))}]))}}
+        windows)}
 
      :datalog (merge datalog {:find find-variables})}))
 
+(defn window->partition-components
+  [{:keys [order-by partition-by]}]
+  (concat
+    (map first partition-by)
+    (map first order-by)))
+
+(defn partition-tree
+  [aggregation-spec]
+  (let [window-name->partition-components
+        (map (juxt first (comp window->partition-components second))
+             (:windows aggregation-spec))]
+    (fn step [current-path]
+      {:type :root-node
+       :children
+       (for [path-component (set (map first (vals window-name->partition-components)))]
+         {:path path-component
+          })})))
+
 (defn run-datalog-aggregation
   [db {:keys [aggregation-spec datalog]}]
-  (transduce
-    (aggregation-runner aggregation-spec)
-    conj
-    {}
-    (crux.api/q db datalog)))
+  (with-open [snapshot (crux.api/new-snapshot db)]
+    (reduce
+      (fn [state [window-name window-spec]]
+        (transduce
+          (aggregation-runner aggregation-spec)
+          conj
+          {}
+          (crux.api/q db snapshot datalog)))
+      {}
+      (:windows aggregation-spec))))
 
 (comment
 
-  (clojure.pprint/pprint
-    (aggregate-decorate-datalog
-      '{:partition-by [[:name ?name]]
-        :project [[:post-count patrik/aggr-count {:post-id ?post-id}]]
-
-        :where [[?post :user/name ?name]
-                [?post :user/post ?post-id]]}))
-
-
   (require 'crux.api)
-  (def crux (crux.api/start-standalone-system {:kv-backend "crux.kv.rocksdb.RocksKv"
-                                               :db-dir "data/db-dir"
-                                               :event-log-dir "data/event-log-db"}))
 
-  (crux.api/submit-tx crux [[:crux.tx/put :a1
-                             {:crux.db/id :a1 :user/name "patrik" :user/post 1}]
-                            [:crux.tx/put :a2
-                             {:crux.db/id :a2 :user/name "patrik" :user/post 2}]
-                            [:crux.tx/put :a3
-                             {:crux.db/id :a3 :user/name "patrik" :user/post 3}]
-                            [:crux.tx/put :a4
-                             {:crux.db/id :a4 :user/name "niclas" :user/post 3}]])
+  (def crux (crux.api/start-standalone-system
+              {:kv-backend "crux.kv.rocksdb.RocksKv"
+               :db-dir "data/db-dir"
+               :event-log-dir "data/event-log-db"}))
 
+  (crux.api/submit-tx
+    crux
+    [[:crux.tx/put :a1
+      {:crux.db/id :a1 :user/name "patrik" :user/post 1 :post/cost 30}]
+     [:crux.tx/put :a2
+      {:crux.db/id :a2 :user/name "patrik" :user/post 2 :post/cost 35}]
+     [:crux.tx/put :a3
+      {:crux.db/id :a3 :user/name "patrik" :user/post 3 :post/cost 5}]
+     [:crux.tx/put :a4
+      {:crux.db/id :a4 :user/name "niclas" :user/post 1 :post/cost 8}]])
 
   (clojure.pprint/pprint
     (run-datalog-aggregation
@@ -144,19 +183,68 @@
           :where [[?post :user/name ?name]
                   [?post :user/post ?post-id]]})))
 
-  (transduce
-    (aggregation-runner
-      {:windows
-       {:aggr/output
-        {:partition-by [[:name :name]]
-         :reductions {:user-count {:aggregator count}}}}})
-    conj
-    {}
-    [{:name "patrik" :post "1"}
-     {:name "patrik" :post "2"}
-     {:name "patrik" :post "3"}
+  (defn avr-cost
+    ([] {:count 0 :total 0})
+    ([{:keys [count total]}] (/ total count))
+    ([acc inputs]
+     (-> acc
+         (update :count inc)
+         (update :total + (:cost inputs)))))
 
-     {:name "niclas" :post "1"}])
+  (defn sum-cost
+    ([] 0)
+    ([acc] acc)
+    ([acc inputs]
+     (+ acc (:cost inputs))))
+
+  (clojure.pprint/pprint
+    (run-datalog-aggregation
+      (crux.api/db crux)
+      (aggregate-decorate-datalog
+        '{:aggrigation {:windows {:user-costs {:partition-by [[:name ?name]]
+                                               :order-by [[:post-id ?post-id :asc]]}}
+
+                        :partition-by [[:name ?name]
+                                       [:post-id ?post-id]]
+
+                        :project [[:avr-cost patrik/avr-cost {:cost ?cost}]
+                                  [:running-cost patrik/sum-cost {:cost ?cost} :user-costs]]}
+
+          :where [[?post :user/name ?name]
+                  [?post :user/post ?post-id]
+                  [?post :post/cost ?cost]]})))
+
+  (clojure.pprint/pprint
+    (aggregate-decorate-datalog
+      '{:aggrigation {:partition-by [[:name ?name]]
+                      :project [[:avr-cost patrik/avr-cost {:cost ?cost}]
+                                [:running-cost patrik/sum-cost {:cost ?cost}]
+                                [:post-count patrik/aggr-count {:post-id ?post-id}]]}
+
+        :where [[?post :user/name ?name]
+                [?post :user/post ?post-id]
+                [?post :post/cost ?cost]]}))
+
+
+  (clojure.pprint/pprint
+    (run-datalog-aggregation
+      (crux.api/db crux)
+      (aggregate-decorate-datalog
+        '{:aggrigation
+          {:partition-by [?name ?post-id]
+           :order-by [[?name :asc]]}
+
+          :where [[?post :user/name ?name]
+                  [?post :user/post ?post-id]]})))
+
+
+
+  (defn running-cost
+    ([] 0)
+    ([i] i)
+    ([acc i]
+     (+ acc i)))
+
 
 
   )
