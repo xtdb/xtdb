7206a1244c86736c6805f206d35810a3cd9cdd56
Ivan Fedorov <hello@spacegangster.io>
2019-04-05 19:53:00 +0300

    docs/user_guide : newbie notes

--- docs/examples.clj
+++ docs/examples.clj
@@ -1,10 +1,12 @@
 (ns examples)
 
 ;; tag::start-system[]
-(require '[crux.api :as api])
+(require '[crux.api :as crux])
+(import (crux.api ICruxAPI))
 
-(def ^crux.api.ICruxAPI system (api/start-standalone-system {:kv-backend "crux.kv.memdb.MemKv"
-                                                             :db-dir "data/db-dir-1"}))
+(def ^crux.api.ICruxAPI system ;; xxx what does the meta do?
+  (crux/start-standalone-system {:kv-backend "crux.kv.memdb.MemKv"
+                                 :db-dir "data/db-dir-1"}))
 ;; end::start-system[]
 
 ;; tag::close-system[]
@@ -12,23 +14,25 @@
 ;; end::close-system[]
 
 ;; tag::start-cluster-node-system[]
-(def ^crux.api.ICruxAPI system (api/start-cluster-node {:kv-backend "crux.kv.memdb.MemKv"
-                                                        :bootstrap-servers "localhost:29092"}))
+(def ^crux.api.ICruxAPI system
+  (api/start-cluster-node {:kv-backend "crux.kv.memdb.MemKv"
+                           :bootstrap-servers "localhost:29092"}))
 ;; end::start-cluster-node-system[]
 
 ;; tag::start-standalone-with-rocks[]
-(def ^crux.api.ICruxAPI system (api/start-standalone-system {:kv-backend "crux.kv.rocksdb.RocksKv"
-                                                             :db-dir "data/db-dir-1"}))
+(def ^crux.api.ICruxAPI system
+  (api/start-standalone-system {:kv-backend "crux.kv.rocksdb.RocksKv"
+                                :db-dir "data/db-dir-1"}))
 ;; end::start-standalone-with-rocks[]
 
 ;; tag::submit-tx[]
 (api/submit-tx
  system
- [[:crux.tx/put :http://dbpedia.org/resource/Pablo_Picasso
-   {:crux.db/id :http://dbpedia.org/resource/Pablo_Picasso
+ [[:crux.tx/put :http://dbpedia.org/resource/Pablo_Picasso ; id for Kafka
+   {:crux.db/id :http://dbpedia.org/resource/Pablo_Picasso ; id for Crux
     :name "Pablo"
     :last-name "Picasso"}
-   #inst "2018-05-18T09:20:27.966-00:00"]])
+   #inst "2018-05-18T09:20:27.966-00:00"]]) ; valid time
 ;; end::submit-tx[]
 
 ;; tag::query[]
@@ -36,6 +40,10 @@
        '{:find [e]
          :where [[e :name "Pablo"]]})
 ;; end::query[]
+
+;; tag::query-entity[]
+(api/entity (api/db system) :http://dbpedia.org/resource/Pablo_Picasso)
+;; end::query-entity[]
 
 ;; tag::query-valid-time[]
 (api/q (api/db system #inst "2018-05-19T09:20:27.966-00:00")
@@ -55,6 +63,12 @@
   ;; tag::should-get[]
   #{[:http://dbpedia.org/resource/Pablo_Picasso]}
   ;; end::should-get[]
+
+  ;; tag::should-get-entity[]
+  {:crux.db/id :http://dbpedia.org/resource/Pablo_Picasso
+   :name "Pablo"
+   :last-name "Picasso"}
+  ;; end::should-get-entity[]
   )
 
 ;; tag::ek-example[]
