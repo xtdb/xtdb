d91c02f1689db4454feaa05f4115bf4495238297
Jon Pither <jon.pither@gmail.com>
2019-04-24 20:54:19 +0100

    Prefer clojure.string

--- tasks/leiningen/project_version.clj
+++ tasks/leiningen/project_version.clj
@@ -1,5 +1,6 @@
 (ns leiningen.project-version
-  (:require [clojure.java.shell :as sh]))
+  (:require [clojure.java.shell :as sh]
+            [clojure.string]))
 
 (defn project-version [project & args]
   (println (:version project)))
@@ -8,7 +9,7 @@
   (let [{:keys [exit out]} (sh/sh "git" "describe" "--tags" "--dirty" "--long")]
     (assert (= 0 exit))
     ;;"19.04-1.0.2-alpha-22-g9452e91b-dirty"
-    (let [[_ tag ahead sha dirty] (re-find #"(.*)\-(\d+)\-([0-9a-z]*)(\-dirty)?$" (.trim out))
+    (let [[_ tag ahead sha dirty] (re-find #"(.*)\-(\d+)\-([0-9a-z]*)(\-dirty)?$" (clojure.string/trim out))
           ahead? (not= ahead "0")
           dirty? (not (empty? dirty))]
       (assert (re-find #"^\d+\.\d+\-\d+\.\d+\.\d+(\-(alpha|beta))?$" tag) "Tag format unexpected.")
