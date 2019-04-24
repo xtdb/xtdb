bf66f8540a29994d2974b9b5552e560fc96c6c40
Jon Pither <jon.pither@gmail.com>
2019-04-24 20:52:45 +0100

    Remove git-version plugin #194

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject juxt/crux :project/ref
+(defproject juxt/crux :derived-from-git
   :description "An open source document database with bitemporal graph queries"
   :url "https://github.com/juxt/crux"
   :license {:name "The MIT License"
@@ -24,7 +24,7 @@
                  [ring-cors "0.1.13" :scope "provided"]
                  [org.eclipse.rdf4j/rdf4j-rio-ntriples "2.4.5" :scope "provided"]
                  [org.eclipse.rdf4j/rdf4j-queryparser-sparql "2.4.5" :scope "provided"]]
-  :plugins [[me.arrdem/lein-git-version "2.0.8"]]
+  :middleware [leiningen.project-version/middleware]
   :profiles { ;; Provided dependencies excluded from uberjar.
              :provided {:dependencies [[org.apache.kafka/kafka_2.11 "2.1.1"]
                                        [org.apache.zookeeper/zookeeper "3.4.13"
@@ -70,13 +70,6 @@
                                   [integrant "0.6.3"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user}}}
-  :git-version {:status-to-version
-                (fn [{:keys [tag version branch ahead ahead? dirty?] :as git}]
-                  ;; e.g. 19.04-1.0.2-alpha
-                  (assert (re-find #"^\d+\.\d+\-\d+\.\d+\.\d+(\-(alpha|beta))?$" tag) "Tag format unexpected.")
-                  (if (and (not ahead?) (not dirty?))
-                    tag
-                    (str tag "-SNAPSHOT")))}
   :java-source-paths ["src"]
   :javac-options ["-source" "8" "-target" "8"
                   "-XDignore.symbol.file"
--- tasks/leiningen/project_version.clj
+++ tasks/leiningen/project_version.clj
@@ -1,4 +1,20 @@
-(ns leiningen.project-version)
+(ns leiningen.project-version
+  (:require [clojure.java.shell :as sh]))
 
 (defn project-version [project & args]
   (println (:version project)))
+
+(defn- version-from-git []
+  (let [{:keys [exit out]} (sh/sh "git" "describe" "--tags" "--dirty" "--long")]
+    (assert (= 0 exit))
+    ;;"19.04-1.0.2-alpha-22-g9452e91b-dirty"
+    (let [[_ tag ahead sha dirty] (re-find #"(.*)\-(\d+)\-([0-9a-z]*)(\-dirty)?$" (.trim out))
+          ahead? (not= ahead "0")
+          dirty? (not (empty? dirty))]
+      (assert (re-find #"^\d+\.\d+\-\d+\.\d+\.\d+(\-(alpha|beta))?$" tag) "Tag format unexpected.")
+      (if (and (not ahead?) (not dirty?))
+        tag
+        (str tag "-SNAPSHOT")))))
+
+(defn middleware [project]
+  (assoc project :version (version-from-git)))
