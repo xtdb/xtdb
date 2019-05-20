57cd5c57ae5fd91a28f0d93d89bb276809f265fe
patrik <patrik.karlin@gmail.com>
2019-05-20 14:55:25 +0200

    bench-runner: update yada version for bench
    This current version has a problem with building a uberjar

--- bench/project.clj
+++ bench/project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [juxt/crux "19.04-1.0.3-alpha-SNAPSHOT"]
-                 [yada "1.3.0-alpha7"]
+                 [yada "1.3.0-alpha10"]
                  [hiccup "2.0.0-alpha2"]
                  [org.eclipse.rdf4j/rdf4j-rio-ntriples "2.4.3"]
                  [org.eclipse.rdf4j/rdf4j-queryparser-sparql "2.4.3"]
