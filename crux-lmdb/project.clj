d82293636f415cf64874ab351b33fc04b0ae2cae
Jon Pither <jon.pither@gmail.com>
2019-07-01 10:35:40 +0100

    Tests are running at the uber level

---
+++ crux-lmdb/project.clj
@@ -0,0 +1,14 @@
+(defproject juxt/crux-lmdb :derived-from-git
+  :description "Crux LMDB"
+  :url "https://github.com/juxt/crux"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.github.jnr/jnr-ffi "2.1.9"]
+                 [org.lmdbjava/lmdbjava "0.6.3" :exclusions [com.github.jnr/jffi]]
+                 [org.lwjgl/lwjgl "3.2.2" :classifier "natives-linux" :native-prefix ""]
+                 [org.lwjgl/lwjgl-lmdb "3.2.2" :classifier "natives-linux" :native-prefix ""]
+                 [org.lwjgl/lwjgl "3.2.2" :classifier "natives-macos" :native-prefix ""]
+                 [org.lwjgl/lwjgl-lmdb "3.2.2" :classifier "natives-macos" :native-prefix ""]
+                 [org.lwjgl/lwjgl-lmdb "3.2.2"]]
+  :middleware [leiningen.project-version/middleware])
--- project.clj
+++ project.clj
@@ -6,17 +6,17 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "0.4.1"]
                  [com.taoensso/encore "2.114.0"]
+
+                 [juxt/crux-core :derived-from-git]
+                 [juxt/crux-lmdb :derived-from-git]
+                 [juxt/crux-kafka :derived-from-git]
+
                  ;; Provided dependencies included in uberjar.
                  [org.clojure/tools.cli "0.4.2" :scope "provided"]
                  [org.apache.kafka/kafka-clients "2.2.0" :scope "provided"]
-                 [com.github.jnr/jnr-ffi "2.1.9" :scope "provided"]
+
                  [org.rocksdb/rocksdbjni "6.0.1" :scope "provided"]
-                 [org.lwjgl/lwjgl "3.2.2" :classifier "natives-linux" :native-prefix "" :scope "provided"]
-                 [org.lwjgl/lwjgl-lmdb "3.2.2" :classifier "natives-linux" :native-prefix "" :scope "provided"]
-                 [org.lwjgl/lwjgl "3.2.2" :classifier "natives-macos" :native-prefix "" :scope "provided"]
-                 [org.lwjgl/lwjgl-lmdb "3.2.2" :classifier "natives-macos" :native-prefix "" :scope "provided"]
-                 [org.lwjgl/lwjgl-lmdb "3.2.2" :scope "provided"]
-                 [org.lmdbjava/lmdbjava "0.6.3" :exclusions [com.github.jnr/jffi] :scope "provided"]
+
                  [ring/ring-core "1.7.1" :scope "provided"]
                  [ring/ring-jetty-adapter "1.7.1" :scope "provided"]
                  [ring/ring-codec "1.1.2" :scope "provided"]
@@ -25,14 +25,7 @@
                  [org.eclipse.rdf4j/rdf4j-queryparser-sparql "2.5.1" :scope "provided"]]
   :middleware [leiningen.project-version/middleware]
   :profiles { ;; Provided dependencies excluded from uberjar.
-             :provided {:dependencies [[org.apache.kafka/kafka_2.12 "2.2.0"]
-                                       [org.apache.zookeeper/zookeeper "3.4.14"
-                                        :exclusions [io.netty/netty
-                                                     jline
-                                                     org.apache.yetus/audience-annotations
-                                                     org.slf4j/slf4j-log4j12
-                                                     log4j]]
-                                       [clj-http "3.10.0"]
+             :provided {:dependencies [[clj-http "3.10.0"]
                                        [http-kit "2.3.0"]]}
              :uberjar {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]
                        :resource-paths ["resources-uberjar"]}
@@ -45,7 +38,8 @@
                                      crux.kv.rocksdb
                                      crux.main.graal]
                      :main crux.main.graal}
-             :dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+             :dev {:dependencies [[juxt/crux-dev :derived-from-git]
+                                  [org.clojure/tools.namespace "0.2.11"]
                                   [criterium "0.4.5"]
                                   [com.datomic/datomic-free "0.9.5697"
                                    :exclusions [org.slf4j/slf4j-nop]]
@@ -73,6 +67,9 @@
   :hiera {:ignore-ns #{dev hakan prof patrik
                        crux.decorators crux.http-server crux.main.graal}
           :path "dev/ns-hierarchy.png"}
+
+  :plugins [[lein-sub "0.3.0"]]
+  :sub ["crux-kafka" "crux-lmdb" "crux-core" "crux-dev"]
 
   :aot [crux.main]
   :main crux.main
