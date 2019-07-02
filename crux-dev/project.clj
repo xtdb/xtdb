3a797bf9accaaecea08a2f697f7d975c03d62e1b
Jon Pither <jon.pither@gmail.com>
2019-07-02 11:26:34 +0100

    Retire crux-dev

--- crux-dev/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject juxt/crux-dev :derived-from-git
-  :description "Crux Dev Dependencies"
-  :url "https://github.com/juxt/crux"
-  :license {:name "The MIT License"
-            :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/test.check "0.10.0-alpha3"]
-                 [ch.qos.logback/logback-classic "1.2.3"]]
-  :middleware [leiningen.project-version/middleware])
