7aba640191f81e0e439e102607e0e1fea6f6e136
Ivan Fedorov <hello@spacegangster.io>
2019-07-19 00:59:47 +0300

    Console/plots : add history fetch fx

--- crux-console/src/juxt/crux_ui/frontend/io/query.cljs
+++ crux-console/src/juxt/crux_ui/frontend/io/query.cljs
@@ -2,14 +2,15 @@
   (:require [clojure.core.async :as async
              :refer [take! put! <! >! timeout chan alt! go go-loop]]
             [re-frame.core :as rf]
-            [juxt.crux-lib.async-http-client :as crux-api]))
+            [juxt.crux-lib.async-http-client :as crux-api]
+            [promesa.core :as p]))
 
 (defn post-opts [body]
   #js {:method "POST"
        :body body
        :headers #js {:Content-Type "application/edn"}})
 
-(def c (crux-api/new-api-client "http://localhost:8080"))
+(def node-client (crux-api/new-api-client "http://localhost:8080"))
 
 (defn on-exec-success [resp]
   (rf/dispatch [:evt.io/query-success resp]))
@@ -20,22 +21,26 @@
 (defn on-tx-success [resp]
   (rf/dispatch [:evt.io/tx-success resp]))
 
+(defn on-histories-success [resp]
+  (rf/dispatch [:evt.io/histories-fetch-success resp]))
+
 (defn submit-tx []
   (let [tx [[:crux.tx/put :dbpedia.resource/Pablo-Picasso3 ; id for Kafka
              {:crux.db/id :dbpedia.resource/Pablo-Picasso3 ; id for Crux
               :name "Pablo"
               :last-name "Picasso3"}]]
-        promise (crux-api/submitTx c tx)]
+        promise (crux-api/submitTx node-client tx)]
     (.then on-tx-success)))
 
 (defn exec-q [query-text vt tt]
-  (let [db (crux-api/db c vt tt)
+  (let [db (crux-api/db node-client vt tt)
         promise (crux-api/q db query-text)]
     (.then promise on-exec-success)))
 
 (defn exec-tx [query-text]
-  (let [promise (crux-api/submitTx c query-text)]
-    (.then promise on-tx-success)))
+  (-> node-client
+      (crux-api/submitTx query-text)
+      (p/then on-tx-success)))
 
 (defn exec [{:keys [query-vt query-tt raw-input query-analysis] :as query}]
   (let [qtype (:crux.ui/query-type query-analysis)]
@@ -47,10 +52,18 @@
         :crux.ui.query-type/tx-single (exec-tx raw-input)))))
 
 (defn fetch-stats []
-  (let [p (crux-api/attributeStats c)]
-    (.then p on-stats-success)))
+  (-> node-client
+      (crux-api/attributeStats)
+      (p/then on-stats-success)))
+
+(defn fetch-history [eid]
+  (crux-api/historyRange node-client eid nil nil nil nil))
+
+(defn fetch-histories [eids]
+  (-> (p/all (map fetch-history eids))
+      (p/then on-histories-success)))
 
 (comment
   (exec-q (pr-str '{:full-results? true
-                  :find [e]
-                  :where [[e :name "Pablo"]]})))
+                    :find [e]
+                    :where [[e :name "Pablo"]]})))
