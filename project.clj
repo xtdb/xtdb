88bf32a82b2dc3311efa0e12d98dd808a5d58ad2
Jon Pither <jon.pither@gmail.com>
2019-07-29 13:12:51 +0100

    Attempt at excluding jdbc from lein check

--- project.clj
+++ project.clj
@@ -1,3 +1,16 @@
+(def modules ["crux-core"
+              "crux-rdf"
+              "crux-rocksdb"
+              "crux-lmdb"
+              "crux-kafka-embedded"
+              "crux-kafka"
+              "crux-jdbc"
+              "crux-http-client"
+              "crux-http-server"
+              "crux-uberjar"
+              "crux-decorators"
+              "crux-test"])
+
 (defproject juxt/crux "derived-from-git"
   :description "An open source document database with bitemporal graph queries"
   :url "https://github.com/juxt/crux"
@@ -5,19 +18,9 @@
             :url "http://opensource.org/licenses/MIT"}
   :middleware [leiningen.project-version/middleware]
   :plugins [[lein-sub "0.3.0"]]
-  :sub ["crux-core"
-        "crux-rdf"
-        "crux-rocksdb"
-        "crux-lmdb"
-        "crux-kafka-embedded"
-        "crux-kafka"
-        "crux-jdbc"
-        "crux-http-client"
-        "crux-http-server"
-        "crux-uberjar"
-        "crux-decorators"
-        "crux-test"]
-  :aliases {"build" ["do" ["sub" "install"] ["sub" "test"]]}
+  :sub modules
+  :aliases {"check" ["sub" "-s" ~(clojure.string/join ":" (remove #{"crux-jdbc"} modules)) "check"]
+            "build" ["do" ["sub" "install"] ["sub" "test"]]}
   :repositories [["snapshots" {:url "https://repo.clojars.org"
                                :username :env/clojars_username
                                :password :env/clojars_password
