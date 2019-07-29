8f7bb072044a2535155ebf2e591415b02e7647e1
Jon Pither <jon.pither@gmail.com>
2019-07-29 11:07:03 +0100

    Comment out Oracle for now

--- crux-test/project.clj
+++ crux-test/project.clj
@@ -19,7 +19,9 @@
                  [com.opentable.components/otj-pg-embedded "0.13.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [mysql/mysql-connector-java "8.0.17"]
-                 [com.oracle/ojdbc "12.2.0.1" :scope "provided"]
+
+                 ;; Uncomment to test Oracle, you'll need to locally install the JAR:
+                 ;; [com.oracle/ojdbc "12.2.0.1"]
 
                  ;; General:
                  [org.clojure/test.check "0.10.0-alpha3"]
