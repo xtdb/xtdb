5a92c89a450c43aeed7354b404c57bcf00e97eca
Håkan Råberg <hakan.raberg@gmail.com>
2019-08-12 10:11:48 +0100

    Attempt at slightly more sophisticated value conversion, sill issues with gettng schemaless JSON to work. #292.

--- crux-kafka-connect/src/crux/kafka/connect.clj
+++ crux-kafka-connect/src/crux/kafka/connect.clj
@@ -1,15 +1,40 @@
 (ns crux.kafka.connect
   (:require [cheshire.core :as json]
             [clojure.string :as str]
+            [clojure.tools.logging :as log]
             [crux.codec :as c])
-  (:import org.apache.kafka.connect.sink.SinkRecord
-           java.util.UUID
+  (:import org.apache.kafka.connect.data.Struct
+           org.apache.kafka.connect.sink.SinkRecord
+           [java.util UUID Map]
            crux.api.ICruxAPI))
 
+(defn- map->edn [m]
+  (->> (for [[k v] m]
+         [(keyword k)
+          (if (instance? Map v)
+            (map->edn v)
+            v)])
+       (into {})))
+
+(defn- record->edn [^SinkRecord record]
+  (let [value (.value record)]
+    (log/info "Raw Value:" value)
+    (cond
+      (and (instance? Struct value)
+           (.valueSchema record))
+      (throw (UnsupportedOperationException. "Avro conversion not yet supported."))
+      (instance? Map value)
+      (map->edn value)
+      (string? value)
+      (json/parse-string (.value record) true)
+      :else
+      (throw (IllegalArgumentException. (str "Unknown message type: " record))))))
+
 (defn transform-sink-record [props ^SinkRecord record]
-  (let [doc (json/parse-string (.value record) true)
-        id (get doc (or (some-> (get props "id.key") (keyword))
-                        :crux.db/id))
+  (let [doc (record->edn record)
+        id (or (.key record)
+               (get doc (or (some-> (get props "id.key") (keyword))
+                            :crux.db/id)))
         id (cond
              (c/valid-id? id)
              (c/id-edn-reader id)
