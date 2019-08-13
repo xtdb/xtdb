8e1e4a5915cb30b7a1880746565b6e9d0492b181
Håkan Råberg <hakan.raberg@gmail.com>
2019-08-13 17:24:18 +0100

    Upgrading the bench project to use the module system.

--- bench/project.clj
+++ bench/project.clj
@@ -4,12 +4,13 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [juxt/crux "19.07-1.1.1-alpha"]
+                 [juxt/crux-core "19.07-1.3.0-alpha"]
+                 [juxt/crux-rdf "19.07-1.3.0-alpha"]
+                 [juxt/crux-rocksdb "19.07-1.3.0-alpha"]
                  [yada "1.3.0-alpha10"]
                  [hiccup "2.0.0-alpha2"]
                  [org.eclipse.rdf4j/rdf4j-rio-ntriples "2.4.3"]
                  [org.eclipse.rdf4j/rdf4j-queryparser-sparql "2.4.3"]
-                 [org.rocksdb/rocksdbjni "5.17.2"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.apache.kafka/kafka-clients "2.1.0"]
                  [buddy/buddy-hashers "1.3.0"]
