dff0fb846a68e9d3aa3bd179e72ef840584aed55
Håkan Råberg <hakan.raberg@gmail.com>
2019-08-13 17:30:54 +0100

    Moving bench towards being a normal module, rename pending.

--- bench/project.clj
+++ bench/project.clj
@@ -1,12 +1,12 @@
-(defproject juxt/crux-bench "0.1.0-SNAPSHOT"
+(defproject juxt/crux-bench "derived-from-git"
   :description "A example standalone webservice with crux"
   :url "https://github.com/juxt/crux"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [juxt/crux-core "19.07-1.3.0-alpha"]
-                 [juxt/crux-rdf "19.07-1.3.0-alpha"]
-                 [juxt/crux-rocksdb "19.07-1.3.0-alpha"]
+                 [juxt/crux-core "derived-from-git"]
+                 [juxt/crux-rdf "derived-from-git"]
+                 [juxt/crux-rocksdb "derived-from-git"]
                  [yada "1.3.0-alpha10"]
                  [hiccup "2.0.0-alpha2"]
                  [org.eclipse.rdf4j/rdf4j-rio-ntriples "2.4.3"]
@@ -18,6 +18,7 @@
                  [com.datomic/datomic-free "0.9.5697" :exclusions [org.slf4j/slf4j-nop]]
 
                  [amazonica "0.3.139"]]
+  :middleware [leiningen.project-version/middleware]
   :global-vars {*warn-on-reflection* true}
   :resource-paths ["resources"
                    #_"/watdiv-data/resources"]
