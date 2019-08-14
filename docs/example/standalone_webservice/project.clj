c9fb4a32db10e5ec603a8cf9b37da614a50586fc
Håkan Råberg <hakan.raberg@gmail.com>
2019-08-14 17:19:45 +0100

    Freshen up the examples a bit, adding logback.xml, ensuring modules are used.

--- docs/example/standalone_webservice/project.clj
+++ docs/example/standalone_webservice/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/juxt/crux"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.7.0"] ; 1.7.1 breaks with Jetty ClassNotFound https://github.com/bhauman/figwheel-main/issues/95
 ;                 [ring/ring-core "1.7.1"]
 ;                 [ring/ring-jetty-adapter "1.7.1"]
@@ -12,6 +12,7 @@
                  [juxt/crux-core "19.07-1.3.0-alpha"]
                  [juxt/crux-rocksdb "19.07-1.3.0-alpha"]
                  [juxt/crux-http-server "19.07-1.3.0-alpha"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [yada "1.3.0-alpha7"]
                  [hiccup "2.0.0-alpha2"]
                  [cljsjs/vega "4.4.0-0"]
