f785731af83f96a90947a3a516497792b70816b4
Håkan Råberg <hakan.raberg@gmail.com>
2019-08-14 16:30:05 +0100

    Updating a bunch of dependencies.

--- crux-http-server/project.clj
+++ crux-http-server/project.clj
@@ -4,9 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-
                  [juxt/crux-core "derived-from-git"]
-
                  [ring/ring-core "1.7.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [ring/ring-codec "1.1.2"]
