2faecc82100f51bcc4b4b8c18488841bf606f929
Jon Pither <jon.pither@gmail.com>
2019-08-31 13:31:31 +0100

    Fix for mysql + connection pooling

--- crux-jdbc/src/crux/jdbc/mysql.clj
+++ crux-jdbc/src/crux/jdbc/mysql.clj
@@ -8,3 +8,6 @@
   tx_time datetime(3) default CURRENT_TIMESTAMP, topic VARCHAR(255) NOT NULL,
   v BLOB NOT NULL, compacted INTEGER NOT NULL)"])
   (jdbc/execute! ds ["create index if not exists tx_events_event_key_idx on tx_events(compacted, event_key)"]))
+
+(defmethod j/->pool-options :mysql [_ {:keys [username user] :as options}]
+  (assoc options :username (or username user)))
