df2a6904cc09d714fefbefde56c53f40a2be388f
Håkan Råberg <hakan.raberg@gmail.com>
2019-09-02 11:36:30 +0100

    Adding :json-verbose Transit serdes for Kafka. Should make topics possible to introspect as JSON, while also possible to roundtrip. #284.

--- crux-kafka/project.clj
+++ crux-kafka/project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/tools.logging "0.5.0"]
                  [juxt/crux-core "derived-from-git"]
                  [org.apache.kafka/kafka-clients "2.3.0"]
-                 [cheshire "5.9.0"]]
+                 [cheshire "5.9.0"]
+                 [com.cognitect/transit-clj "0.8.313" :exclusions [org.msgpack/msgpack]]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}}
   :middleware [leiningen.project-version/middleware]
   :java-source-paths ["src"])
---
+++ crux-kafka/src/crux/kafka/transit/TransitSerializer.java
@@ -0,0 +1,59 @@
+package crux.kafka.transit;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import clojure.java.api.Clojure;
+import clojure.lang.AFn;
+import clojure.lang.IDeref;
+import clojure.lang.IFn;
+import org.apache.kafka.common.serialization.Serializer;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+public class TransitSerializer implements Serializer<Object> {
+    private static final IFn write;
+    private static final IFn writer;
+    private static final IFn writeHandler;
+    private static final IFn ednIdToOriginalId;
+    private static final Object jsonVerbose;
+    private static final Map<?, ?> options;
+
+    static {
+        Clojure.var("clojure.core/require").invoke(Clojure.read("cognitect.transit"));
+        write = Clojure.var("cognitect.transit/write");
+        writer = Clojure.var("cognitect.transit/writer");
+        writeHandler = Clojure.var("cognitect.transit/write-handler");
+        Clojure.var("clojure.core/require").invoke(Clojure.read("crux.codec"));
+        ednIdToOriginalId = (IFn) ((IDeref) Clojure.var("crux.codec/edn-id->original-id")).deref();
+        jsonVerbose = Clojure.read(":json-verbose");
+        options = new HashMap<>() {
+                {
+                    put(Clojure.read(":handlers"),
+                        new HashMap<>() {
+                            {
+                                put(Clojure.var("clojure.core/resolve").invoke(Clojure.read("crux.codec.EDNId")),
+                                    writeHandler.invoke("crux/id", ednIdToOriginalId));
+                            }
+                        });
+                }};
+    }
+
+    public void close() {
+    }
+
+    public void configure(Map<String,?> configs, boolean isKey) {
+    }
+
+    public byte[] serialize(String topic, Object data) {
+        if (data == null) {
+            return null;
+        }
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            write.invoke(writer.invoke(out, jsonVerbose, options), data);
+            return out.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
