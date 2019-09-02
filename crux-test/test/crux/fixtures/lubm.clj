5ea984ecca2ea54fd9da44ce38d63d568006f894
Håkan Råberg <hakan.raberg@gmail.com>
2019-09-02 12:16:16 +0100

    Showing where the larger lubm file is. #328.

--- crux-test/test/crux/fixtures/lubm.clj
+++ crux-test/test/crux/fixtures/lubm.clj
@@ -3,11 +3,12 @@
             [crux.api :as api]
             [crux.rdf :as rdf]))
 
-(def ^:const lubm-triples-resource "lubm/University0_0.ntriples")
+(def ^:const lubm-triples-resource-8k "lubm/University0_0.ntriples")
+(def ^:const lubm-triples-resource-100k "lubm/lubm10.ntriples")
 
 (defn with-lubm-data [f]
   (let [tx-ops (->> (concat (rdf/->tx-ops (rdf/ntriples "lubm/univ-bench.ntriples"))
-                            (rdf/->tx-ops (rdf/ntriples lubm-triples-resource)))
+                            (rdf/->tx-ops (rdf/ntriples lubm-triples-resource-8k)))
                     (rdf/->default-language)
                     vec)]
     (doseq [tx-ops (partition-all 1000 tx-ops)]
