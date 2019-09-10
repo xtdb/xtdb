bbbda6653c03a0ac821f3f623abfbdf664103015
Håkan Råberg <hakan.raberg@gmail.com>
2019-09-10 14:26:15 +0100

    Only consider tags matching release patterns when deriving version.

--- crux-core/tasks/leiningen/project_version.clj
+++ crux-core/tasks/leiningen/project_version.clj
@@ -7,13 +7,13 @@
   (println (:version project)))
 
 (defn- version-from-git []
-  (let [{:keys [exit out]} (sh/sh "git" "describe" "--tags" "--dirty" "--long")]
+  (let [{:keys [exit out]} (sh/sh "git" "describe" "--tags" "--dirty" "--long" "--match" "*.*-*")]
     (assert (= 0 exit))
     ;;"19.04-1.0.2-alpha-22-g9452e91b-dirty"
     (let [[_ tag ahead sha dirty] (re-find #"(.*)\-(\d+)\-([0-9a-z]*)(\-dirty)?$" (clojure.string/trim out))
           ahead? (not= ahead "0")
           dirty? (not (empty? dirty))]
-      (assert (re-find #"^\d+\.\d+\-\d+\.\d+\.\d+(\-(alpha|beta))?$" tag) "Tag format unexpected.")
+      (assert (re-find #"^\d+\.\d+\-\d+\.\d+\.\d+(\-(alpha|beta))?$" tag) (str "Tag format unexpected: " tag))
       (if (and (not ahead?) (not dirty?))
         tag
         (let [[_ prefix minor-version suffix] (re-find #"^(.*\.)(\d)+(\-(alpha|beta))?$" tag)]
