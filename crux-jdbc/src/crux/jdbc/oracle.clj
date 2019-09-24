3d93717e59ed6af48412c2995202a7704a02b366
t-taylor <ttomgzombiez@gmail.com>
2019-09-24 16:32:03 +0100

    Started console docs

--- crux-jdbc/src/crux/jdbc/oracle.clj
+++ crux-jdbc/src/crux/jdbc/oracle.clj
@@ -5,20 +5,12 @@
             [taoensso.nippy :as nippy])
   (:import [oracle.sql TIMESTAMP BLOB]))
 
-#_(defmethod j/setup-schema! :oracle [_ ds] ;; old method
-    (jdbc/execute! ds ["create table tx_events (
-                       event_offset SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, event_key VARCHAR2(255),
-                       tx_time timestamp default CURRENT_TIMESTAMP, topic VARCHAR2(255) NOT NULL,
-                       v BLOB NOT NULL, compacted INTEGER NOT NULL)"])
-    (jdbc/execute! ds ["create index tx_events_event_key_idx on tx_events(compacted, event_key)"]))
-
-(defmethod j/setup-schema! :oracle [_ ds]  ;; new method
-  (when (= 0 (val (first (jdbc/execute-one! ds ["SELECT COUNT(*) FROM user_tables where table_name = 'TX_EVENTS'"]))))
-    (jdbc/execute! ds ["create table tx_events (
-                       event_offset SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, event_key VARCHAR2(255),
-                       tx_time timestamp default CURRENT_TIMESTAMP, topic VARCHAR2(255) NOT NULL,
-                       v BLOB NOT NULL, compacted INTEGER NOT NULL)"])
-    (jdbc/execute! ds ["create index tx_events_event_key_idx on tx_events(compacted, event_key)"])))
+(defmethod j/setup-schema! :oracle [_ ds]
+  (jdbc/execute! ds ["create table tx_events (
+  event_offset SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, event_key VARCHAR2(255),
+  tx_time timestamp default CURRENT_TIMESTAMP, topic VARCHAR2(255) NOT NULL,
+  v BLOB NOT NULL, compacted INTEGER NOT NULL)"])
+  (jdbc/execute! ds ["create index tx_events_event_key_idx on tx_events(compacted, event_key)"]))
 
 (defmethod j/->date :oracle [dbtype ^TIMESTAMP d]
   (assert d)
@@ -33,4 +25,3 @@
 
 (defmethod j/->pool-options :oracle [_ {:keys [username user] :as options}]
   (assoc options :username (or username user)))
-
