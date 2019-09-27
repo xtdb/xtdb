202edfe91e0623036913e3df0a3d05f16dccb81a
Ivan Fedorov <hello@spacegangster.io>
2019-09-27 17:22:49 +0300

    Console : update page-renderer dep

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies
     [[org.clojure/clojure         "1.10.0"]
      [org.clojure/clojurescript   "1.10.520"]
-     [page-renderer               "0.4.0"]
+     [page-renderer               "0.4.2"]
      [reagent                     "0.8.1"]
      [re-frame                    "0.10.8"]
      [garden                      "1.3.9"]
