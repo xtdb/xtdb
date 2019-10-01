bfda848693d69a930d1af8499027329f3fca6697
Ivan Fedorov <hello@spacegangster.io>
2019-10-02 00:23:45 +0300

    Console/server : fix resource serving from jar

--- crux-console/project.clj
+++ crux-console/project.clj
@@ -5,40 +5,39 @@
      [aleph                        "0.4.6"]
      [bidi                         "2.1.6"]
      [hiccup                       "1.0.5"]
-     [page-renderer                "0.4.2"]
-     ;
-     ; cljs deps
-     ; also bidi
-     [org.clojure/clojurescript    "1.10.520"]
-     [reagent                      "0.8.1"]
      [juxt/crux-uberjar            "19.09-1.4.0-alpha"]
-     [re-frame                     "0.10.8"]
-     [garden                       "1.3.9"]
-     [medley                       "1.2.0"]
-     [day8.re-frame/re-frame-10x   "0.3.3"]
-     [funcool/promesa              "2.0.1"]
-     [com.andrewmcveigh/cljs-time  "0.5.2"]
-     [binaryage/oops               "0.6.4"]
-     [day8.re-frame/test           "0.1.5"]
-     [thheller/shadow-cljs         "2.8.52"]
-     [com.google.javascript/closure-compiler-unshaded "v20190819"]
-     [org.clojure/google-closure-library "0.0-20190213-2033d5d9"]]
+     [page-renderer                "0.4.2"]]
 
   :min-lein-version "2.9.1"
   :main crux-ui-server.main
   :aot  [crux-ui-server.main]
+  :uberjar-name "crux-console.jar"
+  :source-paths ["src" "test" "node_modules"]
+  :resource-paths ["resources"]
+
   :profiles
-  {:dev {:main dev
-         :repl-options {:init-ns dev}}}
+  {:dev {:main dev :repl-options {:init-ns dev}}
+   :shadow-cljs
+   {:dependencies
+    [[org.clojure/clojurescript    "1.10.520"]
+     [reagent                      "0.8.1"]
+     [re-frame                     "0.10.8"]
+     [garden                       "1.3.9"]
+     [medley                       "1.2.0"]
+     [funcool/promesa              "2.0.1"]
+     [com.andrewmcveigh/cljs-time  "0.5.2"]
+     [binaryage/oops               "0.6.4"]
+     [day8.re-frame/test           "0.1.5"]
+     [day8.re-frame/re-frame-10x   "0.3.3"]
+     [thheller/shadow-cljs         "2.8.52"]
+     [com.google.javascript/closure-compiler-unshaded "v20190819"]
+     [org.clojure/google-closure-library "0.0-20190213-2033d5d9"]]}}
+
   :repositories [["clojars" "https://repo.clojars.org"]]
   :plugins [;[lein-shadow "0.1.5"] ; nasty guy, deletes original shadow-cljs config if you run it
             [lein-shell  "0.5.0"]] ; https://github.com/hypirion/lein-shell
 
-  :clean-targets ^{:protect false} ["target" "resources/static/crux-ui/compiled"]
-
-  :source-paths ["src" "../common/src" "test" "node_modules"]
-
-  :resource-paths ["resources"]
+  :clean-targets ^{:protect false} ["target" #_"resources/static/crux-ui/compiled"]
 
   :aliases
   {"yarn"
