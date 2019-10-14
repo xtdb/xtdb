894732630c68e98d1df5facffd9ab9ef513e7292
Jon Pither <jon.pither@gmail.com>
2019-10-14 16:05:24 +0100

    Node-test to cover Properties and two-nodes, which fails

--- crux-core/test/crux/node_test.clj
+++ crux-core/test/crux/node_test.clj
@@ -1,9 +1,12 @@
 (ns crux.node-test
   (:require [clojure.test :as t]
+            [crux.io :as cio]
             crux.jdbc
+            crux.kv.memdb
             crux.kv.rocksdb
-            [crux.node :as n]
-            [crux.io :as cio]))
+            [crux.node :as n])
+  (:import crux.moberg.MobergTxLog
+           java.util.Date))
 
 (t/deftest test-properties-to-topology
   (let [t (n/options->topology {:crux.node/topology :crux.jdbc/topology
@@ -21,3 +24,67 @@
         (t/is n))
       (finally
         (cio/delete-dir event-log-dir)))))
+
+(defn- load-props [f]
+  (let [props (java.util.Properties.)]
+    (.load props (clojure.java.io/reader f))
+    (into {}
+          (for [[k v] props]
+            [(keyword k) v]))))
+
+(t/deftest test-properties-file-to-node
+  (let [event-log-dir (cio/create-tmpdir "kv-store")]
+    (try
+      (with-open [n (n/start (assoc (load-props (clojure.java.io/resource "sample.properties"))
+                                    :crux.standalone/event-log-dir (str event-log-dir)))]
+        (t/is (instance? MobergTxLog (-> n :tx-log))))
+      (finally
+        (cio/delete-dir event-log-dir)))))
+
+(t/deftest test-start-up-2-nodes
+  (let [kv-data-dir-1 (cio/create-tmpdir "kv-store1")
+        kv-data-dir-2 (cio/create-tmpdir "kv-store2")
+        event-log-dir-1 (cio/create-tmpdir "event-log-1")
+        event-log-dir-2 (cio/create-tmpdir "event-log-2")]
+    (try
+      (with-open [n (n/start {:crux.node/topology :crux.standalone/topology
+                              :crux.kv/db-dir (str kv-data-dir-1)
+                              :crux.standalone/event-log-dir (str event-log-dir-1)})]
+        (t/is n)
+
+        (let [valid-time (Date.)
+              {:keys [crux.tx/tx-time
+                      crux.tx/tx-id]
+               :as submitted-tx} (.submitTx n [[:crux.tx/put {:crux.db/id :ivan :name "Ivan"} valid-time]])]
+          (t/is (= tx-time (.sync n (:crux.tx/tx-time submitted-tx) nil)))
+          (t/is (= #{[:ivan]} (.q (.db n)
+                                  '{:find [e]
+                                    :where [[e :name "Ivan"]]}))))
+
+        (t/is (= #{[:ivan]} (.q (.db n)
+                                '{:find [e]
+                                  :where [[e :name "Ivan"]]})))
+
+        (with-open [n2 (n/start {:crux.node/topology :crux.standalone/topology
+                                 :crux.kv/db-dir (str kv-data-dir-2)
+                                 :crux.standalone/event-log-dir (str event-log-dir-2)})]
+          (let [valid-time (Date.)
+                {:keys [crux.tx/tx-time
+                        crux.tx/tx-id]
+                 :as submitted-tx} (.submitTx n [[:crux.tx/put {:crux.db/id :ivan :name "Iva"} valid-time]])]
+            (t/is (= tx-time (.sync n (:crux.tx/tx-time submitted-tx) nil)))
+            (t/is (= #{[:ivan]} (.q (.db n)
+                                    '{:find [e]
+                                      :where [[e :name "Iva"]]}))))
+
+          (t/is n2)
+          (println (.status n2)))
+
+        (t/is (= #{[:ivan]} (.q (.db n)
+                                '{:find [e]
+                                  :where [[e :name "Ivan"]]}))))
+      (finally
+        (cio/delete-dir event-log-dir-1)
+        (cio/delete-dir event-log-dir-2)
+        (cio/delete-dir kv-data-dir-1)
+        (cio/delete-dir kv-data-dir-2)))))
