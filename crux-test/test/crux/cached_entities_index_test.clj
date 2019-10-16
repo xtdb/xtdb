5529d5d249bea72b8c29454b9f2b9f3866255bc5
Jon Pither <jon.pither@gmail.com>
2019-10-16 12:14:42 +0100

    Fixing cached-entities-index-test

--- crux-test/test/crux/cached_entities_index_test.clj
+++ crux-test/test/crux/cached_entities_index_test.clj
@@ -8,10 +8,12 @@
             [crux.codec :as c]
             [crux.db :as db]
             [crux.fixtures :as f]
+            [crux.fixtures.kv :as kvf]
             [crux.fixtures.api :refer [*api*]]
-            [crux.fixtures.standalone :as fs]))
+            [crux.fixtures.standalone :as fs]
+            [crux.fixtures.api :as apif]))
 
-(t/use-fixtures :each fs/with-standalone-node)
+(t/use-fixtures :each fs/with-standalone-node kvf/with-kv-dir apif/with-node)
 
 (t/deftest test-cached-index
   (t/testing "cached index sanity"
