20182387036d71c5f7d2d72bd5abf304fe487425
Daniel Mason <dan@juxt.pro>
2019-10-23 16:16:21 +0100

    Extract name and topology map from topology clojure files #360

--- crux-generate-api/src/crux/parse_topology_file.clj
+++ crux-generate-api/src/crux/parse_topology_file.clj
@@ -2,8 +2,20 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(defn parse-namespace [ns-string]
-  (last (string/split ns-string #" ")))
+(defn get-namespace [file-contents]
+  (second (first file-contents)))
+
+(defn filter-first [pred val]
+  (first (filter pred val)))
+
+(defn get-topology-map [file-contents]
+  (->> file-contents
+       (filter-first (fn [xs] (some #(= 'topology %) xs)))
+       (filter-first seq?)
+       (filter-first map?)))
 
 (defn parse-topology-file [filename]
-  (prn (slurp filename)))
+  (let [file-contents (read-string (str "[" (slurp filename) "]"))
+        node-ns (get-namespace file-contents)
+        topology-map (get-topology-map file-contents)]
+    topology-map))
