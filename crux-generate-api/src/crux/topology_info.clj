ad5c8bda7b879dc80e2dda4d6a3f27fd5a506f60
Daniel Mason <dan@juxt.pro>
2019-10-24 11:01:51 +0100

    Generate java code strings from extracted topology keys #360

--- crux-generate-api/src/crux/topology_info.clj
+++ crux-generate-api/src/crux/topology_info.clj
@@ -6,16 +6,21 @@
             [crux.node :as node]
             [crux.kafka :as kafka]))
 
-(defn find-nested-args
-  [topology-map]
+(defn find-nested-args [topology-map]
   (->> (tree-seq map? vals topology-map)
        (filter map?)
        (keep :args)
        (into {})))
 
+(defn format-topology-key [topology-key]
+  (-> (name topology-key)
+      (string/replace "-" "_")
+      (string/upper-case)))
+
+(defn generate-key-strings [topology-key]
+  (str "public string " (format-topology-key topology-key) " = \"" topology-key "\""))
+
 (defn get-topology-info [topology-name]
   (let [topology-map (eval topology-name)
         topology-opts (find-nested-args topology-map)]
     topology-opts))
-
-;(clojure.pprint/pprint (get-topology-info 'crux.kafka/topology))
