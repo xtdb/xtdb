6f24df4e3a7722ca9f1b80f2efd9b8abe73945bb
Daniel Mason <dan@juxt.pro>
2019-10-24 16:30:59 +0100

    Add JavaPoet code to generate classes from topologies #360

--- crux-generate-api/src/crux/gen_topology_classes.clj
+++ crux-generate-api/src/crux/gen_topology_classes.clj
@@ -3,19 +3,46 @@
             [clojure.string :as string]
             [crux.topology-info :as ti]))
 
+(import (com.squareup.javapoet MethodSpec TypeSpec TypeSpec$Builder
+                               FieldSpec FieldSpec$Builder JavaFile)
+        javax.lang.model.element.Modifier)
+
 (defn format-topology-key [key]
   (-> (name key)
       (string/replace "-" "_")
       (string/upper-case)))
 
-(defn generate-key-strings [[key value]]
-  (let [keystring (str "public string " (format-topology-key key) " = \"" key "\"")]
+
+(defn build-key-field[[key value]]
+  (let [class (FieldSpec/builder String (format-topology-key key) (into-array ^Modifier [Modifier/PUBLIC]))]
+    (.initializer class "$S" (into-array [(str key)]))
+    (.build class)))
+
+(defn build-key-default-field[[key value]]
+  (let [class (FieldSpec/builder String (str (format-topology-key key) "_DEFAULT")
+                                 (into-array ^Modifier [Modifier/PUBLIC]))]
+    (.initializer class "$S" (into-array [(str (:default value))]))
+    (.build class)))
+
+(defn add-topology-key-code [class [key value]]
+  (doto class
+    (.addField (build-key-field [key value]))
     (if (:default value)
-      [keystring (str "public string " (format-topology-key key) "_DEFAULT = \"" (:default value) "\"")]
-      [keystring])))
+      (.addField (build-key-default-field [key value])))))
 
-(defn gen-topology-class [class-name topology]
+(defn build-java-class [class-name topology-info]
+  (let [class (TypeSpec/classBuilder class-name)]
+    (.addModifiers class (into-array ^Modifier [Modifier/PUBLIC]))
+    (doall (map #(add-topology-key-code class %) (seq topology-info)))
+    (.build class)))
+
+(defn build-java-file [class-name topology-info]
+  (let [javafile (build-java-class class-name topology-info)
+        output (io/file class-name)]
+    (.writeTo (JavaFile/builder "crux.api" javafile) output)))
+
+(defn gen-topology-file [class-name topology]
   (let [topology-info (ti/get-topology-info topology)]
-    (map generate-key-strings (seq topology-info))))
+    (build-java-file class-name topology-info)))
 
-(gen-topology-class "KafkaNode" 'crux.kafka/topology)
+;(gen-topology-class "KafkaNode" 'crux.kafka/topology)
