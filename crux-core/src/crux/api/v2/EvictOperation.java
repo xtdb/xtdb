3d9d2e3f0a2fa4b687eff0acdb7759a872cd50a4
James Henderson <james@jarohen.me.uk>
2019-11-20 14:02:00 +0000

    temporarily dropping all the time-based params out of EvictOperation

--- crux-core/src/crux/api/v2/EvictOperation.java
+++ crux-core/src/crux/api/v2/EvictOperation.java
@@ -3,60 +3,23 @@ package crux.api.v2;
 import clojure.lang.Keyword;
 import clojure.lang.PersistentVector;
 
-import java.util.Date;
-
 import static crux.api.v2.Util.keyword;
-
-// TODO: Only allow withEndValidTime, keepLatest and keepEarliest if previous is set
 
 public class EvictOperation extends Operation {
     private static final Keyword TX_EVICT = keyword("crux.tx/evict");
 
-    private final Date validTime;
-    private final Date endValidTime;
-    private final Boolean keepLatest;
-    private final Boolean keepEarliest;
     private final CruxId evictId;
 
-    private EvictOperation(CruxId evictId, Date validTime, Date endValidTime, Boolean keepLatest, Boolean keepEarliest) {
+    private EvictOperation(CruxId evictId) {
         this.evictId = evictId;
-        this.validTime = validTime;
-        this.endValidTime = endValidTime;
-        this.keepLatest = keepLatest;
-        this.keepEarliest = keepEarliest;
     }
 
     public static EvictOperation evictOp(CruxId evictId) {
-        return new EvictOperation(evictId, null, null, null, null);
-    }
-
-    public EvictOperation withValidTime(Date validTime) {
-        return new EvictOperation(evictId, validTime, endValidTime, keepLatest, keepEarliest);
-    }
-
-    public EvictOperation withEndValidTime(Date endValidTime) {
-        return new EvictOperation(evictId, validTime, endValidTime, keepLatest, keepEarliest);
-    }
-
-    public EvictOperation keepLatest(Boolean keepLatest) {
-        return new EvictOperation(evictId, validTime, endValidTime, keepLatest, keepEarliest);
-    }
-
-    public EvictOperation keepEarliest(Boolean keepEarliest) {
-        return new EvictOperation(evictId, validTime, endValidTime, keepLatest, keepEarliest);
+        return new EvictOperation(evictId);
     }
 
     @Override
     protected PersistentVector toEdn() {
-        PersistentVector outputVector = PersistentVector.create(TX_EVICT, evictId.toEdn());
-        if(validTime != null)
-            outputVector = outputVector.cons(validTime);
-        if(endValidTime != null)
-            outputVector = outputVector.cons(endValidTime);
-        if(keepLatest != null)
-            outputVector = outputVector.cons(keepLatest);
-        if(validTime != null)
-            outputVector = outputVector.cons(validTime);
-        return outputVector;
+        return PersistentVector.create(TX_EVICT, evictId.toEdn());
     }
 }
