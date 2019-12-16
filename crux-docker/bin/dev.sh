8e2f08411360300b5ab722e2d51976b5bd0544f0
Daniel Mason <dan@juxt.pro>
2019-12-16 12:27:30 +0000

    Add crux.edn and deps.edn to dev.sh

--- crux-docker/bin/dev.sh
+++ crux-docker/bin/dev.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run --rm -ti -p 3000:3000 -v $HOME/.m2:/root/.m2 juxt/crux-http
+docker run --rm -ti -p 3000:3000 \
+       -v $HOME/.m2:/root/.m2 \
+       -v $PWD/crux.edn:/etc/crux.edn \
+       -v $PWD/deps.edn:/usr/lib/crux/deps.edn \
+       juxt/crux-http
