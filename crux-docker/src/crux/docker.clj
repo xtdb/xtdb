d5f0f8a9fe988dbe2a9b99e439e78d3c25cffa3a
Daniel Mason <dan@juxt.pro>
2019-12-17 14:26:00 +0000

    Add integrant repl to docker.clj

--- crux-docker/src/crux/docker.clj
+++ crux-docker/src/crux/docker.clj
@@ -1,12 +1,34 @@
 (ns crux.docker
   (:require [crux.api :as crux]
             [crux.http-server :as srv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [integrant.core :as ig]
+            [integrant.repl :as ir]
+            [nrepl.server :as nrepl])
+  (:import java.io.Closeable))
+
+(defmethod ig/init-key :crux/node [_ config]
+  (crux/start-node config))
+
+(defmethod ig/halt-key! :crux/node [_ ^Closeable node]
+  (.close node))
+
+
+(defmethod ig/init-key :crux/http-server [_ {:keys [crux/crux-node crux/server-config]}]
+  (let [cors-opts (:cors-access-control server-config)
+        server-opts (cond-> server-config
+                      (map? cors-opts) (assoc :cors-access-control (reduce into [] cors-opts)))]
+    (srv/start-http-server crux-node server-opts)))
+
+(defmethod ig/halt-key! :crux/http-server [_ ^Closeable server]
+  (.close server))
+
+(def ig-config
+  (let [{:keys [crux/node-opts crux/server-opts]} (read-string (slurp (io/file "/etc/crux.edn")))]
+    {:crux/node node-opts
+     :crux/http-server {:crux/node (ig/ref :crux/node)
+                        :crux/server-config server-opts}}))
 
 (defn -main []
-  (let [{:keys [crux/node-opts crux/server-opts]} (read-string (slurp (io/file "/etc/crux.edn")))
-        node (crux/start-node node-opts)
-        cors-opts (:cors-access-control server-opts)
-        srv-opts (cond-> server-opts
-                   (map? cors-opts) (assoc :cors-access-control (reduce into [] cors-opts)))]
-    (srv/start-http-server node srv-opts)))
+  (ir/set-prep! (constantly ig-config))
+  (ir/go))
