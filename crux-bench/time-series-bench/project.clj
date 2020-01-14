c19c71305e5b3008bcb0e14df78354c9f64279d9
Tom Taylor <tmt@juxt.pro>
2020-01-14 15:07:17 +0000

    Add git dependency

--- crux-bench/time-series-bench/project.clj
+++ crux-bench/time-series-bench/project.clj
@@ -1,11 +1,11 @@
 (defproject juxt/crux-bench "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.7"]
-                 [juxt/crux-core "20.01-1.6.2-alpha"]
-                 [juxt/crux-kafka "20.01-1.6.2-alpha"]
-                 [juxt/crux-kafka-embedded "20.01-1.6.2-alpha"]
-                 [juxt/crux-rocksdb "20.01-1.6.2-alpha"]
+                 [juxt/crux-core "20.01-1.6.3-alpha-SNAPSHOT"]
+                 [juxt/crux-kafka "20.01-1.6.3-alpha-SNAPSHOT"]
+                 [juxt/crux-kafka-embedded "20.01-1.6.3-alpha-SNAPSHOT"]
+                 [juxt/crux-rocksdb "20.01-1.6.3-alpha-SNAPSHOT"]
                  [ch.qos.logback/logback-classic "1.2.3"]]
-  :jvm-opts ["-Xms3g" "-Xmx3g"]
+  :jvm-opts ["-Xms3g" "-Xmx3g" "-Dlogback.configurationFile=logback.xml"]
   :resource-paths ["resources" "data"]
   :main ^:skip-aot crux.bench.main)
