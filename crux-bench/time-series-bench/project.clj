46928ba8c649328f55f585896f5ab2040232f49f
Tom Taylor <tmt@juxt.pro>
2020-01-14 15:07:17 +0000

    Add git dependency

--- crux-bench/time-series-bench/project.clj
+++ crux-bench/time-series-bench/project.clj
@@ -8,6 +8,4 @@
                  [ch.qos.logback/logback-classic "1.2.3"]]
   :jvm-opts ["-Xms3g" "-Xmx3g" "-Dlogback.configurationFile=logback.xml"]
   :resource-paths ["resources" "data"]
-  :jvm-opts ["-Xms3g" "-Xmx3g"]
-  :main ^:skip-aot crux.bench.main
-  :resource-paths ["resources" "data"])
+  :main ^:skip-aot crux.bench.main)
