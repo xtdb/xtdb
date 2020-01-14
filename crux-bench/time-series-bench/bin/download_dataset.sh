f3dcf6407342e0bb18efeb903216c81b707109a6
Tom Taylor <tmt@juxt.pro>
2020-01-14 09:10:06 +0000

    code golf downloading dataset

--- crux-bench/time-series-bench/bin/download_dataset.sh
+++ crux-bench/time-series-bench/bin/download_dataset.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-wget https://timescaledata.blob.core.windows.net/datasets/devices_small.tar.gz
-wget https://timescaledata.blob.core.windows.net/datasets/weather_small.tar.gz
 mkdir resources
-tar -xf devices_small.tar.gz -C resources
-tar -xf weather_small.tar.gz -C resources
-rm devices_small.tar.gz
-rm weather_small.tar.gz
+curl -L https://timescaledata.blob.core.windows.net/datasets/devices_small.tar.gz | tar xz -C resources
+curl -L https://timescaledata.blob.core.windows.net/datasets/weather_small.tar.gz | tar xz -C resources
