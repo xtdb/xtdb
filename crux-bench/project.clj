a7b55102d70753beee233e2fe5bd14f9432a5eb3
James Henderson <james@jarohen.me.uk>
2020-01-14 18:01:46 +0000

    using 'derived-from-git' in bench project.clj

--- crux-bench/project.clj
+++ crux-bench/project.clj
@@ -1,11 +1,14 @@
-(defproject juxt/crux-bench "0.1.0"
+(defproject juxt/crux-bench "derived-from-git"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.7"]
-                 [juxt/crux-core "20.01-1.6.3-alpha-SNAPSHOT"]
-                 [juxt/crux-kafka "20.01-1.6.3-alpha-SNAPSHOT"]
-                 [juxt/crux-kafka-embedded "20.01-1.6.3-alpha-SNAPSHOT"]
-                 [juxt/crux-rocksdb "20.01-1.6.3-alpha-SNAPSHOT"]
+                 [juxt/crux-core "derived-from-git"]
+                 [juxt/crux-kafka "derived-from-git"]
+                 [juxt/crux-kafka-embedded "derived-from-git"]
+                 [juxt/crux-rocksdb "derived-from-git"]
                  [ch.qos.logback/logback-classic "1.2.3"]]
-  :jvm-opts ["-Xms3g" "-Xmx3g" "-Dlogback.configurationFile=logback.xml"]
+
+  :middleware [leiningen.project-version/middleware]
+
   :resource-paths ["resources" "data"]
+  :jvm-opts ["-Xms3g" "-Xmx3g" "-Dlogback.configurationFile=logback.xml"]
   :main ^:skip-aot crux.bench.main)
