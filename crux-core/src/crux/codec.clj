f421310daccf602850172290d191814eb9f157b0
Jon Pither <jon.pither@gmail.com>
2020-01-17 09:41:13 +0000

    Bumping index version

--- crux-core/src/crux/codec.clj
+++ crux-core/src/crux/codec.clj
@@ -19,7 +19,7 @@
 ;; Indexes
 
 ;; NOTE: Must be updated when existing indexes change structure.
-(def ^:const index-version 5)
+(def ^:const index-version 6)
 (def ^:const index-version-size Long/BYTES)
 
 (def ^:const index-id-size Byte/BYTES)
