d135ca263df26b238a5cf2752f6636b1e9ac5cf5
Tom Taylor <tmt@juxt.pro>
2020-01-17 15:49:09 +0000

    Containing dropwizard

--- crux-metrics/project.clj
+++ crux-metrics/project.clj
@@ -4,6 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [juxt/crux-core "derived-from-git"]]
+                 [juxt/crux-core "derived-from-git"]
+                 [metrics-clojure "2.10.0"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}}
   :middleware [leiningen.project-version/middleware])
--- crux-metrics/src/crux/metrics/dropwizard.clj
+++ crux-metrics/src/crux/metrics/dropwizard.clj
@@ -4,15 +4,7 @@
             [crux.metrics :as c-mets]))
 
 (defn codahale-register-node [reg node]
-  (gauges/gauge-fn))
+  (map (fn [[namesp data]]
+         (gauges/gauge-fn reg (name namesp) (:function data)))
+       c-mets/metrics-map))
 
-(defn codahale-register-node [reg node]
-  (let [metrics (filter (fn [[k v]] (= "metric" (namespace k))) (api/status node))]
-    (apply hash-map (flatten (map (fn [[k v]] [(name k) (gauges/gauge-fn reg (name k) (fn [] (get (api/status node) k)))]) metrics)))))
-
-(def regs (codahale-register-node reg node))
-
-(defn get-metrics []
-  (apply hash-map (flatten (map (fn [[k v]] [k (gauges/value v)]) regs))))
-
-(gauges/value (gauges/gauge reg "doc-count"))
