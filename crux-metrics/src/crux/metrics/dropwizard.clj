123af46050b75848a0fff813dc612ebf54e1bb5f
Tom Taylor <tmt@juxt.pro>
2020-01-22 12:20:10 +0000

    Add console reporter to dropwizard

--- crux-metrics/src/crux/metrics/dropwizard.clj
+++ crux-metrics/src/crux/metrics/dropwizard.clj
@@ -3,6 +3,7 @@
             [metrics.gauges :as gauges]
             [metrics.core :as drpwz-m]
             [metrics.reporters.jmx :as jmx]
+            [metrics.reporters.console :as console]
             crux.metrics.gauges))
 
 (defn register-metrics [reg !metrics]
@@ -22,10 +23,19 @@
                :deps #{:crux.node/bus :crux.node/indexer}}})
 
 (def jmx-reporter
-  {::jmx {:start-fn (fn [{::keys [registry]} {::keys [jmx-reporter-opts]}]
-                      (let [jmx-rep (jmx/reporter registry
-                                                  (merge {:domain "crux.metrics"}
-                                                         jmx-reporter-opts))]
-                        (jmx/start jmx-rep)
-                        jmx-rep))
-          :deps #{::registry}}})
+  {::jmx-reporter {:start-fn (fn [{::keys [registry]} {::keys [jmx-reporter-opts]}]
+                               (let [jmx-rep (jmx/reporter registry
+                                                           (merge {:domain "crux.metrics"}
+                                                                  jmx-reporter-opts))]
+                                 (jmx/start jmx-rep)
+                                 jmx-rep))
+                   :deps #{::registry}}})
+
+(def console-reporter
+  {::console-reporter {:start-fn (fn [{::keys [registry]} {::keys [console-reporter-opts console-reporter-rate]}]
+                                   (let [console-rep (console/reporter registry
+                                                                       (merge {}
+                                                                              console-reporter))]
+                                     (console/start console-rep (or console-reporter-rate 1))
+                                     console-rep))
+                       :deps #{::registry}}})
