1b3da30f63f0ef7e84aa389c62c4eb4995c7b143
James Henderson <james@jarohen.me.uk>
2020-01-22 19:11:59 +0000

    simplify uberjar test and ensure it only uses tmp dirs

--- crux-uberjar/test/uberjar_test.clj
+++ crux-uberjar/test/uberjar_test.clj
@@ -2,50 +2,41 @@
   (:require [clojure.test :as t]
             [clojure.string :as str]
             [clojure.java.io :as io]
-            [clojure.java.shell :as sh]))
+            [clojure.java.shell :as sh]
+            [crux.fixtures :as f]))
 
 ;; This is a hacky way to allow the uberjar tests to be ran from a directory other than crux-uberjar - typically the repo root.
-(def in-uberjar-directory?
-  (let [active-directory (.getCanonicalPath (clojure.java.io/file "."))]
-    (str/ends-with? active-directory "/crux-uberjar")))
-
-(def uberjar-directory
-  (if in-uberjar-directory?
+(def working-directory
+  (if (str/ends-with? (.getCanonicalPath (clojure.java.io/file ".")) "/crux-uberjar")
     "."
     "./crux-uberjar"))
 
-(def results-file
-  (str uberjar-directory "/target/uberjar-test-results"))
-
-(defn- sh-with-err-out [& args]
-  (let [{:keys [exit err]} (apply sh/sh
-                                  (concat args [:dir uberjar-directory]))]
-    (when-not (zero? exit)
-      (binding [*out* *err*]
-        (println err)))))
-
 (t/deftest test-uberjar-can-start
-  (when (.exists (io/file results-file))
-    (io/delete-file results-file))
+  (f/with-tmp-dir "uberjar" [uberjar-dir]
+    (let [uberjar (let [{:keys [exit out] :as res} (sh/sh "lein" "uberjar"
+                                                          :dir working-directory)]
+                    (if (zero? exit)
+                      out
+                      (throw (ex-info "lein uberjar exited with non-zero exit code" res))))
 
-  (let [_ (sh-with-err-out "chmod" "+x" "test/test-uberjar.sh")
-        _ (sh-with-err-out "chmod" "+x" "test/test-start-uberjar.sh")
-        _ (sh-with-err-out "test/test-uberjar.sh")
-        results (slurp results-file)]
-    (t/testing "Results exist"
-      (t/is (string? results)))
+          results (:out (sh "timeout" "10s"
+                            "java" "-jar" uberjar
+                            "-x" (pr-str {:crux.node/topology 'crux.standalone/topology
+                                          :crux.standalone/event-log-dir (str (io/file uberjar-dir "event-log"))
+                                          :crux.kv/db-dir (str (io/file uberjar-dir "db-dir"))})
+                            :dir working-directory))]
 
-    (t/testing "Crux version"
-      (t/is (str/includes? results "Crux version:")))
+      (t/testing "Results exist"
+        (t/is (string? results)))
 
-    (t/testing "Options loaded"
-      (t/is (str/includes? results "options:")))
+      (t/testing "Crux version"
+        (t/is (str/includes? results "Crux version:")))
 
-    (t/testing "Options presented"
-      (t/is (str/includes? results ":server-port")))
+      (t/testing "Options loaded"
+        (t/is (str/includes? results "options:")))
 
-    ;; Not sure how this will stand the test of time - joa
-    (t/testing "Kafka attempted"
-      (t/is (str/includes? results "kafka-producer")))
+      (t/testing "Options presented"
+        (t/is (str/includes? results ":server-port")))
 
-    (io/delete-file results-file)))
+      (t/testing "Server started"
+        (t/is (str/includes? results "org.eclipse.jetty.server.Server - Started"))))))
