291e209b38ee380e1de9573df224e1fb94bdb429
Daniel Mason <dan@juxt.pro>
2020-01-23 14:00:24 +0000

    Use local source and sub install at build time

--- crux-bench/bin/docker-build-push.sh
+++ crux-bench/bin/docker-build-push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 ECR=955308952094.dkr.ecr.eu-west-2.amazonaws.com/crux-bench
-docker build -t $ECR:latest .
+cd ../
+docker build -t $ECR:latest -f crux-bench/ .
 docker push $ECR:latest
