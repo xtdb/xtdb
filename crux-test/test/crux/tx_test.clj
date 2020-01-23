a397aab0434bdfecddfc4e54e7a49c132f3b08e2
Jon Pither <jon.pither@gmail.com>
2020-01-23 13:55:05 +0000

    Introduce docs-indexed?

--- crux-test/test/crux/tx_test.clj
+++ crux-test/test/crux/tx_test.clj
@@ -299,6 +299,8 @@
 
     (Thread/sleep 1000)
 
+    (t/is (db/docs-exist? (:indexer *api*) [content-hash]))
+
     (with-open [snapshot (kv/new-snapshot (:kv-store *api*))]
       (t/is (= {content-hash picasso}
                (db/get-objects (:object-store *api*) snapshot [content-hash])))
