524b9a81036c0908dc204a2107ff24994b410c88
James Henderson <james@jarohen.me.uk>
2020-01-24 16:41:09 +0000

    using meters for the indexed docs/indexed txs

--- crux-metrics/src/crux/metrics/ingest.clj
+++ crux-metrics/src/crux/metrics/ingest.clj
@@ -1,59 +1,40 @@
 (ns crux.metrics.ingest
   (:require [crux.bus :as bus]
             [crux.db :as db]
-            [metrics.timers :as timers]
             [metrics.gauges :as gauges]
+            [metrics.meters :as meters]
             [crux.tx :as tx]))
+
+(defn assign-tx-id-lag [registry {:crux.node/keys [bus indexer tx-log]}]
+  (gauges/gauge-fn registry
+                   ["crux" "ingest" "tx-id-lag"]
+                   #(when-let [latest-tx-id (db/read-index-meta indexer :crux.tx/latest-completed-tx)]
+                      (- (::tx/tx-id (db/latest-submitted-tx tx-log))
+                         (::tx/tx-id latest-tx-id)))))
+
+(defn assign-doc-meter [registry {:crux.node/keys [bus indexer tx-log]}]
+  (let [meter (meters/meter registry ["crux" "ingest" "indexed-docs"])]
+    (bus/listen bus
+                {:crux.bus/event-types #{:crux.tx/indexed-docs}}
+                (fn [{:keys [doc-ids]}]
+                  (meters/mark! meter (count doc-ids))))
+
+    meter))
+
+(defn assign-tx-meter [registry {:crux.node/keys [bus indexer tx-log]}]
+  (let [meter (meters/meter registry ["crux" "ingest" "indexed-txs"])]
+    (bus/listen bus
+                {:crux.bus/event-types #{:crux.tx/indexed-tx}}
+                (fn [_]
+                  (meters/mark! meter)))
+
+    meter))
 
 (defn assign-ingest
   "Assigns listeners to an event bus for a given node.
   Returns an atom containing uptading metrics"
-  [registry {:crux.node/keys [bus indexer tx-log]}]
+  [registry {:crux.node/keys [bus indexer tx-log] :as deps}]
 
-  ;; Create registry and add timer metrics + lag gauge
-  ;; NOTE: might be a way to see currently running timers, so indexing* gauges
-  ;; can be discarded
-  (let [docs-ingest-timer (timers/timer registry ["metrics" "ingest" "docs_timer"])
-        tx-ingest-timer (timers/timer registry ["metrics" "ingest" "tx_timer"])
-        !timer-contexts (atom {:tx {}
-                               :docs {}})
-        ingesting-docs (gauges/gauge-fn registry
-                                        ["metrics" "ingest" "docs_ingesting"]
-                                        #(count (:docs @!timer-contexts)))
-        ingesting-tx (gauges/gauge-fn registry
-                                      ["metrics" "ingest" "tx_ingesting"]
-                                      #(count (:tx @!timer-contexts)))
-        tx-id-lag (gauges/gauge-fn registry
-                                   ["metrics" "ingest" "tx_id_lag"]
-                                   #(when-let [latest-tx-id (db/read-index-meta indexer :crux.tx/latest-completed-tx)]
-                                      (- (::tx/tx-id (db/latest-submitted-tx tx-log))
-                                         (::tx/tx-id latest-tx-id))))]
-    (bus/listen bus
-                {:crux.bus/event-types #{:crux.tx/indexing-docs}}
-                (fn [{:keys [doc-ids]}]
-                  (swap! !timer-contexts assoc-in
-                         [:docs doc-ids]
-                         (timers/start docs-ingest-timer))))
-    (bus/listen bus
-                {:crux.bus/event-types #{:crux.tx/indexed-docs}}
-                (fn [{:keys [doc-ids]}]
-                  (timers/stop (get-in @!timer-contexts [:docs doc-ids]))
-                  (swap! !timer-contexts update :docs dissoc doc-ids)))
-
-    (bus/listen bus
-                {:crux.bus/event-types #{:crux.tx/indexing-tx}}
-                (fn [{:keys [crux.tx/submitted-tx]}]
-                  (swap! !timer-contexts assoc-in
-                         [:tx submitted-tx]
-                         (timers/start tx-ingest-timer))))
-
-    (bus/listen bus
-                {:crux.bus/event-types #{:crux.tx/indexed-tx}}
-                (fn [{:keys [crux.tx/submitted-tx]}]
-                  (timers/stop (get-in @!timer-contexts [:tx submitted-tx]))
-                  (swap! !timer-contexts update :tx dissoc submitted-tx)))
-    {:ingesting-docs ingesting-docs
-     :ingesting-tx ingesting-tx
-     :tx-id-lag tx-id-lag
-     :docs-ingest-timer docs-ingest-timer
-     :tx-ingest-timer tx-ingest-timer}))
+  {:tx-id-lag (assign-tx-id-lag registry deps)
+   :docs-ingest-meter (assign-doc-meter registry deps)
+   :tx-ingest-meter (assign-tx-meter registry deps)})
--- crux-test/test/crux/metrics_test.clj
+++ crux-test/test/crux/metrics_test.clj
@@ -6,33 +6,27 @@
             [crux.metrics.ingest :as ingest-metrics]
             [crux.api :as api]
             [metrics.core :as metrics]
-            [metrics.timers :as timers]
-            [metrics.gauges :as gauges]))
+            [metrics.meters :as meters]
+            [metrics.gauges :as gauges])
+  (:import (java.io Closeable)))
 
-;; Hack to fix v while we wait for https://github.com/metrics-clojure/metrics-clojure/issues/141 to happen
-;; https://github.com/metrics-clojure/metrics-clojure/commit/9650d765c991f648f67b9d7e195edd50330e6f60
-(def ^:private number-recorded timers/number-recorded)
+(t/use-fixtures :each kvf/with-kv-dir fs/with-standalone-node fapi/with-node)
 
-(t/use-fixtures :each kvf/with-kv-dir fs/with-standalone-node apif/with-node)
-
-(t/deftest ingest
+(t/deftest test-ingest-metrics
   (let [registry (metrics/new-registry)
-        ;; Assign metrics
         mets (ingest-metrics/assign-ingest registry #:crux.node{:bus (:bus *api*)
                                                                 :indexer (:indexer *api*)
                                                                 :tx-log (:tx-log *api*)})]
     (t/testing "initial ingest values"
-      (t/is (= 0 (gauges/value (:ingesting-docs mets))))
-      (t/is (= 0 (gauges/value (:ingesting-tx mets))))
       (t/is (nil? (gauges/value (:tx-id-lag mets))))
-      (t/is (= 0 (number-recorded (:docs-ingest-timer mets))))
-      (t/is (= 0 (number-recorded (:tx-ingest-timer mets)))))
+      (t/is (= 0 (meters/count (:docs-ingest-meter mets))))
+      (t/is (= 0 (meters/count (:tx-ingest-meter mets)))))
 
     (fapi/submit+await-tx [[:crux.tx/put {:crux.db/id :test}]])
 
+    (.close ^Closeable (:bus *api*))
+
     (t/testing "post ingest values"
-      (t/is (= 1 (+ (gauges/value (:ingesting-docs mets))
-                    (number-recorded (:docs-ingest-timer mets)))))
-      (t/is (= 1 (+ (gauges/value (:ingesting-tx mets))
-                    (number-recorded (:tx-ingest-timer mets)))))
+      (t/is (= 1 (meters/count (:docs-ingest-meter mets))))
+      (t/is (= 1 (meters/count (:tx-ingest-meter mets))))
       (t/is (zero? (gauges/value (:tx-id-lag mets)))))))
