1bc9fbc13f374ec3a2a963ade5ec49c6f3a101b4
Daniel Mason <dan@juxt.pro>
2020-01-27 10:21:47 +0000

    Address PR feedback

--- crux-bench/bin/download_dataset.sh
+++ crux-bench/bin/download_dataset.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cd crux-bench/
+cd $(dirname "$0")/..
 mkdir data
 curl -L https://timescaledata.blob.core.windows.net/datasets/devices_small.tar.gz | tar xz -C data
 curl -L https://timescaledata.blob.core.windows.net/datasets/weather_small.tar.gz | tar xz -C data
+cd -
