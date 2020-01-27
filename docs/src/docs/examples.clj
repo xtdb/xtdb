6654aec04d2217f46c98231bb41e67ec8cd049c6
James Henderson <james@jarohen.me.uk>
2020-01-27 16:06:35 +0000

    let's make the examples use vector topologies now, get users used to them

--- docs/src/docs/examples.clj
+++ docs/src/docs/examples.clj
@@ -12,7 +12,7 @@
 (defn example-start-standalone []
 ;; tag::start-standalone-node[]
 (def ^crux.api.ICruxAPI node
-  (crux/start-node {:crux.node/topology 'crux.standalone/topology
+  (crux/start-node {:crux.node/topology '[crux.standalone/topology]
                     :crux.node/kv-store 'crux.kv.memdb/kv
                     :crux.kv/db-dir "data/db-dir-1"
                     :crux.standalone/event-log-dir "data/eventlog-1"
@@ -58,7 +58,7 @@ embedded-kafka)
 (defn example-start-cluster []
 ;; tag::start-cluster-node[]
 (def ^crux.api.ICruxAPI node
-  (crux/start-node {:crux.node/topology 'crux.kafka/topology
+  (crux/start-node {:crux.node/topology '[crux.kafka/topology]
                     :crux.node/kv-store 'crux.kv.memdb/kv
                     :crux.kafka/bootstrap-servers "localhost:9092"}))
 ;; end::start-cluster-node[]
@@ -67,7 +67,7 @@ node)
 (defn example-start-rocks []
 ;; tag::start-standalone-with-rocks[]
 (def ^crux.api.ICruxAPI node
-  (crux/start-node {:crux.node/topology 'crux.standalone/topology
+  (crux/start-node {:crux.node/topology '[crux.standalone/topology]
                     :crux.node/kv-store 'crux.kv.rocksdb/kv
                     :crux.kv/db-dir "data/db-dir-1"
                     :crux.standalone/event-log-dir "data/eventlog-1"}))
@@ -77,7 +77,7 @@ node)
 (defn example-start-lmdb []
 ;; tag::start-standalone-with-lmdb[]
 (def ^crux.api.ICruxAPI node
-  (crux/start-node {:crux.node/topology 'crux.standalone/topology
+  (crux/start-node {:crux.node/topology '[crux.standalone/topology]
                     :crux.node/kv-store 'crux.kv.lmdb/kv
                     :crux.kv/db-dir "data/db-dir-1"
                     :crux.standalone/event-log-dir "data/eventlog-1"
@@ -88,7 +88,7 @@ node)
 (defn example-start-jdbc []
 ;; tag::start-jdbc-node[]
 (def ^crux.api.ICruxAPI node
-  (crux/start-node {:crux.node/topology 'crux.jdbc/topology
+  (crux/start-node {:crux.node/topology '[crux.jdbc/topology]
                     :crux.jdbc/dbtype "postgresql"
                     :crux.jdbc/dbname "cruxdb"
                     :crux.jdbc/host "<host>"
