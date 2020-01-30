3d993e74e5e36d0fea873a39cd01931672cb0e99
Daniel Mason <dan@juxt.pro>
2020-01-30 11:49:04 +0000

    Use 'derived-from-git'

--- crux-build/build-tar.sh
+++ crux-build/build-tar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf crux-builder/
 mkdir -p crux-builder
-cp deps.edn build-crux.sh crux-builder/
+cp build-crux.sh crux-builder/
+sed deps.edn -e "s/derived-from-git/${CRUX_VERSION:-$(git describe --tags)}/g" > crux-builder/deps.edn
 tar -czf crux-builder.tar.gz crux-builder/
