5d8a011b54fcd0eeea63241f8e29279b7a5f80e4
James Henderson <james@jarohen.me.uk>
2020-01-30 15:55:58 +0000

    using common deps.edn

--- crux-build/build-tar.sh
+++ crux-build/build-tar.sh
@@ -3,11 +3,12 @@ CRUX_VERSION_SUB="s/derived-from-git/${CRUX_VERSION:-$(git describe --tags)}/g"
 rm -rf crux-builder/
 mkdir -p crux-builder/{clj-uberjar,mvn-uberjar,docker}
 
-cp clj-uberjar/build-uberjar.sh crux-builder/clj-uberjar/
-sed clj-uberjar/deps.edn -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/deps.edn
+sed deps.edn -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/deps.edn
+sed clj-uberjar/build-uberjar.sh -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/build-uberjar.sh
+chmod +x crux-builder/clj-uberjar/build-uberjar.sh
 
 cp docker/build-docker.sh docker/crux.edn docker/Dockerfile crux-builder/docker/
-sed docker/deps.edn -e $CRUX_VERSION_SUB > crux-builder/docker/deps.edn
+sed deps.edn -e $CRUX_VERSION_SUB > crux-builder/docker/deps.edn
 
 cp mvn-uberjar/pom.xml crux-builder/mvn-uberjar/
 sed mvn-uberjar/build-uberjar.sh -e $CRUX_VERSION_SUB > crux-builder/mvn-uberjar/build-uberjar.sh
--- crux-build/clj-uberjar/build-uberjar.sh
+++ crux-build/clj-uberjar/build-uberjar.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-clojure -A:pack
+clojure -Sdeps '{:deps {pack/pack.alpha {:git/url "https://github.com/juxt/pack.alpha.git", :sha "c70740ffc10805f34836da2160fa1899601fac02"}}}' \
+        -m mach.pack.alpha.capsule crux.jar \
+        --application-id crux \
+        --application-version derived-from-git \
+        -m crux.main
