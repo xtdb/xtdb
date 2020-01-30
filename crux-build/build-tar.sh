3a3b7d3cad28e149daf57229909416b9c0476c1e
Daniel Mason <dan@juxt.pro>
2020-01-30 16:36:54 +0000

    Add DEPS_EDN variable to bash script

--- crux-build/build-tar.sh
+++ crux-build/build-tar.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 CRUX_VERSION_SUB="s/derived-from-git/${CRUX_VERSION:-$(git describe --tags)}/g"
+DEPS_EDN="${DEPS_EDN:-deps.edn}"
 rm -rf crux-builder/
 mkdir -p crux-builder/{clj-uberjar,mvn-uberjar,docker}
 
-sed deps.edn -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/deps.edn
+sed $DEPS_EDN -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/deps.edn
 sed clj-uberjar/build-uberjar.sh -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/build-uberjar.sh
 chmod +x crux-builder/clj-uberjar/build-uberjar.sh
 
 cp docker/build-docker.sh docker/crux.edn docker/Dockerfile crux-builder/docker/
-sed deps.edn -e $CRUX_VERSION_SUB > crux-builder/docker/deps.edn
+sed $DEPS_EDN -e $CRUX_VERSION_SUB > crux-builder/docker/deps.edn
 
 cp mvn-uberjar/pom.xml crux-builder/mvn-uberjar/
 sed mvn-uberjar/build-uberjar.sh -e $CRUX_VERSION_SUB > crux-builder/mvn-uberjar/build-uberjar.sh
