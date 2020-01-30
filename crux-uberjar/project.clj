dfd852ffb4129dd8d06ef470d3904fa8f6d9604e
James Henderson <james@jarohen.me.uk>
2020-01-30 10:42:09 +0000

    moving crux-graal out to a separate module (although this should likely be crux-labs)

--- crux-uberjar/project.clj
+++ crux-uberjar/project.clj
@@ -14,15 +14,6 @@
                  [juxt/crux-lmdb "derived-from-git"]
                  [juxt/crux-test "derived-from-git" :scope "test"]]
   :middleware [leiningen.project-version/middleware]
-  :graal {:dependencies [[org.clojure/clojure "1.9.0"]
-                         [org.slf4j/slf4j-simple "1.7.26"]]
-          :clean-targets []
-          :jvm-opts ["-Dclojure.compiler.direct-linking=true"
-                     "-Dclojure.spec.compile-asserts=false"]
-          :aot ^:replace [crux.kv.memdb
-                          crux.kv.rocksdb
-                          crux.main.graal]
-          :main crux.main.graal}
   :aot [crux.main]
   :main crux.main
 
