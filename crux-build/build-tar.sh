baa7c8158c4f43354c5b65178433b78f31b45c43
Daniel Mason <dan@juxt.pro>
2020-01-30 16:42:42 +0000

    Passing custom crux.edn through too

--- crux-build/build-tar.sh
+++ crux-build/build-tar.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 CRUX_VERSION_SUB="s/derived-from-git/${CRUX_VERSION:-$(git describe --tags)}/g"
 DEPS_EDN="${DEPS_EDN:-deps.edn}"
+CRUX_EDN="${CRUX_EDN:-crux.edn}"
 rm -rf crux-builder/
 mkdir -p crux-builder/{clj-uberjar,mvn-uberjar,docker}
 
+cp clj-uberjar/$CRUX_EDN crux-builder/clj-uberjar/crux.edn
 sed $DEPS_EDN -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/deps.edn
 sed clj-uberjar/build-uberjar.sh -e $CRUX_VERSION_SUB > crux-builder/clj-uberjar/build-uberjar.sh
 chmod +x crux-builder/clj-uberjar/build-uberjar.sh
 
-cp docker/build-docker.sh docker/crux.edn docker/Dockerfile crux-builder/docker/
+cp docker/build-docker.sh docker/$CRUX_EDN docker/Dockerfile crux-builder/docker/
 sed $DEPS_EDN -e $CRUX_VERSION_SUB > crux-builder/docker/deps.edn
 
 cp mvn-uberjar/pom.xml crux-builder/mvn-uberjar/
--- crux-build/clj-uberjar/build-uberjar.sh
+++ crux-build/clj-uberjar/build-uberjar.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+mkdir -p config
+cp crux.edn config/
+
 clojure -Sdeps '{:deps {pack/pack.alpha {:git/url "https://github.com/juxt/pack.alpha.git", :sha "c70740ffc10805f34836da2160fa1899601fac02"}}}' \
         -m mach.pack.alpha.capsule crux.jar \
+        -e config \
         --application-id crux \
         --application-version derived-from-git \
         -m crux.main
+
+rm -rf config/
