355739ae75b350b5c2358a4dcb2cbaf28ac9859f
Daniel Mason <dan@juxt.pro>
2020-01-30 11:27:26 +0000

    Add crux-cli to uberjar to make `crux-all`

--- crux-uberjar/project.clj
+++ crux-uberjar/project.clj
@@ -4,8 +4,8 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.cli "0.4.2"]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
+                 [juxt/crux-cli "derived-from-git"]
                  [juxt/crux-kafka "derived-from-git" :exclusions [commons-codec]]
                  [juxt/crux-core "derived-from-git"]
                  [juxt/crux-http-server "derived-from-git" :exclusions [commons-codec]]
