b32fb70c37b922ce01c3b193d7ed232e7dc53929
James Henderson <james@jarohen.me.uk>
2020-01-30 15:37:15 +0000

    moving under clj-uberjar

--- crux-build/build-tar.sh
+++ crux-build/build-tar.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf crux-builder/
-mkdir -p crux-builder
-cp build-crux.sh crux-builder/
-sed deps.edn -e "s/derived-from-git/${CRUX_VERSION:-$(git describe --tags)}/g" > crux-builder/deps.edn
+mkdir -p crux-builder/{clj-uberjar,mvn-uberjar,docker}
+
+cp clj-uberjar/build-uberjar.sh crux-builder/clj-uberjar/
+sed clj-uberjar/deps.edn -e "s/derived-from-git/${CRUX_VERSION:-$(git describe --tags)}/g" > crux-builder/clj-uberjar/deps.edn
+
 tar -czf crux-builder.tar.gz crux-builder/
