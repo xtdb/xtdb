fd23c1609b5f2d1fbf4808fe22ee0d1b8371565a
James Henderson <james@jarohen.me.uk>
2020-01-31 11:19:48 +0000

    refactor crux.cli so we can test it

--- crux-cli/src/crux/cli.clj
+++ crux-cli/src/crux/cli.clj
@@ -16,14 +16,18 @@
    :crux.standalone/kv-store 'crux.kv.memdb/kv
    :crux.standalone/event-log-kv-store 'crux.kv.memdb/kv})
 
+(defn- if-it-exists [^File f]
+  (when (.exists f)
+    f))
+
 (def cli-options
   [["-e" "--edn-file EDN_FILE" "EDN file to load Crux options from"
     :parse-fn io/file
-    :validate [#(.exists ^File %) "EDN file doesn't exist"]]
+    :validate [if-it-exists "EDN file doesn't exist"]]
 
    ["-p" "--properties-file PROPERTIES_FILE" "Properties file to load Crux options from"
     :parse-fn io/file
-    :validate [#(.exists ^File %) "Properties file doesn't exist"]]
+    :validate [if-it-exists "Properties file doesn't exist"]]
 
    ["-x" "--extra-edn-options EDN_OPTIONS" "Extra options as an quoted EDN map."
     :default nil
@@ -31,10 +35,34 @@
 
    ["-h" "--help"]])
 
-;; NOTE: This isn't registered until the node manages to start up
-;; cleanly, so ctrl-c keeps working as expected in case the node
-;; fails to start.
+(defn parse-args [args]
+  (let [{:keys [options errors summary] :as parsed-opts} (cli/parse-opts args cli-options)]
+    (cond
+      (seq errors) {::errors errors}
+
+      (:help options) {::help summary}
+
+      :else (let [{:keys [edn-file properties-file extra-edn-options]} options]
+              {::node-opts (merge default-options
+
+                                  (or (some-> edn-file (cc/load-edn))
+                                      (some-> properties-file (cc/load-properties))
+                                      (some-> (io/file "crux.edn") if-it-exists (cc/load-edn))
+                                      (some-> (io/file "crux.properties") if-it-exists (cc/load-properties))
+                                      (some-> (io/resource "crux.edn") (cc/load-edn))
+                                      (some-> (io/resource "crux.properties") (cc/load-properties)))
+
+                                  extra-edn-options)}))))
+
+(defn- options->table [options]
+  (with-out-str
+    (pp/print-table (for [[k v] options]
+                      {:key k :value v}))))
+
 (defn- shutdown-hook-promise []
+  ;; NOTE: This isn't registered until the node manages to start up
+  ;; cleanly, so ctrl-c keeps working as expected in case the node
+  ;; fails to start.
   (let [main-thread (Thread/currentThread)
         shutdown? (promise)]
     (.addShutdownHook (Runtime/getRuntime)
@@ -49,47 +77,20 @@
                                "crux.shutdown-hook-thread"))
     shutdown?))
 
-(defn- options->table [options]
-  (with-out-str
-    (pp/print-table (for [[k v] options]
-                      {:key k :value v}))))
-
-(defn- if-it-exists [^File f]
-  (when (.exists f)
-    f))
-
-(defn merge-options [{:keys [edn-file properties-file extra-edn-options]}]
-  (merge default-options
-         (some-> (or properties-file
-                     (-> (io/file "crux.properties") if-it-exists)
-                     (io/resource "crux.properties"))
-                 (cc/load-properties))
-         (some-> (or edn-file
-                     (-> (io/file "crux.edn") if-it-exists)
-                     (io/resource "crux.edn"))
-                 (slurp)
-                 (edn/read-string))
-         extra-edn-options))
-
 (defn start-node-from-command-line [args]
   (cio/install-uncaught-exception-handler!)
 
-  (let [{:keys [options errors summary]} (-> (cli/parse-opts args cli-options)
-                                             (update :options merge-options))
-
-        {:keys [version revision]} n/crux-version]
+  (let [{::keys [errors help node-opts]} (parse-args args)]
     (cond
-      (:help options)
-      (println summary)
+      errors (binding [*out* *err*]
+               (doseq [error errors]
+                 (println error))
+               (System/exit 1))
 
-      errors
-      (binding [*out* *err*]
-        (doseq [error errors]
-          (println error))
-        (System/exit 1))
+      help (println help)
 
-      :else
-      (do (log/infof "Crux version: %s revision: %s" version revision)
-          (log/info "options:" (options->table options))
-          (with-open [node (n/start options)]
-            @(shutdown-hook-promise))))))
+      :else (let [{:keys [version revision]} n/crux-version]
+              (log/infof "Crux version: %s revision: %s" version revision)
+              (log/info "options:" (options->table node-opts))
+              (with-open [node (n/start node-opts)]
+                @(shutdown-hook-promise))))))
