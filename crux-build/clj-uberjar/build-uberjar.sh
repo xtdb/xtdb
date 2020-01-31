78257d7ca1209128eeffb83cd4c70738fc57641a
Daniel Mason <dan@juxt.pro>
2020-01-31 10:52:01 +0000

    Allow UBERJAR_NAME to be passed into uberjar build script

--- crux-build/clj-uberjar/build-uberjar.sh
+++ crux-build/clj-uberjar/build-uberjar.sh
@@ -3,7 +3,7 @@ mkdir -p config
 cp crux.edn config/
 
 clojure -Sdeps '{:deps {pack/pack.alpha {:git/url "https://github.com/juxt/pack.alpha.git", :sha "c70740ffc10805f34836da2160fa1899601fac02"}}}' \
-        -m mach.pack.alpha.capsule crux.jar \
+        -m mach.pack.alpha.capsule ${UBERJAR_NAME:-crux.jar} \
         -e config \
         --application-id crux \
         --application-version derived-from-git \
