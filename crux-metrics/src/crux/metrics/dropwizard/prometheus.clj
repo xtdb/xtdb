7d35bc2c9fd692be1a78000d097d5e60bb11851a
Tom Taylor <tmt@juxt.pro>
2020-02-04 09:07:47 +0000

    Create start method for prometheus

--- crux-metrics/src/crux/metrics/dropwizard/prometheus.clj
+++ crux-metrics/src/crux/metrics/dropwizard/prometheus.clj
@@ -1,9 +1,12 @@
 (ns crux.metrics.dropwizard.prometheus
   (:require [crux.metrics.dropwizard :as dropwizard])
   (:import [org.dhatim.dropwizard.prometheus PrometheusReporter]
-           [io.prometheus.client.exporter PushGateway]))
+           [io.prometheus.client.exporter PushGateway]
+           [java.util.concurrent TimeUnit]
+           [java.io Closeable]))
 
 (defn reporter ^PrometheusReporter
+
   [^MetricRegistry reg {::keys [prefix metric-filter pushgateway]}]
   (let [pushgateway (PushGateway. (or pushgateway "localhost:9091"))]
     {:pushgateway pushgateway
@@ -11,3 +14,16 @@
                          prefix (.prefixedWith prefix)
                          metric-filter (.filter metric-filter))
                        pushgateway)}))
+(defn start
+
+  ([^PrometheusReporter reporter seconds]
+   (.start reporter seconds (TimeUnit/SECONDS))
+   (reify Closeable
+     (close [this]
+       (.stop reporter))))
+
+  ([^PrometheusReporter reporter length ^TimeUnit unit]
+   (.start reporter length unit)
+   (reify Closeable
+     (close [this]
+       (.stop reporter)))))
