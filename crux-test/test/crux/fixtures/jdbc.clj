890a7a11f19608c6d647bfb74e9b4dae841dc309
James Henderson <james@jarohen.me.uk>
2020-02-05 18:50:57 +0000

    JDBC test fixture should use a temporary dir rather than creating a file in the working directory

--- crux-test/test/crux/fixtures/jdbc.clj
+++ crux-test/test/crux/fixtures/jdbc.clj
@@ -4,17 +4,20 @@
             [crux.jdbc :as j]
             [next.jdbc :as jdbc]
             [clojure.test :as t]
-            [crux.kafka :as k]))
+            [crux.kafka :as k]
+            [crux.fixtures :as f]
+            [clojure.java.io :as io]))
 
 (def ^:dynamic *dbtype* nil)
 
 (defn with-jdbc-node [dbtype f & [opts]]
-  (let [dbtype (name dbtype)
-        options (merge {:crux.node/topology 'crux.jdbc/topology
-                        :crux.jdbc/dbtype (name dbtype)
-                        :crux.jdbc/dbname "cruxtest"}
-                       opts)
-        ds (jdbc/get-datasource (j/conform-next-jdbc-properties options))]
-    (binding [*dbtype* dbtype]
-      (j/prep-for-tests! dbtype ds)
-      (api/with-opts options f))))
+  (f/with-tmp-dir "db-dir" [db-dir]
+    (let [dbtype (name dbtype)
+          options (merge {:crux.node/topology 'crux.jdbc/topology
+                          :crux.jdbc/dbtype (name dbtype)
+                          :crux.jdbc/dbname (str (io/file db-dir "cruxtest"))}
+                         opts)
+          ds (jdbc/get-datasource (j/conform-next-jdbc-properties options))]
+      (binding [*dbtype* dbtype]
+        (j/prep-for-tests! dbtype ds)
+        (api/with-opts options f)))))
