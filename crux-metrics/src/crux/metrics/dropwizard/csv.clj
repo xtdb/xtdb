7348f2af9afe16e6348bb8a6d8d612df9542118d
Tom Taylor <tmt@juxt.pro>
2020-02-06 14:05:20 +0000

    Flesh out documentation and add keys

--- crux-metrics/src/crux/metrics/dropwizard/csv.clj
+++ crux-metrics/src/crux/metrics/dropwizard/csv.clj
@@ -6,12 +6,12 @@
            (java.time Duration)))
 
 (defn start-reporter ^com.codahale.metrics.CsvReporter
-  [^MetricRegistry reg {::keys [report-frequency locale rate-unit duration-unit metric-filter dir]}]
+  [^MetricRegistry reg {::keys [report-frequency locale rate-unit duration-unit metric-filter file-name]}]
 
   (-> (CsvReporter/forRegistry reg)
       (cond-> locale (.formatFor locale)
               rate-unit (.convertRatesTo rate-unit)
               duration-unit (.convertDurationsTo duration-unit)
               metric-filter (.filter metric-filter))
-      (.build (io/file dir))
+      (.build (io/file file-name))
       (doto (.start (.toMillis ^Duration report-frequency) TimeUnit/MILLISECONDS))))
