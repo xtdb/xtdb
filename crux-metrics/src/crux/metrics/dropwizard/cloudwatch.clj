d5c35543405a1ec101d85bc195e63f38b87901a2
James Henderson <james@jarohen.me.uk>
2020-02-06 09:58:19 +0000

    not supplying region/creds if the user doesn't provide them - the clientbuilder has sensible defaults

--- crux-metrics/src/crux/metrics/dropwizard/cloudwatch.clj
+++ crux-metrics/src/crux/metrics/dropwizard/cloudwatch.clj
@@ -2,7 +2,7 @@
   (:require crux.metrics.dropwizard)
   (:import [io.github.azagniotov.metrics.reporter.cloudwatch CloudWatchReporter]
            [java.io Closeable]
-           [software.amazon.awssdk.services.cloudwatch CloudWatchAsyncClient]
+           [software.amazon.awssdk.services.cloudwatch CloudWatchAsyncClient CloudWatchAsyncClientBuilder]
            [software.amazon.awssdk.regions.providers DefaultAwsRegionProviderChain]
            [software.amazon.awssdk.regions Region]
            [software.amazon.awssdk.auth.credentials DefaultCredentialsProvider]
@@ -10,24 +10,21 @@
            [java.time Duration]
            [com.codahale.metrics MetricRegistry]))
 
-(defn report ^CloudWatchReporter
-
+(defn report ^io.github.azagniotov.metrics.reporter.cloudwatch.CloudWatchReporter
   [^MetricRegistry reg {::keys [region dry-run? jvm-metrics? dimensions]}]
+
   (let [cwac (-> (CloudWatchAsyncClient/builder)
-                 (.region (if region
-                            (Region/of region)
-                            (.getRegion (new DefaultAwsRegionProviderChain))))
-                 (.credentialsProvider (DefaultCredentialsProvider/create))
-                 .build)]
-    (-> (cond-> (CloudWatchReporter/forRegistry reg cwac "crux.metrics.dropwizard.cloudwatch")
-          jvm-metrics? .withJvmMetrics
-          dry-run? .withDryRun
-          dimensions (.withGlobalDimensions (into-array String dimensions)) )
+                 (cond-> region ^CloudWatchAsyncClientBuilder (.region (Region/of region)))
+                 (.build))]
+    (-> (CloudWatchReporter/forRegistry reg cwac "crux.metrics.dropwizard.cloudwatch")
+        (cond-> jvm-metrics? .withJvmMetrics
+                dry-run? .withDryRun
+                dimensions (.withGlobalDimensions (into-array String dimensions)) )
         .build)))
 
 (defn start
-
   [^CloudWatchReporter reporter ^Duration duration]
+
   (.start reporter (.toMillis duration) TimeUnit/MILLISECONDS)
   (reify Closeable
     (close [this]
