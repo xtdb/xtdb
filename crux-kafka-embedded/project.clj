ffe2826157af6726c149e0fd20cb70d27d5da225
James Henderson <james@jarohen.me.uk>
2020-02-07 10:11:59 +0000

    need these exclusions to prevent an SLF4J cycle

--- crux-kafka-embedded/project.clj
+++ crux-kafka-embedded/project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/crux-core "derived-from-git"]
                  [org.apache.kafka/kafka_2.12 "2.4.0"]
-                 [org.apache.zookeeper/zookeeper "3.5.6"]]
+                 [org.apache.zookeeper/zookeeper "3.5.6" :exclusions [log4j org.slf4j/slf4j-log4j12]]]
   :middleware [leiningen.project-version/middleware]
   :pedantic? :warn)
