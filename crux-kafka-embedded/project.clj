483bdb72d02fd6f71c3417b801cd798ed96bf344
James Henderson <james@jarohen.me.uk>
2020-02-07 10:04:07 +0000

    bump Kafka versions to resolve some confusing deps

--- crux-kafka-embedded/project.clj
+++ crux-kafka-embedded/project.clj
@@ -5,14 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/crux-core "derived-from-git"]
-                 [org.apache.kafka/kafka_2.12 "2.3.0"]
-                 [com.fasterxml.jackson.core/jackson-annotations "2.9.9"]
-                 [org.apache.zookeeper/zookeeper "3.4.14"
-                  :exclusions [io.netty/netty
-                               jline
-                               org.apache.yetus/audience-annotations
-                               org.slf4j/slf4j-log4j12
-                               log4j]]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}}
+                 [org.apache.kafka/kafka_2.12 "2.4.0"]
+                 [org.apache.zookeeper/zookeeper "3.5.6"]]
   :middleware [leiningen.project-version/middleware]
   :pedantic? :warn)
--- crux-kafka/project.clj
+++ crux-kafka/project.clj
@@ -6,10 +6,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.5.0"]
                  [juxt/crux-core "derived-from-git"]
-                 [org.apache.kafka/kafka-clients "2.3.0"]
+                 [org.apache.kafka/kafka-clients "2.4.0"]
                  [cheshire "5.9.0"]
-                 [com.cognitect/transit-clj "0.8.313" :exclusions [org.msgpack/msgpack]]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}}
+                 [com.cognitect/transit-clj "0.8.319" :exclusions [org.msgpack/msgpack]]]
   :middleware [leiningen.project-version/middleware]
   :java-source-paths ["src"]
   :javac-options ["-source" "8" "-target" "8"
