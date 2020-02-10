e5334df0dcbe19f60c52a63b4769c9f8b0427217
Tom Taylor <tmt@juxt.pro>
2020-02-10 15:28:09 +0000

    Fix deps collisions in crux-bench

--- crux-bench/project.clj
+++ crux-bench/project.clj
@@ -10,7 +10,12 @@
                  [ch.qos.logback/logback-classic "1.2.3"]
 
                  [clj-http "3.10.0" :exclusions [org.apache.httpcomponents/httpclient]]
-                 [org.apache.httpcomponents/httpclient "4.5.9"]]
+                 [org.apache.httpcomponents/httpclient "4.5.9"]
+                 
+                 ;; Dependancy resolution
+                 [com.fasterxml.jackson.core/jackson-core "2.10.2"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.10.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.10.2"]]
 
   :middleware [leiningen.project-version/middleware]
 
