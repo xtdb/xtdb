b48a7e77b2f6bebe5ad45e9e1e69029f1fc33019
Tom Taylor <tmt@juxt.pro>
2020-02-10 15:21:32 +0000

    Fix reflection

--- crux-metrics/src/crux/metrics/dropwizard/console.clj
+++ crux-metrics/src/crux/metrics/dropwizard/console.clj
@@ -1,5 +1,6 @@
 (ns crux.metrics.dropwizard.console
   "Console reporting"
+  (:require [clojure.string :as string])
   (:import (com.codahale.metrics MetricRegistry
                                  ConsoleReporter
                                  ScheduledReporter)
@@ -7,14 +8,14 @@
            (java.io Closeable)))
 
 (defn start-reporter ^Closeable
-  [^MetricRegistry reg {::keys [report-rate stream locale clock rate-unit duration-unit metric-filter]}]
-  (doto (-> (ConsoleReporter/forRegistry reg) 
-            (cond->
+  [^MetricRegistry reg {::keys [report-rate stream ^java.util.Locale locale clock rate-unit duration-unit metric-filter]}]
+  (doto (-> (ConsoleReporter/forRegistry reg)
+            (doto
               stream (.outputTo stream)
               locale (.formattedFor locale)
               clock (.withClock clock)
-              rate-unit (.convertRatesTo (TimeUnit/of (.toUpperCase rate-unit)))
-              duration-unit (.convertDurationsTo (TimeUnit/of (.toUpperCase duration-unit)))
+              rate-unit (.convertRatesTo (TimeUnit/of (string/upper-case rate-unit)))
+              duration-unit (.convertDurationsTo (TimeUnit/of (string/upper-case duration-unit)))
               metric-filter (.filter metric-filter))
             (.build))
-    (.start (or report-rate 1))))
+    (.start (or report-rate 1) TimeUnit/SECONDS)))
--- crux-metrics/src/crux/metrics/dropwizard/csv.clj
+++ crux-metrics/src/crux/metrics/dropwizard/csv.clj
@@ -1,7 +1,8 @@
 (ns crux.metrics.dropwizard.csv
   (:require [clojure.java.io :as io])
   (:import (com.codahale.metrics CsvReporter MetricRegistry)
-           (java.io Closeable)))
+           (java.io Closeable)
+           (java.util.concurrent TimeUnit)))
 
 (defn start-reporter ^CsvReporter
   [^MetricRegistry reg {::keys [report-rate locale rate-unit duration-unit metric-filter dir]}]
@@ -11,4 +12,4 @@
                            duration-unit (.convertDurationsTo duration-unit)
                            metric-filter (.filter metric-filter))
                          (io/file dir))
-    (.start (or report-rate 1))))
+    (.start (or report-rate 1) TimeUnit/SECONDS)))
