d7df24947830746c9b206ab614ddc5c0ecbe91a9
Tom Taylor <tmt@juxt.pro>
2020-02-10 14:36:49 +0000

    Attempt at console reflection

--- crux-metrics/src/crux/metrics/dropwizard/console.clj
+++ crux-metrics/src/crux/metrics/dropwizard/console.clj
@@ -1,23 +1,25 @@
 (ns crux.metrics.dropwizard.console
   "Console reporting"
-  (:import (com.codahale.metrics MetricRegistry ConsoleReporter)
+  (:import (com.codahale.metrics MetricRegistry
+                                 ConsoleReporter
+                                 ConsoleReporter$Builder)
            (java.util.concurrent TimeUnit)
            (java.io Closeable)))
 
 (defn start-reporter ^ConsoleReporter
   [^MetricRegistry reg {::keys [report-rate stream locale clock rate-unit duration-unit metric-filter]}]
-  (let [reporter
-        (.build
-          (cond-> (ConsoleReporter/forRegistry reg)
-            stream (.outputTo stream)
-            locale (.formattedFor locale)
-            clock (.withClock clock)
-            rate-unit (.convertRatesTo (TimeUnit/of (.toUpperCase rate-unit)))
-            duration-unit (.convertDurationsTo (TimeUnit/of (.toUpperCase duration-unit)))
-            metric-filter (.filter metric-filter)))]
-    (.start
-      reporter
-      (or report-rate 1))
+  (let [reporter ^ConsoleReporter$Builder
+        (doto (-> (ConsoleReporter/forRegistry reg) 
+                  (cond->
+                    stream (.outputTo stream)
+                    locale (.formattedFor locale)
+                    clock (.withClock clock)
+                    rate-unit (.convertRatesTo (TimeUnit/of (.toUpperCase rate-unit)))
+                    duration-unit (.convertDurationsTo (TimeUnit/of (.toUpperCase duration-unit)))
+                    metric-filter (.filter metric-filter))
+                  (.build))
+          (.start
+            (or report-rate 1)))]
     (reify Closeable
       (close [this]
         (.stop reporter)))))
