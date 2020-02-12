cbc8f4a85e3649be034e91012947eb8ec6325cf3
Tom Taylor <tmt@juxt.pro>
2020-02-12 08:42:54 +0000

    Inital rocksdb stats commit

--- crux-rocksdb/project.clj
+++ crux-rocksdb/project.clj
@@ -5,6 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/crux-core "derived-from-git"]
+                 [juxt/crux-metrics "derived-from-git"]
                  [org.rocksdb/rocksdbjni "6.2.2"]
                  [com.github.jnr/jnr-ffi "2.1.9"]]
   :middleware [leiningen.project-version/middleware]
