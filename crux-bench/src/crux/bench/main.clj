4c4c00c8b4ddbf6e641c1d0a21637105c7aa1a1f
Daniel Mason <dan@juxt.pro>
2020-02-17 10:11:45 +0000

    Handle watdiv queries - fix `result-messages`

--- crux-bench/src/crux/bench/main.clj
+++ crux-bench/src/crux/bench/main.clj
@@ -11,9 +11,14 @@
   (bench/post-to-slack (format "*Starting Benchmark*, Crux Version: %s, Commit Hash: %s\n"
                                bench/crux-version bench/commit-hash))
   (bench/with-node [node]
-    (let [result-messages
-          [(bench/results->slack-message (devices/run-devices-bench node) :ts-devices)
-           (bench/results->slack-message (weather/run-weather-bench node) :ts-weather)
-           (bench/results->slack-message (watdiv-crux/run-watdiv-bench node {:test-count 100}) :watdiv-crux)]]
-      (bench/send-email-via-ses (string/join result-messages "\n"))))
+    (let [devices-results (devices/run-devices-bench node)
+          weather-results (weather/run-weather-bench node)
+          watdiv-results (watdiv-crux/run-watdiv-bench node {:test-count 100})
+          result-messages
+          [(bench/results->slack-message devices-results :ts-devices)
+           (bench/results->slack-message weather-results :ts-weather)
+           (bench/results->slack-message
+            [(first watdiv-results) (watdiv-crux/->query-result (rest watdiv-results))]
+            :watdiv-crux)]]
+      (bench/send-email-via-ses (string/join "\n" result-messages))))
   (shutdown-agents))
