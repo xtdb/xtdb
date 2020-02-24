5bb69d5aaad5925d6f9e7e7fdece2d08d821b6a9
Tom Taylor <tmt@juxt.pro>
2020-02-24 09:52:22 +0000

    Add connection to confluent and add visitor count

--- examples/soak/src/crux/soak/main.clj
+++ examples/soak/src/crux/soak/main.clj
@@ -1,12 +1,42 @@
 (ns crux.soak.main
   (:require [ring.util.response :as resp]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [crux.api :as api]))
 
 (def server-id (java.util.UUID/randomUUID))
 
-(defn handler [request]
-  (-> (resp/response (str {:server-id server-id :body (slurp (:body request))}))
-      (resp/content-type "text/plain")))
+(def soak-secrets
+  (-> (System/getenv "SOAK_SECRETS")
+      (json/decode)))
 
-(defn -main [& _]
-  (jetty/run-jetty handler {:port 8080}))
+(def kafka-properties-map
+  {"ssl.endpoint.identification.algorithm" "https"
+   "sasl.mechanism" "PLAIN"
+   "sasl.jaas.config" (str (->> ["org.apache.kafka.common.security.plain.PlainLoginModule"
+                                 "required"
+                                 (format "username=\"%s\"" (get soak-secrets "CONFLUENT_API_TOKEN"))
+                                 (format "password=\"%s\"" (get soak-secrets "CONFLUENT_API_SECRET"))]
+                                (string/join " "))
+                           ";")
+   "security.protocol" "SASL_SSL"})
+
+(defn -main [& args]
+  (with-open [node (api/start-node {:crux.node/topology '[crux.kafka/topology]
+                                    :crux.kafka/bootstrap-servers (get soak-secrets "CONFLUENT_BROKER")
+                                    :crux.node/kv-store 'crux.kv.memdb/kv
+                                    :crux.kafka/replication-factor 3
+                                    :crux.kafka/doc-partitions 6
+                                    :crux.kafka/kafka-properties-map kafka-properties-map})]
+
+    (let [handler (fn [request]
+                    (api/submit-tx node [[:crux.tx/put
+                                          {:crux.db/id (keyword (:remote-addr request))
+                                           :type :visitor}]])
+                    (-> (ring/response (str {:server-id server-id
+                                             :body (slurp (:body request))
+                                             :visitor-count (count (api/q
+                                                                     (api/db node)
+                                                                     {:find ['e]
+                                                                      :where [['e :type :visitor]]}))}))
+                        (ring/content-type "text/plain")))]
+      (jetty/run-jetty handler {:port 8080}))))
