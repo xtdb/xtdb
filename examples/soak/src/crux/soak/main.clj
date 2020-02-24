81052bad48379d9209df0edc295462d2f02ccdb0
Daniel Mason <dan@juxt.pro>
2020-02-24 16:12:39 +0000

    Rename usages of `ring` as `resp`

--- examples/soak/src/crux/soak/main.clj
+++ examples/soak/src/crux/soak/main.clj
@@ -1,12 +1,12 @@
 (ns crux.soak.main
-  (:require [ring.util.response :as ring]
+  (:require [ring.util.response :as resp]
             [ring.adapter.jetty :as jetty]))
 
 (def server-id (java.util.UUID/randomUUID))
 
 (defn handler [request]
-  (-> (ring/response (str {:server-id server-id :body (slurp (:body request))}))
-      (ring/content-type "text/plain")))
+  (-> (resp/response (str {:server-id server-id :body (slurp (:body request))}))
+      (resp/content-type "text/plain")))
 
 (defn -main [& _]
   (jetty/run-jetty handler {:port 8080}))
