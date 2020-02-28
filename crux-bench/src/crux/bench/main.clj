747ca19b0189f9d3b3b7af99ba65970bf89a3942
James Henderson <james@jarohen.me.uk>
2020-02-28 18:17:09 +0000

    refactor out the reduce in crux.bench.main

--- crux-bench/src/crux/bench/main.clj
+++ crux-bench/src/crux/bench/main.clj
@@ -1,33 +1,33 @@
 (ns crux.bench.main
-  (:require [crux.bench :as bench]
-            [crux.bench.ts-weather :as weather]
+  (:require [clojure.string :as string]
+            [crux.bench :as bench]
             [crux.bench.ts-devices :as devices]
-            [clj-http.client :as client]
-            [clojure.tools.cli :as cli]
-            [crux.bench.watdiv-crux :as watdiv-crux]
-            [clojure.string :as string]))
+            [crux.bench.ts-weather :as weather]
+            [crux.bench.watdiv-crux :as watdiv-crux]))
 
 (defn -main []
   (bench/post-to-slack (format "*Starting Benchmark*, Crux Version: %s, Commit Hash: %s\n"
                                bench/crux-version bench/commit-hash))
 
-  (->> (bench/with-nodes [node]
-         [(doto (devices/run-devices-bench node)
-            (bench/post-slack-results :ts-devices))
+  (let [bench-results (bench/with-nodes [node]
+                        [(doto (devices/run-devices-bench node)
+                           (bench/post-slack-results :ts-devices))
 
-          (doto (weather/run-weather-bench node)
-            (bench/post-slack-results :ts-weather))
+                         (doto (weather/run-weather-bench node)
+                           (bench/post-slack-results :ts-weather))
 
-          (let [raw-watdiv-results (watdiv-crux/run-watdiv-bench node {:test-count 100})]
-            (doto [(first raw-watdiv-results)
-                   (watdiv-crux/summarise-query-results (rest raw-watdiv-results))]
-              (bench/post-slack-results :watdiv-crux)))])
+                         (let [raw-watdiv-results (watdiv-crux/run-watdiv-bench node {:test-count 100})]
+                           (doto [(first raw-watdiv-results)
+                                  (watdiv-crux/summarise-query-results (rest raw-watdiv-results))]
+                             (bench/post-slack-results :watdiv-crux)))])]
 
-       (reduce (fn [acc [node-type result]]
-                 (str acc
-                      (format "<h2>%s</h2>" node-type)
-                      (string/join (map (fn [r] (string/join (map #(format "<p>%s</p>" %) r))) result))))
-               "<h1>Crux bench results</h1><br>")
-       (bench/send-email-via-ses))
+    (bench/send-email-via-ses
+     (str "<h1>Crux bench results</h1><br>"
+          (->> (for [[node-type results] bench-results]
+                 (str (format "<h2>%s</h2>" node-type)
+                      (->> (for [result results]
+                             (format "<p>%s</p>" result))
+                           (string/join))))
+               (string/join)))))
 
   (shutdown-agents))
