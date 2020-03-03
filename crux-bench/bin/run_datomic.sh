2f5bdfae080b657fe94d5fea1add90228b4c0d4a
Daniel Mason <dan@juxt.pro>
2020-03-03 15:27:15 +0000

    Add script to run watdiv bench

---
+++ crux-bench/bin/run_datomic.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+aws ecs run-task\
+ --task-definition crux-bench-latest\
+ --cluster crux-bench\
+ --overrides "{\"containerOverrides\": [{\"name\":\"bench-container\",\"command\":[\"crux.bench.watdiv-datomic\"]}]}"\
+ --launch-type FARGATE\
+ --count 1\
+ --network-configuration \
+ "awsvpcConfiguration={subnets=[subnet-5140ba2b],assignPublicIp=\"ENABLED\"}"\
+ --output json
