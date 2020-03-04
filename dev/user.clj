7f36d03bf104dcf51de3d7cc2fbd8d3854630919
James Henderson <james@jarohen.me.uk>
2020-03-04 15:27:42 +0000

    add option of embedded-kafka to user namespace

--- dev/user.clj
+++ dev/user.clj
@@ -3,10 +3,17 @@
             [clojure.tools.namespace.repl :as ctn]
             [integrant.core :as i]
             [integrant.repl.state :refer [system]]
-            [integrant.repl :as ir :refer [clear go suspend resume halt reset reset-all]])
-  (:import [crux.api ICruxAPI]))
+            [integrant.repl :as ir :refer [clear go suspend resume halt reset reset-all]]
+            [crux.io :as cio]
+            [crux.kafka.embedded :as ek]
+            [clojure.java.io :as io])
+  (:import (crux.api ICruxAPI)
+           (java.io Closeable)))
 
 (ctn/disable-reload!)
+
+(def dev-node-dir
+  (io/file "dev/dev-node"))
 
 (defmethod i/init-key :node [_ node-opts]
   (crux/start-node node-opts))
@@ -14,11 +21,32 @@
 (defmethod i/halt-key! :node [_ ^ICruxAPI node]
   (.close node))
 
-(def config
-  {:node (merge {:crux.node/topology ['crux.standalone/topology
-                                      'crux.kv.rocksdb/kv-store]
-                 :crux.kv/db-dir "dev/dev-node/db-dir"
-                 :crux.standalone/event-log-dir "dev/dev-node/event-log"
-                 :crux.kv/sync? true})})
+(def standalone-config
+  {:node {:crux.node/topology ['crux.standalone/topology
+                               'crux.kv.rocksdb/kv-store]
+          :crux.kv/db-dir (str (io/file dev-node-dir "db"))
+          :crux.standalone/event-log-dir (str (io/file dev-node-dir "event-log"))
+          :crux.kv/sync? true}})
 
-(ir/set-prep! (fn [] config))
+(defmethod i/init-key :embedded-kafka [_ {:keys [kafka-port kafka-dir]}]
+  (ek/start-embedded-kafka #::ek{:zookeeper-data-dir (str (io/file kafka-dir "zk-data"))
+                                 :zookeeper-port (cio/free-port)
+                                 :kafka-log-dir (str (io/file kafka-dir "kafka-log"))
+                                 :kafka-port kafka-port}))
+
+(defmethod i/halt-key! :embedded-kafka [_ ^Closeable embedded-kafka]
+  (.close embedded-kafka))
+
+(def embedded-kafka-config
+  (let [kafka-port (cio/free-port)]
+    {:embedded-kafka {:kafka-port kafka-port
+                      :kafka-dir (io/file dev-node-dir "kafka")}
+     :node {:crux.node/topology ['crux.kafka/topology
+                                 'crux.kv.rocksdb/kv-store]
+            :crux.kafka/bootstrap-servers (str "localhost:" kafka-port)
+            :crux.kv/db-dir (str (io/file dev-node-dir "ek-db"))
+            :crux.standalone/event-log-dir (str (io/file dev-node-dir "ek-event-log"))
+            :crux.kv/sync? true}}))
+
+;; swap for `embedded-kafka-config` to use embedded-kafka
+(ir/set-prep! (fn [] standalone-config))
