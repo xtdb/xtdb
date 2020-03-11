d77865ef96301ea6d28865174ecbe3bfae45e0b9
Jon Pither <jon.pither@gmail.com>
2020-03-11 17:10:47 +0000

    Swap out indexer

--- crux-core/src/crux/compaction.clj
+++ crux-core/src/crux/compaction.clj
@@ -1,20 +1,18 @@
 (ns crux.compaction
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.set :as set]
+            [clojure.tools.logging :as log]
             [crux.db :as db]
-            [crux.node :as n]
-            [crux.tx :as tx]
-            [crux.kv :as kv]
             [crux.index :as idx]
-            [clojure.set :as set])
-  (:import java.io.Closeable
-           java.time.Duration
-           crux.codec.EntityTx))
+            [crux.kv :as kv]
+            [crux.node :as n]
+            [crux.tx :as tx])
+  (:import crux.codec.EntityTx))
 
 ;; todo, can be integrated:
 (defn- tx-events->compaction-eids [tx-events]
   (->> tx-events
        (filter (comp #{:crux.tx/put :crux.tx/cas} first))
-       (map first)))
+       (map second)))
 
 (defn- entity-txes->content-hashes [txes]
   (set (for [^EntityTx entity-tx txes]
@@ -32,49 +30,29 @@
         (log/info "Pruning" content-hashes-to-prune)
         (db/delete-objects object-store content-hashes-to-prune)))))
 
-;; Spiked out plumbing:
+(def ^:dynamic valid-time-watermark (fn [tx-time] tx-time))
 
-(defn- index-tx-log [{:keys [!error] ::n/keys [tx-log indexer document-store]} {::keys [^Duration poll-sleep-duration]}]
-  (log/info "Started compactor")
-  (try
-    (while true
-      (let [consumed-txs? (with-open [tx-log (db/open-tx-log tx-log (::tx/tx-id (db/latest-completed-tx indexer)))]
-                            (let [tx-log (iterator-seq tx-log)
-                                  consumed-txs? (not (empty? tx-log))]
-                              (doseq [{:keys [crux.tx.event/tx-events] :as tx} tx-log
-                                      :let [tx (select-keys tx [::tx/tx-time ::tx/tx-id])]]
+(defrecord CompactingIndexer [indexer object-store kv-store]
+  db/Indexer
+  (index-docs [this docs]
+    (db/index-docs indexer docs))
+  (index-tx [this tx tx-events]
+    (db/index-tx indexer tx tx-events)
+    (with-open [snapshot (kv/new-snapshot kv-store)]
+      (doseq [eid (tx-events->compaction-eids tx-events)]
+        (compact object-store snapshot eid (valid-time-watermark (::tx/tx-time tx)) (::tx/tx-time tx)))))
+  (missing-docs [this content-hashes]
+    (db/missing-docs indexer content-hashes))
+  (store-index-meta [this k v]
+    (db/store-index-meta indexer k v))
+  (read-index-meta [this k]
+    (db/read-index-meta indexer k))
+  (latest-completed-tx [this]
+    (db/latest-completed-tx indexer)))
 
-                                ;; What to do here?
-
-                                (when (Thread/interrupted)
-                                  (throw (InterruptedException.))))
-                              consumed-txs?))]
-        (when (Thread/interrupted)
-          (throw (InterruptedException.)))
-        (when-not consumed-txs?
-          (Thread/sleep (.toMillis poll-sleep-duration)))))
-    (catch InterruptedException e)
-    (catch Exception e
-      (reset! !error e)
-      (log/error e "Error compacting")))
-  (log/info "Shut down tx-compactor"))
-
-(defrecord Compactor [^Thread executor-thread, !error]
-  db/TxConsumer
-  (consumer-error [_] @!error)
-  Closeable
-  (close [_]
-    (.interrupt executor-thread)
-    (.join executor-thread)))
-
-(def compactor
-  {:start-fn (fn [deps args]
-               (let [!error (atom nil)]
-                 (->Compactor (doto (Thread. #(index-tx-log (assoc deps :!error !error) args))
-                                (.setName "crux-tx-consumer")
-                                (.start))
-                              !error)))
-   :deps [::n/indexer ::n/document-store ::n/tx-log]
-   :args {::poll-sleep-duration {:default (Duration/ofMillis 100)
-                                 :doc "How long to sleep between running compaction"
-                                 :crux.config/type :crux.config/duration}}})
+(def module
+  {::indexer 'crux.tx/kv-indexer
+   ::n/indexer {:start-fn (fn [{:keys [::indexer ::n/object-store ::n/kv-store]} args]
+                            (let [!error (atom nil)]
+                              (->CompactingIndexer indexer object-store kv-store)))
+                :deps [::indexer ::n/object-store ::n/kv-store]}})
--- crux-test/test/crux/compaction2_test.clj
+++ crux-test/test/crux/compaction2_test.clj
@@ -4,51 +4,32 @@
             [crux.compaction :as cc]
             [crux.fixtures.api :as fapi :refer [*api*]]
             [crux.fixtures.kv :as kvf]
-            [crux.fixtures.standalone :as fs]))
+            [crux.fixtures.standalone :as fs]
+            [crux.node :as n]))
 
-(t/use-fixtures :each fs/with-standalone-node kvf/with-kv-dir fapi/with-node)
+(defn with-compaction [f]
+  (fapi/with-opts (-> fapi/*opts*
+                      (update ::n/topology conj cc/module))
+    f))
 
-(defn- assert-present [& docs]
-  (doseq [doc docs]
-    (assert (.document *api* (c/new-id doc)) doc)))
+(t/use-fixtures :each fs/with-standalone-node with-compaction kvf/with-kv-dir fapi/with-node)
 
 (t/deftest test-compact-below-watermark
-  (t/testing "preserve initial document below watermark"
-    (let [{:crux.tx/keys [tx-time]} (fapi/submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivan-2015"} #inst "2015"]])]
-      (assert-present {:crux.db/id :ivan :name "Ivan-2015"})
-      (let [db (.db *api*)]
-        (with-open [snapshot (.newSnapshot db)]
-          (cc/compact (:object-store *api*) snapshot :ivan #inst "2017" tx-time)))
-      (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2015"})))))
+  (with-redefs [cc/valid-time-watermark (fn [_] #inst "2017")]
+    (t/testing "preserve initial document below watermark"
+      (let [{:crux.tx/keys [tx-time]} (fapi/submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivan-2015"} #inst "2015"]])]
+        (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2015"})))))
 
-  (t/testing "compact away below valid-time watermark"
-    (let [{:crux.tx/keys [tx-time]} (fapi/submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivan-2016"} #inst "2016"]
-                                                           [:crux.tx/put {:crux.db/id :ivan :name "Ivan-2020"} #inst "2020"]])]
-      (assert-present {:crux.db/id :ivan :name "Ivan-2015"}
-                      {:crux.db/id :ivan :name "Ivan-2016"}
-                      {:crux.db/id :ivan :name "Ivan-2020"})
-      (let [db (.db *api*)]
-        (with-open [snapshot (.newSnapshot db)]
-          (cc/compact (:object-store *api*) snapshot :ivan #inst "2017" tx-time)))
-      (t/is (not (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2015"}))))
-      (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2016"})))
-      (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2020"})))))
+    (t/testing "compact away below valid-time watermark"
+      (let [{:crux.tx/keys [tx-time]} (fapi/submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivan-2016"} #inst "2016"]
+                                                             [:crux.tx/put {:crux.db/id :ivan :name "Ivan-2020"} #inst "2020"]])]
+        (t/is (not (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2015"}))))
+        (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2016"})))
+        (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2020"})))))
 
-  (t/testing "do not compact away document that exists above and below watermark"
-    (let [{:crux.tx/keys [tx-time]} (fapi/submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivan-2015"} #inst "2015"]
-                                                           [:crux.tx/put {:crux.db/id :ivan :name "Ivan-2015"} #inst "2021"]])]
-      (assert-present {:crux.db/id :ivan :name "Ivan-2015"}
-                      {:crux.db/id :ivan :name "Ivan-2016"}
-                      {:crux.db/id :ivan :name "Ivan-2020"})
-      (let [db (.db *api*)]
-        (with-open [snapshot (.newSnapshot db)]
-          (cc/compact (:object-store *api*) snapshot :ivan #inst "2017" tx-time)))
-      (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2015"})))
-      (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2016"})))
-      (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2020"})))))
-
-  (t/testing "handle no data present below watermark"
-    (assert-present {:crux.db/id :ivan :name "Ivan-2015"})
-    (let [db (.db *api*)]
-      (with-open [snapshot (.newSnapshot db)]
-        (cc/compact (:object-store *api*) snapshot :ivan #inst "2000" (:crux.tx/tx-time (.latestCompletedTx *api*)))))))
+    (t/testing "do not compact away document that exists above and below watermark"
+      (let [{:crux.tx/keys [tx-time]} (fapi/submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivan-2015"} #inst "2015"]
+                                                             [:crux.tx/put {:crux.db/id :ivan :name "Ivan-2015"} #inst "2021"]])]
+        (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2015"})))
+        (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2016"})))
+        (t/is (.document *api* (c/new-id {:crux.db/id :ivan :name "Ivan-2020"})))))))
