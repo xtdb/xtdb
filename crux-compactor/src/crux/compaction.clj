ec8f78d5026a9d7d82fe26ccb9564f9228b6f608
Jon Pither <jon.pither@gmail.com>
2020-03-12 16:27:36 +0000

    Rehousing the compaction ns

--- crux-compactor/src/crux/compaction.clj
+++ crux-compactor/src/crux/compaction.clj
@@ -1,65 +1,68 @@
 (ns crux.compaction
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.set :as set]
+            [clojure.tools.logging :as log]
             [crux.db :as db]
-            [crux.node :as n]
-            [crux.tx :as tx]
+            [crux.index :as idx]
             [crux.kv :as kv]
-            [crux.index :as idx])
-  (:import java.io.Closeable
-           java.time.Duration
-           crux.codec.EntityTx))
+            [crux.node :as n]
+            [crux.tx :as tx])
+  (:import crux.codec.EntityTx
+           java.util.Calendar))
+
+(defn- tx-events->compaction-eids [tx-events]
+  (->> tx-events
+       (filter (comp #{:crux.tx/put :crux.tx/cas} first))
+       (map second)))
+
+(defn- entity-txes->content-hashes [txes]
+  (set (for [^EntityTx entity-tx txes]
+         (.content-hash entity-tx))))
 
 (defn compact [object-store snapshot eid valid-time tx-time]
   (with-open [i (kv/new-iterator snapshot)]
-    ;; These are candidates for killing:
-    (doseq [^EntityTx entity-tx (->> (idx/entity-history-seq-descending i eid valid-time tx-time)
-                                     (rest))]
-      (log/info "Pruning" entity-tx)
-      (db/delete-objects object-store [(.content-hash entity-tx)]))))
+    (let [[^EntityTx tx & txes] (take-while (fn [^EntityTx tx]
+                                              (db/get-single-object object-store snapshot (.content-hash tx)))
+                                            (idx/entity-history-seq-descending i eid valid-time tx-time))
+          old-content-hashes (entity-txes->content-hashes txes)
+          new-content-hashes (when tx
+                               (with-open [i2 (kv/new-iterator snapshot)]
+                                 (entity-txes->content-hashes (idx/entity-history-seq-ascending i2 eid (.vt tx) tx-time))))
+          content-hashes-to-prune (set/difference old-content-hashes new-content-hashes)]
+      (when (seq content-hashes-to-prune)
+        (log/debug "Pruning" content-hashes-to-prune)
+        (db/delete-objects object-store content-hashes-to-prune)))))
 
-;; Spiked out plumbing:
+(defn valid-time-watermark [tt-vt-interval-s tx-time]
+  (.getTime
+   (doto (Calendar/getInstance)
+     (.setTime tx-time)
+     (.add Calendar/SECOND (- tt-vt-interval-s)))))
 
-(defn- index-tx-log [{:keys [!error] ::n/keys [tx-log indexer document-store]} {::keys [^Duration poll-sleep-duration]}]
-  (log/info "Started compactor")
-  (try
-    (while true
-      (let [consumed-txs? (with-open [tx-log (db/open-tx-log tx-log (::tx/tx-id (db/latest-completed-tx indexer)))]
-                            (let [tx-log (iterator-seq tx-log)
-                                  consumed-txs? (not (empty? tx-log))]
-                              (doseq [{:keys [crux.tx.event/tx-events] :as tx} tx-log
-                                      :let [tx (select-keys tx [::tx/tx-time ::tx/tx-id])]]
+(defrecord CompactingIndexer [indexer object-store kv-store tt-vt-interval-s]
+  db/Indexer
+  (index-docs [this docs]
+    (db/index-docs indexer docs))
+  (index-tx [this tx tx-events]
+    (db/index-tx indexer tx tx-events)
+    (with-open [snapshot (kv/new-snapshot kv-store)]
+      (let [vt-watermark (valid-time-watermark tt-vt-interval-s (::tx/tx-time tx))]
+        (doseq [eid (tx-events->compaction-eids tx-events)]
+          (compact object-store snapshot eid vt-watermark (::tx/tx-time tx))))))
+  (missing-docs [this content-hashes]    (db/missing-docs indexer content-hashes))
+  (store-index-meta [this k v]
+    (db/store-index-meta indexer k v))
+  (read-index-meta [this k]
+    (db/read-index-meta indexer k))
+  (latest-completed-tx [this]
+    (db/latest-completed-tx indexer)))
 
-                                ;; What to do here?
+(def interval-30-days (* 60 60 24 30))
 
-                                (when (Thread/interrupted)
-                                  (throw (InterruptedException.))))
-                              consumed-txs?))]
-        (when (Thread/interrupted)
-          (throw (InterruptedException.)))
-        (when-not consumed-txs?
-          (Thread/sleep (.toMillis poll-sleep-duration)))))
-    (catch InterruptedException e)
-    (catch Exception e
-      (reset! !error e)
-      (log/error e "Error compacting")))
-  (log/info "Shut down tx-compactor"))
-
-(defrecord Compactor [^Thread executor-thread, !error]
-  db/TxConsumer
-  (consumer-error [_] @!error)
-  Closeable
-  (close [_]
-    (.interrupt executor-thread)
-    (.join executor-thread)))
-
-(def compactor
-  {:start-fn (fn [deps args]
-               (let [!error (atom nil)]
-                 (->Compactor (doto (Thread. #(index-tx-log (assoc deps :!error !error) args))
-                                (.setName "crux-tx-consumer")
-                                (.start))
-                              !error)))
-   :deps [::n/indexer ::n/document-store ::n/tx-log]
-   :args {::poll-sleep-duration {:default (Duration/ofMillis 100)
-                                 :doc "How long to sleep between running compaction"
-                                 :crux.config/type :crux.config/duration}}})
+(def module
+  {::indexer 'crux.tx/kv-indexer
+   ::n/indexer {:start-fn (fn [{:keys [::indexer ::n/object-store ::n/kv-store]} {:keys [::tt-vt-interval-s]}]
+                            (->CompactingIndexer indexer object-store kv-store tt-vt-interval-s))
+                :deps [::indexer ::n/object-store ::n/kv-store]
+                :args {::tt-vt-interval-s {:doc "Interval in seconds between transaction-time and valid-time watermark"
+                                           :default interval-30-days
+                                           :crux.config/type :crux.config/nat-int}}}})
