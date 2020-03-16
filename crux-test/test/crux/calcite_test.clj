b0119a90bb273c60b8b9176b190657aa56c8206a
Jon Pither <jon.pither@gmail.com>
2020-03-16 14:38:49 +0000

    Tidy tidy

--- crux-test/test/crux/calcite_test.clj
+++ crux-test/test/crux/calcite_test.clj
@@ -10,19 +10,51 @@
   (:import java.sql.DriverManager
            crux.calcite.CruxSchemaFactory))
 
-(t/use-fixtures :each fs/with-standalone-node kvf/with-kv-dir fapi/with-node)
+;; https://github.com/juxt/crux/issues/514
+;; Aggregations, Joins
+;; https://calcite.apache.org/docs/cassandra_adapter.html
+
+;; What is a table? (list of columns & also a grouping of documents (i.e. mongo collections))
+;; What do joins mean
+;; Table could be a datalog rule
+;; Inner maps are ? ignored
+
+(def ^:dynamic ^java.sql.Connection *conn*)
+(defn- with-jdbc-connection [f]
+  (with-open [conn (DriverManager/getConnection "jdbc:calcite:model=crux-calcite/resources/model.json")]
+    (binding [*conn* conn]
+      (f))))
+
+(defn- query [q]
+  (let [stmt (.createStatement *conn*)]
+    (->> q (.executeQuery stmt) resultset-seq)))
+
+(t/use-fixtures :each fs/with-standalone-node kvf/with-kv-dir fapi/with-node with-jdbc-connection)
 
 (t/deftest test-hello-world-query
-  (f/transact! *api* (f/people [{:crux.db/id :ivan :name "Ivan" :homeworld "Earth"}]))
+  (f/transact! *api* (f/people [{:crux.db/id :ivan :name "Ivan" :homeworld "Earth"}
+                                {:crux.db/id :malcolm :name "Malcolm" :homeworld "Mars"}]))
 
   (t/testing "Can query value by single field"
     (t/is (= #{["Ivan"]} (api/q (api/db *api*) '{:find [name]
                                                  :where [[e :name "Ivan"]
                                                          [e :name name]]}))))
+  (t/testing "retrieve data"
+    (t/is (= [{:name "Ivan"}
+              {:name "Malcolm"}]
+             (query "SELECT PERSON.NAME FROM PERSON"))))
+  (t/testing "multiple columns"
+    (t/is (= [{:name "Ivan" :homeworld "Earth"}
+              {:name "Malcolm" :homeworld "Mars"}]
+             (query "SELECT PERSON.NAME,PERSON.HOMEWORLD FROM PERSON"))))
 
-  (let [conn (DriverManager/getConnection "jdbc:calcite:model=crux-calcite/resources/model.json")
-        stmt (.createStatement conn)]
+  ;; TODO Broken for various reasons:
+  (t/testing "multiple columns"
+    (t/is (= #{{:name "Ivan" :homeworld "Earth" :id ":ivan"}
+               {:name "Malcolm" :homeworld "Mars" :id ":malcolm"}}
+             (set (query "SELECT * FROM PERSON"))))))
 
-    ;; Matches the hardcoded SW
-    (t/is (= [{:name "Ivan"}] (resultset-seq
-                               (.executeQuery stmt "SELECT PERSON.NAME FROM PERSON"))))))
+;; Mongo leverage this concept of collections:
+   ;; for (String collectionName : mongoDb.listCollectionNames()) {
+   ;;    builder.put(collectionName, new MongoTable(collectionName));
+   ;;  }
