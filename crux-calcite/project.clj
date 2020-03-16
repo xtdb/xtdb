99df4850758a4b24ac6251d5a9ed1961526c9ead
Jon Pither <jon.pither@gmail.com>
2020-03-16 16:47:54 +0000

    Avatica working

--- crux-calcite/project.clj
+++ crux-calcite/project.clj
@@ -5,7 +5,12 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/crux-core "derived-from-git"]
-                 [org.apache.calcite/calcite-core "1.21.0" :exclusions [org.apache.commons/commons-lang3]]]
+                 [org.apache.calcite/calcite-core "1.21.0" :exclusions [org.apache.commons/commons-lang3
+                                                                        org.apache.calcite.avatica/avatica-core
+                                                                        com.fasterxml.jackson.core/jackson-core
+                                                                        com.fasterxml.jackson.core/jackson-databind
+                                                                        com.fasterxml.jackson.core/jackson-annotations]]
+                 [org.apache.calcite.avatica/avatica-server "1.16.0"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}}
   :middleware [leiningen.project-version/middleware]
   :java-source-paths ["src"]
