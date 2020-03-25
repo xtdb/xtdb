a631bfd5610b2a7793a356f3e66855afa58a9f7e
James Henderson <james@jarohen.me.uk>
2020-03-25 09:15:13 +0000

    refactoring how we deal with time:
    - allowing user to select a date/time using date/time controls
    - using ZonedDateTime to make sure all the selected times are in a consistent TZ
    - rendering vt/tt on the current + forecast reports
    - using the same 'render-weather-report' fn for both current + forecast

--- examples/soak/src/crux/soak/main.clj
+++ examples/soak/src/crux/soak/main.clj
@@ -15,29 +15,34 @@
   (:import crux.api.NodeOutOfSyncException
            java.io.Closeable
            java.text.SimpleDateFormat
-           (java.time Duration Instant)
+           (java.time ZonedDateTime Duration Instant LocalDate LocalTime ZoneId)
+           (java.time.format DateTimeFormatter)
            java.util.Date
            org.eclipse.jetty.server.Server))
 
+(def london (ZoneId/of "Europe/London"))
+
 (defn get-current-weather [node location valid-time]
-  (api/entity (api/db node valid-time) (keyword (str location "-current"))))
+  (when-let [etx (api/entity-tx (api/db node valid-time) (keyword (str location "-current")))]
+    (merge (api/document node (:crux.db/content-hash etx))
+           (select-keys etx [:crux.db/valid-time :crux.tx/tx-time]))))
 
-(defn get-weather-forecast [node location valid-time]
-  (let [past-five-days (->> (iterate #(.minus ^Instant % (Duration/ofDays 1))
-                                     (.toInstant valid-time))
-                            (take 5)
-                            (map #(Date/from %)))]
-    (->> past-five-days
-         (keep (fn [transaction-time]
-                 (try
-                   [transaction-time
-                    (api/entity
-                     (api/db node valid-time transaction-time)
-                     (keyword (str location "-forecast")))]
-                   (catch NodeOutOfSyncException e
-                     nil)))))))
-
-(def date-formatter (SimpleDateFormat. "dd/MM/yyyy"))
+(defn get-weather-forecast [node location ^Date valid-time]
+  (->> (for [tx-time (->> (iterate #(.minus ^ZonedDateTime % (Duration/ofDays 1))
+                                   (-> (ZonedDateTime/ofInstant (.toInstant valid-time) london)
+                                       (.with (LocalTime/of 1 0))))
+                          (take 5)
+                          (map #(Date/from (.toInstant %))))]
+         ;; TODO if there's no report at any day, it'll return the most recent report
+         ;; which might mean two reports from one day :/
+         (when-let [db (try
+                         (api/db node valid-time tx-time)
+                         (catch NodeOutOfSyncException e
+                           nil))]
+           (when-let [etx (api/entity-tx db (keyword (str location "-forecast")))]
+             (merge (api/document node (:crux.db/content-hash etx))
+                    (select-keys etx [:crux.db/valid-time :crux.tx/tx-time])))))
+       (remove nil?)))
 
 (defn filter-weather-map [{:keys [main wind weather]}]
   (let [filtered-main (-> (set/rename-keys main {:temp :temperature})
@@ -45,36 +50,30 @@
     (when-not (empty? filtered-main)
       (-> filtered-main
           (assoc :wind_speed (:speed wind))
-          (assoc :current_weather (:description (first weather)))))))
+          (assoc :current_weather (:description (first weather)))
+          (update :temperature (comp #(- % 273) bigdec))
+          (update :feels_like (comp #(- % 273) bigdec))))))
 
-(defn render-weather-map [weather-map]
-  (for [[k v] weather-map]
-    [:p
-     [:b (-> (str (name k) ": ")
-             (string/replace #"_" " ")
-             string/upper-case)]
-     v]))
+(defn render-time [^Date time]
+  (.format (DateTimeFormatter/ofPattern "dd/MM/yyyy HH:mm")
+           (ZonedDateTime/ofInstant (.toInstant time) london)))
 
-(defn render-current-weather [current-weather]
-  [:div#current-weather
-   [:h2 "Current Weather"]
-   (into [:div#weather]
-         (->> current-weather
-              filter-weather-map
-              render-weather-map))])
+(defn render-weather-report [weather-report]
+  [:div.weather-reports
+   [:div.bitemp-inst
+    "vt = "
+    (render-time (:crux.db/valid-time weather-report))]
+   [:div.bitemp-inst
+    "tt = "
+    (render-time (:crux.tx/tx-time weather-report))]
+   (for [[k v] (filter-weather-map weather-report)]
+     [:p.weather-report
+      [:b (-> (str (name k) ": ")
+              (string/replace #"_" " ")
+              string/upper-case)]
+      v])])
 
-(defn render-weather-forecast [weather-forecast]
-  [:div#weather-forecast
-   [:h2 "Forecast History"]
-   (into [:div#forecasts]
-         (for [[forecast-date forecast] weather-forecast]
-           (into [:div#forecast
-                  [:h3 (.format date-formatter forecast-date)]]
-                 (->> forecast
-                      filter-weather-map
-                      render-weather-map))))])
-
-(defn render-weather-page [{:keys [location-id location-name date
+(defn render-weather-page [{:keys [location-id location-name ^Date valid-time
                                    current-weather weather-forecast]}]
   (str
    (html
@@ -82,39 +81,52 @@
      [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"}]]
     [:body
      [:style "body { display: inline-flex; font-family: Helvetica }
-                 #location { text-align: center; width: 300px; padding-right: 30px; }
-                 #current-weather { width: 300px; padding-right: 30px; }
-                 #forecasts { display: inline-flex; }
-                 #forecast { padding-right: 10px; }
-                 #forecast h3 { margin-top: 0px; } "]
+              #location { text-align: center; width: 300px; padding-right: 30px; }
+              #current-weather { width: 300px; padding-right: 30px; }
+              #forecasts { display: inline-flex; }
+              #forecasts .weather-report { padding-right: 10px; }
+              #forecasts h3 { margin-top: 0px; }
+              .bitemp-inst {font-family: monospace}"]
      [:div#location
       [:h1 location-name]
-      [:form {:action "/weather.html"}
-       [:input {:type "hidden" :name "Location" :value location-id}]
-       (into [:select {:name "Date"}]
-             (for [next-date (->> (iterate #(.plus ^Instant % (Duration/ofDays 1)) (.toInstant (Date.)))
-                                  (take 5)
-                                  (map #(Date/from %)))]
-               (let [formatted-date (.format date-formatter next-date)]
-                 [:option {:value (.getTime next-date)
-                           :selected (when (= formatted-date (.format date-formatter date)) "selected")}
-                  formatted-date])))
-       [:p [:input {:type "submit" :value "Select Date"}]]]]
+      (let [zdt (ZonedDateTime/ofInstant (.toInstant valid-time) london)]
+        [:form {:action "/weather.html"}
+         [:input {:type "hidden" :name "location" :value location-id}]
+         [:p [:input {:type "date",
+                      :name "date",
+                      :value (.format DateTimeFormatter/ISO_LOCAL_DATE zdt)}]]
+         [:p [:input {:type "time",
+                      :name "time",
+                      :value (.format (DateTimeFormatter/ofPattern "HH:mm") zdt)}]]
+         [:p [:input {:type "submit" :value "Select Date"}]]])]
      (when current-weather
-       (render-current-weather current-weather))
-     (render-weather-forecast weather-forecast)])))
+       [:div#current-weather
+        [:h2 "Current Weather"]
+        (render-weather-report current-weather)])
+     [:div#weather-forecast
+      [:h2 "Forecast History"]
+      [:div#forecasts
+       (->> weather-forecast (map render-weather-report))]]])))
 
 (defn weather-handler [req {:keys [crux-node]}]
-  (let [location-id (get-in req [:query-params "Location"])
-        date (some-> (get-in req [:query-params "Date"]) (Long/parseLong) (Date.))]
+  (let [location-id (get-in req [:query-params "location"])
+        valid-time (-> (ZonedDateTime/of (or (some-> (get-in req [:query-params "date"])
+                                                     (LocalDate/parse))
+                                             (LocalDate/now london))
+                                         (or (some-> (get-in req [:query-params "time"])
+                                                     (LocalTime/parse))
+                                             (LocalTime/now london))
+                                         london)
+                       .toInstant
+                       (Date/from))]
     (resp/response
      (render-weather-page {:location-id location-id
                            :location-name (-> (api/entity (api/db crux-node)
                                                           (keyword (str location-id "-current")))
                                               (:location-name))
-                           :date date
-                           :current-weather (get-current-weather crux-node location-id date)
-                           :weather-forecast (get-weather-forecast crux-node location-id date)}))))
+                           :valid-time valid-time
+                           :current-weather (get-current-weather crux-node location-id valid-time)
+                           :weather-forecast (get-weather-forecast crux-node location-id valid-time)}))))
 
 (defn render-homepage [location-names]
   (str
@@ -129,15 +141,12 @@
       [:h1 "Crux Weather Service"]
       [:h2 "Locations"]
       [:form {:target "_blank" :action "/weather.html"}
-       [:input {:type "hidden" :name "Date" :value (System/currentTimeMillis)}]
        [:p
-        (into [:select {:name "Location"}]
-              (map
-               (fn [location-name]
-                 [:option {:value (-> (string/replace location-name #" " "-")
-                                      (string/lower-case))}
-                  location-name])
-               location-names))]
+        (into [:select {:name "location"}]
+              (for [location-name (sort location-names)]
+                [:option {:value (-> (string/replace location-name #" " "-")
+                                     (string/lower-case))}
+                 location-name]))]
        [:p [:input {:type "submit" :value "View Location"}]]]]])))
 
 (defn homepage-handler [req {:keys [crux-node]}]
