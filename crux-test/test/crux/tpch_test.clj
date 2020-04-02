6e866edb5215375538e45fbbf22124bf4a4b19c5
Jon Pither <jon.pither@gmail.com>
2020-04-02 11:22:29 +0100

    ingest customers

--- crux-test/test/crux/tpch_test.clj
+++ crux-test/test/crux/tpch_test.clj
@@ -1,17 +1,18 @@
 (ns crux.tpch-test
   (:require [clojure.test :as t]
-            [crux.db :as db]
             [crux.fixtures :as f]
             [crux.fixtures.api :as fapi :refer [*api*]]
             [crux.fixtures.calcite :as cf :refer [query]]
-            [crux.fixtures.tpch :as tf]
             [crux.fixtures.kv :as kvf]
-            [crux.fixtures.standalone :as fs]))
+            [crux.fixtures.standalone :as fs]
+            [crux.fixtures.tpch :as tf])
+  (:import io.airlift.tpch.TpchTable))
 
 (t/use-fixtures :each fs/with-standalone-node cf/with-calcite-module kvf/with-kv-dir fapi/with-node cf/with-calcite-connection)
 
 (t/deftest test-tpch-schema
   (t/is (f/transact! *api* (tf/tpch-tables->crux-sql-schemas)))
 
-  (t/is (= [{:name "Ivan" :age 21}]
-           (query "SELECT * FROM CUSTOMER"))))
+  (f/transact! *api* (take 5 (tf/tpch-table->docs (first (TpchTable/getTables)))))
+
+  (t/is (= 5 (count (query "SELECT * FROM CUSTOMER")))))
