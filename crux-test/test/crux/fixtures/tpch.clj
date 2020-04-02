ff2a46eaddc03b3f209ae4ab876c2b326a1f3528
Jon Pither <jon.pither@gmail.com>
2020-04-02 11:22:29 +0100

    ingest customers

--- crux-test/test/crux/fixtures/tpch.clj
+++ crux-test/test/crux/fixtures/tpch.clj
@@ -1,20 +1,52 @@
 (ns crux.fixtures.tpch
-  (:require  [clojure.test :as t])
-  (:import [io.airlift.tpch TpchTable TpchColumn TpchColumnTypes TpchColumnType TpchColumnType$Base]))
+  (:import [io.airlift.tpch TpchColumn TpchColumnType TpchColumnType$Base TpchEntity TpchTable]))
 
 (def tpch-column-types->crux-calcite-type
   {TpchColumnType$Base/INTEGER :integer
    TpchColumnType$Base/VARCHAR :varchar
-   TpchColumnType$Base/IDENTIFIER :varchar
+   TpchColumnType$Base/IDENTIFIER :integer
    TpchColumnType$Base/DOUBLE :double})
 
 (defn tpch-table->crux-sql-schema [^TpchTable t]
   {:crux.db/id (keyword "crux.sql.schema" (.getTableName t))
    :crux.sql.table/name (.getTableName t)
    :crux.sql.table/columns (for [^TpchColumn c (.getColumns t)]
-                             {:crux.db/attribute (keyword (.getColumnName c))
-                              :crux.sql.column/name (.getColumnName c)
+                             {:crux.db/attribute (keyword (.getSimplifiedColumnName c))
+                              :crux.sql.column/name (.getSimplifiedColumnName c)
                               :crux.sql.column/type (tpch-column-types->crux-calcite-type (.getBase (.getType c)))})})
 
 (defn tpch-tables->crux-sql-schemas []
   (map tpch-table->crux-sql-schema (TpchTable/getTables)))
+
+(defn tpch-generated->doc [^TpchTable t ^TpchEntity b]
+  (into {:crux.db/id (keyword (str (.getTableName t) "-" (.getRowNumber b)))}
+        (for [^TpchColumn c (.getColumns t)]
+          [(keyword (.getSimplifiedColumnName c))
+           (condp = (.getBase (.getType c))
+             TpchColumnType$Base/IDENTIFIER
+             (.getIdentifier c b)
+             TpchColumnType$Base/INTEGER
+             (.getInteger c b)
+             TpchColumnType$Base/VARCHAR
+             (.getString c b)
+             TpchColumnType$Base/DOUBLE
+             (.getDouble c b))])))
+
+(defn tpch-table->docs [^TpchTable t]
+  ;; first happens to be customers (;; 150000)
+  (map (partial tpch-generated->doc t) (seq (.createGenerator ^TpchTable t 1 1 1))))
+
+(comment
+  (first (tpch-tables->crux-sql-schemas))
+  {:crux.db/id :crux.sql.schema/customer,
+   :crux.sql.table/name "customer",
+   :crux.sql.table/columns [{:crux.db/attribute :c_custkey, :crux.sql.column/name "c_custkey", :crux.sql.column/type :varchar}
+                            {:crux.db/attribute :c_name, :crux.sql.column/name "c_name", :crux.sql.column/type :varchar}
+                            {:crux.db/attribute :c_address, :crux.sql.column/name "c_address", :crux.sql.column/type :varchar}
+                            {:crux.db/attribute :c_nationkey, :crux.sql.column/name "c_nationkey", :crux.sql.column/type :varchar}
+                            {:crux.db/attribute :c_phone, :crux.sql.column/name "c_phone", :crux.sql.column/type :varchar}
+                            {:crux.db/attribute :c_acctbal, :crux.sql.column/name "c_acctbal", :crux.sql.column/type :double}
+                            {:crux.db/attribute :c_mktsegment, :crux.sql.column/name "c_mktsegment", :crux.sql.column/type :varchar}
+                            {:crux.db/attribute :c_comment, :crux.sql.column/name "c_comment", :crux.sql.column/type :varchar}]}
+
+  (first (tpch-table->docs (first (TpchTable/getTables)) 5)))
