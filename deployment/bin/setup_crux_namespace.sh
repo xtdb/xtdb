f903cb732136aaec33d45239aa7c11bfc9e71d69
Daniel Mason <dan@juxt.pro>
2020-04-07 10:36:17 +0100

    Remove deployment/ from repository
    Seems to contain a few scripts and templates for deploying Crux to AWS
    EKS, but serves no actual purpose in the repository. As, if we need
    these in any way, we can get them back from git, I'm removing them
    from the repository.

--- deployment/bin/setup_crux_namespace.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source "${BASH_SOURCE%/*}/shared.sh"
-
-kubectl create -f templates/crux-namespace.yml
--- deployment/bin/shared.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source config/env
-
-function with-common-env() {
-    export $(grep -v '^#' config/env | xargs -0)
-}
