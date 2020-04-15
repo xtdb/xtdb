086187552ec1e821f48766cfba88f26866d5ca91
Jon Pither <jon.pither@gmail.com>
2020-04-15 17:36:45 +0100

    Explicit about dependency used for removing warnings

--- crux-calcite/project.clj
+++ crux-calcite/project.clj
@@ -6,11 +6,13 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/crux-core "derived-from-git"]
                  [cheshire "5.10.0"]
-
                  [org.apache.calcite/calcite-core "1.21.0"]
                  [org.apache.calcite.avatica/avatica-server "1.16.0"]
 
-                 ;; dependency conflict resolution
+                 ;; remove illegal reflective warnings:
+                 [com.google.protobuf/protobuf-java "3.9.2"]
+
+                 ;; dependency conflict resolution:
                  [org.apache.calcite.avatica/avatica-core "1.16.0"]
                  [com.fasterxml.jackson.core/jackson-annotations "2.10.0"]
                  [com.fasterxml.jackson.core/jackson-databind "2.10.0"]]
