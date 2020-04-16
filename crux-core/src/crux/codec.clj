randomseed-io/crux

Removing more C

crux-core/src/crux/codec.clj
@@ -30,7 +30,7 @@
 
 
 ; two main indexes for querying
-(def ^:const ^:private avec-index-id 1)
+(def ^:const ^:private ave-index-id 1)
 (def ^:const ^:private aecv-index-id 2)
 
 ; how they work
@@ -38,7 +38,7 @@
   (api/submit-tx syst [:crux.tx/put {:crux.db/id :ids/ivan :name "ivan"}])
 
   ; [roughly speaking] for queries by attr name and value
-  ; in avec-index-id
+  ; in ave-index-id
   ; [:name "ivan" :ids/ivan "ivan-content-hash"]
 
   ; [roughly speaking] for entity queries by id
@@ -524,43 +524,39 @@
     (assert (= content-hash->doc-index-id index-id))
     (Id. (mem/slice-buffer k index-id-size id-size) 0)))
 
-(defn encode-avec-key-to
+(defn encode-ave-key-to
   (^org.agrona.MutableDirectBuffer[b aid]
-   (encode-avec-key-to b aid empty-buffer empty-buffer empty-buffer))
+   (encode-ave-key-to b aid empty-buffer empty-buffer))
   (^org.agrona.MutableDirectBuffer[b aid v]
-   (encode-avec-key-to b aid v empty-buffer empty-buffer))
-  (^org.agrona.MutableDirectBuffer[b aid v entity]
-   (encode-avec-key-to b aid v entity empty-buffer))
+   (encode-ave-key-to b aid v empty-buffer))
   (^org.agrona.MutableDirectBuffer
-   [^MutableDirectBuffer b ^DirectBuffer aid ^DirectBuffer v ^DirectBuffer entity ^DirectBuffer content-hash]
+   [^MutableDirectBuffer b ^DirectBuffer aid ^DirectBuffer v ^DirectBuffer entity]
    (assert (= aid-size (.capacity aid)) (mem/buffer->hex aid))
    (assert (or (= id-size (.capacity entity))
                (zero? (.capacity entity))) (mem/buffer->hex entity))
-   (assert (or (= id-size (.capacity content-hash))
-               (zero? (.capacity content-hash))) (mem/buffer->hex content-hash))
-   (let [^MutableDirectBuffer b (or b (mem/allocate-buffer (+ index-id-size id-size (.capacity v) (.capacity entity) (.capacity content-hash))))]
+   (let [^MutableDirectBuffer b (or b (mem/allocate-buffer (+ index-id-size id-size (.capacity v) (.capacity entity))))]
      (mem/limit-buffer
       (doto b
-        (.putByte 0 avec-index-id)
+        (.putByte 0 ave-index-id)
         (.putBytes index-id-size aid 0 aid-size)
         (.putBytes (+ index-id-size aid-size) v 0 (.capacity v))
-        (.putBytes (+ index-id-size aid-size (.capacity v)) entity 0 (.capacity entity))
-        (.putBytes (+ index-id-size aid-size (.capacity v) (.capacity entity)) content-hash 0 (.capacity content-hash)))
-      (+ index-id-size aid-size (.capacity v) (.capacity entity) (.capacity content-hash))))))
+        (.putBytes (+ index-id-size aid-size (.capacity v)) entity 0 (.capacity entity)))
+      (+ index-id-size aid-size (.capacity v) (.capacity entity))))))
 
 (defrecord EntityValueContentHash [eid value content-hash])
 
-(defn decode-avec-key->evc-from
-  ^crux.codec.EntityValueContentHash [^DirectBuffer k]
+(defrecord EntityValue [eid value])
+
+(defn decode-ave-key->ev-from
+  ^crux.codec.EntityValue [^DirectBuffer k]
   (let [length (long (.capacity k))]
-    (assert (<= (+ index-id-size aid-size id-size id-size) length) (mem/buffer->hex k))
+    (assert (<= (+ index-id-size aid-size id-size) length) (mem/buffer->hex k))
     (let [index-id (.getByte k 0)]
-      (assert (= avec-index-id index-id))
-      (let [value-size (- length aid-size id-size id-size index-id-size)
+      (assert (= ave-index-id index-id))
+      (let [value-size (- length aid-size id-size index-id-size)
             value (mem/slice-buffer k (+ index-id-size aid-size) value-size)
-            entity (Id. (mem/slice-buffer k (+ index-id-size aid-size value-size) id-size) 0)
-            content-hash (Id. (mem/slice-buffer k (+ index-id-size aid-size value-size id-size) id-size) 0)]
-        (->EntityValueContentHash entity value content-hash)))))
+            entity (Id. (mem/slice-buffer k (+ index-id-size aid-size value-size) id-size) 0)]
+        (->EntityValue entity value)))))
 
 (defn encode-aecv-key-to
   (^org.agrona.MutableDirectBuffer [b aid]

crux-core/src/crux/index.clj
@@ -55,7 +55,7 @@
 ;; AVE
 
 (defn- attribute-value+placeholder [k ^ValueEntityValuePeekState peek-state]
-  (let [value (.value (c/decode-avec-key->evc-from k))]
+  (let [value (.value (c/decode-ave-key->ev-from k))]
     (set! (.last-k peek-state) k)
     (set! (.value peek-state) value)
     [value :crux.index.binary-placeholder/value]))
@@ -63,7 +63,7 @@
 (defrecord DocAttributeValueEntityValueIndex [i ^DirectBuffer aid ^ValueEntityValuePeekState peek-state]
   db/Index
   (seek-values [this k]
-    (when-let [k (->> (c/encode-avec-key-to
+    (when-let [k (->> (c/encode-ave-key-to
                        (.get seek-buffer-tl)
                        aid
                        (or k c/empty-buffer))
@@ -72,23 +72,25 @@
 
   (next-values [this]
     (when-let [last-k (.last-k peek-state)]
-      (let [prefix-size (- (mem/capacity last-k) c/id-size c/id-size)]
-        (when-let [k (some->> (mem/inc-unsigned-buffer! (mem/limit-buffer (mem/copy-buffer last-k prefix-size (.get seek-buffer-tl)) prefix-size))
+      (let [prefix-size (- (mem/capacity last-k) c/id-size)]
+        (when-let [k (some->> (-> (mem/copy-buffer last-k prefix-size (.get seek-buffer-tl))
+                                  (mem/limit-buffer prefix-size)
+                                  mem/inc-unsigned-buffer!)
                               (kv/seek i))]
           (attribute-value+placeholder k peek-state))))))
 
 (defn new-doc-attribute-value-entity-value-index [snapshot aid]
   (let [aid (c/->aid-buffer aid)
-        prefix (c/encode-avec-key-to nil aid)]
+        prefix (c/encode-ave-key-to nil aid)]
     (->DocAttributeValueEntityValueIndex (new-prefix-kv-iterator (kv/new-iterator snapshot) prefix) aid (ValueEntityValuePeekState. nil nil))))
 
 (declare vectorize-value)
 
-(defn- attribute-value-entity-entity+value [snapshot i ^DirectBuffer current-k aid value entity-as-of-idx object-store attr-dict peek-eb ^DocAttributeValueEntityEntityIndexState peek-state ]
+(defn- attribute-value-entity-entity+value [snapshot i ^DirectBuffer current-k aid value entity-as-of-idx object-store attr-dict peek-eb ^DocAttributeValueEntityEntityIndexState peek-state]
   (loop [k current-k]
-    (let [limit (- (mem/capacity k) c/id-size)]
+    (let [limit (mem/capacity k)]
       (set! (.peek peek-state) (mem/inc-unsigned-buffer! (mem/limit-buffer (mem/copy-buffer k limit peek-eb) limit))))
-    (or (let [eid (.eid (c/decode-avec-key->evc-from k))
+    (or (let [eid (.eid (c/decode-ave-key->ev-from k))
               eid-buffer (c/->id-buffer eid)
               [_ ^EntityTx entity-tx] (db/seek-values entity-as-of-idx eid-buffer)]
           (when entity-tx
@@ -104,7 +106,7 @@
 
 (defn- attribute-value-value+prefix-iterator ^crux.index.ValueAndPrefixIterator [i ^DocAttributeValueEntityValueIndex value-entity-value-idx aid prefix-eb]
   (let [value (.value ^ValueEntityValuePeekState (.peek-state value-entity-value-idx))
-        prefix (c/encode-avec-key-to prefix-eb aid value)]
+        prefix (c/encode-ave-key-to prefix-eb aid value)]
     (ValueAndPrefixIterator. value (new-prefix-kv-iterator i prefix))))
 
 (defrecord DocAttributeValueEntityEntityIndex [snapshot i ^DirectBuffer aid value-entity-value-idx entity-as-of-idx object-store attr-dict prefix-eb peek-eb ^DocAttributeValueEntityEntityIndexState peek-state]
@@ -114,7 +116,7 @@
       (let [value+prefix-iterator (attribute-value-value+prefix-iterator i value-entity-value-idx aid prefix-eb)
             value (.value value+prefix-iterator)
             i (.prefix-iterator value+prefix-iterator)]
-        (when-let [k (->> (c/encode-avec-key-to
+        (when-let [k (->> (c/encode-ave-key-to
                            (.get seek-buffer-tl)
                            aid
                            value
@@ -394,7 +396,7 @@
                v (vectorize-value v)
                :let [v (c/->value-buffer v)]
                :when (pos? (mem/capacity v))]
-           [(c/encode-avec-key-to nil k v id content-hash)
+           [(c/encode-ave-key-to nil k v id)
             (c/encode-aecv-key-to nil k id content-hash v)])
          (apply concat))))
 

xtdb/xtdb

crux-core/src/crux/codec.clj
@@ -30,7 +30,7 @@
 
 
 ; two main indexes for querying
-(def ^:const ^:private avec-index-id 1)
+(def ^:const ^:private ave-index-id 1)
 (def ^:const ^:private aecv-index-id 2)
 
 ; how they work
@@ -38,8 +38,8 @@
   (api/submit-tx syst [:crux.tx/put {:crux.db/id :ids/ivan :name "ivan"}])
 
   ; [roughly speaking] for queries by attr name and value
-  ; in avec-index-id
-  ; [:name "ivan" :ids/ivan "ivan-content-hash"]
+  ; in ave-index-id
+  ; [:name "ivan" :ids/ivan]
 
   ; [roughly speaking] for entity queries by id
   ; in aecv-index-id
@@ -508,43 +508,39 @@
     (assert (= content-hash->doc-index-id index-id))
     (Id. (mem/slice-buffer k index-id-size id-size) 0)))
 
-(defn encode-avec-key-to
+(defn encode-ave-key-to
   (^org.agrona.MutableDirectBuffer[b attr]
-   (encode-avec-key-to b attr empty-buffer empty-buffer empty-buffer))
+   (encode-ave-key-to b attr empty-buffer empty-buffer))
   (^org.agrona.MutableDirectBuffer[b attr v]
-   (encode-avec-key-to b attr v empty-buffer empty-buffer))
-  (^org.agrona.MutableDirectBuffer[b attr v entity]
-   (encode-avec-key-to b attr v entity empty-buffer))
+   (encode-ave-key-to b attr v empty-buffer))
   (^org.agrona.MutableDirectBuffer
-   [^MutableDirectBuffer b ^DirectBuffer attr ^DirectBuffer v ^DirectBuffer entity ^DirectBuffer content-hash]
+   [^MutableDirectBuffer b ^DirectBuffer attr ^DirectBuffer v ^DirectBuffer entity]
    (assert (= id-size (.capacity attr)) (mem/buffer->hex attr))
    (assert (or (= id-size (.capacity entity))
                (zero? (.capacity entity))) (mem/buffer->hex entity))
-   (assert (or (= id-size (.capacity content-hash))
-               (zero? (.capacity content-hash))) (mem/buffer->hex content-hash))
-   (let [^MutableDirectBuffer b (or b (mem/allocate-buffer (+ index-id-size id-size (.capacity v) (.capacity entity) (.capacity content-hash))))]
+   (let [^MutableDirectBuffer b (or b (mem/allocate-buffer (+ index-id-size id-size (.capacity v) (.capacity entity))))]
      (mem/limit-buffer
       (doto b
-        (.putByte 0 avec-index-id)
+        (.putByte 0 ave-index-id)
         (.putBytes index-id-size attr 0 id-size)
         (.putBytes (+ index-id-size id-size) v 0 (.capacity v))
-        (.putBytes (+ index-id-size id-size (.capacity v)) entity 0 (.capacity entity))
-        (.putBytes (+ index-id-size id-size (.capacity v) (.capacity entity)) content-hash 0 (.capacity content-hash)))
-      (+ index-id-size id-size (.capacity v) (.capacity entity) (.capacity content-hash))))))
+        (.putBytes (+ index-id-size id-size (.capacity v)) entity 0 (.capacity entity)))
+      (+ index-id-size id-size (.capacity v) (.capacity entity))))))
 
 (defrecord EntityValueContentHash [eid value content-hash])
 
-(defn decode-avec-key->evc-from
-  ^crux.codec.EntityValueContentHash [^DirectBuffer k]
+(defrecord EntityValue [eid value])
+
+(defn decode-ave-key->ev-from
+  ^crux.codec.EntityValue [^DirectBuffer k]
   (let [length (long (.capacity k))]
-    (assert (<= (+ index-id-size id-size id-size id-size) length) (mem/buffer->hex k))
+    (assert (<= (+ index-id-size id-size id-size) length) (mem/buffer->hex k))
     (let [index-id (.getByte k 0)]
-      (assert (= avec-index-id index-id))
-      (let [value-size (- length id-size id-size id-size index-id-size)
+      (assert (= ave-index-id index-id))
+      (let [value-size (- length id-size id-size index-id-size)
             value (mem/slice-buffer k (+ index-id-size id-size) value-size)
-            entity (Id. (mem/slice-buffer k (+ index-id-size id-size value-size) id-size) 0)
-            content-hash (Id. (mem/slice-buffer k (+ index-id-size id-size value-size id-size) id-size) 0)]
-        (->EntityValueContentHash entity value content-hash)))))
+            entity (Id. (mem/slice-buffer k (+ index-id-size id-size value-size) id-size) 0)]
+        (->EntityValue entity value)))))
 
 (defn encode-aecv-key-to
   (^org.agrona.MutableDirectBuffer [b attr]

crux-core/src/crux/index.clj
@@ -55,7 +55,7 @@
 ;; AVE
 
 (defn- attribute-value+placeholder [k ^ValueEntityValuePeekState peek-state]
-  (let [value (.value (c/decode-avec-key->evc-from k))]
+  (let [value (.value (c/decode-ave-key->ev-from k))]
     (set! (.last-k peek-state) k)
     (set! (.value peek-state) value)
     [value :crux.index.binary-placeholder/value]))
@@ -63,7 +63,7 @@
 (defrecord DocAttributeValueEntityValueIndex [i ^DirectBuffer attr ^ValueEntityValuePeekState peek-state]
   db/Index
   (seek-values [this k]
-    (when-let [k (->> (c/encode-avec-key-to
+    (when-let [k (->> (c/encode-ave-key-to
                        (.get seek-buffer-tl)
                        (c/->id-buffer attr)
                        (or k c/empty-buffer))
@@ -72,22 +72,25 @@
 
   (next-values [this]
     (when-let [last-k (.last-k peek-state)]
-      (let [prefix-size (- (mem/capacity last-k) c/id-size c/id-size)]
-        (when-let [k (some->> (mem/inc-unsigned-buffer! (mem/limit-buffer (mem/copy-buffer last-k prefix-size (.get seek-buffer-tl)) prefix-size))
+      (let [prefix-size (- (mem/capacity last-k) c/id-size)]
+        (when-let [k (some->> (-> (mem/copy-buffer last-k prefix-size (.get seek-buffer-tl))
+                                  (mem/limit-buffer prefix-size)
+                                  mem/inc-unsigned-buffer!)
                               (kv/seek i))]
           (attribute-value+placeholder k peek-state))))))
 
 (defn new-doc-attribute-value-entity-value-index [snapshot attr]
-  (let [prefix (c/encode-avec-key-to nil (c/->id-buffer attr))]
+  (let [attr (c/->id-buffer attr)
+        prefix (c/encode-ave-key-to nil attr)]
     (->DocAttributeValueEntityValueIndex (new-prefix-kv-iterator (kv/new-iterator snapshot) prefix) attr (ValueEntityValuePeekState. nil nil))))
 
 (declare vectorize-value)
 
 (defn- attribute-value-entity-entity+value [snapshot i ^DirectBuffer current-k attr value entity-as-of-idx object-store peek-eb ^DocAttributeValueEntityEntityIndexState peek-state]
   (loop [k current-k]
-    (let [limit (- (mem/capacity k) c/id-size)]
+    (let [limit (mem/capacity k)]
       (set! (.peek peek-state) (mem/inc-unsigned-buffer! (mem/limit-buffer (mem/copy-buffer k limit peek-eb) limit))))
-    (or (let [eid (.eid (c/decode-avec-key->evc-from k))
+    (or (let [eid (.eid (c/decode-ave-key->ev-from k))
               eid-buffer (c/->id-buffer eid)
               [_ ^EntityTx entity-tx] (db/seek-values entity-as-of-idx eid-buffer)]
           (when entity-tx
@@ -102,7 +105,7 @@
 
 (defn- attribute-value-value+prefix-iterator ^crux.index.ValueAndPrefixIterator [i ^DocAttributeValueEntityValueIndex value-entity-value-idx attr prefix-eb]
   (let [value (.value ^ValueEntityValuePeekState (.peek-state value-entity-value-idx))
-        prefix (c/encode-avec-key-to prefix-eb (c/->id-buffer attr) value)]
+        prefix (c/encode-ave-key-to prefix-eb (c/->id-buffer attr) value)]
     (ValueAndPrefixIterator. value (new-prefix-kv-iterator i prefix))))
 
 (defrecord DocAttributeValueEntityEntityIndex [snapshot i attr value-entity-value-idx entity-as-of-idx object-store prefix-eb peek-eb ^DocAttributeValueEntityEntityIndexState peek-state]
@@ -112,7 +115,7 @@
       (let [value+prefix-iterator (attribute-value-value+prefix-iterator i value-entity-value-idx attr prefix-eb)
             value (.value value+prefix-iterator)
             i (.prefix-iterator value+prefix-iterator)]
-        (when-let [k (->> (c/encode-avec-key-to
+        (when-let [k (->> (c/encode-ave-key-to
                            (.get seek-buffer-tl)
                            (c/->id-buffer attr)
                            value
@@ -340,7 +343,7 @@
                v (vectorize-value v)
                :let [v (c/->value-buffer v)]
                :when (pos? (mem/capacity v))]
-           [(c/encode-avec-key-to nil k v id content-hash)
+           [(c/encode-ave-key-to nil k v id)
             (c/encode-aecv-key-to nil k id content-hash v)])
          (apply concat))))
 