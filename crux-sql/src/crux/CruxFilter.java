c6092e30e5f3473b76af2dbef03d2f525e57c02b
Jon Pither <jon.pither@gmail.com>
2020-04-18 18:10:45 +0100

    Bespoke Rules

---
+++ crux-sql/src/crux/CruxFilter.java
@@ -0,0 +1,25 @@
+package crux.calcite;
+
+import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelOptPlanner;
+import org.apache.calcite.plan.RelTraitSet;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.core.Filter;
+import org.apache.calcite.rex.RexNode;
+
+public class CruxFilter extends Filter implements CruxRel {
+    CruxFilter(RelOptCluster cluster, RelTraitSet traitSet, RelNode child, RexNode condition) {
+        super(cluster, traitSet, child, condition);
+        assert getConvention() == CruxRel.CONVENTION;
+        assert getConvention() == child.getConvention();
+    }
+
+    @Override public Filter copy(RelTraitSet relTraitSet, RelNode input, RexNode condition) {
+        return new CruxFilter(getCluster(), relTraitSet, input, condition);
+    }
+
+    @Override public void implement(Implementor implementor) {
+        implementor.visitChild(0, getInput());
+        implementor.add(CruxUtils.resolve("crux.calcite/filter->clause").invoke(condition));
+    }
+}
--- crux-sql/src/crux/CruxRel.java
+++ crux-sql/src/crux/CruxRel.java
@@ -4,20 +4,22 @@ import org.apache.calcite.plan.Convention;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.plan.RelOptTable;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public interface CruxRel extends RelNode {
     void implement(Implementor implementor);
 
     Convention CONVENTION = new Convention.Impl("CRUX", CruxRel.class);
 
     class Implementor {
-        //        final List<Pair<String, String>> list = new ArrayList<>();
-
         RelOptTable table;
         CruxTable cruxTable;
+        final List<Object> clauses = new ArrayList<>();
 
-        // public void add(String findOp, String aggOp) {
-        //     list.add(Pair.of(findOp, aggOp));
-        // }
+        public void add(Object clause) {
+            clauses.add(clause);
+        }
 
         public void visitChild(int ordinal, RelNode input) {
             assert ordinal == 0;
