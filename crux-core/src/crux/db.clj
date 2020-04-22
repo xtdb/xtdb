0a590b72d11346d2703bb4ece78428112289df15
Håkan Råberg <hakan.raberg@gmail.com>
2020-04-22 15:27:57 +0200

    Start of IndexStore protocol.

--- crux-core/src/crux/db.clj
+++ crux-core/src/crux/db.clj
@@ -19,8 +19,41 @@
   (index-tx [this tx tx-events])
   (store-index-meta [this k v])
   (read-index-meta [this k])
-  (latest-completed-tx [this]))
+  (latest-completed-tx [this])
+  (open-index-store ^java.io.Closeable [this]))
 ;; end::Indexer[]
+
+;; tag::IndexStore[]
+(defprotocol IndexStore
+  ;; AVE
+  (new-doc-attribute-value-entity-value-index [this a])
+  ;; this way of passing other indexes into each other could be hidden
+  ;; by having a function that creates both.
+  (new-doc-attribute-value-entity-entity-index [this a v-doc-idx entity-as-of-idx])
+  ;; AEV
+  (new-doc-attribute-entity-value-entity-index [this a entity-as-of-idx])
+  (new-doc-attribute-entity-value-value-index [this a e-doc-idx])
+
+  ;; optimisation, not strictly necessary, can be implemented in other ways
+  (or-known-triple-fast-path [this eid a v valid-time transact-time])
+
+  ;; bitemporal index
+  (new-entity-as-of-index [this valid-time transact-time])
+
+  ;; can be rewritten in terms of the above
+  ;; (idx/entity-at entity-as-of-idx value)
+  ;; (idx/entities-at this [eid] valid-time transact-time)
+
+  ;; history seqs, could be redone as lower-level indexes
+
+  ;; valid time history
+  (entity-history-seq-ascending [this eid valid-time transact-time])
+  (entity-history-seq-descending [this eid valid-time transact-time])
+
+  ;; full history
+  (entity-history [this eid])
+  (entity-history-range [this eid valid-time-start transaction-time-start valid-time-end transaction-time-end]))
+;; end::IndexStore[]
 
 ;; tag::TxLog[]
 (defprotocol TxLog
