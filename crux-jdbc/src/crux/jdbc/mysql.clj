142afee3f37b21199b84beee3d8f688f67d88eb3
Paweł Wilk <pw@gnu.org>
2020-04-23 19:56:02 +0200

    Changed create table query for mysql
    CURRENT_TIMESTAMP without precision is a syntax error when DATETIME has precision defined (since MySQL 5.7.x).

--- crux-jdbc/src/crux/jdbc/mysql.clj
+++ crux-jdbc/src/crux/jdbc/mysql.clj
@@ -5,7 +5,7 @@
 (defmethod j/setup-schema! :mysql [_ ds]
   (jdbc/execute! ds ["create table if not exists tx_events (
   event_offset int auto_increment PRIMARY KEY, event_key VARCHAR(255),
-  tx_time datetime(3) default CURRENT_TIMESTAMP, topic VARCHAR(255) NOT NULL,
+  tx_time datetime(3) default CURRENT_TIMESTAMP(3), topic VARCHAR(255) NOT NULL,
   v BLOB NOT NULL, compacted INTEGER NOT NULL)"])
   (jdbc/execute! ds ["create index if not exists tx_events_event_key_idx on tx_events(compacted, event_key)"]))
 
