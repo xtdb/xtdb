c278e503f101dc8312918c8eeeb201890b925038
Paweł Wilk <pw@gnu.org>
2020-04-23 21:22:22 +0200

    Added MySQL-compliant index creation on tx_events
    In MySQL the syntax "create index ... if not exists" is not supported.

--- crux-jdbc/src/crux/jdbc/mysql.clj
+++ crux-jdbc/src/crux/jdbc/mysql.clj
@@ -7,7 +7,11 @@
   event_offset int auto_increment PRIMARY KEY, event_key VARCHAR(255),
   tx_time datetime(3) default CURRENT_TIMESTAMP(3), topic VARCHAR(255) NOT NULL,
   v BLOB NOT NULL, compacted INTEGER NOT NULL)"])
-  (jdbc/execute! ds ["create index if not exists tx_events_event_key_idx on tx_events(compacted, event_key)"]))
+  (when (= 0
+           (:IdxPresent
+            (first (jdbc/execute! ds ["SELECT COUNT(1) IdxPresent FROM INFORMATION_SCHEMA.STATISTICS
+                                      WHERE table_schema=DATABASE() AND table_name='tx_events' AND index_name='tx_events_event_key_idx'"]))))
+    (jdbc/execute! ds ["create index tx_events_event_key_idx on tx_events(compacted, event_key)"])))
 
 (defmethod j/->pool-options :mysql [_ {:keys [username user] :as options}]
   (assoc options :username (or username user)))
