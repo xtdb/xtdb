6e787db725f60b1fb32d66ca5331cd6cf391e54f
Håkan Råberg <hakan.raberg@gmail.com>
2020-04-25 13:51:27 +0200

    Not sure why tools.reader has been added as a real dependency in crux-core

--- crux-core/project.clj
+++ crux-core/project.clj
@@ -9,11 +9,11 @@
                  [org.clojure/spec.alpha "0.2.176"]
                  [com.stuartsierra/dependency "0.2.0"]
                  [com.taoensso/nippy "2.14.0"]
-                 [org.clojure/tools.reader "1.3.2"]
                  [com.taoensso/encore "2.114.0"]
                  [org.agrona/agrona "1.0.7"]
                  [com.github.jnr/jnr-ffi "2.1.9" :scope "provided"]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}}
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]
+                                  [org.clojure/tools.reader "1.3.2"]]}}
   :middleware [leiningen.project-version/middleware]
   :java-source-paths ["src"]
   :javac-options ["-source" "8" "-target" "8"
