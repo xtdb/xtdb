a3eb4d382af53d45cfea97de13a2792468d1a193
Jon Pither <jon.pither@gmail.com>
2020-04-27 09:59:52 +0100

    BLOG -> LONGBLOB for mysql #816

--- crux-jdbc/src/crux/jdbc/mysql.clj
+++ crux-jdbc/src/crux/jdbc/mysql.clj
@@ -6,7 +6,7 @@
   (jdbc/execute! ds ["create table if not exists tx_events (
   event_offset int auto_increment PRIMARY KEY, event_key VARCHAR(255),
   tx_time datetime(3) default CURRENT_TIMESTAMP(3), topic VARCHAR(255) NOT NULL,
-  v BLOB NOT NULL, compacted INTEGER NOT NULL)"])
+  v LONGBLOB NOT NULL, compacted INTEGER NOT NULL)"])
   (when (zero? (-> (jdbc/execute! ds ["SELECT COUNT(1) IdxPresent FROM INFORMATION_SCHEMA.STATISTICS
                                       WHERE table_schema=DATABASE() AND table_name='tx_events' AND index_name='tx_events_event_key_idx'"])
                    first
