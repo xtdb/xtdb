ff5cbe2357e580ef5792abb9dfbd6bf37b28d4aa
Lucio D'Alessandro <lda@juxt.pro>
2020-04-28 17:08:50 +0100

    Add crux table in ui

--- crux-http-server/src/crux/ui/subscriptions.cljs
+++ crux-http-server/src/crux/ui/subscriptions.cljs
@@ -5,4 +5,20 @@
 (rf/reg-sub
  ::metadata
  (fn [db _]
-   (:metadata db)))
+   (let [{:strs [query-results find-clause linked-entities]}
+         (:metadata db)
+         columns (map (fn [column]
+                        {:column-key column
+                         :column-name (str column)
+                         :render-fn
+                         (fn [_ v]
+                           (if-let [link (get linked-entities v)]
+                             [:a {:href link} v]
+                             v))
+                         :render-only #{:filter :sort}})
+                      find-clause)
+         rows (map #(zipmap find-clause %) query-results)]
+     {:columns columns
+      :rows rows
+      :loading? false
+      :filters {:input (into #{} find-clause)}})))
--- crux-http-server/src/crux/ui/views.cljs
+++ crux-http-server/src/crux/ui/views.cljs
@@ -4,11 +4,13 @@
    [crux.ui.events :as events]
    [crux.ui.subscriptions :as subs]
    [reagent.core :as r]
+   [crux.uikit.table :as table]
    [re-frame.core :as rf]))
 
 (defn view []
   (let [metadata @(rf/subscribe [::subs/metadata])]
-    [:div
-     [:pre
+    [:div.container
+     [table/table metadata]
+     #_[:pre
       (with-out-str
         (pprint/pprint metadata))]]))
