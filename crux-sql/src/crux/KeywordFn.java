8f35ab725fcf9b501cffa297a648873de97849b1
Jon Pither <jon.pither@gmail.com>
2020-05-01 10:43:39 +0100

    CRUXID->KEYWORD

--- crux-sql/src/crux/KeywordFn.java
+++ crux-sql/src/crux/KeywordFn.java
@@ -3,8 +3,8 @@ package crux.calcite;
 import org.apache.calcite.linq4j.function.Parameter;
 import clojure.lang.Keyword;
 
-public class CruxIdFn {
-    public String eval (String x) {
-        return ":" + x;
+public class KeywordFn {
+    public Keyword eval (String x) {
+        return Keyword.intern(x);
     }
 }
