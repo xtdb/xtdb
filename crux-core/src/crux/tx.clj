34f4aef28a84c639dacb333d969c1b51c97d0966
James Henderson <james@jarohen.me.uk>
2020-05-13 16:24:46 +0100

    remove :crux.tx/put pre-commit-fn sanity check

--- crux-core/src/crux/tx.clj
+++ crux-core/src/crux/tx.clj
@@ -191,18 +191,8 @@
 
            (map ->new-entity-tx)))))
 
-(defmethod index-tx-event :crux.tx/put [[op k v start-valid-time end-valid-time] tx {:keys [object-store indexer] :as tx-consumer}]
-  ;; This check shouldn't be required, under normal operation - the ingester checks for this before indexing
-  ;; keeping this around _just in case_ - e.g. if we're refactoring the ingest code
-  {:pre-commit-fn (fn []
-                    (let [content-hash (c/new-id v)]
-                      (with-open [index-store (db/open-index-store indexer)]
-                        (assert (db/get-document index-store content-hash)
-                                (format "Put, incorrect doc state for: '%s', tx-id '%s'"
-                                        content-hash (:crux.tx/tx-id tx))))
-                      true))
-
-   :etxs (put-delete-etxs k start-valid-time end-valid-time (c/new-id v) tx tx-consumer)})
+(defmethod index-tx-event :crux.tx/put [[op k v start-valid-time end-valid-time] tx tx-consumer]
+  {:etxs (put-delete-etxs k start-valid-time end-valid-time (c/new-id v) tx tx-consumer)})
 
 (defmethod index-tx-event :crux.tx/delete [[op k start-valid-time end-valid-time] tx tx-consumer]
   {:etxs (put-delete-etxs k start-valid-time end-valid-time nil tx tx-consumer)})
