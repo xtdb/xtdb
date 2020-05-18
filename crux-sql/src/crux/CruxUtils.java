7214102dc3ff88d49129f4c3cd6464dd089cb4a4
Jon Pither <jon.pither@gmail.com>
2020-05-18 20:00:07 +0100

    hints

--- crux-sql/src/crux/CruxUtils.java
+++ crux-sql/src/crux/CruxUtils.java
@@ -2,6 +2,8 @@ package crux.calcite;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
+import org.apache.calcite.linq4j.tree.Expressions;
+import org.apache.calcite.linq4j.tree.Expression;
 
 public class CruxUtils {
     private static IFn requiringResolve = Clojure.var("clojure.core/requiring-resolve");
@@ -10,6 +12,10 @@ public class CruxUtils {
         IFn fn = (IFn) requiringResolve.invoke(Clojure.read(symbolName));
         IFn helper = (IFn) requiringResolve.invoke(Clojure.read("crux.calcite/clojure-helper-fn"));
         return (IFn) helper.invoke(fn);
+    }
+
+    public static Expression lambda(java.lang.reflect.Method m, Expression[] args) {
+        return Expressions.call(m, args);
     }
 
     public static Object invokeStaticMethod(java.lang.reflect.Method m, Object[] args) {
