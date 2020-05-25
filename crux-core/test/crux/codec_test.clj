37cd37a72769e63094d4d79f7cb5d89fa2edb3fa
Håkan Råberg <hakan.raberg@gmail.com>
2020-05-25 13:52:53 +0200

    Redoing boolean and char values branch based on master-1.9.

--- crux-core/test/crux/codec_test.clj
+++ crux-core/test/crux/codec_test.clj
@@ -72,14 +72,14 @@
                                 gen/double
                                 (gen/fmap #(Date. (long %)) gen/large-integer)
                                 gen/string
+                                gen/char
                                 gen/boolean])]
                 (let [buffer (c/->value-buffer v)]
                   (if (c/can-decode-value-buffer? buffer)
-                    (t/is (if (and (double? v) (Double/isNaN v))
-                            (Double/isNaN (c/decode-value-buffer buffer))
-                            (= v (c/decode-value-buffer buffer)))
-                          (str (pr-str v) " " (class v)))
-                    (t/is (and (string? v)
-                               (> (count v) @#'c/max-string-index-length)
-                               (= @#'c/object-value-type-id
-                                  (.getByte (c/value-buffer-type-id buffer) 0))))))))
+                    (if (and (double? v) (Double/isNaN v))
+                      (Double/isNaN (c/decode-value-buffer buffer))
+                      (= v (c/decode-value-buffer buffer)))
+                    (and (string? v)
+                         (> (count v) @#'c/max-string-index-length)
+                         (= @#'c/object-value-type-id
+                            (.getByte (c/value-buffer-type-id buffer) 0)))))))
