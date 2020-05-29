457b346aa5d3e78e7d98e689b57708e91562c982
Håkan Råberg <hakan.raberg@gmail.com>
2020-05-29 17:28:42 +0200

    A rough way of minimising iterator reuse, using a smaller 'cache' of delayed iterators to be reused in the KvIndexStore, but does add coupling between the methods as one can only act on one layer at a time. The join engine adheres to this.

--- crux-core/src/crux/index/BinaryJoinLayeredVirtualIndexPeekState.java
+++ crux-core/src/crux/index/BinaryJoinLayeredVirtualIndexPeekState.java
@@ -3,11 +3,9 @@ package crux.index;
 public class BinaryJoinLayeredVirtualIndexPeekState {
     public Object seq;
     public Object key;
-    public Object nestedIndexStore;
 
-    public BinaryJoinLayeredVirtualIndexPeekState(Object seq, Object key, Object nestedIndexStore) {
+    public BinaryJoinLayeredVirtualIndexPeekState(Object seq, Object key) {
         this.seq = seq;
         this.key = key;
-        this.nestedIndexStore = nestedIndexStore;
     }
 }
