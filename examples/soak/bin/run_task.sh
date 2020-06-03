d31788c8f378c74a101b754b6b559cddb547aec2
James Henderson <james@jarohen.me.uk>
2020-06-03 17:33:10 +0100

    soak cloudformation uses crux-cloud resources

--- examples/soak/bin/run_task.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-aws ecs run-task\
- --task-definition crux-soak\
- --cluster crux-soak\
- --launch-type FARGATE\
- --count 1\
- --network-configuration \
- "awsvpcConfiguration={subnets=[subnet-5140ba2b],assignPublicIp=\"ENABLED\"}"\
- --output json
---
+++ examples/soak/cloudformation/update-stack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+
+aws s3 sync --delete --exclude '*.sh' $(dirname $0) s3://crux-cloudformation/crux-soak/
+
+aws cloudformation update-stack \
+    --capabilities CAPABILITY_IAM \
+    --template-url https://crux-cloudformation.s3-eu-west-1.amazonaws.com/crux-soak/crux-soak.yml \
+    --stack-name crux-soak
