967dc6da9dcec9d6f19cde771bf266d540c7c683
James Henderson <james@jarohen.me.uk>
2020-06-03 18:11:58 +0100

    juxt:team tag

--- crux-cloud/update-stack.sh
+++ crux-cloud/update-stack.sh
@@ -7,4 +7,5 @@ aws s3 sync --delete --exclude '*.sh' $(dirname $0) s3://crux-cloudformation/cru
 aws cloudformation update-stack \
     --capabilities CAPABILITY_IAM \
     --template-url https://crux-cloudformation.s3-eu-west-1.amazonaws.com/crux-cloud/crux-cloud.yml \
-    --stack-name crux-cloud
+    --stack-name crux-cloud \
+    --tags Key=juxt:team,Value=crux-core
--- examples/soak/cloudformation/update-stack.sh
+++ examples/soak/cloudformation/update-stack.sh
@@ -7,4 +7,5 @@ aws s3 sync --delete --exclude '*.sh' $(dirname $0) s3://crux-cloudformation/cru
 aws cloudformation update-stack \
     --capabilities CAPABILITY_IAM \
     --template-url https://crux-cloudformation.s3-eu-west-1.amazonaws.com/crux-soak/crux-soak.yml \
-    --stack-name crux-soak
+    --stack-name crux-soak \
+    --tags Key=juxt:team,Value=crux-core
