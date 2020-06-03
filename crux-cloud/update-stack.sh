08c876e3c59bccf4335b964eccb208072ffe2ea7
James Henderson <james@jarohen.me.uk>
2020-06-03 16:43:05 +0100

    crux-cloud nested stack

---
+++ crux-cloud/update-stack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+
+aws s3 sync --delete --exclude '*.sh' $(dirname $0) s3://crux-cloudformation/crux-cloud/
+
+aws cloudformation update-stack \
+    --capabilities CAPABILITY_IAM \
+    --template-url https://crux-cloudformation.s3-eu-west-1.amazonaws.com/crux-cloud/crux-cloud.yml \
+    --stack-name crux-cloud
