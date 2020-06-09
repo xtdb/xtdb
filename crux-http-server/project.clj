5466ba9e6da09162e86279d7cbdb85134433b28f
Daniel Mason <dan@juxt.pro>
2020-06-09 11:17:52 +0100

    Add CSV/TSV handling to /_crux/query

--- crux-http-server/project.clj
+++ crux-http-server/project.clj
@@ -4,6 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.csv "1.0.0"]
                  [hiccup "2.0.0-alpha2"]
                  [juxt/crux-core "derived-from-git"]
                  [metosin/reitit "0.4.2"]
