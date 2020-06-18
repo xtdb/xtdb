9d8aa082fa7bc7da8721b8384dc399c69432836a
Håkan Råberg <hakan.raberg@gmail.com>
2020-06-18 16:15:31 +0200

    Sort collections as well for consistency.

--- crux-core/src/crux/codec.clj
+++ crux-core/src/crux/codec.clj
@@ -394,6 +394,11 @@
         (id->buffer id to)
         (id-function to (nippy/fast-freeze this)))))
 
+  Collection
+  (id->buffer [this to]
+    (id-function to (binding [*sort-unordered-colls* true]
+                      (nippy/fast-freeze this))))
+
   Map
   (id->buffer [this to]
     (id-function to (binding [*sort-unordered-colls* true]
