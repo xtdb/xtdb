5bb0561da287444e7e72d7d27b2409c43739e8e0
Håkan Råberg <hakan.raberg@gmail.com>
2020-06-18 20:33:18 +0200

    Missed upgrading index-version in test.

--- crux-test/test/crux/api_test.clj
+++ crux-test/test/crux/api_test.clj
@@ -90,7 +90,7 @@
     (t/is (nil? (api/sync *api* (Duration/ofSeconds 10))))))
 
 (t/deftest test-status
-  (t/is (= (merge {:crux.index/index-version 8}
+  (t/is (= (merge {:crux.index/index-version 9}
                   (when (instance? crux.kafka.KafkaTxLog (:tx-log *api*))
                     {:crux.zk/zk-active? true}))
            (select-keys (api/status *api*) [:crux.index/index-version :crux.zk/zk-active?])))
