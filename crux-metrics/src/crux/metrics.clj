245245a924bcff6de48ad48092240c96767bce14
Daniel Mason <dan@juxt.pro>
2020-07-09 17:12:27 +0100

    Add meters/timers into returned crux.metrics map.

--- crux-metrics/src/crux/metrics.clj
+++ crux-metrics/src/crux/metrics.clj
@@ -5,7 +5,7 @@
             [crux.status :as status])
   (:import [java.time Duration]
            [java.util.concurrent TimeUnit]
-           [com.codahale.metrics MetricRegistry Gauge]))
+           [com.codahale.metrics MetricRegistry Gauge Meter Timer]))
 
 (def registry-module {:start-fn (fn [deps {::keys [with-indexer-metrics?  with-query-metrics?]}]
                                   (cond-> (dropwizard/new-registry)
@@ -22,11 +22,25 @@
 (defrecord StatusReporter [^MetricRegistry registry]
   status/Status
   (status-map [this]
-    (into {}
-          (map
-           (fn [[^String name ^Gauge gauge]]
-             [(keyword name) (.getValue gauge)])
-           (.getGauges registry)))))
+    {:crux.metrics
+     (into (sorted-map)
+           (concat
+            (map
+             (fn [[^String name ^Gauge gauge]]
+               {name (.getValue gauge)})
+             (.getGauges registry))
+            (mapcat
+             (fn [[^String name ^Meter meter]]
+               {(str name "/1-min-rate") (.getOneMinuteRate meter)
+                (str name "/5-min-rate") (.getFiveMinuteRate meter)
+                (str name "/15-min-rate") (.getFifteenMinuteRate meter)})
+             (.getMeters registry))
+            (mapcat
+             (fn [[^String name ^Timer timer]]
+               {(str name "/1-min-rate") (.getOneMinuteRate timer)
+                (str name "/5-min-rate") (.getFiveMinuteRate timer)
+                (str name "/15-min-rate") (.getFifteenMinuteRate timer)})
+             (.getTimers registry))))}))
 
 (def all-metrics-loaded {:start-fn (fn [_ _])
                          :deps #{::registry}})
