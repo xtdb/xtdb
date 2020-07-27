e29dc06c5b4b455513da38b22e21fffece633309
Jeremy Taylor <jdt@juxt.pro>
2020-07-27 10:28:19 +0100

    Add failing test - tx-fn should see in-tx query results

--- crux-test/test/crux/tx_test.clj
+++ crux-test/test/crux/tx_test.clj
@@ -674,6 +674,34 @@
                       :crux.tx/current-tx submitted-tx}
                      (api/entity (api/db *api*) :tx-metadata)))))))))
 
+(t/deftest tx-fn-sees-in-tx-query-results
+  (fix/submit+await-tx [[:crux.tx/put {:crux.db/id :foo, :foo 1}]])
+  (let [tx (fix/submit+await-tx [[:crux.tx/put {:crux.db/id :foo, :foo 2}]
+                                 [:crux.tx/put {:crux.db/id :put
+                                                :crux.db/fn '(fn [ctx doc]
+                                                               [[:crux.tx/put doc]])}]
+                                 [:crux.tx/fn :put {:crux.db/id :bar, :ref :foo}]
+                                 [:crux.tx/put {:crux.db/id :doubling-fn
+                                                :crux.db/fn '(fn [ctx]
+                                                               (let [db (crux.api/db ctx)]
+
+                                                                 [[:crux.tx/put {:crux.db/id :prn-out
+                                                                                 :e (crux.api/entity db :bar)
+                                                                                 :q (first (crux.api/q db {:find '[e v]
+                                                                                                           :where '[[e :crux.db/id :bar]
+                                                                                                                    [e :ref v]]}))}]
+                                                                  [:crux.tx/put (-> (crux.api/entity db :foo)
+                                                                                    (update :foo * 2))]]))}]
+                                 [:crux.tx/fn :doubling-fn]])]
+
+    (t/is (crux/tx-committed? *api* tx))
+    (t/is (= {:crux.db/id :foo, :foo 4}
+             (crux/entity (crux/db *api*) :foo)))
+    (t/is (= {:crux.db/id :prn-out
+              :e {:crux.db/id :bar :ref :foo}
+              :q [:bar :foo]}
+             (crux/entity (crux/db *api*) :prn-out)))))
+
 (t/deftest tx-log-evict-454 []
   (fix/submit+await-tx [[:crux.tx/put {:crux.db/id :to-evict}]])
   (fix/submit+await-tx [[:crux.tx/cas {:crux.db/id :to-evict} {:crux.db/id :to-evict :test "test"}]])
