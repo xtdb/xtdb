5ca5fdd3dea373ec7ef1abd7c4fac5a31c23a780
Daniel Mason <dan@juxt.pro>
2020-08-06 10:14:33 +0100

    Add tests for `current-queries`

--- crux-test/test/crux/api_test.clj
+++ crux-test/test/crux/api_test.clj
@@ -85,6 +85,7 @@
       (t/is (= #{[:ivan]} (api/q (api/db *api*)
                                  '{:find [e]
                                    :where [[e :name "Ivan"]]})))
+
       (t/is (= #{} (api/q (api/db *api* #inst "1999")
                           '{:find [e]
                             :where [[e :name "Ivan"]]})))
@@ -437,3 +438,39 @@
                    (-> (db/fetch-docs (:document-store *server-api*) #{arg-doc-id})
                        (get arg-doc-id)
                        (dissoc :crux.db/id)))))))))
+
+(t/deftest test-current-queries
+  (let [submitted-tx (api/submit-tx *api* [[:crux.tx/put {:crux.db/id :ivan :name "Ivan"}]])]
+    (t/is (= submitted-tx (api/await-tx *api* submitted-tx)))
+    (t/is (true? (api/tx-committed? *api* submitted-tx)))
+    (let [db (api/db *api*)]
+      (t/is (api/q (api/db *api*)
+                   '{:find [e]
+                     :where [[e :name "Ivan"]]}))
+
+      (t/testing "test current-query - post successful query"
+        (let [current-queries (api/current-queries *api*)]
+          (t/is (= :completed (:status (first current-queries))))))
+
+      (t/is (thrown-with-msg? Exception
+                              #"Find refers to unknown variable: f"
+                              (api/q db '{:find [f], :where [[e :crux.db/id _]]})))
+
+      (t/testing "test current-query - post failed query"
+        (let [malformed-query (first (api/current-queries *api*))]
+          (t/is (= '{:find [f], :where [[e :crux.db/id _]]} (:query malformed-query)))
+          (t/is (= :failed (:status malformed-query)))))
+
+      (t/testing "test current-query - streaming query"
+        (with-open [res (api/open-q db '{:find [e]
+                                         :where [[e :name "Ivan"]]})]
+
+          (t/testing "test current-query - streaming query (not realized)"
+            (let [streaming-query (first (api/current-queries *api*))]
+              (= '{:find [e] :where [[e :name "Ivan"]]} (:query streaming-query))
+              (= :in-progress (:status streaming-query))))
+
+          (t/is (iterator-seq res))
+
+          (t/testing "test current-query - streaming query (result realized)"
+            (= :complete (:status (first (api/current-queries *api*))))))))))
