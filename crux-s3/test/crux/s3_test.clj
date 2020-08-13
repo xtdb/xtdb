6f3d937b4036a6348826fe33ab8909c512c78905
James Henderson <james@jarohen.me.uk>
2020-08-13 10:17:36 +0100

    checkpoint tests

--- crux-s3/test/crux/s3_test.clj
+++ crux-s3/test/crux/s3_test.clj
@@ -1,27 +1,32 @@
 (ns crux.s3-test
-  (:require [crux.s3 :as s3]
-            [clojure.test :as t]
+  (:require [clojure.test :as t]
             [crux.doc-store-test :as dst]
+            [crux.s3 :as s3]
             [crux.system :as sys])
-  (:import (java.util UUID)
-           (crux.s3 S3Configurator)
-           (software.amazon.awssdk.regions Region)
-           (software.amazon.awssdk.services.s3 S3AsyncClient)
-           (software.amazon.awssdk.services.s3.model GetObjectRequest PutObjectRequest)))
+  (:import crux.s3.S3Configurator
+           java.util.UUID
+           software.amazon.awssdk.regions.Region
+           software.amazon.awssdk.services.s3.S3AsyncClient))
 
 (def test-s3-bucket
   (or (System/getProperty "crux.s3.test-bucket")
       (System/getenv "CRUX_S3_TEST_BUCKET")))
 
+(def test-s3-region
+  (or (System/getProperty "crux.s3.test-region")
+      (System/getenv "CRUX_S3_TEST_REGION")))
+
 (def ^:dynamic ^S3AsyncClient *client*)
 
-(t/use-fixtures :once
-  (fn [f]
-    (when test-s3-bucket
-      (binding [*client* (-> (S3AsyncClient/builder)
-                             (.region (Region/EU_WEST_2))
-                             (.build))]
+(defn with-s3-client [f]
+  (when test-s3-bucket
+    (let [builder (S3AsyncClient/builder)
+          _ (when test-s3-region
+              (.region builder (Region/of test-s3-region)))]
+      (binding [*client* (.build builder)]
         (f)))))
+
+(t/use-fixtures :once with-s3-client)
 
 (defn ->configurator [_]
   (reify S3Configurator
