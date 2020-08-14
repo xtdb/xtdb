561983d240185efb61c09d69f6f7017d55e643d6
Daniel Mason <dan@juxt.pro>
2020-08-14 16:36:44 +0100

    Remove " from server titles.

--- crux-http-server/src/crux/http_server/util.clj
+++ crux-http-server/src/crux/http_server/util.clj
@@ -56,8 +56,7 @@
               [:a {:href "/_crux/index"}
                [:img.crux-logo__img {:src "/crux-horizontal-bw.svg.png" }]]]
              [:span.mobile-hidden
-              [:b (when-let [label (get-in options [:node-options :crux.http-server/label])]
-                    (format "\"%s\"" label))]]
+              [:b (when-let [label (get-in options [:node-options :crux.http-server/label])] label)]]
              [:div.header__links
               [:a.header__link {:href "/_crux/query"} "Query"]
               [:a.header__link {:href "/_crux/status"} "Status"]
