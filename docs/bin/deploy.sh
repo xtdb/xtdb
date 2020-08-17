559fc984124bbbf5c6a91b68bab79f125dd8d1f0
James Henderson <james@jarohen.me.uk>
2020-08-17 15:23:56 +0100

    deploy docs to personal staging area by default (`./bin/deploy.sh /` for live deploy)

--- docs/bin/deploy.sh
+++ docs/bin/deploy.sh
@@ -2,6 +2,9 @@
 set -x
 set -e
 
+PREFIX=${1:-"/_$(whoami)"}
+echo Deploying to https://opencrux.com$PREFIX
+
 cd $(dirname "$0")/..
-aws s3 sync --delete build/site s3://opencrux-docs --cache-control no-cache
+aws s3 sync --delete build/site s3://opencrux-docs$PREFIX --cache-control no-cache
 cd -
