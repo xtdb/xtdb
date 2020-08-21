90a5ffdc2c5625c204dbd5b6681193fd21bcaade
User <jon@juxt.pro>
2020-08-21 10:18:57 +0100

    Pred-fn wiring

--- crux-lucene/src/crux/lucene.clj
+++ crux-lucene/src/crux/lucene.clj
@@ -1,14 +1,33 @@
 (ns crux.lucene
-  (:import [org.apache.lucene.store Directory FSDirectory]))
+  (:require [crux.io :as cio])
+  (:import java.io.Closeable
+           org.apache.lucene.analysis.Analyzer
+           org.apache.lucene.analysis.standard.StandardAnalyzer
+           org.apache.lucene.index.DirectoryReader
+           org.apache.lucene.queryparser.classic.QueryParser
+           [org.apache.lucene.search IndexSearcher ScoreDoc]
+           [org.apache.lucene.store Directory FSDirectory]))
 
-(defrecord LuceneNode [directory]
+(defrecord LuceneNode [directory analyzer]
   java.io.Closeable
   (close [this]
-    (.close ^Directory directory)))
+    (doseq [^Closeable c [directory]]
+      (.close c))))
+
+(defn search [node, k, v]
+  (let [{:keys [^Analyzer analyzer ^Directory directory]} (:crux.lucene/node (:crux.node/topology (meta node)))
+        directory-reader (DirectoryReader/open directory)
+        index-searcher (IndexSearcher. directory-reader)
+        qp (QueryParser. k analyzer)
+        q (.parse qp v)
+        score-docs (.-scoreDocs (.search index-searcher q 10))]
+    (cio/->cursor (fn []
+                    (.close directory-reader))
+                  (map #(.doc index-searcher (.-doc ^ScoreDoc %)) score-docs))))
 
 (defn- start-lucene-node [_ {::keys [db-dir]}]
   (let [directory (FSDirectory/open db-dir)]
-    (LuceneNode. directory)))
+    (LuceneNode. directory (StandardAnalyzer.))))
 
 (def module {::node {:start-fn start-lucene-node
                      :args {::db-dir {:doc "Lucene DB Dir"
