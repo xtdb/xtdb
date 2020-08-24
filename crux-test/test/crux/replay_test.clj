fcb521044e50990a45e0263a66fe05cc6d2b84eb
James Henderson <james@jarohen.me.uk>
2020-08-24 15:33:17 +0100

    fix batching in standalone's open-tx-log, fixes #1052

--- crux-test/test/crux/replay_test.clj
+++ crux-test/test/crux/replay_test.clj
@@ -1,13 +1,7 @@
 (ns crux.replay-test
   (:require [clojure.test :as t]
-            [crux.fixtures :refer [*api*] :as fix]
-            [crux.fixtures.kafka :as fk]
-            crux.jdbc
-            [crux.fixtures.jdbc :as fj]
-            [crux.fixtures.http-server :as fh]
             [crux.api :as crux]
-            [crux.fixtures :as f]
-            [clojure.java.io :as io])
+            [crux.fixtures :as fix])
   (:import java.time.Duration))
 
 (t/deftest drop-db
@@ -31,3 +25,28 @@
         (t/is (crux/sync node (Duration/ofSeconds 2)))
         (t/is (= {:crux.db/id :hello}
                  (crux/entity (crux/db node) :hello)))))))
+
+(t/deftest test-more-txs
+  (let [n 1000]
+    (fix/with-tmp-dir "event-log-dir" [event-log-dir]
+      (fix/with-tmp-dir "db-dir-1" [db-dir-1]
+        (with-open [node (crux/start-node {:crux.node/topology '[crux.standalone/topology
+                                                                 crux.kv.rocksdb/kv-store]
+                                           :crux.kv/db-dir (str db-dir-1)
+                                           :crux.standalone/event-log-dir (str event-log-dir)
+                                           :crux.standalone/event-log-kv-store 'crux.kv.rocksdb/kv})]
+          (dotimes [x n]
+            (fix/submit+await-tx node [[:crux.tx/put {:crux.db/id (str "id-" x)}]]))))
+
+      (fix/with-tmp-dir "db-dir-2" [db-dir-2]
+        (with-open [node (crux/start-node {:crux.node/topology '[crux.standalone/topology
+                                                                 crux.kv.rocksdb/kv-store]
+                                           :crux.kv/db-dir (str db-dir-2)
+                                           :crux.standalone/event-log-dir (str event-log-dir)
+                                           :crux.standalone/event-log-kv-store 'crux.kv.rocksdb/kv})]
+          (t/is (= {:crux.tx/tx-id (dec n)}
+                   (crux/latest-submitted-tx node)))
+          (t/is (crux/sync node (Duration/ofSeconds 5)))
+          (t/is (= n
+                   (count (crux/q (crux/db node) '{:find [?e]
+                                                   :where [[?e :crux.db/id]]})))))))))
