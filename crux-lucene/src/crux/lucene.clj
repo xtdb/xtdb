e0c5eaf870dc380f8f334ffa7b2eba60f09e0e7b
User <jon@juxt.pro>
2020-09-10 09:32:48 +0100

    Tidy up

--- crux-lucene/src/crux/lucene.clj
+++ crux-lucene/src/crux/lucene.clj
@@ -1,8 +1,14 @@
 (ns crux.lucene
-  (:require [crux.io :as cio])
-  (:import java.io.Closeable
+  (:require [crux.db :as db]
+            [crux.io :as cio]
+            [crux.lucene :as l]
+            [crux.memory :as mem]
+            [crux.node :as n])
+  (:import crux.ByteUtils
+           java.io.Closeable
            org.apache.lucene.analysis.Analyzer
            org.apache.lucene.analysis.standard.StandardAnalyzer
+           org.apache.lucene.document.Document
            org.apache.lucene.index.DirectoryReader
            org.apache.lucene.queryparser.classic.QueryParser
            [org.apache.lucene.search IndexSearcher ScoreDoc]
@@ -24,6 +30,30 @@
     (cio/->cursor (fn []
                     (.close directory-reader))
                   (map #(.doc index-searcher (.-doc ^ScoreDoc %)) score-docs))))
+
+;; Potential improvements / options:
+
+;; option 2
+;; Put the C into the doc
+;; Simpler, docs don't benefit from structural sharing across Cs
+
+;; option 3
+;; Go to AVE, don't store eid or C in the index.
+;; Requires sophisticated eviction, to use unindex-eids equivalent
+
+(defn full-text [node db attr v]
+  (with-open [search-results ^crux.api.ICursor (search node (name attr) v)]
+    (let [{:keys [entity-resolver-fn index-store]} db
+          eids (keep (fn [^Document doc]
+                       (let [eid (mem/->off-heap (.-bytes (.getBinaryValue doc "eid")))
+                             v (db/encode-value index-store (.get ^Document doc (name attr)))
+                             vs-in-crux (db/aev index-store attr eid v entity-resolver-fn)]
+                         (not-empty (filter (fn [v-in-crux]
+                                              (= 0 (ByteUtils/compareBuffers v-in-crux v Integer/MAX_VALUE)))
+                                            vs-in-crux))
+                         eid))
+                     (iterator-seq search-results))]
+      (boolean (seq eids)))))
 
 (defn- start-lucene-node [_ {::keys [db-dir]}]
   (let [directory (FSDirectory/open db-dir)]
