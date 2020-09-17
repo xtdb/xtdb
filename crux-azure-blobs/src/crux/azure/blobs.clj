e3d21d6480bfc204f205a8354c0df2d0e6937e35
Henrik Mohr <luposlip@gmail.com>
2020-09-17 16:40:11 +0200

    Add Azure Blob Storage doc-store module (crux-azure-blobs) (#1072)
    * Initial commit of crux-blobs (WIP)
    
    * Updated README
    
    * Cleanup
    
    * Fix linked entities
    
    * Remove unneeded dependency
    
    * Async workaround, need to synchronize ingestion of data
    
    * Remove println
    
    * Delete wrong test ns
    
    * Confirm sync/async works via async-tx
    
    * Add blobs_test.clj
    
    * Use nippy for serialization/deserialization
    
    * Require SAS token as arg, and let test succeed when no env set
    
    * Update README regd. SAS token
    
    * Add info on how to acquire a Storage Account SAS token
    
    * Rename crux-blobs -> crux-azure-blobs
    
    * Add Azure to env. variables and properties
    
    CRUX_BLOBS* -> CRUX_AZURE_BLOBS*
    crux.blobs* -> crux.azure.blobs*
    
    * Fix failing dependencies
    
    * Refactoring for new module config (WIP)
    
    Currently getting error in crux/query_state.clj
    
    * Remove non-existing ->configurator
    
    * Fix user namespace by using default tx-log & indexer
    
    * Fix the test
    
    * Update README for new node module config
    
    * Use specs from crux.system
    
    * we'll go 'alpha' for this one for now
    
    * Updating README and adding doc page in line with recent changes to S3
    
    Co-authored-by: James Henderson <james@jarohen.me.uk>

---
+++ crux-azure-blobs/src/crux/azure/blobs.clj
@@ -0,0 +1,61 @@
+(ns crux.azure.blobs
+  (:require [clj-http.client :as http]
+            [taoensso.nippy :as nippy]
+            [crux.db :as db]
+            [crux.system :as sys]
+            [crux.document-store :as ds]
+            [crux.lru :as lru]))
+
+(defn- get-blob [sas-token storage-account container blob-name]
+  ;; TODO : ETag
+  (try
+    (-> (format "https://%s.blob.core.windows.net/%s/%s?%s" storage-account container blob-name sas-token)
+        http/get
+        :body
+        ((fn [^String s] (.getBytes s))))
+    (catch Exception _ ;; TODO : Log "not found" etc.
+      nil)))
+
+(defn- put-blob [sas-token storage-account container blob-name blob-bytes]
+  ;; TODO ETag
+  (-> (format "https://%s.blob.core.windows.net/%s/%s?%s" storage-account container blob-name sas-token)
+      (http/put {:headers {"x-ms-blob-type" "BlockBlob"}
+                 :body blob-bytes})))
+
+(defrecord AzureBlobsDocumentStore [sas-token storage-account container]
+  db/DocumentStore
+
+  (submit-docs [_ docs]
+    (->> (for [[id doc] docs]
+           (future
+             (put-blob sas-token storage-account container
+                       (str id)
+                       (nippy/freeze doc))))
+         vec
+         (run! deref)))
+
+  (fetch-docs [_ docs]
+    (reduce
+     #(if-let [doc (get-blob sas-token storage-account container (str %2))]
+        (assoc %1 %2 (nippy/thaw doc))
+        %1)
+     {}
+     docs)))
+
+(defn ->document-store {::sys/args {:sas-token {:required? true
+                                                :spec ::sys/string
+                                                :doc "Azure Blob Storage SAS Token"}
+                                    :storage-account {:required? true
+                                                      :spec ::sys/string
+                                                      :doc "Azure Storage Account Name"}
+                                    :container {:required? true,
+                                                :spec ::sys/string
+                                                :doc "Azure Blob Storage Container"}
+                                    :doc-cache-size ds/doc-cache-size-opt}}
+  [{:keys [sas-token storage-account container doc-cache-size]}]
+  (ds/->CachedDocumentStore
+   (lru/new-cache doc-cache-size)
+   (->AzureBlobsDocumentStore sas-token
+                              storage-account
+                              container)))
+
