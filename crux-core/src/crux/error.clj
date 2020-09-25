3ec83072eecd8b58b1bc95910cba43222d153ec8
James Henderson <james@jarohen.me.uk>
2020-09-25 11:18:15 +0100

    add 'cause' to crux.IAE

--- crux-core/src/crux/error.clj
+++ crux-core/src/crux/error.clj
@@ -4,9 +4,13 @@
   ([k] (illegal-arg k {}))
 
   ([k {::keys [^String message] :as data}]
+   (illegal-arg k data nil))
+
+  ([k {::keys [^String message] :as data} cause]
    (let [message (or message (format "Illegal argument: '%s'" k))]
      (crux.IllegalArgumentException. message
                                      (merge {::error-type :illegal-argument
                                              ::error-key k
                                              ::message message}
-                                            data)))))
+                                            data)
+                                     cause))))
