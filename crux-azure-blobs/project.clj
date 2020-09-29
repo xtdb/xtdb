268c523a3f098b29209b8caaeb170c75c750b965
James Henderson <james@jarohen.me.uk>
2020-09-29 09:03:19 +0100

    bump tools.logging for dependency warning

--- crux-azure-blobs/project.clj
+++ crux-azure-blobs/project.clj
@@ -1,6 +1,6 @@
 (defproject juxt/crux-azure-blobs "crux-git-version-alpha"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "1.0.0"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [juxt/crux-core "crux-git-version-beta"]
                  [clj-http "3.10.1"]]
 
