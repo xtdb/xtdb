d18edfc2db4ab66e5679d2bfafd2b53f434395d9
User <jon@juxt.pro>
2020-10-01 12:22:00 +0100

    Tests for ensuring we filter out past-data that would be included by range-searchs

--- crux-lucene/test/crux/lucene_test.clj
+++ crux-lucene/test/crux/lucene_test.clj
@@ -164,3 +164,21 @@
     (t/is (seq (c/q db {:find '[?e ?v]
                         :where '[[(text-search :name "Ivan") [[?e ?v]]]
                                  [?e :crux.db/id]]})))))
+
+(t/deftest test-past-fuzzy-results-excluded
+  (submit+await-tx [[:crux.tx/put {:crux.db/id "ivan0" :name "Ivan"}]])
+  (submit+await-tx [[:crux.tx/delete "ivan0"]])
+  (submit+await-tx [[:crux.tx/put {:crux.db/id "ivan1" :name "Ivana"}]])
+
+  (let [q {:find '[?e ?v ?s]
+           :where '[[(text-search :name "Ivan*") [[?e ?v ?s]]]
+                    [?e :crux.db/id]]}]
+    (with-open [db (c/open-db *api*)]
+      (t/is (= ["ivan1"] (map first (c/q db q)))))))
+
+(t/deftest test-exlude-future-results
+  (let [q {:find '[?e] :where '[[(text-search :name "Ivan") [[?e]]] [?e :crux.db/id]]}]
+    (submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivanka"}]])
+    (with-open [before-db (c/open-db *api*)]
+      (submit+await-tx [[:crux.tx/put {:crux.db/id :ivan :name "Ivan"}]])
+      (t/is (empty? (c/q before-db q))))))
