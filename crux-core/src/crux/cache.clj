40b711d742c303af30be58cd3b6c9769fb6d8045
Håkan Råberg <hakan.raberg@gmail.com>
2020-10-06 11:34:31 +0200

    Tweak so opts gets picked up.

--- crux-core/src/crux/cache.clj
+++ crux-core/src/crux/cache.clj
@@ -9,4 +9,9 @@
 (defn evict [^ICache cache k]
   (.evict cache k))
 
-(def ->cache lru/->lru-cache)
+(defn ->cache
+  {::sys/args {:cache-size {:doc "Cache size"
+                            :default (* 128 1024)
+                            :spec ::sys/nat-int}}}
+  [opts]
+  (lru/->lru-cache opts))
