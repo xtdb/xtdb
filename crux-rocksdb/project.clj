1d8ff5c2b130ebc9507a8d6e80fe6b503e9ab2e8
Håkan Råberg <hakan.raberg@gmail.com>
2020-11-03 15:44:00 +0100

    Upgrading to Rocks 6.12.7

--- crux-rocksdb/project.clj
+++ crux-rocksdb/project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/crux-core "crux-git-version-beta"]
                  [juxt/crux-metrics "crux-git-version-alpha" :scope "provided"]
-                 [org.rocksdb/rocksdbjni "6.8.1"]
+                 [org.rocksdb/rocksdbjni "6.12.7"]
                  [com.github.jnr/jnr-ffi "2.1.12"]]
   :middleware [leiningen.project-version/middleware]
   :pedantic? :warn)
