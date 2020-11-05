6fbd65d8ba59e6f2d6c34bf586bf9fd07dd7786d
Daniel Mason <dan@juxt.pro>
2020-11-05 14:27:04 +0000

    Combine metrics.query listeners into one listener

--- crux-metrics/src/crux/metrics/query.clj
+++ crux-metrics/src/crux/metrics/query.clj
@@ -1,25 +1,24 @@
 (ns ^:no-doc crux.metrics.query
   (:require [crux.bus :as bus]
-            [crux.node :as node]
             [crux.query :as q]
-            [crux.metrics.dropwizard :as dropwizard])
-  (:import java.util.Date))
+            [crux.metrics.dropwizard :as dropwizard]))
 
 (defn assign-listeners
   [registry {:crux/keys [bus]}]
   (let [!timer-store (atom {})
         query-timer (dropwizard/timer registry ["query" "timer"])]
-    (bus/listen bus {:crux/event-types #{:crux.query/submitted-query}}
-                (fn [event]
-                  (swap! !timer-store assoc (:crux.query/query-id event) (dropwizard/start query-timer))))
-    (bus/listen bus {:crux/event-types #{:crux.query/completed-query}}
-                (fn [event]
-                  (dropwizard/stop (get @!timer-store (:crux.query/query-id event)))
-                  (swap! !timer-store dissoc (:crux.query/query-id event))))
-    (bus/listen bus {:crux/event-types #{:crux.query/failed-query}}
-                (fn [event]
-                  (dropwizard/stop (get @!timer-store (:crux.query/query-id event)))
-                  (swap! !timer-store dissoc (:crux.query/query-id event))))
+    (bus/listen bus {:crux/event-types #{::q/submitted-query
+                                         ::q/completed-query
+                                         ::q/failed-query}}
+                (fn [{:keys [crux.query/query-id crux/event-type]}]
+                  (case event-type
+                    ::q/submitted-query
+                    (swap! !timer-store assoc query-id (dropwizard/start query-timer))
+
+                    (::q/completed-query ::q/failed-query)
+                    (do
+                      (dropwizard/stop (get @!timer-store query-id))
+                      (swap! !timer-store dissoc query-id)))))
     {:query-timer query-timer
      :current-query-count (dropwizard/gauge registry
                                             ["query" "currently-running"]
