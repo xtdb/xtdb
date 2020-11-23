88417cf28f691bf38532750b01da183e11038858
Dan Mason <danielmason978@gmail.com>
2020-11-23 22:35:37 +0000

    Docs - add second playbook for blogs (#1222)
    * Add second playbook for blogs, make script use it if specified
    
    * Add note to README about --with-blog

--- docs/bin/build.sh
+++ docs/bin/build.sh
@@ -3,17 +3,21 @@ set -x
 set -e
 
 OPTS=
+PLAYBOOK="antora-playbook.yml"
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --with-local-bundle)
             OPTS+=" --ui-bundle-url=../../crux-site/build/ui-bundle.zip"
             shift;;
+	--with-blog)
+	    PLAYBOOK="antora-playbook-blog.yml"
+	    shift;;
         *) echo "Unknown parameter passed: $1"; exit 1;;
     esac
 done
 
 (
     cd $(dirname "$0")/..
-    antora --clean --fetch $OPTS antora-playbook.yml
+    antora --clean --fetch $OPTS $PLAYBOOK
 )
