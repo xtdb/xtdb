429aeee2acb18ec99195461916c2727567dc3d77
James Henderson <james@jarohen.me.uk>
2020-12-14 17:22:22 +0000

    we no longer need tags to be correct to push snapshot releases, we just use dev-SNAPSHOT

--- crux-core/tasks/leiningen/project_version.clj
+++ crux-core/tasks/leiningen/project_version.clj
@@ -1,38 +1,38 @@
 (ns leiningen.project-version
   (:require [clojure.java.shell :as sh]
-            [clojure.string]
-            [clojure.walk :refer [postwalk]]))
+            [clojure.walk :refer [postwalk]]
+            [clojure.string :as str]))
 
 (defn project-version [project & args]
   (println (:version project)))
 
+(defn- parse-git-describe [out]
+  (when-let [[_ tag ahead dirty] (re-find #"(.*)\-(\d+)\-(?:[0-9a-z]*)(\-dirty)?$" (clojure.string/trim out))]
+    {:tag tag
+     :ahead? (not= ahead "0")
+     :dirty? (not (empty? dirty))}))
+
 (defn- version-from-git []
   (let [{:keys [exit out]} (sh/sh "git" "describe" "--tags" "--dirty" "--long" "--match" "*.*-*")]
-    (assert (= 0 exit))
-    ;;"20.06-1.9.0-22-g9452e91b-dirty"
-    (let [[_ tag ahead sha dirty] (re-find #"(.*)\-(\d+)\-([0-9a-z]*)(\-dirty)?$" (clojure.string/trim out))
-          ahead? (not= ahead "0")
-          dirty? (not (empty? dirty))]
-      (assert (re-find #"^\d+\.\d+\-\d+\.\d+\.\d+(-(alpha|beta))?$" tag) (str "Tag format unexpected: " tag))
-      (if (and (not ahead?) (not dirty?))
-        {:prefix tag}
-        (let [[_ prefix minor-version suffix] (re-find #"^(.*\.)(\d+)(-(alpha|beta))?$" tag)]
-          {:prefix (str prefix (inc (Integer/parseInt minor-version)))
-           :suffix "-SNAPSHOT"})))))
+    (assert (zero? exit))
+    (when-let [{:keys [tag ahead? dirty?]} (parse-git-describe (str/trim out))]
+      (when-not (or ahead? dirty?)
+        (assert (re-find #"^\d+\.\d+\-\d+\.\d+\.\d+(-(alpha|beta))?$" tag) (str "Tag format unexpected: " tag))
+        tag))))
 
 (def mvn-group-override
   (System/getenv "CRUX_MVN_GROUP"))
 
 (defn middleware [project]
-  (let [git-version (version-from-git)
-        prefix (or (System/getenv "CRUX_GIT_VERSION_PREFIX") (:prefix git-version))
-        suffix (or (System/getenv "CRUX_GIT_VERSION_SUFFIX") (:suffix git-version))]
+  (let [git-version (version-from-git)]
     (-> project
         (->> (postwalk (fn [x]
                          (case x
-                           "crux-git-version" (str prefix suffix)
-                           "crux-git-version-alpha" (str prefix "-alpha" suffix)
-                           "crux-git-version-beta" (str prefix "-beta" suffix)
+                           "crux-git-version" (or git-version "dev-SNAPSHOT")
+                           "crux-git-version-alpha" (or (some-> git-version (str "-alpha"))
+                                                        "dev-SNAPSHOT")
+                           "crux-git-version-beta" (or (some-> git-version (str "-beta"))
+                                                       "dev-SNAPSHOT")
                            x))))
         (cond-> mvn-group-override (-> (assoc :group mvn-group-override)
                                        (->> (postwalk (fn [x]
