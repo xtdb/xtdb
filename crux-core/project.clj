a8bca13efe8de2e91ea765eb8079f6775a85ff36
Alistair O'Neill <aon@juxt.pro>
2020-12-21 19:25:19 +0000

    Implemented tests for the transactions and reorganised.

--- crux-core/project.clj
+++ crux-core/project.clj
@@ -14,20 +14,23 @@
                  [org.clojure/tools.cli "1.0.194"]
                  [org.agrona/agrona "1.7.2"]
                  [com.github.jnr/jnr-ffi "2.1.9" :scope "provided"]
-                 [edn-query-language/eql "1.0.0"]]
+                 [edn-query-language/eql "1.0.0"]
+                 [junit/junit "4.12"]
+                 [org.hamcrest/hamcrest "2.2"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}
              :cli-e2e-test {:jvm-opts ["-Dlogback.configurationFile=../resources/logback-test.xml"]
                             :dependencies [[juxt/crux-http-server "crux-git-version-alpha"]]}
              :test {:dependencies [[juxt/crux-test "crux-git-version"]
-
                                    ;; dependency conflict
                                    [org.tukaani/xz "1.8"]
                                    [org.ow2.asm/asm "5.0.4"]]}}
   :middleware [leiningen.project-version/middleware]
-  :java-source-paths ["src"]
+  :java-source-paths ["src" "test"]
   :javac-options ["-source" "8" "-target" "8"
                   "-XDignore.symbol.file"
                   "-Xlint:all,-options,-path"
                   "-Werror"
                   "-proc:none"]
-  :pedantic? :warn)
+  :junit ["test"]
+  :pedantic? :warn
+  :plugins [[lein-junit "1.1.9"]])
