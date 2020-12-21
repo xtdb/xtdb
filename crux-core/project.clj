edf4febe057536c0e847caab9cff871bac9266f6
Håkan Råberg <hakan.raberg@gmail.com>
2020-12-21 13:58:28 +0100

    Upgrade to Agrona 1.8.0

--- crux-core/project.clj
+++ crux-core/project.clj
@@ -12,7 +12,7 @@
                  [org.clojure/tools.reader "1.3.4"]
                  [org.clojure/data.json "1.0.0"]
                  [org.clojure/tools.cli "1.0.194"]
-                 [org.agrona/agrona "1.7.2"]
+                 [org.agrona/agrona "1.8.0"]
                  [com.github.jnr/jnr-ffi "2.1.9" :scope "provided"]
                  [edn-query-language/eql "1.0.0"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}
