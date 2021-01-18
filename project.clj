c58d6f2798788c64636ead008ef5894bc816b1c5
Håkan Råberg <hakan.raberg@gmail.com>
2021-01-18 10:36:08 +0100

    Upgrading dependencies.

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.6.0"]
-                 [org.clojure/spec.alpha "0.2.176"]
+                 [org.clojure/spec.alpha "0.2.194"]
                  [org.apache.arrow/arrow-vector "2.0.0"]
                  [org.apache.arrow/arrow-memory-netty "2.0.0"]
-                 [org.roaringbitmap/RoaringBitmap "0.9.0"]]
+                 [org.roaringbitmap/RoaringBitmap "0.9.3"]]
   :profiles {:uberjar {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}
              :dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]
                                   [org.clojure/test.check "0.10.0"]
