8b95adbd4cf554562144c0846d6fa60bc8ec9367
Håkan Råberg <hakan.raberg@gmail.com>
2021-01-27 14:34:02 +0100

    Upgrading Arrow and tempoarily adding patch to DenseUnionVector.

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.6.0"]
                  [org.clojure/spec.alpha "0.2.194"]
-                 [org.apache.arrow/arrow-vector "2.0.0"]
-                 [org.apache.arrow/arrow-memory-netty "2.0.0"]
+                 [org.apache.arrow/arrow-vector "3.0.0"]
+                 [org.apache.arrow/arrow-memory-netty "3.0.0"]
                  [org.roaringbitmap/RoaringBitmap "0.9.3"]]
   :profiles {:uberjar {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}
              :dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]
