3f4b728417f831c21ed9e6a696cd382fc2f2baa5
James Henderson <james@jarohen.me.uk>
2021-01-28 21:30:07 +0000

    Adding the real Postgres driver to the crux-jdbc project.clj because it's referred to from the docs.
    See #1403 - thanks @coyotesqrl!

--- crux-jdbc/project.clj
+++ crux-jdbc/project.clj
@@ -10,12 +10,16 @@
                  [org.clojure/java.data "1.0.86"]
                  [com.zaxxer/HikariCP "3.4.5"]
                  [com.taoensso/nippy "3.1.1"]
+
+                 ;; Sample driver dependencies
+                 [org.postgresql/postgresql "42.2.18" :scope "provided"]
                  [com.oracle.ojdbc/ojdbc8 "19.3.0.0" :scope "provided"]
                  [com.h2database/h2 "1.4.200" :scope "provided"]
-                 [com.opentable.components/otj-pg-embedded "0.13.1" :scope "provided"]
                  [org.xerial/sqlite-jdbc "3.28.0" :scope "provided"]
                  [mysql/mysql-connector-java "8.0.17" :scope "provided"]
                  [com.microsoft.sqlserver/mssql-jdbc "8.2.2.jre8" :scope "provided"]]
 
   :middleware [leiningen.project-version/middleware]
-  :pedantic? :warn)
+  :pedantic? :warn
+
+  :profiles {:dev {:dependencies [[com.opentable.components/otj-pg-embedded "0.13.1"]]}})
