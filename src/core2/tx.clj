052d95f52c330c908a1098f900b97ea7d7adf873
Håkan Råberg <hakan.raberg@gmail.com>
2021-02-11 17:34:54 +0100

    Clearer type hint, not that the record uses this anyway.

--- src/core2/tx.clj
+++ src/core2/tx.clj
@@ -1,6 +1,6 @@
 (ns core2.tx
-  (:import [java.util Date Map]))
+  (:import [java.util Date SortedMap]))
 
 (defrecord TransactionInstant [^long tx-id, ^Date tx-time])
 
-(defrecord Watermark [^long chunk-idx ^Map chunk-object-key->idx])
+(defrecord Watermark [^long chunk-idx ^SortedMap chunk-object-key->idx])
