f52cfaab58cf5ea75d9d7c1e0757fd69567fca08
Steven Deobald <steven@deobald.ca>
2021-02-22 17:24:57 -0600

    Creating 'community' Antora component
    - first step in carving up the "about" page
    - requires redirects from old Antora pages since we can't commit
      changes to `crux` and `crux-site` synchronously

--- docs/bin/build.sh
+++ docs/bin/build.sh
@@ -10,14 +10,14 @@ while [[ "$#" -gt 0 ]]; do
         --with-local-bundle)
             OPTS+=" --ui-bundle-url=../../crux-site/build/ui-bundle.zip"
             shift;;
-	--with-blog)
-	    PLAYBOOK="antora-playbook-blog.yml"
-	    shift;;
+        --with-blog)
+            PLAYBOOK="antora-playbook-blog.yml"
+            shift;;
         *) echo "Unknown parameter passed: $1"; exit 1;;
     esac
 done
 
 (
     cd $(dirname "$0")/..
-    antora --clean --fetch $OPTS $PLAYBOOK
+    antora --clean --redirect-facility static --fetch $OPTS $PLAYBOOK
 )
