8039838e23691b80b1af5e12c1868ea2fcb3042c
James Henderson <jms@juxt.pro>
2021-03-02 11:21:53 +0000

    ICursor extends Spliterator

--- src/main/java/core2/ICursor.java
+++ src/main/java/core2/ICursor.java
@@ -1,9 +1,23 @@
 package core2;
 
+import java.util.Spliterator;
 import java.util.function.Consumer;
 
-public interface ICursor<E> extends AutoCloseable {
-    boolean tryAdvance(Consumer<? super E> action);
+public interface ICursor<E> extends Spliterator<E>, AutoCloseable {
+    @Override
+    default Spliterator<E> trySplit() {
+        return null;
+    }
+
+    @Override
+    default int characteristics() {
+        return IMMUTABLE;
+    }
+
+    @Override
+    default long estimateSize() {
+        return Long.MAX_VALUE;
+    }
 
     @Override
     default void close() {
