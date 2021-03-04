bf1a4e661ab0cbb9f9cf43564a67353c0c27426a
Håkan Råberg <hakan.raberg@gmail.com>
2021-03-04 08:48:33 +0100

    Extra sanity check group-by tests.

--- test/core2/operator/group_by_test.clj
+++ test/core2/operator/group_by_test.clj
@@ -73,4 +73,38 @@
                     {:a 2, :b 20, :cnt 1}
                     {:a 3, :b 10, :cnt 1}
                     {:a 3, :b 20, :cnt 1}}]
+                 (mapv set (tu/<-cursor group-by-cursor))))))
+
+    (t/testing "group without aggregate"
+      (with-open [in-cursor (tu/->cursor (Schema. [a-field b-field])
+                                         [[{:a 1 :b 10}
+                                           {:a 1 :b 20}
+                                           {:a 2 :b 10}
+                                           {:a 2 :b 20}]
+                                          [{:a 1 :b 10}
+                                           {:a 1 :b 20}
+                                           {:a 2 :b 10}
+                                           {:a 3 :b 20}
+                                           {:a 3 :b 10}]])
+                  group-by-cursor (group-by/->group-by-cursor tu/*allocator* in-cursor [(group-by/->group-spec "a")])]
+
+        (t/is (= [#{{:a 1}
+                    {:a 2}
+                    {:a 3}}]
+                 (mapv set (tu/<-cursor group-by-cursor))))))
+
+    (t/testing "aggregate without group"
+      (with-open [in-cursor (tu/->cursor (Schema. [a-field b-field])
+                                         [[{:a 1 :b 10}
+                                           {:a 1 :b 20}
+                                           {:a 2 :b 10}
+                                           {:a 2 :b 20}]
+                                          [{:a 1 :b 10}
+                                           {:a 1 :b 20}
+                                           {:a 2 :b 10}
+                                           {:a 3 :b 20}
+                                           {:a 3 :b 10}]])
+                  group-by-cursor (group-by/->group-by-cursor tu/*allocator* in-cursor [(group-by/->function-spec "b" "cnt" (Collectors/counting))])]
+
+        (t/is (= [#{{:cnt 9}}]
                  (mapv set (tu/<-cursor group-by-cursor))))))))
