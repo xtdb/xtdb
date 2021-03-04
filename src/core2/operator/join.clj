565a7896d5aa56f3ce8ea8e763b442bfa909be6b
Håkan Råberg <hakan.raberg@gmail.com>
2021-03-04 09:18:37 +0100

    Tweaks to avoid copyFromSafe in a few places, repeated tuple right copy out of left loop.

--- src/core2/operator/join.clj
+++ src/core2/operator/join.clj
@@ -22,9 +22,18 @@
             out-vec (.getVector out-root (.getField in-vec))]
         (.copyFromSafe out-vec idx out-idx in-vec)))))
 
+(defn- copy-tuple-repeated [^VectorSchemaRoot in-root ^long idx ^VectorSchemaRoot out-root ^long repeat]
+  (let [out-idx (.getRowCount out-root)]
+    (dotimes [n (util/root-field-count in-root)]
+      (let [^ValueVector in-vec (.getVector in-root n)
+            out-vec (.getVector out-root (.getField in-vec))]
+        (util/set-value-count out-vec (+ out-idx repeat))
+        (dotimes [m repeat]
+          (.copyFrom out-vec idx (+ out-idx m) in-vec))))))
+
 (defn- cross-product [^VectorSchemaRoot left-root ^long left-idx ^VectorSchemaRoot right-root ^VectorSchemaRoot out-root]
   (let [out-idx (.getRowCount out-root)
-        right-row-count (.getRowCount right-root)]
+        row-count (.getRowCount right-root)]
     (dotimes [n (util/root-field-count right-root)]
       (let [^ValueVector right-vec (.getVector right-root n)
             out-vec (.getVector out-root (.getField right-vec))]
@@ -32,9 +41,10 @@
     (dotimes [n (util/root-field-count left-root)]
       (let [^ValueVector left-vec (.getVector left-root n)
             out-vec (.getVector out-root (.getField left-vec))]
-        (dotimes [idx right-row-count]
-          (.copyFromSafe out-vec left-idx (+ out-idx idx) left-vec))))
-    (util/set-vector-schema-root-row-count out-root (+ out-idx right-row-count))))
+        (util/set-value-count out-vec (+ out-idx row-count))
+        (dotimes [m row-count]
+          (.copyFrom out-vec left-idx (+ out-idx m) left-vec))))
+    (util/set-vector-schema-root-row-count out-root (+ out-idx row-count))))
 
 (deftype CrossJoinCursor [^BufferAllocator allocator
                           ^ICursor left-cursor
@@ -145,14 +155,14 @@
                                            join-schema (->join-schema left-schema (.getSchema right-root))
                                            out-root (VectorSchemaRoot/create join-schema allocator)
                                            probe-phase (fn [^long right-idx right-key]
-                                                         (let [idx-pairs (.get join-key->left-idx-pairs right-key)]
+                                                         (when-let [^List idx-pairs (.get join-key->left-idx-pairs right-key)]
                                                            (doseq [^ints idx-pair idx-pairs
                                                                    :let [left-root-idx (aget idx-pair 0)
                                                                          left-idx (aget idx-pair 1)
                                                                          left-root ^VectorSchemaRoot (.get left-roots left-root-idx)]]
-                                                             (copy-tuple left-root left-idx out-root)
-                                                             (copy-tuple right-root right-idx out-root))
-                                                           (util/set-vector-schema-root-row-count out-root (+ (.getRowCount out-root) (count idx-pairs)))))
+                                                             (copy-tuple left-root left-idx out-root))
+                                                           (copy-tuple-repeated right-root right-idx out-root (.size idx-pairs))
+                                                           (util/set-vector-schema-root-row-count out-root (+ (.getRowCount out-root) (.size idx-pairs)))))
                                            right-pointer (ArrowBufPointer.)
                                            right-vec (util/maybe-single-child-dense-union (.getVector right-root right-column-name))]
                                        (cond
