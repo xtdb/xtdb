94e79fe6511294786356c921128dae66d7368b59
Håkan Råberg <hakan.raberg@gmail.com>
2021-03-17 16:43:30 +0000

    Import Håkan's HLL sketch

---
+++ crux-core/src/crux/hyper_log_log.clj
@@ -0,0 +1,77 @@
+(ns crux.hyper-log-log
+  (:require [crux.memory :as mem])
+  (:import [org.agrona DirectBuffer MutableDirectBuffer]
+           java.nio.ByteOrder))
+
+;; http://dimacs.rutgers.edu/~graham/pubs/papers/cacm-sketch.pdf
+
+;; http://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf
+(defn ->hyper-log-log
+  (^org.agrona.MutableDirectBuffer []
+   (->hyper-log-log 1024))
+  (^org.agrona.MutableDirectBuffer [^long m]
+   (mem/allocate-unpooled-buffer (* Integer/BYTES m))))
+
+(defn hyper-log-log-update ^org.agrona.MutableDirectBuffer [^MutableDirectBuffer hll v]
+  (let [m (/ (.capacity hll) Integer/BYTES)
+        b (Integer/numberOfTrailingZeros m)
+        x (mix-collection-hash (hash v) 0)
+        j (bit-and (bit-shift-right x (- Integer/SIZE b)) (dec m))
+        w (bit-and x (dec (bit-shift-left 1 (- Integer/SIZE b))))]
+    (doto hll
+      (.putInt (* j Integer/BYTES)
+               (max (.getInt hll (* j Integer/BYTES) ByteOrder/BIG_ENDIAN)
+                    (- (inc (Integer/numberOfLeadingZeros w)) b))
+               ByteOrder/BIG_ENDIAN))))
+
+(defn hyper-log-log-estimate ^double [^DirectBuffer hll]
+  (let [m (/ (.capacity hll) Integer/BYTES)
+        z (/ 1.0 (double (loop [n 0
+                                acc 0.0]
+                           (if (< n (.capacity hll))
+                             (recur (+ n Integer/BYTES)
+                                    (+ acc (Math/pow 2.0 (- (.getInt hll n ByteOrder/BIG_ENDIAN)))))
+                             acc))))
+        am (/ 0.7213 (inc (/ 1.079 m)))
+        e (* am (Math/pow m 2.0) z)]
+    (cond
+      (<= e (* (double (/ 5 2)) m))
+      (let [v (long (loop [n 0
+                           acc 0]
+                           (if (< n (.capacity hll))
+                             (recur (+ n Integer/BYTES)
+                                    (+ acc (if (zero? (.getInt hll n ByteOrder/BIG_ENDIAN))
+                                             1
+                                             0)))
+                             acc)))]
+        (if (zero? v)
+          e
+          (* m (Math/log (/ m v)))))
+      (> e (* (double (/ 1 30)) (Integer/toUnsignedLong -1)))
+      (* (Math/pow -2.0 32)
+         (Math/log (- 1 (/ e (Integer/toUnsignedLong -1)))))
+      :else
+      e)))
+
+(defn hyper-log-log-merge ^DirectBuffer [^DirectBuffer hll-a ^DirectBuffer hll-b]
+  (assert (= (.capacity hll-a) (.capacity hll-b)))
+  (loop [n 0
+         result ^MutableDirectBuffer (mem/allocate-unpooled-buffer (.capacity hll-a))]
+    (if (= n (.capacity result))
+      result
+      (recur (+ n Integer/BYTES)
+             (doto result
+               (.putInt n (max (.getInt hll-a n ByteOrder/BIG_ENDIAN)
+                               (.getInt hll-b n ByteOrder/BIG_ENDIAN)) ByteOrder/BIG_ENDIAN))))))
+
+(defn hyper-log-log-estimate-union ^double [^DirectBuffer hll-a ^DirectBuffer hll-b]
+  (hyper-log-log-estimate (hyper-log-log-merge hll-a hll-b)))
+
+(defn hyper-log-log-estimate-intersection ^double [^DirectBuffer hll-a ^DirectBuffer hll-b]
+  (- (+ (hyper-log-log-estimate hll-a)
+        (hyper-log-log-estimate hll-b))
+     (hyper-log-log-estimate-union hll-a hll-b)))
+
+(defn hyper-log-log-estimate-difference ^double [^DirectBuffer hll-a ^DirectBuffer hll-b]
+  (- (hyper-log-log-estimate hll-a)
+     (hyper-log-log-estimate-intersection hll-a hll-b)))
