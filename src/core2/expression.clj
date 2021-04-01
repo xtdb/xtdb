4568797172cdc61711d94de1d45ecd88858f8abd
Håkan Råberg <hakan.raberg@gmail.com>
2021-04-01 16:36:58 +0200

    Adding some more heuristics around the return type inference. Rewrites of compare ops.

--- src/core2/expression.clj
+++ src/core2/expression.clj
@@ -1,11 +1,13 @@
 (ns core2.expression
-  (:require [clojure.walk :as w]
+  (:require [clojure.set :as set]
+            [clojure.walk :as w]
             [core2.types :as types]
             [core2.util :as util])
   (:import core2.operator.project.ProjectionSpec
            org.apache.arrow.memory.RootAllocator
            org.apache.arrow.vector.types.Types$MinorType
-           [org.apache.arrow.vector BigIntVector BitVector FieldVector Float8Vector ValueVector VectorSchemaRoot]
+           [org.apache.arrow.vector BigIntVector BitVector FieldVector Float8Vector NullVector
+            TimeStampMilliVector VarBinaryVector VarCharVector ValueVector VectorSchemaRoot]
            org.apache.arrow.vector.complex.DenseUnionVector))
 
 (set! *unchecked-math* :warn-on-boxed)
@@ -13,53 +15,105 @@
 (defn variables [expr]
   (filter symbol? (tree-seq sequential? rest expr)))
 
-(defn- infer-return-type [types expression]
-  (if-let [tag (:tag expression)]
-    (types/->arrow-type tag)
-    (cond
-      (= #{Float8Vector} types) (.getType Types$MinorType/FLOAT8)
-      (= #{BigIntVector Float8Vector} types) (.getType Types$MinorType/FLOAT8)
-      (= #{BigIntVector} types) (.getType Types$MinorType/BIGINT)
-      (= #{BitVector} types) (.getType Types$MinorType/BIT))))
-
-(def arrow-type->vector-type
-  {(.getType Types$MinorType/BIGINT) BigIntVector
+(def ^:private arrow-type->vector-type
+  {(.getType Types$MinorType/NULL) NullVector
+   (.getType Types$MinorType/BIGINT) BigIntVector
    (.getType Types$MinorType/FLOAT8) Float8Vector
+   (.getType Types$MinorType/VARBINARY) VarBinaryVector
+   (.getType Types$MinorType/VARCHAR) VarCharVector
+   (.getType Types$MinorType/TIMESTAMPMILLI) TimeStampMilliVector
    (.getType Types$MinorType/BIT) BitVector})
+
+(def ^:private compare-op? '#{< <= > >= = !=})
+
+(def ^:private logic-op? '#{and or not})
+
+(defn- adjust-compare-op [op args types]
+  (if (set/subset? types #{(.getType Types$MinorType/BIGINT)
+                           (.getType Types$MinorType/FLOAT8)
+                           (.getType Types$MinorType/TIMESTAMPMILLI)})
+    (cons op args)
+    (let [comp (if (= types #{(.getType Types$MinorType/VARBINARY)})
+                 `java.util.Arrays/equals
+                 `compare)]
+      (case op
+        < `(neg? (~comp ~@args))
+        <= `(not (pos? (~comp ~@args)))
+        >  `(pos? (~comp ~@args))
+        >= `(not (neg? (~comp ~@args)))
+        = `(zero? (compare ~@args))
+        != `(not (zero? (~comp ~@args)))))))
+
+(defn- infer-return-type [var->type expression]
+  (cond
+    (symbol? expression)
+    [expression (get var->type expression)]
+    (not (sequential? expression))
+    [expression (types/->arrow-type (class expression))]
+    :else
+    (let [[op & args] expression
+          arg+types (for [expression args]
+                      (infer-return-type var->type expression))
+          types (set (map second arg+types))
+          args (map first arg+types)]
+      (cond
+        (compare-op? op)
+        [(adjust-compare-op op args types)
+         (.getType Types$MinorType/BIT)]
+
+        (logic-op? op)
+        [`(boolean ~(cons op args))
+         (.getType Types$MinorType/BIT)]
+
+        :else
+        [(cons op args)
+         (cond
+           (= #{(.getType Types$MinorType/FLOAT8)} types) (.getType Types$MinorType/FLOAT8)
+           (= #{(.getType Types$MinorType/BIGINT)
+                (.getType Types$MinorType/FLOAT8)} types) (.getType Types$MinorType/FLOAT8)
+           (= #{(.getType Types$MinorType/BIGINT)} types) (.getType Types$MinorType/BIGINT)
+           (= #{(.getType Types$MinorType/BIT)} types) (.getType Types$MinorType/BIT)
+           :else
+           (first types))]))))
 
 (defn- primitive-vector-type? [^Class type]
   (= "org.apache.arrow.vector" (.getPackageName type)))
 
+(defn- normalize-expression [expression]
+  (w/postwalk #(cond
+                 (vector? %)
+                 (seq %)
+                 (keyword? %)
+                 (symbol (name %))
+                 :else
+                 %)
+              expression))
+
 (defn- generate-code [types expression]
   (let [vars (variables expression)
-        arrow-return-type (infer-return-type (set types) expression)
+        var->type (zipmap vars types)
+        expression (normalize-expression expression)
+        [expression arrow-return-type] (infer-return-type var->type expression)
         ^Class vector-return-type (get arrow-type->vector-type arrow-return-type)
         inner-acc-sym (with-meta (gensym 'inner-acc) {:tag (symbol (.getName vector-return-type))})
         return-type-id (types/arrow-type->type-id arrow-return-type)
         idx-sym (gensym 'idx)
-        var->type (zipmap vars types)
         expanded-expression (w/postwalk
                              #(if-let [type (get var->type %)]
                                 (cond
                                   (= BitVector type)
                                   `(= 1 (.get ~% ~idx-sym))
-                                  (primitive-vector-type? type)
+                                  (primitive-vector-type? (get arrow-type->vector-type type))
                                   `(.get ~% ~idx-sym)
                                   :else
                                   `(.getObject ~% ~idx-sym))
-                                (cond
-                                  (vector? %)
-                                  (seq %)
-                                  (keyword? %)
-                                  (symbol (name %))
-                                  :else
-                                  %))
+                                %)
                              expression)
         expanded-expression (if (= BitVector vector-return-type)
                               `(if ~expanded-expression 1 0)
                               expanded-expression)]
-    `(fn [[~@(for [[k ^Class v] (map vector vars types)]
-               (with-meta k {:tag (symbol (.getName v))}))]
+    `(fn [[~@(for [[k v] (map vector vars types)]
+               (with-meta k {:tag (symbol (.getName ^Class (get arrow-type->vector-type v)))}))]
           ^DenseUnionVector acc#
           ^long row-count#]
        (let [~inner-acc-sym (.getVectorByType acc# ~return-type-id)]
@@ -76,7 +130,7 @@
     (project [_ in allocator]
       (let [in-vecs (vec (for [var (variables expression)]
                            (util/maybe-single-child-dense-union (.getVector in (name var)))))
-            types (mapv class in-vecs)
+            types (mapv #(.getType (.getFieldType (.getField ^ValueVector %))) in-vecs)
             inner-expr-code (memo-generate-code types expression)
             inner-expr-fn (memo-eval inner-expr-code)
             ^DenseUnionVector acc (.createVector (types/->primitive-dense-union-field col-name) allocator)]
--- test/core2/expression_test.clj
+++ test/core2/expression_test.clj
@@ -4,25 +4,30 @@
             [core2.test-util :as test-util]
             [core2.util :as util])
   (:import org.apache.arrow.memory.RootAllocator
-           [org.apache.arrow.vector FieldVector Float8Vector ValueVector VectorSchemaRoot]))
+           [org.apache.arrow.vector BigIntVector FieldVector Float8Vector ValueVector VectorSchemaRoot]))
 
 (t/deftest can-compile-simple-expression
   (with-open [allocator (RootAllocator.)
               a (Float8Vector. "a" allocator)
-              b (Float8Vector. "b" allocator)]
+              b (Float8Vector. "b" allocator)
+              d (BigIntVector. "d" allocator)]
     (let [rows 1000]
       (.setValueCount a rows)
       (.setValueCount b rows)
+      (.setValueCount d rows)
       (dotimes [n rows]
         (.set a n (double n))
-        (.set b n (double n))))
+        (.set b n (double n))
+        (.set d n n)))
 
-    (with-open [in (VectorSchemaRoot/of (into-array FieldVector [a b]))]
+    (with-open [in (VectorSchemaRoot/of (into-array FieldVector [a b d]))]
       (let [expr '(+ a b)
             expr-projection-spec (expr/->expression-projection-spec "c" expr)]
         (t/is (= '(a b) (expr/variables expr)))
         (with-open [^ValueVector acc (.project expr-projection-spec in allocator)]
-          (let [xs (test-util/->list (util/maybe-single-child-dense-union acc))]
+          (let [v (util/maybe-single-child-dense-union acc)
+                xs (test-util/->list v)]
+            (t/is (instance? Float8Vector v))
             (t/is (= (mapv (comp double +) (range 1000) (range 1000))
                      xs)))))
 
@@ -36,9 +41,30 @@
 
       (t/testing "support keyword and vectors"
         (let [expr '[:+ a [:+ b 2]]
-            expr-projection-spec (expr/->expression-projection-spec "c" expr)]
-        (t/is (= '(a b) (expr/variables expr)))
-        (with-open [^ValueVector acc (.project expr-projection-spec in allocator)]
-          (let [xs (test-util/->list (util/maybe-single-child-dense-union acc))]
-            (t/is (= (mapv (comp double +) (range 1000) (range 1000) (repeat 2))
-                     xs)))))))))
+              expr-projection-spec (expr/->expression-projection-spec "c" expr)]
+          (t/is (= '(a b) (expr/variables expr)))
+          (with-open [^ValueVector acc (.project expr-projection-spec in allocator)]
+            (let [xs (test-util/->list (util/maybe-single-child-dense-union acc))]
+              (t/is (= (mapv (comp double +) (range 1000) (range 1000) (repeat 2))
+                       xs))))))
+
+      (t/testing "mixing types"
+        (let [expr '(+ 2 d)
+              expr-projection-spec (expr/->expression-projection-spec "c" expr)]
+          (t/is (= '(d) (expr/variables expr)))
+          (with-open [^ValueVector acc (.project expr-projection-spec in allocator)]
+            (let [v (util/maybe-single-child-dense-union acc)
+                  xs (test-util/->list (util/maybe-single-child-dense-union acc))]
+              (t/is (instance? BigIntVector v))
+              (t/is (= (mapv + (repeat 2) (range 1000))
+                       xs)))))
+
+        (let [expr '(+ 2.0 d)
+              expr-projection-spec (expr/->expression-projection-spec "c" expr)]
+          (t/is (= '(d) (expr/variables expr)))
+          (with-open [^ValueVector acc (.project expr-projection-spec in allocator)]
+            (let [v (util/maybe-single-child-dense-union acc)
+                  xs (test-util/->list (util/maybe-single-child-dense-union acc))]
+              (t/is (instance? Float8Vector v))
+              (t/is (= (mapv (comp double +) (repeat 2) (range 1000))
+                       xs)))))))))
