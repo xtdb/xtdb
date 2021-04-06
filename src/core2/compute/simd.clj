f21b4437d9b5e68b31bde683a7461b66c0cf692a
Håkan Råberg <hakan.raberg@gmail.com>
2021-04-06 13:34:25 +0200

    Removing compute spike, superseded by expression system.

--- src/core2/compute/simd.clj
+++
@@ -1,5 +0,0 @@
-(ns core2.compute.simd
-  (:require [core2.compute :as cc])
-  (:import jdk.incubator.vector.VectorOperators
-           jdk.incubator.vector.DoubleVector
-           jdk.incubator.vector.LongVector))
