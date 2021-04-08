ecd007802c6142f421b6b299e467c6087479fbd2
Håkan Råberg <hakan.raberg@gmail.com>
2021-04-08 16:15:46 +0200

    Comment showing how to run TPC-H in the REPL.

--- dev/user.clj
+++ dev/user.clj
@@ -7,3 +7,29 @@
 
 (defn reset []
   (ctn/refresh))
+
+(comment
+  (def node (time (core2.core/->local-node (core2.util/->path "target/tpch-queries-sf-001"))))
+  (with-open [watermark (core2.core/open-watermark node)]
+    (let [^core2.core.Node node node]
+      (with-bindings {#'core2.tpch-queries/*node* node
+                      #'core2.tpch-queries/*op-factory*
+                      (core2.operator/->operator-factory (.allocator node)
+                                                         (.metadata-manager node)
+                                                         (.temporal-manager node)
+                                                         (.buffer-pool node))
+                      #'core2.tpch-queries/*watermark* watermark}
+        (time
+         (doseq [q [#'core2.tpch-queries/tpch-q1-pricing-summary-report
+                    #'core2.tpch-queries/tpch-q3-shipping-priority
+                    #'core2.tpch-queries/tpch-q5-local-supplier-volume
+                    #'core2.tpch-queries/tpch-q6-forecasting-revenue-change
+                    #'core2.tpch-queries/tpch-q7-volume-shipping
+                    #'core2.tpch-queries/tpch-q8-national-market-share
+                    #'core2.tpch-queries/tpch-q9-product-type-profit-measure
+                    #'core2.tpch-queries/tpch-q10-returned-item-reporting
+                    #'core2.tpch-queries/tpch-q12-shipping-modes-and-order-priority
+                    #'core2.tpch-queries/tpch-q14-promotion-effect
+                    #'core2.tpch-queries/tpch-q19-discounted-revenue]]
+           (prn q)
+           (prn (time (count (@q))))))))))
