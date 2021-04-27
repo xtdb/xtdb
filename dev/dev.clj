07a828a9df96d4e20b70d83908329e4d738f09e4
James Henderson <jms@juxt.pro>
2021-04-27 12:00:37 +0100

    more TPC-H dev code

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,10 +1,12 @@
 (ns dev
-  (:require [core2.core :as c2]
+  (:require [clojure.java.io :as io]
+            [core2.core :as c2]
             [core2.tpch :as tpch]
+            [core2.tpch-test :as tpch-test]
             [core2.util :as util]
             [integrant.core :as i]
             [integrant.repl :as ir]
-            [clojure.java.io :as io])
+            [core2.test-util :as tu])
   (:import [ch.qos.logback.classic Level Logger]
            org.slf4j.LoggerFactory))
 
@@ -57,4 +59,9 @@
   (def !submit-tpch
     (future
       (time
-       (tpch/submit-docs! node 0.1)))))
+       (tpch/submit-docs! node 0.1))))
+
+  (with-open [wm (c2/open-watermark node)
+              res (c2/open-q node wm tpch/tpch-q5-local-supplier-volume)]
+    (time
+     (count (into [] (mapcat seq (tu/<-cursor res)))))))
