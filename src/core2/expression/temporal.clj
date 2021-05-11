44ede707f6dfb2381faa0e0faad61291b24e100b
Håkan Råberg <hakan.raberg@gmail.com>
2021-05-11 14:02:02 +0200

    Fix copy paste issue.

--- src/core2/expression/temporal.clj
+++ src/core2/expression/temporal.clj
@@ -6,30 +6,30 @@
 
 ;; SQL:2011 Time-related-predicates
 
-(defmethod expr/codegen-call [:overlaps Date Date Date Date] [{[{x-start :code} {x-start :code} {y-start :code}  {y-end :code} ] :args}]
+(defmethod expr/codegen-call [:overlaps Date Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y-end :code} ] :args}]
   {:code `(and (< x-start y-end) (> x-end y-start))
    :return-type Boolean})
 
-(defmethod expr/codegen-call [:contains Date Date Date] [{[{x-start :code} {x-start :code} {y-start :code}  {y :code} ] :args}]
+(defmethod expr/codegen-call [:contains Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y :code} ] :args}]
   {:code `(and (<= x-start y) (> x-end y))
    :return-type Boolean})
 
-(defmethod expr/codegen-call [:contains Date Date Date Date] [{[{x-start :code} {x-start :code} {y-start :code}  {y-end :code} ] :args}]
+(defmethod expr/codegen-call [:contains Date Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y-end :code} ] :args}]
   {:code `(and (<= x-start y-start) (>= x-end y-end))
    :return-type Boolean})
 
-(defmethod expr/codegen-call [:precedes Date Date Date Date] [{[{x-start :code} {x-start :code} {y-start :code}  {y-end :code} ] :args}]
+(defmethod expr/codegen-call [:precedes Date Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y-end :code} ] :args}]
   {:code `(<= x-end y-start)
    :return-type Boolean})
 
-(defmethod expr/codegen-call [:succeedes Date Date Date Date] [{[{x-start :code} {x-start :code} {y-start :code}  {y-end :code} ] :args}]
+(defmethod expr/codegen-call [:succeedes Date Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y-end :code} ] :args}]
   {:code `(>= x-start y-end)
    :return-type Boolean})
 
-(defmethod expr/codegen-call [:immediately-precedes Date Date Date Date] [{[{x-start :code} {x-start :code} {y-start :code}  {y-end :code}] :args}]
+(defmethod expr/codegen-call [:immediately-precedes Date Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y-end :code}] :args}]
   {:code `(= x-end y-start)
    :return-type Boolean})
 
-(defmethod expr/codegen-call [:immediately-succeedes Date Date Date Date] [{[{x-start :code} {x-start :code} {y-start :code}  {y-end :code} ] :args}]
+(defmethod expr/codegen-call [:immediately-succeedes Date Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y-end :code} ] :args}]
   {:code `(= x-start y-end)
    :return-type Boolean})
