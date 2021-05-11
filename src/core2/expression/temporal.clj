016cc5b75974fa01db534b3595fa20db05f1a3ae
Håkan Råberg <hakan.raberg@gmail.com>
2021-05-11 14:40:12 +0200

    Adding commented out operators on durations, no type support for this atm.

--- src/core2/expression/temporal.clj
+++ src/core2/expression/temporal.clj
@@ -2,7 +2,8 @@
   (:require [core2.expression :as expr]
             [core2.expression.metadata :as expr.meta]
             [core2.types :as types])
-  (:import java.util.Date))
+  (:import java.util.Date
+           java.time.Duration))
 
 ;; SQL:2011 Time-related-predicates
 
@@ -33,3 +34,43 @@
 (defmethod expr/codegen-call [:immediately-succeedes Date Date Date Date] [{[{x-start :code} {x-end :code} {y-start :code}  {y-end :code} ] :args}]
   {:code `(= x-start y-end)
    :return-type Boolean})
+
+;; SQL:2011 Operations involving datetimes and intervals
+
+;; TODO: no support for duration vectors yet.
+
+;; (defmethod expr/codegen-call [:- Date Date] [{:keys [emitted-args]}]
+;;   {:code `(- ~@emitted-args)
+;;    :return-type Duration})
+
+;; (defmethod expr/codegen-call [:- Date Duration] [{:keys [emitted-args]}]
+;;   {:code `(- ~@emitted-args)
+;;    :return-type Date})
+
+;; (defmethod expr/codegen-call [:+ Date Duration] [{:keys [emitted-args]}]
+;;   {:code `(+ ~@emitted-args)
+;;    :return-type Date})
+
+;; (defmethod expr/codegen-call [:- Duration Duration] [{:keys [emitted-args]}]
+;;   {:code `(+ ~@emitted-args)
+;;    :return-type Duration})
+
+;; (defmethod expr/codegen-call [:+ Duration Date] [{:keys [emitted-args]}]
+;;   {:code `(+ ~@emitted-args)
+;;    :return-type Date})
+
+;; (defmethod expr/codegen-call [:+ Duration Duration] [{:keys [emitted-args]}]
+;;   {:code `(+ ~@emitted-args)
+;;    :return-type Duration})
+
+;; (defmethod expr/codegen-call [:* Duration Number] [{:keys [emitted-args]}]
+;;   {:code `(* ~@emitted-args)
+;;    :return-type Duration})
+
+;; (defmethod expr/codegen-call [:* Number Duration] [{:keys [emitted-args]}]
+;;   {:code `(* ~@emitted-args)
+;;    :return-type Duration})
+
+;; (defmethod expr/codegen-call [:/ Duration Number] [{:keys [emitted-args]}]
+;;   {:code `(quot ~@emitted-args)
+;;    :return-type Duration})
