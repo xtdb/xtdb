f164585ed40a8e92373af18945d3e6b6a96e064c
Steven Deobald <steven@deobald.ca>
2021-05-28 11:36:11 -0600

    Gradle library build based on examples from:
    https://docs.gradle.org/current/samples/sample_building_kotlin_libraries.html
    https://docs.gradle.org/current/userguide/publishing_setup.html
    
    ...this is not yet configured to actually publish to a repository.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
+    `java-library`
+    `maven-publish`
 }
 
 group = "juxt"
@@ -16,7 +18,12 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // TODO: I'm not sure if we need both `implementation` and `api`? -sd
     implementation("juxt:crux-core:21.04-1.16.0-beta")
+    api("juxt:crux-core:21.04-1.16.0-beta")
 
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
