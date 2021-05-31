6ba849b42fa04145576895d6de1114ab079d580b
Steven Deobald <steven@deobald.ca>
2021-05-31 17:01:55 -0600

    MavenCentral publishing instructions done.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,74 @@ plugins {
     kotlin("jvm") version "1.4.21"
     `java-library`
     `maven-publish`
+    signing
 }
 
-group = "juxt"
+group = "pro.juxt"
 version = "0.0.1-SNAPSHOT"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
 repositories {
     mavenCentral()
     maven {
         name = "Clojars"
         url = uri("https://repo.clojars.org/")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "crux-kotlin-dsl"
+            from(components["java"])
+            pom {
+                name.set("Crux Kotlin DSL")
+                description.set("A Kotlin DSL for Crux which enables pretty transactions and queries")
+                url.set("https://github.com/crux-labs/crux-kotlin-dsl")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("AlistairONeill")
+                        name.set("Alistair O'Neill")
+                        email.set("aon@juxt.pro")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/crux-labs/crux-kotlin-dsl.git")
+                    developerConnection.set("scm:git:ssh://github.com:crux-labs/crux-kotlin-dsl.git")
+                    url.set("https://github.com/crux-labs/crux-kotlin-dsl")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "ossrh"
+            credentials(PasswordCredentials::class)
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
 
