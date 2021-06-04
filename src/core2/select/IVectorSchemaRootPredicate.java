b87c68ea3f2314db57a640b2c2f6cd198bc4ffc2
James Henderson <jms@juxt.pro>
2021-06-04 13:10:18 +0100

    unused interfaces

--- src/core2/select/IVectorSchemaRootPredicate.java
+++
@@ -1,7 +0,0 @@
-package core2.select;
-
-import org.apache.arrow.vector.VectorSchemaRoot;
-
-public interface IVectorSchemaRootPredicate {
-    boolean test(VectorSchemaRoot root, int idx);
-}
--- src/core2/select/IVectorSchemaRootSelector.java
+++
@@ -1,8 +0,0 @@
-package core2.select;
-
-import org.apache.arrow.vector.VectorSchemaRoot;
-import org.roaringbitmap.RoaringBitmap;
-
-public interface IVectorSchemaRootSelector {
-    RoaringBitmap select(VectorSchemaRoot root);
-}
