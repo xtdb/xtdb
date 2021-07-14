0ee90644730d5644c34a21f5c7af14065d088aad
James Henderson <james@jarohen.me.uk>
2021-07-14 10:48:04 +0100

    missed `lein-parent` migration on crux-lmdb

--- crux-lmdb/project.clj
+++ crux-lmdb/project.clj
@@ -1,9 +1,16 @@
 (defproject pro.juxt.crux/crux-lmdb "crux-git-version"
   :description "Crux LMDB"
-  :url "https://github.com/juxt/crux"
-  :license {:name "The MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+
+  :plugins [[lein-parent "0.3.8"]]
+
+  :parent-project {:path "../project.clj"
+                   :inherit [:repositories :deploy-repositories
+                             :managed-dependencies
+                             :pedantic? :global-vars
+                             :license :url :pom-addition]}
+
   :scm {:dir ".."}
+
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.logging "1.1.0"]
                  [pro.juxt.crux/crux-core "crux-git-version"]
@@ -14,4 +21,5 @@
                  [org.lwjgl/lwjgl "3.2.3" :classifier "natives-macos" :native-prefix ""]
                  [org.lwjgl/lwjgl-lmdb "3.2.3" :classifier "natives-macos" :native-prefix ""]
                  [org.lwjgl/lwjgl-lmdb "3.2.3"]]
+
   :middleware [leiningen.project-version/middleware])
