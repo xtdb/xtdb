3ee96f33a4d4a92988c6b5dede2d10bcedd24ce1
Alistair O'Neill <aon@juxt.pro>
2021-07-27 11:46:45 +0100

    Upgrade to Kotlin 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.5.10"
     `java-library`
     `maven-publish`
     signing
