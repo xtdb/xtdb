aa3094c58c4920e7387bb088c4046dd0ef2a496a
James Henderson <james@jarohen.dev>
2021-07-28 17:35:27 +0100

    setup for deployment to Central

--- labs/crux-corda/crux-corda-state/build.gradle.kts
+++ labs/crux-corda/crux-corda-state/build.gradle.kts
@@ -1,3 +1,63 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
+    signing
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set("Crux Corda State")
+                description.set("A Crux module that allows you to pipe verified Corda transactions into a Crux node, to then query using Crux’s bitemporal Datalog query engine. ")
+                url.set("https://github.com/juxt/crux")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("juxt")
+                        name.set("JUXT")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/juxt/crux.git")
+                    developerConnection.set("scm:git:ssh://github.com:juxt/crux.git")
+                    url.set("https://github.com/juxt/crux")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "ossrh"
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+            url = uri(if (project.hasProperty("release")) releasesRepoUrl else snapshotsRepoUrl)
+
+            credentials {
+                username = project.properties["ossrhUsername"] as String
+                password = project.properties["ossrhPassword"] as String
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["maven"])
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.javadoc {
 }
