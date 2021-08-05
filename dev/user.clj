adfc726f84033d5246575ad5b84f8cba942b70f0
James Henderson <jms@juxt.pro>
2021-08-05 11:46:47 +0100

    ctn/set-refresh-dirs now that we have namespaces in different directories

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,16 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :as ctn]))
+  (:require [clojure.tools.namespace.repl :as ctn]
+            [clojure.java.io :as io])
+  (:import java.io.File))
 
 (ctn/disable-reload!)
+
+(apply ctn/set-refresh-dirs (for [^File dir (concat (.listFiles (io/file "."))
+                                                    (.listFiles (io/file "modules")))
+                                  :when (and (.isDirectory dir)
+                                             (.exists (io/file dir "project.clj")))
+                                  sub-dir #{"src" "test"}]
+                              (io/file dir sub-dir)))
 
 (defn reset []
   (ctn/refresh))
