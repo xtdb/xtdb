d838c01bea1e46806a3c67be52fb735981b9b215
James Henderson <jms@juxt.pro>
2021-08-12 17:07:43 +0100

    Revert types.spec refactor for the time being, fails CI
    This reverts commit 1f6125d9430b585cd935770560640da0d96e7408.
    This reverts commit c9e82e3e0506271d8f6c3f6e36971d5e178befd8.

--- core/src/core2/types/spec.clj
+++ core/src/core2/types/spec.clj
@@ -13,145 +13,143 @@
 
 (defmulti arrow-type-spec (comp keyword :name))
 
-(s/def :arrow.type/name (some-fn keyword? string?))
-(s/def :arrow/type (s/keys :req-un [:arrow.type/name]))
+(s/def :arrow.schema.field.type.int/bitWidth #{Byte/SIZE Short/SIZE Integer/SIZE Long/SIZE})
+(s/def :arrow.schema.field.type.int/isSigned boolean?)
 
 (defmethod arrow-type-spec :null [_]
-  :arrow/type)
-
-(s/def :arrow.type.int/bitWidth #{Byte/SIZE Short/SIZE Integer/SIZE Long/SIZE})
-(s/def :arrow.type.int/isSigned boolean?)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
 (defmethod arrow-type-spec :int [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.int/bitWidth]
-          :opt-un [:arrow.type.int/isSigned]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.int/bitWidth]
+          :opt-un [:arrow.schema.field.type.int/isSigned]))
 
-(s/def :arrow.type.floatingpoint/precision (s/and (s/conformer keyword)
-                                                  #{:HALF :SINGLE :DOUBLE}))
+(s/def :arrow.schema.field.type.floatingpoint/precision (s/and (s/conformer keyword)
+                                                               #{:HALF :SINGLE :DOUBLE}))
 
 (defmethod arrow-type-spec :floatingpoint [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.floatingpoint/precision]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.floatingpoint/precision]))
 
 (defmethod arrow-type-spec :binary [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
 (defmethod arrow-type-spec :utf8 [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
 (defmethod arrow-type-spec :bool [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
-(s/def :arrow.type.decimal/precision nat-int?)
-(s/def :arrow.type.decimal/scale nat-int?)
-(s/def :arrow.type.decimal/bitWidth #{128 256})
+(s/def :arrow.schema.field.type.decimal/precision nat-int?)
+(s/def :arrow.schema.field.type.decimal/scale nat-int?)
+(s/def :arrow.schema.field.type.decimal/bitWidth #{128 256})
 
 (defmethod arrow-type-spec :decimal [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.decimal/precision
-                   :arrow.type.decimal/scale]
-          :opt-un [:arrow.type.decimal/bitWidth]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.decimal/precision
+                   :arrow.schema.field.type.decimal/scale]
+          :opt-un [:arrow.schema.field.type.decimal/bitWidth]))
 
-(s/def :arrow.type.date/unit (s/and (s/conformer keyword)
-                                    #{:DAY :MILLISECOND}))
+(s/def :arrow.schema.field.type.date/unit (s/and (s/conformer keyword)
+                                                 #{:DAY :MILLISECOND}))
 
 (defmethod arrow-type-spec :date [_]
-  (s/keys :req-un [:arrow.type/name]
-          :opt-un [:arrow.type.date/unit]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.date/unit]))
 
-(s/def :arrow.type.time/unit (s/and (s/conformer keyword)
-                                    #{:SECOND :MILLISECOND :MICROSECOND :NANOSECOND}))
-(s/def :arrow.type.time/bitWidth #{Integer/SIZE Long/SIZE})
+(s/def :arrow.schema.field.type/time-unit (s/and (s/conformer keyword)
+                                                 #{:SECOND :MILLISECOND :MICROSECOND :NANOSECOND}))
+
+(s/def :arrow.schema.field.type.time/unit :arrow.schema.field.type/time-unit)
+(s/def :arrow.schema.field.type.time/bitWidth #{Integer/SIZE Long/SIZE})
 
 (defmethod arrow-type-spec :time [_]
-  (s/keys :req-un [:arrow.type/name]
-          :opt-un [:arrow.type.time/unit
-                   :arrow.type.time/bitWidth]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.time/unit
+                   :arrow.schema.field.type.time/bitWidth]))
 
-(s/def :arrow.type.timestamp/unit :arrow.type.time/unit)
-(s/def :arrow.type.timestamp/timezone string?)
+(s/def :arrow.schema.field.type.timestamp/unit :arrow.schema.field.type/time-unit)
+(s/def :arrow.schema.field.type.timestamp/timezone string?)
 
 (defmethod arrow-type-spec :timestamp [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.timestamp/unit
-                   :arrow.type.timestamp/timezone]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.timestamp/unit
+                   :arrow.schema.field.type.timestamp/timezone]))
 
-(s/def :arrow.type.interval/unit (s/and (s/conformer keyword)
-                                        #{:YEAR_MONTH :DAY_TIME}))
+(s/def :arrow.schema.field.type.interval/unit (s/and (s/conformer keyword)
+                                                     #{:YEAR_MONTH :DAY_TIME}))
 
 (defmethod arrow-type-spec :interval [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.interval/unit]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.interval/unit]))
 
 (defmethod arrow-type-spec :list [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
 (defmethod arrow-type-spec :struct [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
-(s/def :arrow.type.union/mode (s/and (s/conformer keyword)
-                                     #{:SPARSE :DENSE}))
-(s/def :arrow.type.union/typeIds (s/coll-of nat-int?))
+(s/def :arrow.schema.field.type.union/mode (s/and (s/conformer keyword)
+                                                  #{:SPARSE :DENSE}))
+(s/def :arrow.schema.field.type.union/typeIds (s/coll-of nat-int?))
 
 (defmethod arrow-type-spec :union [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.union/mode]
-          :opt-un [:arrow.type.union/typeIds]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.union/mode
+                   :arrow.schema.field.type.union/typeIds]))
 
-(s/def :arrow.type.fixedsizebinary/byteWidth nat-int?)
+(s/def :arrow.schema.field.type.fixedsizebinary/byteWidth nat-int?)
 
 (defmethod arrow-type-spec :fixedsizebinary [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.fixedsizebinary/byteWidth]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.fixedsizebinary/byteWidth]))
 
 (s/def :arrow.schema.field.type.fixedsizelist/listSize nat-int?)
 
 (defmethod arrow-type-spec :fixedsizelist [_]
-  (s/keys :req-un [:arrow.type/name
-                   :arrow.type.fixedsizelist/listSize]))
+  (s/keys :req-un [:arrow.schema.field.type/name
+                   :arrow.schema.field.type.fixedsizelist/listSize]))
 
-(s/def :arrow.type.map/keysSorted boolean?)
+(s/def :arrow.schema.field.type.map/keysSorted boolean?)
 
 (defmethod arrow-type-spec :map [_]
-  (s/keys :req-un [:arrow.type/name]
-          :opt-un [:arrow.type.map/keysSorted]))
+  (s/keys :req-un [:arrow.schema.field.type/name]
+          :opt-un [:arrow.schema.field.type.map/keysSorted]))
 
-(s/def :arrow.type.duration/unit :arrow.type/time-unit)
+(s/def :arrow.schema.field.type.duration/unit :arrow.schema.field.type/time-unit)
 
 (defmethod arrow-type-spec :duration [_]
-  (s/keys :req-un [:arrow.type/name]
-          :opt-un [:arrow.type.duration/unit]))
+  (s/keys :req-un [:arrow.schema.field.type/name]
+          :opt-un [:arrow.schema.field.type.duration/unit]))
 
 (defmethod arrow-type-spec :largeutf8 [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
 (defmethod arrow-type-spec :largebinary [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
 (defmethod arrow-type-spec :largelist [_]
-  :arrow/type)
+  (s/keys :req-un [:arrow.schema.field.type/name]))
 
-(s/def :arrow/key-value (s/map-of string? string?))
+(s/def :arrow.schema.field.type/name (some-fn keyword? string?))
+(s/def :arrow.schema.field/type (s/multi-spec arrow-type-spec :name))
 
-(s/def :arrow.field/type (s/multi-spec arrow-type-spec :name))
-(s/def :arrow.field/name string?)
-(s/def :arrow.field/nullable boolean?)
-(s/def :arrow.field/children (s/coll-of :arrow.schema/field))
-(s/def :arrow.field/metadata :arrow/key-value)
-(s/def :arrow/field (s/keys :req-un [:arrow.field/type]
-                            :opt-on [:arrow.field/name :arrow.field/nullable :arrow.field/children :arrow.field/metadata]))
+(s/def :arrow.schema.field/name string?)
+(s/def :arrow.schema.field/nullable boolean?)
+(s/def :arrow.schema.field/children (s/coll-of :arrow.schema/field))
+(s/def :arrow.schema/field (s/keys :req-un [:arrow.schema.field/name :arrow.schema.field/type]
+                                   :opt-on [:arrow.schema.field/nullable :arrow.schema.field/children :arrow.schema/metadata]))
 
-(s/def :arrow.schema/fields (s/coll-of :arrow/field :min-count 1))
-(s/def :arrow.schema/metadata :arrow/key-value)
+(s/def :arrow.schema/fields (s/coll-of :arrow.schema/field :min-count 1))
+(s/def :arrow.schama/metadata (s/map-of string? string?))
 (s/def :arrow/schema (s/keys :req-un [:arrow.schema/fields] :opt-un [:arrow.schema/metadata]))
 
 (defn ->arrow-type ^org.apache.arrow.vector.types.pojo.ArrowType [arrow-type]
-  (s/assert :arrow/type arrow-type)
+  (s/assert :arrow.schema.field/type arrow-type)
   (.readValue arrow-type-reader (json/write-str arrow-type)))
 
 (defn ->field ^org.apache.arrow.vector.types.pojo.Field [field]
-  (s/assert :arrow/field field)
+  (s/assert :arrow.schema/field field)
   (.readValue field-reader (json/write-str field)))
 
 (defn ->schema ^org.apache.arrow.vector.types.pojo.Schema [schema]
