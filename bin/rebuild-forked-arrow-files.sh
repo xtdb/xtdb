a56114c2b29a4b0980cd7c44b5e83b56f48da6dd
James Henderson <jms@juxt.pro>
2021-10-12 15:51:53 +0100

    Add script to rebuild forked Arrow file

---
+++ bin/rebuild-forked-arrow-files.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$#" -eq 0 ]]; then echo "Usage: $0 <path-to-arrow-fork>"; exit 1; fi
+
+ARROW_PATH=$1
+CORE2_PATH=$(realpath $(dirname $0)/..)
+
+mkdir -p "$CORE2_PATH/core/src/org/apache/arrow/vector/complex"
+
+(
+    cd $1/java
+    mvn -pl vector compile -am
+
+    echo
+    echo Copying files...
+
+    cp vector/target/generated-sources/org/apache/arrow/vector/complex/DenseUnionVector.java "$CORE2_PATH/core/src/org/apache/arrow/vector/complex/"
+)
