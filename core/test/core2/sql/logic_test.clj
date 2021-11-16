23bb295bce5e3386827825dddb48a76d75227dfb
Håkan Råberg <hakan.raberg@gmail.com>
2021-11-16 14:18:08 +0100

    Basic parser for Sqllogitest format.

---
+++ core/test/core2/sql/logic_test.clj
@@ -0,0 +1,86 @@
+(ns core2.sql.logic-test
+  (:require [clojure.string :as str])
+  (:import java.security.MessageDigest
+           java.nio.charset.StandardCharsets))
+
+(defn- md5 ^String [^String s]
+  (->> (.getBytes s StandardCharsets/UTF_8)
+       (.digest (MessageDigest/getInstance "MD5"))
+       (BigInteger. 1)
+       (format "%032x")))
+
+(defmulti parse-record (fn [[x & xs]]
+                         (keyword (first (str/split x #"\s+")))))
+
+(defmethod parse-record :statement [[x & xs]]
+  (let [[_ mode] (str/split x #"\s+")
+        statement (str/join xs)]
+    {:type :statement
+     :mode (keyword mode)
+     :statement statement}))
+
+(defmethod parse-record :query [[x & xs]]
+  (let [[_ type-string sort-mode label] (str/split x #"\s+")
+        _ (prn xs)
+        [query _ result] (partition-by #{"----"} xs)
+        query (str/join query)
+        record {:type :query
+                :query query
+                :type-string (mapv str type-string)
+                :sort-mode (keyword sort-mode)
+                :label label}]
+    (if-let [[_ values hash] (and (= 1 (count result))
+                                  (re-find #"^(\d+) values hashing to (\p{XDigit}{32})$" (first result)))]
+      (assoc record :result-set-size (Long/parseLong values) :result-set-md5sum hash)
+      (assoc record :result-set (vec result)))))
+
+(defmethod parse-record :skipif [[x & xs]]
+  (let [[_ database-name] (str/split x #"\s+")]
+    (assoc (parse-record xs) :skipif database-name)))
+
+(defmethod parse-record :onlyif [[x & xs]]
+  (let [[_ database-name] (str/split x #"\s+")]
+    (assoc (parse-record xs) :onlyif database-name)))
+
+(defmethod parse-record :halt [xs]
+  (assert (= 1 (count xs)))
+  {:type :halt})
+
+(defmethod parse-record :hash-threshold [[x :as xs]]
+  (assert (= 1 (count xs)))
+  (let [[_ max-result-set-size] (str/split x #"\s+")]
+    {:type :hash-threshold
+     :max-result-set-size (Long/parseLong max-result-set-size)}))
+
+(defn parse-script [script]
+  (->> (str/split-lines script)
+       (map #(str/replace % #"\s*#.+$" ""))
+       (partition-by str/blank?)
+       (remove #(every? str/blank? %))
+       (map parse-record)))
+
+(comment
+  (parse-script
+   "# full line comment
+
+statement ok
+CREATE TABLE t1(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER)
+
+statement ok
+INSERT INTO t1(e,c,b,d,a) VALUES(103,102,100,101,104) # end of line comment
+
+halt
+
+hash-threshold 32
+
+query I nosort
+SELECT CASE WHEN c>(SELECT avg(c) FROM t1) THEN a*2 ELSE b*10 END
+  FROM t1
+ ORDER BY 1
+----
+30 values hashing to 3c13dee48d9356ae19af2515e05e6b54
+
+skipif postgresql
+query III rowsort label-xyzzy
+SELECT a x, b y, c z FROM t1
+"))
