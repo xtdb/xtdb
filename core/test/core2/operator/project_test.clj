3b16d69a79cdbd6a2c0c6954e8e2d7a82f249963
James Henderson <jms@juxt.pro>
2021-12-13 17:35:22 +0000

    simplify project-test/select-test, don't need to do exprs manually any more

--- core/test/core2/operator/project_test.clj
+++ core/test/core2/operator/project_test.clj
@@ -1,13 +1,10 @@
 (ns core2.operator.project-test
   (:require [clojure.test :as t]
+            [core2.expression :as expr]
             [core2.operator.project :as project]
             [core2.test-util :as tu]
-            [core2.types :as ty]
-            [core2.vector.indirect :as iv])
-  (:import core2.operator.project.ProjectionSpec
-           core2.types.LegType
-           org.apache.arrow.vector.BigIntVector
-           org.apache.arrow.vector.types.pojo.Schema))
+            [core2.types :as ty])
+  (:import org.apache.arrow.vector.types.pojo.Schema))
 
 (t/use-fixtures :each tu/with-allocator)
 
@@ -19,23 +16,7 @@
                                    [{:a 100, :b 83}]])
               project-cursor (project/->project-cursor tu/*allocator* cursor
                                                        [(project/->identity-projection-spec "a")
-
-                                                        (reify ProjectionSpec
-                                                          (project [_ allocator in-rel]
-                                                            (let [row-count (.rowCount in-rel)
-
-                                                                  a-col (.vectorForName in-rel "a")
-                                                                  ^BigIntVector a-vec (.getVector a-col)
-
-                                                                  b-col (.vectorForName in-rel "b")
-                                                                  ^BigIntVector b-vec (.getVector b-col)
-
-                                                                  out (BigIntVector. "c" allocator)]
-                                                              (.setValueCount out row-count)
-                                                              (dotimes [idx row-count]
-                                                                (.set out idx (+ (.get a-vec (.getIndex a-col idx))
-                                                                                 (.get b-vec (.getIndex b-col idx)))))
-                                                              (iv/->direct-vec out))))])]
+                                                        (expr/->expression-projection-spec "c" '(+ a b) {})])]
     (t/is (= [[{:a 12, :c 22}, {:a 0, :c 15}]
               [{:a 100, :c 183}]]
              (tu/<-cursor project-cursor)))))
--- core/test/core2/operator/select_test.clj
+++ core/test/core2/operator/select_test.clj
@@ -1,12 +1,10 @@
 (ns core2.operator.select-test
   (:require [clojure.test :as t]
+            [core2.expression :as expr]
             [core2.operator.select :as select]
             [core2.test-util :as tu]
             [core2.types :as ty])
-  (:import core2.operator.select.IRelationSelector
-           org.apache.arrow.vector.BigIntVector
-           org.apache.arrow.vector.types.pojo.Schema
-           org.roaringbitmap.RoaringBitmap))
+  (:import org.apache.arrow.vector.types.pojo.Schema))
 
 (t/use-fixtures :each tu/with-allocator)
 
@@ -19,17 +17,7 @@
                                      [{:a 100, :b 83}]
                                      [{:a 83, :b 100}]])
                 select-cursor (select/->select-cursor tu/*allocator* cursor
-                                                      (reify IRelationSelector
-                                                        (select [_ _al in-rel]
-                                                          (let [idxs (RoaringBitmap.)
-                                                                ^BigIntVector a-vec (.getVector (.vectorForName in-rel "a"))
-                                                                ^BigIntVector b-vec (.getVector (.vectorForName in-rel "b"))]
-                                                            (dotimes [idx (.rowCount in-rel)]
-                                                              (when (> (.get a-vec idx)
-                                                                       (.get b-vec idx))
-                                                                (.add idxs idx)))
-
-                                                            idxs))))]
+                                                      (expr/->expression-relation-selector '(> a b) {}))]
       (t/is (= [[{:a 12, :b 10}]
                 [{:a 100, :b 83}]]
                (tu/<-cursor select-cursor))))))
