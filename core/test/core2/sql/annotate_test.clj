de14916dd5c0c40e74094187bef7c2a0fe73993e
Håkan Råberg <hakan.raberg@gmail.com>
2022-01-27 09:26:51 +0100

    Refactor out test helpers.

--- core/test/core2/sql/annotate_test.clj
+++ core/test/core2/sql/annotate_test.clj
@@ -48,99 +48,110 @@ SELECT t1.d-t1.e, SUM(t1.a)
                 :dependent-columns #{{:identifiers ["t1" "b"] :table-id 5 :table-scope-id 1 :scope-id 9 :qualified? true :type :outer}}}]}
              (sql/analyze-query tree)))))
 
+(defn- invalid? [re q]
+  (let [[err :as errs] (:errs (sql/analyze-query (sql/parse q)))
+        err (or err "")]
+    (t/is (= 1 (count errs)) (pr-str errs))
+    (t/is (re-find re err))))
+
+(defn- valid? [q]
+  (let [errs (:errs (sql/analyze-query (sql/parse q)))]
+    (t/is (empty? errs))))
+
 (t/deftest test-parsing-errors-are-reported
-  (t/is (re-find #"Parse error at line 1, column 1:\nSELEC\n" (first (:errs (sql/analyze-query (sql/parse "SELEC")))))))
+  (invalid? #"Parse error at line 1, column 1:\nSELEC\n"
+            "SELEC"))
 
 (t/deftest test-scope-rules
-  (t/is (re-find #"XTDB requires fully-qualified columns: a at line 1, column 8"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT a FROM foo"))))))
+  (invalid? #"XTDB requires fully-qualified columns: a at line 1, column 8"
+            "SELECT a FROM foo")
+  (invalid? #"Table not in scope: bar at line 1, column 8"
+            "SELECT bar.a FROM foo")
 
-  (t/is (re-find #"Table not in scope: bar at line 1, column 8"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT bar.a FROM foo"))))))
-  (t/is (re-find #"Table not in scope: bar"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT bar.a FROM bar AS foo"))))))
+  (invalid? #"Table not in scope: bar"
+            "SELECT bar.a FROM bar AS foo")
 
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT bar.a FROM bar")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT bar.a FROM foo AS bar")))))
+  (valid? "SELECT bar.a FROM bar")
+  (valid? "SELECT bar.a FROM foo AS bar")
 
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT bar.a FROM foo AS bar ORDER BY bar.y")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT bar.a AS a FROM foo AS bar ORDER BY a")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT bar.x, bar.a + 1 FROM foo AS bar ORDER BY bar.a + 1")))))
+  (valid? "SELECT bar.a FROM foo AS bar ORDER BY bar.y")
+  (valid? "SELECT bar.a AS a FROM foo AS bar ORDER BY a")
+  (valid? "SELECT bar.x, bar.a + 1 FROM foo AS bar ORDER BY bar.a + 1")
 
-  (t/is (re-find #"Table not in scope: baz"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT bar.a FROM foo AS bar ORDER BY baz.y"))))))
+  (invalid? #"Table not in scope: baz"
+            "SELECT bar.a FROM foo AS bar ORDER BY baz.y")
 
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1 WHERE EXISTS (SELECT x.b FROM t1 AS x WHERE x.b < t1.b)")))))
+  (valid? "SELECT t1.b FROM t1 WHERE EXISTS (SELECT x.b FROM t1 AS x WHERE x.b < t1.b)")
+  (valid? "SELECT t1.a FROM t1 WHERE EXISTS (SELECT t1.a, COUNT(x.a) FROM t1 AS x WHERE x.b < t1.b GROUP BY x.a HAVING x.a = 1) GROUP BY t1.a")
 
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.a FROM t1 WHERE EXISTS (SELECT t1.a, COUNT(x.a) FROM t1 AS x WHERE x.b < t1.b GROUP BY x.a HAVING x.a = 1) GROUP BY t1.a")))))
+  (valid? "SELECT t1.b FROM t1, LATERAL (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2")
 
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1, LATERAL (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2")))))
-  (t/is (re-find #"Table not in scope: t1"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1, (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2"))))))
-  (t/is (re-find #"Table not in scope: t2"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1, LATERAL (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2, (SELECT x.b FROM t1 AS x WHERE x.b < t2.b) AS t3"))))))
+  (invalid? #"Table not in scope: t1"
+            "SELECT * FROM t1, (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2")
+  (invalid? #"Table not in scope: t2"
+            "SELECT * FROM t1, LATERAL (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2, (SELECT x.b FROM t1 AS x WHERE x.b < t2.b) AS t3")
 
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1 JOIN t2 USING (x)")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y)")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT * FROM foo, LATERAL (SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y AND t1.x = foo.x)) AS t2")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT * FROM foo WHERE foo.x = (SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y AND t1.x = foo.x))")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM foo, t1 JOIN t2 ON (t1.x = foo.y)")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM bar, (t1 NATURAL JOIN t2) JOIN t3 ON (t1.x = t3.y)")))))
+  (valid? "SELECT t1.b FROM t1 JOIN t2 USING (x)")
+  (valid? "SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y)")
+  (valid? "SELECT * FROM foo, LATERAL (SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y AND t1.x = foo.x)) AS t2")
+  (valid? "SELECT * FROM foo WHERE foo.x = (SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y AND t1.x = foo.x))")
+  (valid? "SELECT t1.b FROM foo, t1 JOIN t2 ON (t1.x = foo.y)")
+  (valid? "SELECT t1.b FROM bar, (t1 NATURAL JOIN t2) JOIN t3 ON (t1.x = t3.y)")
 
-  (t/is (re-find #"Table not in scope: foo"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM (SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y AND t1.x = foo.x)) AS t2"))))))
-  (t/is (re-find #"Table not in scope: t1"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT t2.b FROM (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2, t1"))))))
-  (t/is (re-find #"Table not in scope: bar"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT bar.a FROM foo WHERE EXISTS (SELECT bar.b FROM bar WHERE foo.a < bar.b)"))))))
-  (t/is (re-find #"Table not in scope: foo"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM foo AS baz WHERE EXISTS (SELECT bar.b FROM bar WHERE foo.a < bar.b)")))))))
+  (invalid? #"Table not in scope: foo"
+            "SELECT * FROM (SELECT t1.b FROM t1 JOIN t2 ON (t1.x = t2.y AND t1.x = foo.x)) AS t2")
+  (invalid? #"Table not in scope: t1"
+            "SELECT t2.b FROM (SELECT x.b FROM t1 AS x WHERE x.b < t1.b) AS t2, t1")
+  (invalid? #"Table not in scope: bar"
+            "SELECT bar.a FROM foo WHERE EXISTS (SELECT bar.b FROM bar WHERE foo.a < bar.b)")
+  (invalid? #"Table not in scope: foo"
+            "SELECT * FROM foo AS baz WHERE EXISTS (SELECT bar.b FROM bar WHERE foo.a < bar.b)"))
 
 (t/deftest test-variable-duplication
-  (t/is (re-find #"Table variable duplicated: baz"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM foo AS baz, baz"))))))
-  (t/is (re-find #"CTE query name duplicated: foo"
-                 (first (:errs (sql/analyze-query (sql/parse "WITH foo AS (SELECT 1 FROM foo), foo AS (SELECT 1 FROM foo) SELECT * FROM foo"))))))
-  (t/is (re-find #"Column name duplicated: foo"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM (SELECT 1, 2 FROM foo) AS bar (foo, foo)")))))))
+  (invalid? #"Table variable duplicated: baz"
+            "SELECT * FROM foo AS baz, baz")
+  (invalid? #"CTE query name duplicated: foo"
+            "WITH foo AS (SELECT 1 FROM foo), foo AS (SELECT 1 FROM foo) SELECT * FROM foo")
+  (invalid? #"Column name duplicated: foo"
+            "SELECT * FROM (SELECT 1, 2 FROM foo) AS bar (foo, foo)"))
 
 (t/deftest test-grouping-columns
-  (t/is (re-find #"Column reference is not a grouping column: t1.a"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT t1.a FROM t1 GROUP BY t1.b"))))))
-  (t/is (re-find #"Column reference is not a grouping column: t1.a"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1 GROUP BY t1.b HAVING t1.a"))))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b, COUNT(t1.a) FROM t1 GROUP BY t1.b")))))
-  (t/is (re-find #"Column reference is not a grouping column: t1.a"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT t1.a, COUNT(t1.b) FROM t1"))))))
+  (invalid? #"Column reference is not a grouping column: t1.a"
+            "SELECT t1.a FROM t1 GROUP BY t1.b")
+  (invalid? #"Column reference is not a grouping column: t1.a"
+            "SELECT t1.b FROM t1 GROUP BY t1.b HAVING t1.a")
+  (valid? "SELECT t1.b, COUNT(t1.a) FROM t1 GROUP BY t1.b")
+  (invalid? #"Column reference is not a grouping column: t1.a"
+            "SELECT t1.a, COUNT(t1.b) FROM t1")
 
-  (t/is (re-find #"Outer column reference is not an outer grouping column: t1.b"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1 WHERE (1, 1) = (SELECT t1.b, COUNT(*) FROM t2)"))))))
-  (t/is (re-find #"Within group varying column reference is an outer column: t1.b"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1 WHERE 1 = (SELECT COUNT(t1.b) FROM t2)"))))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT t1.b FROM t1 WHERE 1 = (SELECT t1.b, COUNT(t2.a) FROM t2) GROUP BY t1.b"))))))
+  (invalid? #"Outer column reference is not an outer grouping column: t1.b"
+            "SELECT t1.b FROM t1 WHERE (1, 1) = (SELECT t1.b, COUNT(*) FROM t2)")
+  (invalid? #"Within group varying column reference is an outer column: t1.b"
+            "SELECT t1.b FROM t1 WHERE 1 = (SELECT COUNT(t1.b) FROM t2)")
+  (valid? "SELECT t1.b FROM t1 WHERE 1 = (SELECT t1.b, COUNT(t2.a) FROM t2) GROUP BY t1.b"))
 
 (t/deftest test-clauses-not-allowed-to-contain-aggregates-or-queries
-  (t/is (re-find #"Aggregate functions cannot contain aggregate functions"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT COUNT(SUM(t1.b)) FROM t1"))))))
-  (t/is (re-find #"Aggregate functions cannot contain nested queries"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT COUNT((SELECT 1 FROM foo)) FROM t1"))))))
+  (invalid? #"Aggregate functions cannot contain aggregate functions"
+            "SELECT COUNT(SUM(t1.b)) FROM t1")
+  (invalid? #"Aggregate functions cannot contain nested queries"
+            "SELECT COUNT((SELECT 1 FROM foo)) FROM t1")
 
-  (t/is (re-find #"Sort specifications cannot contain aggregate functions"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 ORDER BY COUNT(t1.a)"))))))
-  (t/is (re-find #"Sort specifications cannot contain nested queries"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 ORDER BY (SELECT 1 FROM foo)"))))))
+  (invalid? #"Sort specifications cannot contain aggregate functions"
+            "SELECT * FROM t1 ORDER BY COUNT(t1.a)")
+  (invalid? #"Sort specifications cannot contain nested queries"
+            "SELECT * FROM t1 ORDER BY (SELECT 1 FROM foo)")
 
-  (t/is (re-find #"WHERE clause cannot contain aggregate functions"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 WHERE COUNT(t1.a)"))))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 WHERE 1 = (SELECT COUNT(t1.a) FROM t1)"))))))
+  (invalid? #"WHERE clause cannot contain aggregate functions"
+            "SELECT * FROM t1 WHERE COUNT(t1.a)")
+  (valid? "SELECT * FROM t1 WHERE 1 = (SELECT COUNT(t1.a) FROM t1)"))
 
 (t/deftest test-fetch-and-offset-type
-  (t/is (re-find #"Fetch first row count must be an integer"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 FETCH FIRST 'foo' ROWS ONLY"))))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 FETCH FIRST 1 ROWS ONLY")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 FETCH FIRST :foo ROWS ONLY")))))
+  (invalid? #"Fetch first row count must be an integer"
+            "SELECT * FROM t1 FETCH FIRST 'foo' ROWS ONLY")
+  (valid? "SELECT * FROM t1 FETCH FIRST 1 ROWS ONLY")
+  (valid? "SELECT * FROM t1 FETCH FIRST :foo ROWS ONLY")
 
-  (t/is (re-find #"Offset row count must be an integer"
-                 (first (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 OFFSET 'foo' ROWS"))))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 OFFSET 1 ROWS")))))
-  (t/is (empty? (:errs (sql/analyze-query (sql/parse "SELECT * FROM t1 OFFSET :foo ROWS"))))))
+  (invalid? #"Offset row count must be an integer"
+            "SELECT * FROM t1 OFFSET 'foo' ROWS")
+  (valid? "SELECT * FROM t1 OFFSET 1 ROWS")
+  (valid? "SELECT * FROM t1 OFFSET :foo ROWS"))
