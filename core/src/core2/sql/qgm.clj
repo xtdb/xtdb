a24056afdb7b080db62714d681ec9bf78f1d9b93
Håkan Råberg <hakan.raberg@gmail.com>
2022-02-17 19:19:35 +0100

    Adding recursive rule support to QGM internal triple store spike.

--- core/src/core2/sql/qgm.clj
+++ core/src/core2/sql/qgm.clj
@@ -67,8 +67,8 @@
                        :retract-entity (s/cat :op #{:db/retractEntity} :e :db/id)))
 
 (s/def :db.query/find (s/coll-of :db/logic-var :kind vector? :min-count 1))
-(s/def :db.query.where.clause/rule (s/cat :rule-name symbol? :args (s/* logic-var?)))
-(s/def :db.query.where.clause/triple (s/cat :e any? :a (some-fn keyword? logic-var?) :v any?))
+(s/def :db.query.where.clause/rule (s/and list? (s/cat :rule-name symbol? :args (s/* logic-var?))))
+(s/def :db.query.where.clause/triple (s/and vector? (s/cat :e any? :a (some-fn keyword? logic-var?) :v any?)))
 (s/def :db.query.where/clause (s/or :triple :db.query.where.clause/triple
                                     :rule :db.query.where.clause/rule))
 (s/def :db.query/where (s/coll-of :db.query.where/clause :kind vector? :min-count 1))
@@ -152,8 +152,8 @@
                    (s/assert :db/query)
                    (s/conform :db/query))
         {tuple-keys :keys :keys [find in where] :or {in '[$]}} query
-        keys (when keys
-               (map (comp keyword name) keys))
+        tuple-keys (when tuple-keys
+                     (map (comp keyword name) tuple-keys))
         {db '$ rules '% :as env} (zipmap in inputs)
         name->rules (some->> rules
                              (s/conform :db.query/rules)
@@ -181,13 +181,34 @@
                                  (take-while (complement logic-var?)))]
                 (some->> (not-empty (apply datoms db index pattern))
                          (mapcat (partial datom-step env clauses)))))
+            (rule-step [env [[_ {:keys [rule-name args] :as rule}] & next-clauses :as clauses]]
+              (when-let [rule (get name->rules rule-name)]
+                (let [active-env (select-keys env (conj args '$ '%))
+                      rule-key (mapv env args)
+                      rule-table (::rule-table (meta env))]
+                  (mapcat
+                   (fn [{{:keys [bound-vars free-vars]} :rule-head :keys [rule-body] :as rule-leg}]
+                     (let [rule-leg-key [(System/identityHashCode rule-leg) rule-key]]
+                       (if (contains? rule-table rule-leg-key)
+                         []
+                         (let [vars (concat bound-vars free-vars)
+                               rename-map (zipmap args vars)
+                               rename-map-inv (set/map-invert rename-map)
+                               active-env (vary-meta
+                                           (set/rename-keys active-env rename-map)
+                                           update ::rule-table (fnil conj #{}) rule-leg-key)]
+                           (some->> (not-empty (clause-step active-env rule-body))
+                                    (mapcat #(clause-step (merge env (set/rename-keys (select-keys % vars) rename-map-inv)) next-clauses)))))))
+                   rule))))
             (clause-step [env [[clause-type] :as clauses]]
               (if clause-type
                 (case clause-type
-                  :triple (triple-step env clauses))
-                [(cond->> (mapv env find)
-                   tuple-keys (zipmap tuple-keys))]))]
-      (clause-step env where))))
+                  :triple (triple-step env clauses)
+                  :rule (rule-step env clauses))
+                [env]))]
+      (for [env (clause-step env where)]
+        (cond->> (mapv env find)
+          tuple-keys (zipmap tuple-keys))))))
 
 (defn entity [db eid]
   (reduce
@@ -282,3 +303,58 @@
          [?b :qgm.box.body/quantifiers ?q]
          [?q :qgm.quantifier/type ?t]]
        db 'b3)))
+
+(comment
+
+  (let [db (transact {}
+                     '[{:db/id :john :parent :douglas}
+                       {:db/id :bob :parent :john}
+                       {:db/id :ebbon :parent :bob}])]
+    (= #{[:ebbon :bob]
+         [:bob :john]
+         [:john :douglas]
+         [:bob :douglas]
+         [:ebbon :john]
+         [:ebbon :douglas]}
+
+       (set (q '{:find [?a ?b]
+                 :in [$ %]
+                 :where [(ancestor ?a ?b)]}
+               db
+               '[[(ancestor ?a ?b)
+                  [?a :parent ?b]]
+                 [(ancestor ?a ?b)
+                  [?a :parent ?c]
+                  (ancestor ?c ?b)]]))))
+
+  (let [db (transact {}
+                     '[{:db/id :a :edge :b}
+                       {:db/id :b :edge :c}
+                       {:db/id :c :edge :d}
+                       {:db/id :d :edge :a}])]
+    (= #{[:a :a]
+         [:a :d]
+         [:a :c]
+         [:a :b]
+         [:b :a]
+         [:b :d]
+         [:b :c]
+         [:b :b]
+         [:c :a]
+         [:c :d]
+         [:c :c]
+         [:c :b]
+         [:d :b]
+         [:d :c]
+         [:d :d]
+         [:d :a]}
+
+       (set (q '{:find [?x ?y]
+                 :in [$ %]
+                 :where [(path ?x ?y)]}
+               db
+               '[[(path ?x ?y)
+                  [?x :edge ?y]]
+                 [(path ?x ?y)
+                  [?x :edge ?z]
+                  (path ?z ?y)]])))))
