7aaf9a6175ecba1b5028d31a1b1a95652054a722
Håkan Råberg <hakan.raberg@gmail.com>
2022-02-22 00:06:35 +0100

    Basic dot rendered for QGM.

--- core/src/core2/sql/qgm.clj
+++ core/src/core2/sql/qgm.clj
@@ -1,5 +1,6 @@
 (ns core2.sql.qgm
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.string :as str]
+            [clojure.spec.alpha :as s]))
 
 ;; Query Graph Model using internal triple store.
 ;; http://projectsweb.cs.washington.edu/research/projects/db/weld/pirahesh-starburst-92.pdf
@@ -49,112 +50,123 @@
 (s/def :qgm/node (s/or :box :qgm/box :quantifier :qgm/quantifier :predicate :qgm/predicate))
 (s/def :qgm/graph (s/coll-of :qgm/node :kind set?))
 
-(comment
-  '[{:db/id b1
-     :qgm.box/type :qgm.box.type/base-table
-     :qgm.box.base-table/name inventory}
-    {:db/id b2
-     :qgm.box/type :qgm.box.type/base-table
-     :qgm.box.base-table/name quotations}
-    {:db/id b3
-     :qgm.box/type :qgm.box.type/select
-     :qgm.box.head/distinct? true
-     :qgm.box.head/columns [partno descr suppno]
-     :qgm.box.body/columns [q1.partno q1.descr q2.suppno]
-     :qgm.box.body/distinct :qgm.box.body.distinct/enforce
-     :qgm.box.body/quantifiers #{q1 q2 q4}}
-    {:db/id b4
-     :qgm.box/type :qgm.box.type/select
-     :qgm.box.head/distinct? false
-     :qgm.box.head/columns [price]
-     :qgm.box.body/columns [q3.price]
-     :qgm.box.body/distinct :qgm.box.body.distinct/permit
-     :qgm.box.body/quantifiers #{q3}}
-
-    {:db/id q1
-     :qgm.quantifier/type :qgm.quantifier.type/foreach
-     :qgm.quantifier/columns [partno descr]
-     :qgm.quantifier/ranges-over b1}
-    {:db/id q2
-     :qgm.quantifier/type :qgm.quantifier.type/foreach
-     :qgm.quantifier/columns [partno price]
-     :qgm.quantifier/ranges-over b2}
-    {:db/id q3
-     :qgm.quantifier/type :qgm.quantifier.type/foreach
-     :qgm.quantifier/columns [partno price]
-     :qgm.quantifier/ranges-over b2}
-    {:db/id q4
-     :qgm.quantifier/type :qgm.quantifier.type/all
-     :qgm.quantifier/columns [price]
-     :qgm.quantifier/ranges-over b4}
-
-    {:db/id p1
-     :qgm.predicate/expression (= q1.descr "engine")
-     :qgm.predicate/quantifiers #{q1}}
-    {:db/id p2
-     :qgm.predicate/expression (= q1.partno q2.partno)
-     :qgm.predicate/quantifiers #{q1 q2}}
-    {:db/id p3
-     :qgm.predicate/expression (<= q2.partno q4.partno)
-     :qgm.predicate/quantifiers #{q2 q4}}
-    {:db/id p4
-     :qgm.predicate/expression (= q2.partno q3.partno)
-     :qgm.predicate/quantifiers #{q2 q3}}])
-
 ;; GraphViz:
 ;; https://dreampuf.github.io/GraphvizOnline/
 
+(defn- box->dot [box qs]
+  (case (:qgm.box/type box)
+    :qgm.box.type/base-table
+    (format "
+  subgraph cluster_%s {
+    label=\"BASE (%s)\"
+    style=dashed
+    %s [shape=none, label=\"%s\", fontname=courier]
+  }" (:db/id box) (:db/id box) (:db/id box) (:qgm.box.base-table/name box))
+    :qgm.box.type/select
+    (format "
+  subgraph cluster_%s {
+    label=\"SELECT (%s)\"
+    style=dashed
+    {
+      rank=source
+      %s [label = \"%s\", shape=record, fontname=courier]
+    }
+    {
+      rank=same
+      %s
+    }
+  }"
+            (:db/id box)
+            (:db/id box)
+            (:db/id box)
+            (format "{ HEAD: DISTINCT=%s\\l | { { { %s\\l } | { %s\\l } } } | BODY: DISTINCT=%s\\l }"
+                    (:qgm.box.head/distinct? box)
+                    (str/join "\\l|" (:qgm.box.head/columns box))
+                    (str/join "\\l|" (:qgm.box.body/columns box))
+                    (str/upper-case (name (:qgm.box.body/distinct box))))
+            (let [id->q (zipmap (map :db/id qs) qs)]
+              (str/join "\n      "
+                        (for [q (sort (:qgm.box.body/quantifiers box))]
+                          (format "%s [label=\"%s(%s)\", shape=circle, style=filled, margin=0, fontname=courier]"
+                                  q q (str/upper-case (first (name (get-in id->q [q :qgm.quantifier/type] "F")))))))))))
 
-;; digraph G {
-;;     compound = true
-;;     fontname = courier
-;;   subgraph cluster_3 {
-;;     label = "SELECT (3)"
-;;     style = dashed
-;;     {
-;;       rank = source
-;;       b3 [label = "head: distinct=true\l[partno   , descr   , suppno   ]\l[q1.partno, q1.descr, q2.suppno]\lbody: distinct=enforce\l", shape=box, fontname = courier]
-;;     }
-;;     {
-;;       rank = same
-;;       q1 [label="q1(F)", shape=circle, style=filled, margin=0, fontname = courier]
-;;       q2 [label="q2(F)", shape=circle, style=filled, margin=0, fontname = courier]
-;;       q4 [label="q4(A)", shape=circle, style=filled, margin=0, fontname = courier]
-;;     }
-;;   }
+(defn- quantifier->dot [q]
+  (format "%s -> %s [label=\"%s\" lhead=cluster_%s, fontname=courier]"
+          (:db/id q) (:qgm.quantifier/ranges-over q) (str/join ", " (:qgm.quantifier/columns q)) (:qgm.quantifier/ranges-over q)))
 
-;;   subgraph cluster_4 {
-;;     label = "SELECT (4)"
-;;     style = dashed
-;;     {
-;;       rank = source
-;;       b4 [label = "head: distinct=false\l[price   ]\l[q3.price]\lbody: distinct=permit\l", shape=box, fontname = courier]
-;;     }
-;;     {
-;;       rank = same
-;;       q3 [label="q3(F)", shape=circle, style=filled, margin=0, fontname = courier]
-;;     }
-;;   }
+(defn- predicate->dot [p]
+  (format "%s -> %s [label=\"%s\", dir=none, fontname=courier, color=grey]"
+          (first (:qgm.predicate/quantifiers p))
+          (or (second (:qgm.predicate/quantifiers p))
+              (first (:qgm.predicate/quantifiers p)))
+          (str/replace (str (:qgm.predicate/expression p)) "\"" "'")))
 
-;;   subgraph cluster_2 {
-;;       label = "BASE (2)"
-;;       style=dashed
-;;       b2 [shape=none, label="quotations", fontname = courier]
-;;   }
+(defn- qgm->dot [bs qs ps]
+  (format "
+digraph {
+  compound=true
+  fontname=courier
+  %s
 
-;;   subgraph cluster_1 {
-;;       label = "BASE (1)"
-;;       style=dashed
-;;       b1 [shape=none, label="inventory", fontname = courier]
-;;   }
+  %s
 
-;;   q1 -> b1 [label="partno, descr" lhead=cluster_1, fontname = courier]
-;;   q2 -> b2 [label="partno, price" lhead=cluster_2, fontname = courier]
-;;   q3 -> b2 [label="partno, price" lhead=cluster_2, fontname = courier]
-;;   q4 -> b4 [label="price" lhead=cluster_4, fontname = courier]
+  %s
+}"
+          (str/join "\n"
+                    (for [b (sort-by :db/id bs)]
+                      (box->dot b qs)))
+          (str/join "\n  " (map quantifier->dot (sort-by :db/id qs)))
+          (str/join "\n  " (map predicate->dot (sort-by :db/id ps)))))
 
-;;   q1 -> q1 [label="q1.descr = \"engine\"", dir=none, fontname = courier]
-;;   q1 -> q2 [label="q1.partno = q2.partno", dir=none, fontname = courier]
-;;   q2 -> q4 [label="q2.partno <= q4.partno", dir=none, fontname = courier]
-;;   q2 -> q3 [label="q2.partno = q3.partno", dir=none, fontname = courier]
-;; }
+(comment
+
+  (println
+   (qgm->dot
+    '[{:db/id b1
+       :qgm.box/type :qgm.box.type/base-table
+       :qgm.box.base-table/name inventory}
+      {:db/id b2
+       :qgm.box/type :qgm.box.type/base-table
+       :qgm.box.base-table/name quotations}
+      {:db/id b3
+       :qgm.box/type :qgm.box.type/select
+       :qgm.box.head/distinct? true
+       :qgm.box.head/columns [partno descr suppno]
+       :qgm.box.body/columns [q1.partno q1.descr q2.suppno]
+       :qgm.box.body/distinct :qgm.box.body.distinct/enforce
+       :qgm.box.body/quantifiers #{q1 q2 q4}}
+      {:db/id b4
+       :qgm.box/type :qgm.box.type/select
+       :qgm.box.head/distinct? false
+       :qgm.box.head/columns [price]
+       :qgm.box.body/columns [q3.price]
+       :qgm.box.body/distinct :qgm.box.body.distinct/permit
+       :qgm.box.body/quantifiers #{q3}}]
+    '[{:db/id q1
+       :qgm.quantifier/type :qgm.quantifier.type/foreach
+       :qgm.quantifier/columns [partno descr]
+       :qgm.quantifier/ranges-over b1}
+      {:db/id q2
+       :qgm.quantifier/type :qgm.quantifier.type/foreach
+       :qgm.quantifier/columns [partno price]
+       :qgm.quantifier/ranges-over b2}
+      {:db/id q3
+       :qgm.quantifaier/type :qgm.quantifier.type/foreach
+       :qgm.quantifier/columns [partno price]
+       :qgm.quantifier/ranges-over b2}
+      {:db/id q4
+       :qgm.quantifier/type :qgm.quantifier.type/all
+       :qgm.quantifier/columns [price]
+       :qgm.quantifier/ranges-over b4}]
+    '[{:db/id p1
+       :qgm.predicate/expression (= q1.descr "engine")
+       :qgm.predicate/quantifiers #{q1}}
+      {:db/id p2
+       :qgm.predicate/expression (= q1.partno q2.partno)
+       :qgm.predicate/quantifiers #{q1 q2}}
+      {:db/id p3
+       :qgm.predicate/expression (<= q2.partno q4.partno)
+       :qgm.predicate/quantifiers #{q2 q4}}
+      {:db/id p4
+       :qgm.predicate/expression (= q2.partno q3.partno)
+       :qgm.predicate/quantifiers #{q2 q3}}])))
