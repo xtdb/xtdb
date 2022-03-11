3080cbcc85f023458a80c4b991702109a8aa55d9
Håkan Råberg <hakan.raberg@gmail.com>
2022-03-11 10:51:29 +0100

    Removing example of better variant of passing lhs for ANY/ALL in as a single parameter as this will derail into maintaining the needed projection - maybe one for later.

--- core/test/core2/sql/plan_test.clj
+++ core/test/core2/sql/plan_test.clj
@@ -327,19 +327,6 @@
                  [:table [{:subquery__1_$exists$ false}]]]]]])
 
     ;; ALL as expression in WHERE clause:
-
-    ;; NOTE: Potential alternatives:
-
-    ;; 1. collect all dependent columns inside lhs and send in one by
-    ;; one, and expand lhs and then postwalk-replace the columns with
-    ;; their parameters. Due to the nil? check we may want to
-    ;; calculate lhs in one project first?
-
-    ;; 2. calculate lhs on the outside around the independent relation
-    ;; and send it in as a single value. This would be easier if we
-    ;; had a Map operator, as it requires maintaining the full
-    ;; projection of the incoming relation. Though we can most likely
-    ;; calculate this in most cases.
     (valid? "SELECT x.y FROM x WHERE x.z > ALL (SELECT y.z FROM y)"
             '[:rename {x__2_y y}
               [:project [x__2_y]
@@ -391,40 +378,6 @@
                        [:project [y__3_z]
                         [:rename y__3 [:scan [z]]]]]]]]
                    [:table [{:subquery__1_$any$ false}]]]]]]]])
-
-
-    ;; ANY variant which calculates lhs as a single parameter and
-    ;; expands its expression in the correct scope as a project
-    ;; without need for postwalk-replace etc.
-    (valid? "SELECT x.y FROM x WHERE (x.z = 1) > ANY (SELECT y.z FROM y)"
-            '[:rename {x__2_y y}
-              [:project [x__2_y]
-               [:select subquery__1_$any_out$
-                [:apply
-                 :cross-join
-                 ;; Pass in lhs as a single parameter.
-                 {subquery__1_$any_in$ ?subquery__1_$any_in$}
-                 #{}
-                 ;; Calculate lhs, needs to maintain the rest of the
-                 ;; projection. If there are several ANY/ALL like
-                 ;; this, they need to ensure their introduced
-                 ;; parameters are still projected by the time their
-                 ;; wrapping apply is reached. Some ways of expanding
-                 ;; these would probably maintain that invariant, a
-                 ;; parameter is added, used, and then maybe dropped
-                 ;; as the next projection wouldn't know of it.
-                 [:project [x__2 {subquery__1_$any_in$ (= x__2_z 1)}]
-                  [:rename x__2 [:scan [y z]]]]
-                 [:top {:limit 1}
-                  [:union-all
-                   [:project [{subquery__1_$any_out$ true}]
-                    ;; Usage of single lhs parameter.
-                    [:select (> ?subquery__1_$any_in$ subquery__1_z)
-                     [:rename subquery__1
-                      [:rename {y__3_z z}
-                       [:project [y__3_z]
-                        [:rename y__3 [:scan [z]]]]]]]]
-                   [:table [{:subquery__1_$any_out$ false}]]]]]]]])
 
     ;; LATERAL derived table
     (valid? "SELECT x.y, y.z FROM x, LATERAL (SELECT z.z FROM z WHERE z.z = x.y) AS y"
