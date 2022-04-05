6930a2ef52b1f0ee25271ce9e0262514c5cb4bf5
Håkan Råberg <hakan.raberg@gmail.com>
2022-04-05 18:00:24 +0200

    Fixing some message flow issues. Default type to JSON, actually execute query and return results over pgwire.

--- core/dev/core2/sql/pgwire.clj
+++ core/dev/core2/sql/pgwire.clj
@@ -1,8 +1,10 @@
 (ns core2.sql.pgwire
-  (:require [core2.sql.plan :as plan]
+  (:require [core2.api :as c2]
+            [core2.sql.plan :as plan]
             [core2.sql.analyze :as sem]
             [core2.sql :as sql]
             [core2.rewrite :as r]
+            [core2.local-node :as node]
             [core2.util :as util]
             [clojure.string :as str])
   (:import java.nio.charset.StandardCharsets
@@ -103,6 +105,10 @@
                           (fn [out]
                             (write-c-string out command))))
 
+(def ^:private ^:const ^long text-format-code 0)
+
+(def ^:private ^:const ^long json-oid 114)
+
 (defn- send-row-description [^DataOutputStream out columns]
   (send-message-with-body out
                           (byte \T)
@@ -111,10 +117,10 @@
                             (doseq [column columns
                                     :let [table-oid 0
                                           column-attribute-number 0
-                                          oid 0
+                                          oid json-oid
                                           data-type-size 0
                                           type-modifier 0
-                                          format-code 0]]
+                                          format-code text-format-code]]
                               (doto out
                                 (write-c-string column)
                                 (.writeInt table-oid)
@@ -127,7 +133,7 @@
 (def ^:private message-code->type '{\P :pgwire/parse
                                     \B :pgwire/bind
                                     \Q :pgwire/query
-                                    \F :pgwire/flush
+                                    \H :pgwire/flush
                                     \S :pgwire/sync
                                     \X :pgwire/terminate
                                     \D :pgwire/describe
@@ -194,6 +200,10 @@
   (.flush out)
   session)
 
+(defmethod handle-message :pgwire/sync [session message ^DataOutputStream out]
+  (send-ready-for-query out (byte \I))
+  session)
+
 (defmethod handle-message :pgwire/terminate [session message out]
   (util/try-close out)
   session)
@@ -230,12 +240,13 @@
         (assoc-in session [:parameters k] v))
 
       ;; HACK: not running query and returning the data.
-      (let [{:keys [plan]} (meta bind-message)]
-        (doseq [x [1 2]
-                y [3 4]]
-          (send-data-row out [(.getBytes (str x) StandardCharsets/UTF_8)
-                              (.getBytes (str y) StandardCharsets/UTF_8)]))
-        (send-command-complete out (str (statement-command parse-message) " 2"))
+      (let [node (:node (meta session))
+            projection (mapv keyword (query-projection (:tree (meta parse-message))))
+            result (c2/sql-query node (:pgwire.parse/query-string parse-message))]
+        (doseq [row result]
+          (send-data-row out (for [column (mapv row projection)]
+                               (.getBytes (str column) StandardCharsets/UTF_8))))
+        (send-command-complete out (str (statement-command parse-message) " " (count result)))
         session))))
 
 (defmethod handle-message :pgwire/query [session message out]
@@ -262,8 +273,6 @@
     (if (.isClosed socket)
       session
       (do (prn session)
-          (send-ready-for-query out (byte \I))
-
           (let [message-code (char (.readByte in))]
             (if-let [message-type (get message-code->type message-code)]
               (let [size (- (.readInt in) Integer/BYTES)
@@ -303,6 +312,8 @@
           (doseq [[k v] parameters]
             (send-parameter-status out k v))
 
+          (send-ready-for-query out (byte \I))
+
           (update session :parameters merge (:pgwire.startup-message/parameters startup-message)))
 
         :else
@@ -312,9 +323,9 @@
   (try
     (with-open [in (DataInputStream. (.getInputStream socket))
                 out (DataOutputStream. (.getOutputStream socket))]
-      (let [session {:parameters (:parameters server)
-                     :prepared-statements {}
-                     :portals {}}
+      (let [session (with-meta {:parameters (:parameters server)
+                                :prepared-statements {}
+                                :portals {}} {:node (:node server)})
             session (pg-establish-connection session in out)]
         (pg-message-exchange session socket in out)))
     (catch Throwable t
@@ -335,29 +346,30 @@
         (when-not (.isClosed server-socket)
           (throw e))))))
 
-(defrecord PgWireServer [parameters ^ServerSocket server-socket ^ExecutorService pool]
+(defrecord PgWireServer [parameters node ^ServerSocket server-socket ^ExecutorService pool]
   Closeable
   (close [_]
     (util/try-close server-socket)
     (util/shutdown-pool pool)))
 
-(defn ->pg-wire-server ^core2.sql.pgwire.PgWireServer [parameters ^long port ^long num-threads]
+(defn ->pg-wire-server ^core2.sql.pgwire.PgWireServer [parameters node ^long port ^long num-threads]
   (let [server-socket (ServerSocket. port)
         pool (Executors/newFixedThreadPool num-threads (util/->prefix-thread-factory "pgwire-connection-"))
-        server (->PgWireServer parameters server-socket pool)]
+        server (->PgWireServer parameters node server-socket pool)]
     (doto (Thread. #(pg-accept server))
       (.start))
     server))
 
 (comment
   (do (require '[juxt.clojars-mirrors.nextjdbc.v1v2v674.next.jdbc :as jdbc])
-      (with-open [server (->pg-wire-server {"server_version" "14"
+      (require '[cheshire.core :as json])
+      (with-open [node (node/start-node {})
+                  server (->pg-wire-server {"server_version" "14"
                                             "server_encoding" "UTF8"
                                             "TimeZone" "UTC"}
+                                           node
                                            5432
                                            16)]
-        (Thread/sleep 1000)
         (with-open [c (jdbc/get-connection "jdbc:postgresql://:5432/test?user=test&password=test")]
-          (Thread/sleep 1000)
-          (prn (jdbc/execute! c ["SELECT * FROM (VALUES 1, 2) AS x (a), (VALUES 3, 4) AS y (b)"]))
-          (Thread/sleep 1000)))))
+          (vec (for [row (jdbc/execute! c ["SELECT * FROM (VALUES 1, 2) AS x (a), (VALUES 3, 4) AS y (b)"])]
+                 (update-vals row (comp json/parse-string str))))))))
