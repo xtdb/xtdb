05a4b320d4e321a909dfe861b2af216f21615cdc
Håkan Råberg <hakan.raberg@gmail.com>
2022-04-25 17:48:35 +0200

    Add top-level test dir to refresh dirs.

--- dev/user.clj
+++ dev/user.clj
@@ -6,12 +6,13 @@
 (alter-var-root #'*warn-on-reflection* (constantly true))
 (ctn/disable-reload!)
 
-(apply ctn/set-refresh-dirs (for [^File dir (concat (.listFiles (io/file "."))
-                                                    (.listFiles (io/file "modules")))
-                                  :when (and (.isDirectory dir)
-                                             (.exists (io/file dir "deps.edn")))
-                                  sub-dir #{"src" "test"}]
-                              (io/file dir sub-dir)))
+(apply ctn/set-refresh-dirs (cons (io/file "test")
+                                  (for [^File dir (concat (.listFiles (io/file "."))
+                                                          (.listFiles (io/file "modules")))
+                                        :when (and (.isDirectory dir)
+                                                   (.exists (io/file dir "deps.edn")))
+                                        sub-dir #{"src" "test"}]
+                                    (io/file dir sub-dir))))
 
 (defn reset []
   (ctn/refresh))
