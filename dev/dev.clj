a38d97616e3e62665921a32f1bb75615addde24b
Dan Stone <wotbrew@gmail.com>
2022-04-26 17:19:41 +0100

    Add clj-async-profiler and dev util

--- dev/dev.clj
+++ dev/dev.clj
@@ -7,7 +7,8 @@
             [integrant.core :as i]
             [integrant.repl :as ir]
             [core2.operator :as op]
-            [core2.snapshot :as snap])
+            [core2.snapshot :as snap]
+            [clj-async-profiler.core :as clj-async-profiler])
   (:import [ch.qos.logback.classic Level Logger]
            java.time.Duration
            org.slf4j.LoggerFactory))
@@ -55,6 +56,41 @@
 (def go ir/go)
 (def halt ir/halt)
 (def reset ir/reset)
+
+(def profiler-port 5001)
+
+(defonce profiler-server
+         (delay
+           (let [port profiler-port
+                 url (str "http://localhost:" port)]
+             (println "Starting serving profiles on" url)
+             (clj-async-profiler/serve-files port))))
+
+(defmacro profile
+  "Profiles the given code body with clj-async-profiler, see (browse-profiler) to look at the resulting flamegraph.
+  e.g (profile (reduce + (my-function)))
+  Options are the same as clj-async-profiler/profile."
+  [options? & body]
+  `(clj-async-profiler/profile ~options? ~@body))
+
+(defn start-profiler
+  "Start clj-async-profiler see also: (stop-profiler) (browse-profiler)
+  Options are the same as clj-async-profiler/start."
+  ([] (clj-async-profiler/start))
+  ([options] (clj-async-profiler/start options)))
+
+(defn stop-profiler
+  "Stops clj-async-profiler, see (browse-profiler) to go look at the profiles in a nice little UI."
+  []
+  (let [file (clj-async-profiler/stop)]
+    (println "Saved flamegraph to" (str file))))
+
+(defn browse-profiler
+  "Opens the clj-async-profiler page in your browser, you can go look at your flamegraphs and start/stop the profiler
+  from here."
+  []
+  @profiler-server
+  (clojure.java.browse/browse-url (str "http://localhost:" profiler-port)))
 
 (comment
   (def !submit-tpch
