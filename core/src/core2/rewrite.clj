bfe18e137a78c5eddcad83bac01e191db0702056
Håkan Råberg <hakan.raberg@gmail.com>
2022-04-26 15:13:51 +0200

    New implementation of zmatch. About 15% faster, only uses zipper for the data side, pattern is a fixed expression tree. Removing a bunch of old comments and spikes from the rewrite ns.

--- core/src/core2/rewrite.clj
+++ core/src/core2/rewrite.clj
@@ -8,60 +8,6 @@
 
 ;; Zipper pattern matching
 
-(defn- zip-next-skip-subtree [loc]
-  (or (z/right loc)
-      (loop [p loc]
-        (when-let [p (z/up p)]
-          (or (z/right p)
-              (recur p))))))
-
-(defn zip-match
-  ([pattern-loc loc]
-   (zip-match pattern-loc loc {}))
-  ([pattern-loc loc acc]
-   (loop [pattern-loc pattern-loc
-          loc loc
-          acc acc]
-     (cond
-       (or (nil? pattern-loc)
-           (z/end? pattern-loc))
-       acc
-
-       (or (nil? loc)
-           (z/end? loc))
-       nil
-
-       (and (z/branch? pattern-loc)
-            (z/branch? loc))
-       (when (= (count (z/children pattern-loc))
-                (count (z/children loc)))
-         (recur (z/down pattern-loc) (z/down loc) acc))
-
-       :else
-       (let [pattern-node (z/node pattern-loc)
-             node (z/node loc)]
-         (cond
-           (or (= pattern-node node)
-               (and (keyword? pattern-node)
-                    (ident? node)
-                    (= (name pattern-node) (name node)))
-               (and (instance? Pattern pattern-node)
-                    (re-find pattern-node (str node))))
-           (recur (zip-next-skip-subtree pattern-loc) (zip-next-skip-subtree loc) acc)
-
-           (and (symbol? pattern-node)
-                (= node (get acc pattern-node node)))
-           (recur (z/next pattern-loc)
-                  (zip-next-skip-subtree loc)
-                  (cond-> acc
-                    (not= '_ pattern-node) (assoc pattern-node
-                                                  (if (:z (meta pattern-node))
-                                                    loc
-                                                    node))))
-
-           :else
-           nil))))))
-
 (defn ->zipper [x]
   (cond
     (:zip/make-node (meta x))
@@ -78,6 +24,60 @@
     :else
     (throw (IllegalArgumentException. (str "No zipper constructor for: " (type x))))))
 
+(defn- build-zmatcher [pattern]
+  (cond
+    (= '_ pattern)
+    (fn [acc _]
+      acc)
+
+    (and (symbol? pattern)
+         (:z (meta pattern)))
+    (fn [acc z]
+      (when-not (contains? acc pattern)
+        (assoc acc pattern z)))
+
+    (symbol? pattern)
+    (fn [acc z]
+      (let [node (z/node z)]
+        (when (= node (get acc pattern node))
+          (assoc acc pattern node))))
+
+    (keyword? pattern)
+    (fn [acc z]
+      (let [node (z/node z)]
+        (when (and (ident? node)
+                   (= (name pattern) (name node)))
+          acc)))
+
+    (instance? Pattern pattern)
+    (fn [acc z]
+      (let [node (z/node z)]
+        (when (and (string? node)
+                   (re-find pattern node))
+          acc)))
+
+    (sequential? pattern)
+    (let [matchers (object-array (map build-zmatcher pattern))
+          len (count matchers)]
+      (fn [acc z]
+        (when-let [z (z/down z)]
+          (when (= len (inc (count (z/rights z))))
+            (loop [acc acc
+                   z z
+                   n 0]
+              (if (= n len)
+                acc
+                (when z
+                  (when-let [acc ((aget matchers n) acc z)]
+                    (recur acc (z/right z) (inc n))))))))))
+
+    :else
+    (fn [acc z]
+      (when (= pattern (z/node z))
+        acc))))
+
+(def zmatchers (atom {}))
+
 (defmacro zmatch {:style/indent 1} [loc & [pattern expr & clauses :as all-clauses]]
   (when pattern
     (if (> (count all-clauses) 1)
@@ -85,9 +85,13 @@
                    #{pattern}
                    (->> (flatten pattern)
                         (filter symbol?)
-                        (remove '#{_})))]
+                        (remove '#{_})))
+            matcher-key (binding  [*print-meta* true]
+                          (pr-str pattern))]
+        (swap! zmatchers update matcher-key (fn [matcher]
+                                              (or matcher (build-zmatcher pattern))))
         `(let [loc# (->zipper ~loc)]
-           (if-let [{:syms [~@vars] :as acc#} (zip-match (->zipper '~pattern) loc#)]
+           (if-let [{:syms [~@vars] :as acc#} ((get @zmatchers '~matcher-key) {} loc#)]
              ~expr
              (zmatch loc# ~@clauses))))
       pattern)))
@@ -371,430 +375,3 @@
    (stop-td-tu f (with-tu-monoid z m))))
 
 (def select once-td-tu)
-
-(comment
-
-  (defn use-attributes
-    ([attr-fn loc]
-     (use-attributes attr-fn conj loc))
-    ([attr-fn f loc]
-     (loop [loc (z/right (z/down loc))
-            acc (f)]
-       (if loc
-         (recur (z/right loc)
-                (if (z/branch? loc)
-                  (if-some [v (attr-fn loc)]
-                    (f acc v)
-                    acc)
-                  acc))
-         (f acc)))))
-
-  (declare repmin globmin locmin)
-
-  (defn repmin [loc]
-    (zmatch loc
-      [:fork _ _] (use-attributes repmin
-                                  (completing
-                                   (fn
-                                     ([] [:fork])
-                                     ([x y] (conj x y))))
-                                  loc)
-      [:leaf _] [:leaf (globmin loc)]))
-
-  (defn locmin [loc]
-    (zmatch loc
-      [:fork _ _] (use-attributes locmin
-                                  (completing
-                                   (fn
-                                     ([] Long/MAX_VALUE)
-                                     ([x y] (min x y))))
-                                  loc)
-      [:leaf n] n))
-
-  (defn globmin [loc]
-    (if-let [p (z/up loc)]
-      (globmin p)
-      (locmin loc)))
-
-  ;; https://web.fe.up.pt/~jacome/downloads/CEFP15.pdf
-  ;; "Watch out for that tree! A Tutorial on Shortcut Deforestation"
-
-  (defn repm [t]
-    (case (first t)
-      :leaf [(fn [z]
-               [:leaf z]) (second t)]
-      :fork (let [[t1 m1] (repm (second t))
-                  [t2 m2] (repm (last t))]
-              [(fn [z]
-                 [:fork (t1 z) (t2 z)]) (min m1 m2)])))
-
-  (let [[t m] (repm [:fork [:fork [:leaf 1] [:leaf 2]] [:fork [:leaf 3] [:leaf 4]]])]
-    (t m))
-
-  ;; http://hackage.haskell.org/package/ZipperAG
-  ;; https://www.sciencedirect.com/science/article/pii/S0167642316000812
-  ;; "Embedding attribute grammars and their extensions using functional zippers"
-
-  ;; Chapter 3 & 4:
-
-  (defn dcli [ag]
-    (case (ctor ag)
-      :root {}
-      :let (case (ctor (parent ag))
-             :root (dcli (parent ag))
-             :cons-let (env (parent ag)))
-      (case (ctor (parent ag))
-        (:cons :cons-let) (assoc (dcli (parent ag))
-                                 (lexeme (parent ag) 1)
-                                 (parent ag))
-        (dcli (parent ag)))))
-
-  (defn dclo [ag]
-    (case (ctor ag)
-      :root (dclo ($ ag 1))
-      :let (dclo ($ ag 1))
-      (:cons :cons-let) (dclo ($ ag 3))
-      :empty (dcli ag)))
-
-  (defn env [ag]
-    (case (ctor ag)
-      :root (dclo ag)
-      :let (case (ctor (parent ag))
-             :cons-let (dclo ag)
-             (env (parent ag)))
-      (env (parent ag))))
-
-  (defn lev [ag]
-    (case (ctor ag)
-      :root 0
-      :let (case (ctor (parent ag))
-             :cons-let (inc (lev (parent ag)))
-             (lev (parent ag)))
-      (lev (parent ag))))
-
-  (defn must-be-in [m n]
-    (if (contains? m n)
-      []
-      [n]))
-
-  (defn must-not-be-in [m n ag]
-    (let [r (get m n)]
-      (if (and r (= (lev ag) (lev r)))
-        [n]
-        [])))
-
-  (defn errs [ag]
-    (case (ctor ag)
-      (:cons :cons-let) (->> [(must-not-be-in (dcli ag) (lexeme ag 1) ag)
-                              (errs ($ ag 2))
-                              (errs ($ ag 3))]
-                             (reduce into))
-      :variable (must-be-in (env ag) (lexeme ag 1))
-      (use-attributes errs into ag)))
-
-  (errs
-   (z/vector-zip
-    [:root
-     [:let
-      [:cons "a" [:plus [:variable "b"] [:constant 3]]
-       [:cons "c" [:constant 8]
-        [:cons-let "w" [:let
-                        [:cons "c" [:times [:variable "a"] [:variable "b"]]
-                         [:empty]]
-                        [:times [:variable "c"] [:variable "b"]]]
-         [:cons "b" [:minus [:times [:variable "c"] [:constant 3]] [:variable "c"]]
-          [:empty]]]]]
-      [:minus [:times [:variable "c"] [:variable "w"]] [:variable "a"]]]]))
-
-  ;; let a = z + 3
-  ;;     c = 8
-  ;;     a = (c ∗ 3) − c
-  ;; in (a + 7) ∗ c
-
-  (errs
-   (z/vector-zip
-    [:root
-     [:let
-      [:cons "a" [:plus [:variable "z"] [:constant 3]]
-       [:cons "c" [:constant 8]
-        [:cons "a" [:minus [:times [:variable "c"] [:constant 3]] [:variable "c"]]
-         [:empty]]]]
-      [:times [:plus [:variable "a"] [:constant 7]] [:variable "c"]]]]))
-
-  ;; https://github.com/christoff-buerger/racr
-  ;; https://dl.acm.org/doi/pdf/10.1145/2814251.2814257
-  ;; "Reference Attribute Grammar Controlled Graph Rewriting: Motivation and Overview"
-
-  (defn find-l-decl [n name]
-    (when n
-      (if (and (= :Decl (ctor n))
-               (= (lexeme n 2) name))
-        n
-        (recur (z/left n) name))))
-
-  (declare l-decl)
-
-  (defn g-decl [n name]
-    (case (ctor (parent n))
-      :Block (or (find-l-decl n name)
-                 (some-> n
-                         #_(parent)
-                         (parent)
-                         (g-decl name)))
-      :Prog (or (find-l-decl n name)
-                (z/vector-zip [:DErr]))))
-
-  (defn l-decl [n name]
-    (case (ctor n)
-      :Decl (when (= (lexeme n 1) name)
-              n)))
-
-  (defn type' [n]
-    (case (ctor n)
-      :Use (type' (g-decl n (lexeme n 1)))
-      :Decl (lexeme n 1)
-      :DErr "ErrorType"
-      (:Prog :Block) (type' (z/rightmost (z/down n)))))
-
-  (defn well-formed? [n]
-    (case (ctor n)
-      :Use (not= (type' n) "ErrorType")
-      :Decl (= (g-decl n (lexeme n 2)) n)
-      :DErr false
-      (:Prog :Block) (use-attributes well-formed?
-                                     (completing
-                                      (fn
-                                        ([] true)
-                                        ([x y] (and x y))))
-                                     n)))
-
-  (well-formed?
-   (z/vector-zip
-    [:Prog
-     [:Decl "Integer" "a"]
-     [:Block [:Use "b"] [:Use  "a"] [:Decl "Real" "a"] [:Use "a"]]
-     [:Use "a"]
-     [:Decl "Real" "a"]]))
-
-  ;; Based number example
-
-  (defn ^:dynamic base [n]
-    (case (ctor n)
-      :basechar (case (lexeme n 1)
-                  "o" 8
-                  "d" 10)
-      :based-num (base ($ n 2))
-      (:num :digit) (base (parent n))))
-
-  (defn ^:dynamic value [n]
-    (case (ctor n)
-      :digit (let [v (Double/parseDouble (lexeme n 1))]
-               (if (> v (base n))
-                 Double/NaN
-                 v))
-      :num (if (= 2 (count (z/children n)))
-             (value ($ n 1))
-             (+ (* (base n)
-                   (value ($ n 1)))
-                (value ($ n 2))))
-      :based-num (value ($ n 1))))
-
-  (time
-   (= 229.0
-      (with-memoized-attributes
-        [base value]
-        (value
-         (z/vector-zip
-          [:based-num
-           [:num
-            [:num
-             [:num
-              [:num
-               [:digit "3"]]
-              [:digit "4"]]
-             [:digit "5"]]]
-           [:basechar "o"]]))))))
-
-(comment
-  (defn ->attributed-tree [tree attr-vars]
-    (with-memoized-attributes attr-vars
-      #(loop [loc (z/vector-zip tree)]
-         (if (z/end? loc)
-           (z/node loc)
-           (recur (z/next (if (z/branch? loc)
-                            (if-let [acc (some->> (for [k attr-vars
-                                                        :let [v (k loc)]
-                                                        :when (some? v)]
-                                                    [(:name (meta k)) v])
-                                                  (not-empty)
-                                                  (into {}))]
-                              (z/edit loc vary-meta merge acc)
-                              loc)
-                            loc)))))))
-
-  (let [tree [:fork [:fork [:leaf 1] [:leaf 2]] [:fork [:leaf 3] [:leaf 4]]]
-        tree (->attributed-tree tree [#'repmin #'locmin #'globmin])]
-    (keep meta (tree-seq vector? seq tree)))
-
-  (zip-match (z/vector-zip '[:leaf n])
-             (z/vector-zip [:leaf 2]))
-
-  ((full-td-tp (adhoc-tp id-tp (fn [x] (prn x) (when (number? x) (str x)))))
-   (z/vector-zip [:fork [:fork [:leaf 1] [:leaf 2]] [:fork [:leaf 3] [:leaf 4]]]))
-
-  ((full-bu-tu (mono-tu (fn [x] (prn x) (when (number? x) [x]))))
-   (z/vector-zip [:fork [:fork [:leaf 1] [:leaf 2]] [:fork [:leaf 3] [:leaf 4]]]))
-
-  (= ["a" "c" "b" "c"]
-     ((full-td-tu (mono-tu
-                   #(zmatch %
-                      [:assign s _ _] [s]
-                      [:nested-let s _ _] [s]
-                      _ [])))
-      (z/vector-zip
-       [:let
-        [:assign "a"
-         [:add [:var "b"] [:const 0]]
-         [:assign "c" [:const 2]
-          [:nested-let "b" [:let [:assign "c" [:const 3] [:empty-list]]
-                            [:add [:var "c"] [:var "c"]]]
-           [:empty-list]]]]
-        [:sub [:add [:var "a"] [:const 7]] [:var "c"]]]))))
-
-;; Experimental comprehension rewrites based on rules in
-;; https://okmij.org/ftp/meta-programming/quel.pdf
-
-;; See also:
-;; https://lists.w3.org/Archives/Public/public-rif-wg/2008Oct/att-0054/p457-fegaras.pdf
-;; https://db.inf.uni-tuebingen.de/staticfiles/publications/Comprehensions.pdf
-;; https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.88.9148&rep=rep1&type=pdf
-;; https://www.researchgate.net/publication/2724181_Improving_List_Comprehension_Database_Queries
-;; https://homepages.inf.ed.ac.uk/jcheney/publications/cheney13icfp.pdf
-;; http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.365.3145&rep=rep1&type=pdf
-
-(defn- normalize-comprehension [comprehension]
-  (letfn [(stage-1-symbolic-reduction [z]
-            (zmatch z
-              [:get l [:record attrs]]
-              ;;=> RecordBeta
-              (letfn [(step [z]
-                        (case (ctor z)
-                          :attr (when (= l (lexeme z 1))
-                                  (lexeme z 2))
-                          nil))]
-                ((once-td-tu (mono-tu step)) ($ z -1)))
-
-              [:for x [:yield M] N]
-              ;;=> ForYield
-              (letfn [(step [[n :as z]]
-                        (case (ctor z)
-                          :for (when (= x (lexeme z 1))
-                                 n)
-                          (when (= x n)
-                            M)))]
-                (z/node ((stop-td-tp (mono-tp step)) ($ z -1))))
-
-              [:for x [:for y L M] N]
-              ;;=> ForFor
-              [:for y L [:for x M N]] ;; if y not free in N
-
-              [:for x [:where L M] N]
-              ;;=> ForWhere1
-              [:where L [:for x M N]]
-
-              [:for x [] N]
-              ;;=> ForEmpty1
-              []
-
-              [:for x [:union-all L M] N]
-              ;;=>ForUnionAll1
-              [:union-all [:for x L N] [:for x M N]]
-
-              [:where true M]
-              ;;=> WhereTrue
-              M
-
-              [:where false M]
-              ;;=> WhereFalse
-              []))
-
-          (stage-2-adhoc-rules [z]
-            (zmatch z
-              [:for x L [:union-all M N]]
-              ;;=> ForUnionAll2
-              [:union-all [:for x L M] [:for x L N]]
-
-              [:for x M []]
-              ;;=> ForEmpty2
-              []
-
-              [:where L [:union-all M N]]
-              ;;=> WhereUnion
-              [:union-all [:where L M] [:where L N]]
-
-              [:where L []]
-              ;;=> WhereEmpty
-              []
-
-              [:where L [:where M N]]
-              ;;=> WhereWhere
-              [:where [:and L M] N]
-
-              [:where L [:for x M N]]
-              ;;=> WhereFor
-              [:for x M [:where L N]]))]
-
-    (->> (z/vector-zip comprehension)
-         (innermost (mono-tp stage-1-symbolic-reduction))
-         (innermost (mono-tp stage-2-adhoc-rules))
-         (z/node))))
-
-(comment
-  (time
-   (= '[:for e [:table employee]
-        [:for d [:table department]
-         [:where [:and [:> [:get :wage e] 20] [:= [:get :deptID d] [:get :deptID e]]]
-          [:yield [:record
-                   [:attrs
-                    [:attr :name [:get :name e]]
-                    [:attr :dep [:get :name d]]
-                    [:attr :wage [:get :wage e]]]]]]]]
-      (doto (normalize-comprehension
-             '[:for e [:for e [:table employee]
-                       [:where [:> [:get :wage e] 20]
-                        [:yield e]]]
-               [:for d [:table department]
-                [:where [:= [:get :deptID d] [:get :deptID e]]
-                 [:yield [:record
-                          [:attrs
-                           [:attr :name [:get :name e]]
-                           [:attr :dep [:get :name d]]
-                           [:attr :wage [:get :wage e]]]]]]]])
-        (clojure.pprint/pprint))))
-
-  (time
-   (= '[:for o [:table orders]
-        [:for p [:table products]
-         [:where
-          [:and [:eq [:get :oid o] oid] [:eq [:get :pid p] [:get :pid o]]]
-          [:yield
-           [:record
-            [:attrs
-             [:attr :pid [:get :pid p]]
-             [:attr :name [:get :name p]]
-             [:attr :sale [:* [:get :price p] [:get :qty o]]]]]]]]]
-      (doto (normalize-comprehension
-             '[:for y [:for o [:table orders]
-                       [:where [:eq [:get :oid o] oid]
-                        [:yield [:record
-                                 [:attrs
-                                  [:attr :pid [:get :pid o]]
-                                  [:attr :qty [:get :qty o]]]]]]]
-               [:for p [:table products]
-                [:where [:eq [:get :pid p] [:get :pid y]]
-                 [:yield [:record
-                          [:attrs
-                           [:attr :pid [:get :pid p]]
-                           [:attr :name [:get :name p]]
-                           [:attr :sale [:* [:get :price p] [:get :qty y]]]]]]]]])
-        (clojure.pprint/pprint)))))
