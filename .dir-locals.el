f20d89091b5b5b3e8bcaf65e03bea6b9d20c6cfb
Håkan Råberg <hakan.raberg@gmail.com>
2022-05-05 08:47:34 +0200

    Moving jvm-opts down into dev alias and add cider-clojure-cli-aliases to dir-locals for cider.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
 ((nil . ((cider-ns-refresh-before-fn . "integrant.repl/suspend")
-         (cider-ns-refresh-after-fn  . "integrant.repl/resume"))))
+         (cider-ns-refresh-after-fn  . "integrant.repl/resume")
+         (cider-clojure-cli-aliases . "dev"))))
