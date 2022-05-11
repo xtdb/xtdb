38f5cdbeadacb3b2fb58101c47e54772912ec3ad
Håkan Råberg <hakan.raberg@gmail.com>
2022-05-11 17:13:47 +0200

    Redoing error handling in new parser so it can show more relevant positions, matching the right-most failure.

--- core/src/core2/sql/parser.clj
+++ core/src/core2/sql/parser.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str])
   (:import java.io.File
            java.nio.IntBuffer
-           [java.util ArrayDeque HashMap HashSet List Map]
+           [java.util ArrayDeque HashMap HashSet List Map Set]
            [java.util.regex Matcher Pattern]))
 
 ;; https://arxiv.org/pdf/1509.02439v1.pdf
@@ -13,195 +13,172 @@
 
 (set! *unchecked-math* :warn-on-boxed)
 
-(defrecord ParseState [ast errs ^int idx])
+(defrecord ParseState [ast ^int idx])
 
 (defrecord ParseFailure [^String in errs ^int idx])
 
 (definterface IParser
-  (^core2.sql.parser.ParseState parse [^String in ^int idx ^java.util.Map memos ^boolean errors?]))
+  (^core2.sql.parser.ParseState parse [^String in ^int idx ^java.util.Map memos ^java.util.Map errors?]))
 
-(def ^:private epsilon-errs #{[:expected "<EOF>"]})
+(defn add-error! [^Map errors ^Set error ^long idx]
+  (when errors
+    (if-let [^Set errors-at-idx (.get errors idx)]
+      (.add errors-at-idx error)
+      (.put errors idx (doto (HashSet.)
+                         (.add error)))))
+  nil)
+
+(def ^:private epsilon-err [:expected "<EOF>"])
 
 (defrecord EpsilonParser []
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (if (= (.length in) idx)
-      (ParseState. [] nil idx)
-      (ParseState. nil epsilon-errs idx))))
+      (ParseState. [] idx)
+      (add-error! errors epsilon-err idx))))
 
-(def ^:private ws-errs #{[:expected "<WS>"]})
+(def ^:private ws-err [:expected "<WS>"])
 
-(defrecord WhitespaceParser [^java.util.regex.Pattern pattern ^core2.sql.parser.IParser parser]
+(defrecord WhitespaceParser [^Pattern pattern ^IParser parser]
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (let [m (.matcher pattern in)
           m (.region m idx (.length in))
           m (.useTransparentBounds m true)]
       (cond
         (.lookingAt m)
-        (.parse parser in (.end m) memos errors?)
+        (.parse parser in (.end m) memos errors)
 
         (zero? idx)
-        (.parse parser in idx memos errors?)
+        (.parse parser in idx memos errors)
 
         :else
-        (ParseState. nil ws-errs idx)))))
+        (add-error! errors ws-err idx)))))
 
 (defrecord NonTerminalParser [^int rule-id ^objects rules]
   IParser
-  (parse [_ in idx memos errors?]
-    (.parse ^IParser (aget rules rule-id) in idx memos errors?)))
+  (parse [_ in idx memos errors]
+    (.parse ^IParser (aget rules rule-id) in idx memos errors)))
 
-(defrecord RuleParser [rule-name ^int rule-id raw? ^IParser parser errs]
+(defrecord RuleParser [rule-name ^int rule-id raw? ^IParser parser]
   IParser
-  (parse [_ in idx memos errors?]
-    (let [state (.parse parser in idx memos errors?)]
-      (ParseState. (when-let [ast (.ast state)]
+  (parse [_ in idx memos errors]
+    (when-let [state (.parse parser in idx memos errors)]
+      (ParseState. (let [ast (.ast state)]
                      (if (raw? ast)
                        ast
                        [(with-meta
                           (into [rule-name] ast)
                           {:start-idx idx
                            :end-idx (.idx state)})]))
-                   (when-let [rule-errs (.errs state)]
-                     (if (and errors? (:regexp? (meta rule-errs)))
-                       errs
-                       rule-errs))
                    (.idx state)))))
 
 (defrecord MemoizeParser [^RuleParser parser]
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (let [memo-key (IntBuffer/wrap (doto (int-array 2)
                                      (aset 0 idx)
                                      (aset 1 (.rule-id parser))))]
-      (if-let [state (.get memos memo-key)]
-        state
-        (doto (.parse parser in idx memos errors?)
-          (->> (.put memos memo-key)))))))
+      (let [state (.getOrDefault memos memo-key ::not-found)]
+        (if (= ::not-found state)
+          (doto (.parse parser in idx memos errors)
+            (->> (.put memos memo-key)))
+          state)))))
 
 (defrecord MemoizeLeftRecParser [^RuleParser parser]
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (let [memo-key (IntBuffer/wrap (doto (int-array 2)
                                      (aset 0 idx)
                                      (aset 1 (.rule-id parser))))]
-      (if-let [state (.get memos memo-key)]
-        state
-        (loop [last-state (ParseState. nil nil idx)]
-          (.put memos memo-key last-state)
-          (let [^ParseState new-state (.parse parser in idx memos errors?)]
-            (if (.ast new-state)
-              (if (<= (.idx new-state) (.idx last-state))
+      (let [state (.getOrDefault memos memo-key ::not-found)]
+        (if (= ::not-found state)
+          (loop [^ParseState last-state nil]
+            (.put memos memo-key last-state)
+            (if-let [^ParseState new-state (.parse parser in idx memos errors)]
+              (if (and last-state (<= (.idx new-state) (.idx last-state)))
                 (do (.remove memos memo-key)
                     last-state)
                 (recur new-state))
               (do (.remove memos memo-key)
-                  (if (and (nil? (.ast last-state))
-                           (.errs new-state))
-                    new-state
-                    last-state)))))))))
+                  last-state)))
+          state)))))
 
-(defrecord HideParser [^core2.sql.parser.IParser parser]
+(defrecord HideParser [^IParser parser]
   IParser
-  (parse [_ in idx memos errors?]
-    (let [state (.parse parser in idx memos errors?)]
-      (ParseState. (when (.ast state)
-                     [])
-                   (.errs state)
-                   (.idx state)))))
+  (parse [_ in idx memos errors]
+    (when-let [state (.parse parser in idx memos errors)]
+      (ParseState. [] (.idx state)))))
 
-(defrecord OptParser [^core2.sql.parser.IParser parser]
+(defrecord OptParser [^IParser parser]
   IParser
-  (parse [_ in idx memos errors?]
-    (let [state (.parse parser in idx memos errors?)]
-      (if (.ast state)
-        state
-        (ParseState. [] nil idx)))))
+  (parse [_ in idx memos errors]
+    (if-let [state (.parse parser in idx memos errors)]
+      state
+      (ParseState. [] idx))))
 
-(defrecord NegParser [^core2.sql.parser.IParser parser]
+(defrecord NegParser [^IParser parser]
   IParser
-  (parse [_ in idx memos errors?]
-    (let [state (.parse parser in idx memos errors?)]
-      (if (.ast state)
-        (ParseState. nil (when errors?
-                           #{[:unexpected (subs in idx (.idx state))]}) idx)
-        (ParseState. [] nil idx)))))
+  (parse [_ in idx memos errors]
+    (if-let [state (.parse parser in idx memos nil)]
+      (add-error! errors [:unexpected (subs in idx (.idx state))] idx)
+      (ParseState. [] idx))))
 
-(defrecord RepeatParser [^core2.sql.parser.IParser parser ^boolean star?]
+(defrecord RepeatParser [^IParser parser ^boolean star?]
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (loop [ast []
            idx idx
            n 0]
-      (let [state (.parse parser in idx memos errors?)]
-        (if (.ast state)
-          (recur (into ast (.ast state))
-                 (.idx state)
-                 (inc n))
-          (if (or star? (pos? n))
-            (ParseState. ast nil idx)
-            state))))))
+      (if-let [state (.parse parser in idx memos errors)]
+        (recur (into ast (.ast state))
+               (.idx state)
+               (inc n))
+        (when (or star? (pos? n))
+          (ParseState. ast idx))))))
 
-(defrecord CatParser [^java.util.List parsers]
+(defrecord CatParser [^List parsers]
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (loop [ast []
            idx idx
            n 0]
       (if (< n (.size parsers))
-        (let [state (.parse ^IParser (.get parsers n) in idx memos errors?)]
-          (if (.ast state)
-            (recur (into ast (.ast state))
-                   (.idx state)
-                   (inc n))
-            (ParseState. nil (.errs state) (.idx state))))
-        (ParseState. ast nil idx)))))
+        (when-let [state (.parse ^IParser (.get parsers n) in idx memos errors)]
+          (recur (into ast (.ast state))
+                 (.idx state)
+                 (inc n)))
+        (ParseState. ast idx)))))
 
 (defrecord OrdParser [^IParser parser1 ^IParser parser2]
   IParser
-  (parse [_ in idx memos errors?]
-    (let [state1 (.parse parser1 in idx memos errors?)
-          errors? (if errors?
-                    (nil? (.ast state1))
-                    errors?)
-          state2 (.parse parser2 in idx memos errors?)
-          cmp (Integer/compare (.idx state1) (.idx state2))]
-      (cond
-        (and (.ast state1)
-             (or (nil? (.ast state2))
-                 (not (neg? cmp))))
-        state1
-
-        (.ast state2)
-        state2
-
-        (pos? cmp)
-        state1
-
-        (neg? cmp)
-        state2
-
-        :else
-        (ParseState. nil (when errors?
-                           (into (.errs state1) (.errs state2))) (.idx state1))))))
+  (parse [_ in idx memos errors]
+    (let [state1 (.parse parser1 in idx memos errors)
+          state2 (.parse parser2 in idx memos errors)]
+      (if state1
+        (if state2
+          (if (<= (.idx state2) (.idx state1))
+            state1
+            state2)
+          state1)
+        state2))))
 
 (defrecord StringParser [^String string errs]
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (if (.regionMatches in true idx string 0 (.length string))
-      (ParseState. [string] nil (+ idx (.length string)))
-      (ParseState. nil errs idx))))
+      (ParseState. [string] (+ idx (.length string)))
+      (add-error! errors errs idx))))
 
 (defrecord RegexpParser [^Pattern pattern errs matcher-fn]
   IParser
-  (parse [_ in idx memos errors?]
+  (parse [_ in idx memos errors]
     (let [m (.matcher pattern in)
           m (.region m idx (.length in))
           m (.useTransparentBounds m true)]
       (if (.lookingAt m)
-        (ParseState. (matcher-fn m) nil (.end m))
-        (ParseState. nil errs idx)))))
+        (ParseState. (matcher-fn m) (.end m))
+        (add-error! errors errs idx)))))
 
 (defn- left-recursive? [grammar rule-name]
   (let [visited (HashSet.)
@@ -270,7 +247,7 @@
                         ":"
                         " one of:")))
         (doseq [[_ msg] errs]
-          (println (name msg)))))))
+          (println msg))))))
 
 (defn build-ebnf-parser
   ([grammar ws-pattern]
@@ -280,35 +257,33 @@
   ([grammar ws-pattern ->raw?]
    (let [rule->id (zipmap (keys grammar) (range))
          rules (object-array (count rule->id))]
-     (letfn [(build-parser [{:keys [tag hide] :as parser}]
+     (letfn [(build-parser [rule-name {:keys [tag hide] :as parser}]
                (cond-> (case tag
                          :nt (->NonTerminalParser (get rule->id (:keyword parser)) rules)
-                         :star (->RepeatParser (build-parser (:parser parser)) true)
-                         :plus (->RepeatParser (build-parser (:parser parser)) false)
-                         :opt (->OptParser (build-parser (:parser parser)))
-                         :cat (->CatParser (mapv build-parser (:parsers parser)))
+                         :star (->RepeatParser (build-parser rule-name (:parser parser)) true)
+                         :plus (->RepeatParser (build-parser rule-name (:parser parser)) false)
+                         :opt (->OptParser (build-parser rule-name (:parser parser)))
+                         :cat (->CatParser (mapv (partial build-parser rule-name ) (:parsers parser)))
                          :alt (->> (reverse (:parsers parser))
-                                   (mapv build-parser)
+                                   (mapv (partial build-parser rule-name ))
                                    (reduce
                                     (fn [parser2 parser1]
                                       (->OrdParser parser1 parser2))))
-                         :ord (->OrdParser (build-parser (:parser1 parser))
-                                           (build-parser (:parser2 parser)))
-                         :neg (->WhitespaceParser ws-pattern (->NegParser (build-parser (:parser parser))))
+                         :ord (->OrdParser (build-parser rule-name (:parser1 parser))
+                                           (build-parser rule-name (:parser2 parser)))
+                         :neg (->WhitespaceParser ws-pattern (->NegParser (build-parser rule-name (:parser parser))))
                          :epsilon (->WhitespaceParser ws-pattern (->EpsilonParser))
                          :regexp (->WhitespaceParser ws-pattern (->RegexpParser (:regexp parser)
-                                                                                (with-meta
-                                                                                  #{[:expected (:regexp parser)]}
-                                                                                  {:regexp? true})
+                                                                                [:expected (rule-kw->name rule-name)]
                                                                                 (fn [^Matcher m]
                                                                                   [(.group m)])))
                          :string (->WhitespaceParser ws-pattern
                                                      (if (re-find #"^\w+$" (:string parser))
                                                        (->RegexpParser (Pattern/compile (str (Pattern/quote (:string parser)) "\\b")
                                                                                         Pattern/CASE_INSENSITIVE)
-                                                                       #{[:expected (:string parser)]}
+                                                                       [:expected (:string parser)]
                                                                        (constantly [(:string parser)]))
-                                                       (->StringParser (:string parser) #{[:expected (:string parser)]}))))
+                                                       (->StringParser (:string parser) [:expected (:string parser)]))))
                  hide (->HideParser)))]
        (doseq [[k v] grammar
                :let [rule-id (int (get rule->id k))
@@ -319,15 +294,15 @@
                      memo-fn (if (left-recursive? grammar k)
                                ->MemoizeLeftRecParser
                                ->MemoizeParser)
-                     parser (->RuleParser k rule-id raw? (build-parser v) #{[:expected (rule-kw->name k)]})]]
+                     parser (->RuleParser k rule-id raw? (build-parser k v))]]
          (aset rules rule-id (memo-fn parser)))
        (fn [in start-rule]
-         (when-let [state (-> (->trailing-ws-parser start-rule)
-                              ^IParser (build-parser)
-                              (.parse in 0 (HashMap.) true))]
-           (if (.errs state)
-             (ParseFailure. in (.errs state) (.idx state))
-             (first (.ast state)))))))))
+         (let [errors (HashMap.)]
+           (if-let [state (-> ^IParser (build-parser nil (->trailing-ws-parser start-rule))
+                              (.parse in 0 (HashMap.) errors))]
+             (first (.ast state))
+             (let [[k v] (last (sort-by key errors))]
+               (ParseFailure. in v k)))))))))
 
 (comment
 
@@ -424,6 +399,8 @@ HEADER_COMMENT: #'// *\\d.*?\\n' ;
 (comment
   (sql-parser "SELECT * FROMfoo" :directly_executable_statement)
 
+  (println (failure->str (sql-parser "* FROMfoo" :term)))
+
   (sql-parser "  (SELECT avg(c) FROM t1)  " :subquery)
 
   (sql-parser "a[0]" :value_expression_primary)
@@ -447,7 +424,10 @@ HEADER_COMMENT: #'// *\\d.*?\\n' ;
                "SELECT DISTINCT col2 FROM tab1 WHERE NULL BETWEEN NULL AND - col2"
                ;; spec doesn't seem to allow expressions inside IN.
                "SELECT * FROM tab1 WHERE NULL NOT IN ( col0 * col0 )"
-               "SELECT INTERVAL '3 4' DAY TO HOUR t FROM foo WHERE foo.a = 42"]
+               "SELECT INTERVAL '3 4' DAY TO HOUR t FROM foo WHERE foo.a = 42"
+               "SELECT * FROMfoo"
+               "SELECT + NULLIF ( + 67, + + NULLIF ( + + 46, 66 - CASE WHEN 51 IN ( + 91 ) THEN + SUM ( CAST ( NULL AS INTEGER ) ) + - 92 ELSE - ( - 47 ) END ) ) FROM (VALUES 0) AS no_from
+"]
           :let [tree (sql-parser sql :directly_executable_statement)]
           :when (failure? tree)]
     (println (failure->str tree)))
