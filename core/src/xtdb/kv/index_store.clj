b652818e7dfb1300e19bc5b1399532b580e59652
Jon Pither <jon@juxt.pro>
2022-05-30 15:04:15 +0100

    Stats TLs

--- core/src/xtdb/kv/index_store.clj
+++ core/src/xtdb/kv/index_store.clj
@@ -29,6 +29,12 @@
 
 (set! *unchecked-math* :warn-on-boxed)
 
+(defn- buffer-tl ^ThreadLocal []
+  (ThreadLocal/withInitial
+   (reify Supplier
+     (get [_]
+       (ExpandableDirectByteBuffer. 32)))))
+
 (def ^:private ^ThreadLocal seek-buffer-tl
   (ThreadLocal/withInitial
    (reify Supplier
@@ -460,6 +466,9 @@
   (let [^long hll-size (/ (- (.capacity b) Long/BYTES Long/BYTES) 2)]
     (mem/slice-buffer b (+ Long/BYTES Long/BYTES hll-size) hll-size)))
 
+(def ^:private ^ThreadLocal stats-key-buffer-tl (buffer-tl))
+(def ^:private ^ThreadLocal doc-value-buffer-tl (buffer-tl))
+
 (defn- stats-kvs [stats-kvs-cache persistent-kv-store docs]
   (let [persistent-kv-snapshot (atom nil)
         get-persistent-value (fn [k-buf]
@@ -471,7 +480,7 @@
       (let [attr-key-bufs (->> docs
                                (into {} (comp (mapcat keys)
                                               (distinct)
-                                              (map (juxt identity #(encode-stats-key-to nil (c/->value-buffer %)))))))]
+                                              (map (juxt identity #(encode-stats-key-to (.get stats-key-buffer-tl) (c/value->buffer % (.get doc-value-buffer-tl))))))))]
         (->> docs
              (reduce (fn [acc doc]
                        (let [e (:crux.db/id doc)]
@@ -974,12 +983,6 @@
         (.putInt buf (* Integer/BYTES idx) (nth idxs idx)))
       buf)
     mem/empty-buffer))
-
-(defn- buffer-tl ^ThreadLocal []
-  (ThreadLocal/withInitial
-   (reify Supplier
-     (get [_]
-       (ExpandableDirectByteBuffer. 32)))))
 
 (def ^:private ^ThreadLocal content-hash-buffer-tl (buffer-tl))
 (def ^:private ^ThreadLocal content-buffer-tl (buffer-tl))
