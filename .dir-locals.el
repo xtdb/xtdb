e0dc4f0cc963eb0bd75ba313ca5bd416354b7a78
James Henderson <jms@juxt.pro>
2022-06-01 15:09:04 +0100

    add dir-local for proper indentation (in emacs, at least) of `clojure.core.match/match`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
 ((nil . ((cider-ns-refresh-before-fn . "integrant.repl/suspend")
          (cider-ns-refresh-after-fn  . "integrant.repl/resume")
-         (cider-clojure-cli-aliases . "dev"))))
+         (cider-clojure-cli-aliases . "dev")))
+ (clojure-mode . ((eval . (define-clojure-indent
+                            (match 1))))))
