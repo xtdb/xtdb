4117db42f7f5d4d1dba7604487d1e8f5426c6c88
James Henderson <jms@juxt.pro>
2022-07-05 11:09:19 +0100

    now works on JDK17, resolves xtdb/core2#252 - N.B.: most things now additionally require `:core2` deps.edn alias

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil . ((cider-ns-refresh-before-fn . "integrant.repl/suspend")
          (cider-ns-refresh-after-fn  . "integrant.repl/resume")
-         (cider-clojure-cli-aliases . "dev")))
+         (cider-clojure-cli-aliases . ":core2:dev")))
  (clojure-mode . ((eval . (define-clojure-indent
                             (match 1))))))
