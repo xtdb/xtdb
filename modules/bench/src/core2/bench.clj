82df1165d96033553cc8457091e211fef9bbb643
James Henderson <jms@juxt.pro>
2022-08-04 10:19:54 +0100

    fix reflection warning in core2.bench

--- modules/bench/src/core2/bench.clj
+++ modules/bench/src/core2/bench.clj
@@ -3,7 +3,8 @@
             [clojure.tools.logging :as log]
             [core2.kafka :as k]
             [core2.local-node :as node]
-            [core2.s3 :as s3])
+            [core2.s3 :as s3]
+            [core2.util :as util])
   (:import core2.local_node.Node
            [java.nio.file Files Path]
            java.nio.file.attribute.FileAttribute
@@ -33,7 +34,7 @@
                       (/ (- (System/nanoTime) start-ns#) 1e6)))))))
 
 (defn finish-chunk [^Node node]
-  (.finishChunk ^core2.indexer.Indexer (.indexer node)))
+  (.finishChunk (util/component node :core2.indexer/indexer)))
 
 (defn ^core2.local_node.Node start-node
   ([] (start-node (str (UUID/randomUUID))))
