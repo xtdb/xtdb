f95412434d331f2d096f35379d66ce92269f8a41
James Henderson <jms@juxt.pro>
2022-08-17 17:46:57 +0100

    test case for xtdb/core2#328

--- test/core2/node_test.clj
+++ test/core2/node_test.clj
@@ -27,3 +27,17 @@
                (c2/sql-query tu/*node* "SELECT foo.id, foo.application_time_start, foo.application_time_end FROM foo"
                              {:basis {:tx !tx2}
                               :basis-timeout (java.time.Duration/ofMillis 500)}))))))
+
+(t/deftest test-update-set-field-from-param-328
+  (c2/submit-tx tu/*node* [[:sql "INSERT INTO users (id, first_name, last_name) VALUES (?, ?, ?)"
+                            [["susan", "Susan", "Smith"]]]])
+
+  #_ ; FIXME #328
+  (let [!tx (c2/submit-tx tu/*node* [[:sql "UPDATE users FOR PORTION OF APP_TIME FROM ? TO ? AS u SET first_name = ? WHERE u.id = ?"
+                                      [[#inst "2021", util/end-of-time, "sue", "susan"]]]])]
+
+    (t/is (= #{["Susan" "Smith", (util/->zdt #inst "2020") (util/->zdt #inst "2021")]
+               ["Sue" "Smith", (util/->zdt #inst "2021") (util/->zdt util/end-of-time)]}
+             (->> (c2/sql-query tu/*node* "SELECT u.first_name, u.last_name, u.application_time_start, u.application_time_end FROM users u"
+                                {:basis {:tx !tx}})
+                  (into #{} (map (juxt :first_name :last_name :application_time_start :application_time_end))))))))
