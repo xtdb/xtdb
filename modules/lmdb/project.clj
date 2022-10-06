df7ef914da79c0e21180f24271947cc64f8832bc
Dan Stone <wotbrew@gmail.com>
2022-10-06 15:48:59 +0100

    Include :java-doc-opts and :classifiers in lmdb due to new java sources

--- modules/lmdb/project.clj
+++ modules/lmdb/project.clj
@@ -1,23 +1,6 @@
 (defproject com.xtdb/xtdb-lmdb "<inherited>"
   :description "XTDB LMDB"
 
-  :plugins [[lein-parent "0.3.8"]]
-
-  :parent-project {:path "../../project.clj"
-                   :inherit [:version :repositories :deploy-repositories
-                             :managed-dependencies
-                             :pedantic? :global-vars
-                             :license :url :pom-addition]}
-
-  :scm {:dir "../.."}
-
-  :java-source-paths ["src"]
-  :javac-options ["-source" "8" "-target" "8"
-                  "-XDignore.symbol.file"
-                  "-Xlint:all,-options,-path"
-                  "-Werror"
-                  "-proc:none"]
-
   :dependencies [[org.clojure/clojure]
                  [org.clojure/tools.logging]
                  [com.xtdb/xtdb-core]
@@ -28,4 +11,36 @@
                  [org.lwjgl/lwjgl-lmdb "3.3.1" :classifier "natives-macos" :native-prefix ""]
                  [org.lwjgl/lwjgl "3.3.1" :classifier "natives-macos-arm64" :native-prefix ""]
                  [org.lwjgl/lwjgl-lmdb "3.3.1" :classifier "natives-macos-arm64" :native-prefix ""]
-                 [org.lwjgl/lwjgl-lmdb "3.3.1"]])
+                 [org.lwjgl/lwjgl-lmdb "3.3.1"]]
+
+  :plugins [[lein-javadoc "0.3.0"]
+            [lein-parent "0.3.8"]]
+
+  :parent-project {:path "../../project.clj"
+                   :inherit [:version :repositories :deploy-repositories
+                             :managed-dependencies
+                             :pedantic? :global-vars
+                             :license :url :pom-addition]}
+
+  :scm {:dir "../.."}
+
+  :java-source-paths ["src"]
+
+  :javac-options ["-source" "8" "-target" "8"
+                  "-XDignore.symbol.file"
+                  "-Xlint:all,-options,-path"
+                  "-Werror"
+                  "-proc:none"]
+
+  :javadoc-opts {:package-names ["xtdb"]
+                 :output-dir "target/javadoc/out"
+                 :additional-args ["-windowtitle" "XTDB LMDB Javadoc"
+                                   "-quiet"
+                                   "-Xdoclint:none"
+                                   "-link" "https://docs.oracle.com/javase/8/docs/api/"
+                                   "-link" "https://www.javadoc.io/static/org.clojure/clojure/1.10.3"]}
+
+  :classifiers {:sources {:prep-tasks ^:replace []}
+                :javadoc {:prep-tasks ^:replace ["javadoc"]
+                          :omit-source true
+                          :filespecs ^:replace [{:type :path, :path "target/javadoc/out"}]}})
