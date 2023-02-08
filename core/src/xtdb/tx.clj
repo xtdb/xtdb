d73342a4aad8aae111976cfc1a667ff79a61abb0
Jan Sulmont <1694614+jsulmont@users.noreply.github.com>
2023-02-08 10:28:54 +0000

    send bus event when starting tx-ingester

--- core/src/xtdb/tx.clj
+++ core/src/xtdb/tx.clj
@@ -506,6 +506,7 @@
                                  :secondary-indices :xtdb/secondary-indices}}
   [{:keys [tx-log tx-indexer document-store bus index-store secondary-indices batch-preferred-doc-count]}]
   (log/info "Started tx-ingester")
+  (bus/send bus {::xt/event-type ::tx-ingester-starting})
 
   (let [!error (atom nil)
         secondary-indices @(:!secondary-indices secondary-indices)
