5c354a1d99c8ba71c6615a3118c615591400a835
Jan Sulmont <1694614+jsulmont@users.noreply.github.com>
2023-02-09 11:02:52 +0000

    adjust clock

--- modules/healthcheck/src/xtdb/healthcheck.clj
+++ modules/healthcheck/src/xtdb/healthcheck.clj
@@ -1,13 +1,10 @@
 (ns xtdb.healthcheck
   "Healthcheck for XTDB."
   (:require
-   [clojure.pprint :as pp]
+   [clojure.data.json :as json]
    [xtdb.system :as sys]
    [xtdb.bus :as bus]
    [xtdb.api :as xt]
-   [xtdb.tx :as tx]
-   [xtdb.node :as node]
-   [xtdb.checkpoint :as ckpt]
    [ring.adapter.jetty :as jetty]
    [compojure.core :as ccore]
    [compojure.route :as croute])
@@ -23,11 +20,21 @@
 
 (def ^:const default-server-port 7000)
 
+(defonce base-time (System/nanoTime))
+
+(defn- adjust-clock
+  [now]
+  (/ (- now base-time) (double 1e9)))
+
 (defn- ->routes
   [events]
-  (let [->event-type (fn [m] (get m ::xt/event-type))]
+  (let [->event-type (fn [m] (dissoc m ::xt/event-type))]
     (ccore/routes
-     (ccore/GET "/healthz" params (str (->> @events (map ->event-type) last)))
+     (ccore/context "/healthz" []
+                    (ccore/GET "/hist" []
+                               (json/write-str (->> @events
+                                                    #_(sort-by :timestamp)
+                                                    (mapv ->event-type)))))
      (croute/not-found "Page not found!"))))
 
 (defn ->server {::sys/deps {:bus :xtdb/bus}
@@ -38,13 +45,9 @@
                             :jetty-opts
                             {:doc "Extra options to pass to Jetty, see https://ring-clojure.github.io/ring/ring.adapter.jetty.html"}}}
   [{:keys [bus port jetty-opts] :as options}]
-  (assert bus)
-  (let [events (atom [{::xt/event-type ::node/node-starting}])
+  (let [events (atom [(update (bus/->event :healthz :xtdb.node/node-starting) :clock adjust-clock)])
         app (->routes events)
         ^Server server (jetty/run-jetty app (merge {:port port :join? false} jetty-opts))
-        listener (bus/listen bus {::xt/event-types
-                                  #{::node/node-closing
-                                    ::ckpt/index-restoring
-                                    ::tx/tx-ingester-starting}}
-                             #(swap! events conj %))]
+        listener (bus/listen bus {::xt/event-types #{:xtdb.node/node-closing :healthz}}
+                             #(swap! events conj (update % :clock adjust-clock)))]
     (->HTTPServer server listener events options)))
