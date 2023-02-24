3c01f49a8a2db48b8772c28c8c9c462121595d1c
James Henderson <jms@juxt.pro>
2023-02-24 14:59:03 +0000

    should shift the core2.edn data_readers to the wire-formats module too

---
+++ wire-formats/src/data_readers.clj
@@ -0,0 +1,4 @@
+{c2/period-duration core2.edn/period-duration-reader
+ c2/interval-ym core2.edn/interval-ym-reader
+ c2/interval-dt core2.edn/interval-dt-reader
+ c2/interval-mdn core2.edn/interval-mdn-reader}
