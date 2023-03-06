23a812183377cd6c175210223415a1f0add54991
Dan Stone <wotbrew@gmail.com>
2023-03-06 10:55:28 +0000

    Do not throw on missing in pull/entity

--- core/src/xtdb/tx_document_store_safety.clj
+++ core/src/xtdb/tx_document_store_safety.clj
@@ -12,24 +12,30 @@
 (def ^:dynamic *exp-backoff-opts* nil)
 
 (defn fetch-docs
-  "A version of fetch docs that when in a transaction (*in-tx* is true) implements additional safety semantics
-  that are important for the integrity of transaction processing across multiple nodes.
-
-  - Exceptions thrown by the doc store are retried according to the options of xio/exp-backoff, avoiding an ingester panic for errors relating to unavailability.
-  - Missing documents cause an IllegalStateException to be propagated to the caller (without backoff), this will cause an ingester panic.
+  "Like db/fetch-docs, but exceptions thrown by the doc store cause retries according to the options of xio/exp-backoff, avoiding an ingester panic for errors relating to unavailability.
 
   Options to the xio/exp-backoff loop can be varied with *exp-backoff-opts*.
 
   If not in a transaction, identical to db/fetch-docs."
   [document-store ids]
   (if *in-tx*
-    (let [doc-hashes (into #{} (map c/new-id) ids)
-          docs (xio/exp-backoff #(db/fetch-docs document-store doc-hashes) *exp-backoff-opts*)
-          fetched-doc-hashes (set (keys docs))]
-      (when-not (= fetched-doc-hashes doc-hashes)
-        (let [missing-docs (set/difference doc-hashes fetched-doc-hashes)
-              ex-data {:cognitect.anomalies/category :cognitect.anomalies/not-found
-                       :missing-docs missing-docs}]
-          (throw (xtdb.IllegalStateException. (str "missing docs: " (pr-str missing-docs)) ex-data nil))))
-      docs)
+    (xio/exp-backoff #(db/fetch-docs document-store ids) *exp-backoff-opts*)
     (db/fetch-docs document-store ids)))
+
+(defn fetch-docs-throw-on-missing
+  "Like fetch-docs, but missing documents cause an IllegalStateException to be propagated to the caller (without backoff), in a transaction - this will cause an ingester panic.
+  This is important during transaction processing to avoid consistency issues if documents were present at one point in time but are no longer available (due to say, corruption of the doc store, accidental deletion or a backup restore)
+
+  CAVEAT: currently missing entries are valid for pull/entity calls during transaction functions, so this only applies for fetching referenced docs, tx fn args etc.
+
+  Always returns xtdb.codec/Id instances as the id keys, regardless of the type of id that was supplied."
+  [document-store ids]
+  (let [doc-hashes (into #{} (map c/new-id) ids)
+        docs (fetch-docs document-store ids)
+        fetched-doc-hashes (set (keys docs))]
+    (when-not (= fetched-doc-hashes doc-hashes)
+      (let [missing-docs (set/difference doc-hashes fetched-doc-hashes)
+            ex-data {:cognitect.anomalies/category :cognitect.anomalies/not-found
+                     :missing-docs missing-docs}]
+        (throw (xtdb.IllegalStateException. (str "missing docs: " (pr-str missing-docs)) ex-data nil))))
+    docs))
