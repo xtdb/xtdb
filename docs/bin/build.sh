cecb7245c2a6ae3c5f81fb6dd77d1f304a79e044
James Henderson <james@jarohen.dev>
2023-03-17 09:14:19 +0000

    `npm bin` removed in NPM 9, replaced with `npx`

--- docs/bin/build.sh
+++ docs/bin/build.sh
@@ -5,8 +5,6 @@ PLAYBOOK="antora-playbook.yml"
 
 (
     cd $(dirname "$0")/..
-    ANTORA="$(npm bin)/antora"
-    [[ -e "$ANTORA" ]] || npm install
-    CI=true $ANTORA --clean --redirect-facility static --fetch $PLAYBOOK
+    CI=true npx antora --clean --redirect-facility static --fetch $PLAYBOOK
     echo "Built: file://$(pwd)/build/site/index.html"
 )
