0d8be64147ae33a52ef6b5e611f0961c10c76cb8
Daniel Mason <dan@juxt.pro>
2023-05-12 14:04:22 +0100

    Ensure envs for azure config present for running test.

--- src/test/clojure/xtdb/azure_test.clj
+++ src/test/clojure/xtdb/azure_test.clj
@@ -8,21 +8,26 @@
 
 (def storage-account "xtdbazureobjectstoretest")
 (def container "xtdb-test")
+(def config-present? (some? (and (System/getenv "AZURE_CLIENT_ID")
+                                 (System/getenv "AZURE_CLIENT_SECRET")
+                                 (System/getenv "AZURE_TENANT_ID"))))
 
 (def ^:dynamic *obj-store*)
 
 (t/use-fixtures :each
   (fn [f]
-    (let [sys (-> {::azure/blob-object-store {:storage-account storage-account
-                                              :container container
-                                              :prefix (str "xtdb.azure-test." (UUID/randomUUID))}}
-                  ig/prep
-                  ig/init)]
-      (try
-        (binding [*obj-store* (::azure/blob-object-store sys)]
-          (f))
-        (finally
-          (ig/halt! sys))))))
+    (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET & AZURE_TENANT_ID set)? - " config-present?)
+    (when config-present?
+      (let [sys (-> {::azure/blob-object-store {:storage-account storage-account
+                                                :container container
+                                                :prefix (str "xtdb.azure-test." (UUID/randomUUID))}}
+                    ig/prep
+                    ig/init)]
+        (try
+          (binding [*obj-store* (::azure/blob-object-store sys)]
+            (f))
+          (finally
+            (ig/halt! sys)))))))
 
 ;; TODO: Throws an error when trying to return an IllegalStateException - classcast issue?
 (os-test/def-obj-store-tests ^:azure azure [f]
