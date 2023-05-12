cfdded1a09c42cc78593f20eac9c641fe1ebbf31
Daniel Mason <dan@juxt.pro>
2023-05-12 16:02:57 +0100

    Add event-hub-log implementation

--- modules/azure/build.gradle.kts
+++ modules/azure/build.gradle.kts
@@ -19,5 +19,6 @@ dependencies {
     api(project(":core"))
 
     api("com.azure", "azure-storage-blob", "12.22.0")
+    api("com.azure", "azure-messaging-eventhubs", "5.15.0")
     api("com.azure", "azure-identity", "1.9.0")
 }
--- modules/azure/src/main/clojure/xtdb/azure.clj
+++ modules/azure/src/main/clojure/xtdb/azure.clj
@@ -3,10 +3,13 @@
             [clojure.string :as string]
             [xtdb.util :as util]
             [juxt.clojars-mirrors.integrant.core :as ig]
+            [xtdb.log :as xtdb-log]
             [xtdb.azure
-             [object-store :as os]])
-  (:import  [com.azure.storage.blob BlobServiceClientBuilder]
-            [com.azure.identity DefaultAzureCredentialBuilder]))
+             [object-store :as os]
+             [log :as tx-log]])
+  (:import [com.azure.storage.blob BlobServiceClientBuilder]
+           [com.azure.identity DefaultAzureCredentialBuilder]
+           [com.azure.messaging.eventhubs EventHubClientBuilder EventProcessorClientBuilder]))
 
 (derive ::blob-object-store :xtdb/object-store)
 
@@ -35,3 +38,31 @@
                                         (.buildClient)))
         blob-client (.getBlobContainerClient blob-service-client container)]
     (os/->AzureBlobObjectStore blob-client prefix)))
+
+(s/def ::fully-qualified-namespace string?)
+(s/def ::event-hub-name string?)
+(s/def ::max-wait-time ::util/duration)
+
+(derive ::event-hub-log :xtdb/log)
+
+(defmethod ig/prep-key ::event-hub-log [_ opts]
+  (-> (merge {:max-wait-time "PT1S"} opts)
+      (util/maybe-update :max-wait-time util/->duration)))
+
+(defmethod ig/pre-init-spec ::event-hub-log [_]
+  (s/keys :req-un [::fully-qualified-namespace ::event-hub-name ::max-wait-time]))
+
+(defmethod ig/init-key ::event-hub-log [_ {:keys [fully-qualified-namespace event-hub-name max-wait-time]}]
+  (let [event-hub-client-builder (-> (EventHubClientBuilder.)
+                                     (.consumerGroup "$DEFAULT")
+                                     (.credential fully-qualified-namespace
+                                                  event-hub-name
+                                                  (.build (DefaultAzureCredentialBuilder.))))
+        subscriber-handler (xtdb-log/->notifying-subscriber-handler nil)]
+    (tx-log/->EventHubLog subscriber-handler
+                          (.buildAsyncProducerClient event-hub-client-builder)
+                          (.buildConsumerClient event-hub-client-builder)
+                          max-wait-time)))
+
+(defmethod ig/halt-key! ::event-hub-log [_ log]
+  (util/try-close log))
