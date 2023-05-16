e9a8b634d994e4a62a5053e5f9a91d14dff81440
Daniel Mason <dan@juxt.pro>
2023-05-16 14:40:30 +0100

    Update azure test + dev file.

--- src/dev/clojure/azure_testing.clj
+++ src/dev/clojure/azure_testing.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.io :as io]
             [xtdb.node :as xtdb]
             [xtdb.util :as util]
+            [xtdb.api :as xt]
             [xtdb.azure :as azure]
             [integrant.core :as i]
             [integrant.repl :as ir])
@@ -15,6 +16,8 @@
 (def node)
 (def storage-account "xtdb")
 (def container "xtdb-azure-test")
+(def fully-qualified-namespace "xtdbeventhublogtest.servicebus.windows.net")
+(def eventhub "xtdb-azure-module-test")
 
 (defmethod i/init-key ::xtdb [_ {:keys [node-opts]}]
   (alter-var-root #'node (constantly (xtdb/start-node node-opts)))
@@ -24,16 +27,36 @@
   (util/try-close node)
   (alter-var-root #'node (constantly nil)))
 
-(def azure-default-auth
+(def azure-object-store
   {::xtdb {:node-opts {:xtdb.log/local-directory-log {:root-path (io/file dev-node-dir "log")}
                        :xtdb.buffer-pool/buffer-pool {:cache-path (io/file dev-node-dir "buffers")}
                        ::azure/blob-object-store {:storage-account storage-account
                                                   :container container}}}})
 
-(ir/set-prep! (fn [] azure-default-auth))
+(def azure-log
+  {::xtdb {:node-opts {:xtdb.buffer-pool/buffer-pool {:cache-path (io/file dev-node-dir "buffers")}
+                       ::azure/event-hub-log {:fully-qualified-namespace fully-qualified-namespace
+                                              :event-hub-name eventhub
+                                              :max-wait-time "PT1S"}}}})
+
+(ir/set-prep! (fn [] azure-log))
 
 (comment
   (ir/go)
+  (xt/status node)
+  (def submit (xt/submit-tx node [[:put :posts {:xt/id 1234
+                                                :user-id 5678
+                                                :text "hello world!"}]]))
+  
+  (def a
+    (.appendRecord (:xtdb.azure/event-hub-log (:system node))
+                   (java.nio.ByteBuffer/wrap (.getBytes "Hello3"))))
+
+  
+  (.readRecords (:xtdb.azure/event-hub-log (:system node)) 0 100)
+  
+  (xt/q node '{:find [text]
+               :where [($ :posts [text])]})
   (node)
   (.putObject (:xtdb.azure/blob-object-store (:system node)) "bar" (java.nio.ByteBuffer/wrap (.getBytes "helloworld")))
   (ir/halt)
--- src/test/clojure/xtdb/azure_test.clj
+++ src/test/clojure/xtdb/azure_test.clj
@@ -1,33 +1,44 @@
 (ns xtdb.azure-test
   (:require [clojure.test :as t]
             [juxt.clojars-mirrors.integrant.core :as ig]
-            [xtdb.object-store-test :as os-test]
+            [xtdb.api :as xt]
             [xtdb.azure :as azure]
+            [xtdb.object-store-test :as os-test]
+            [xtdb.node :as node]
+            [xtdb.test-util :as tu]
             [clojure.tools.logging :as log])
   (:import java.util.UUID))
 
 (def storage-account "xtdbazureobjectstoretest")
 (def container "xtdb-test")
+(def fully-qualified-namespace "xtdbeventhublogtest.servicebus.windows.net")
+(def eventhub "xtdb-azure-module-test")
 (def config-present? (some? (and (System/getenv "AZURE_CLIENT_ID")
                                  (System/getenv "AZURE_CLIENT_SECRET")
                                  (System/getenv "AZURE_TENANT_ID"))))
 
 (def ^:dynamic *obj-store*)
 
-(t/use-fixtures :each
-  (fn [f]
-    (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET & AZURE_TENANT_ID set)? - " config-present?)
-    (when config-present?
-      (let [sys (-> {::azure/blob-object-store {:storage-account storage-account
-                                                :container container
-                                                :prefix (str "xtdb.azure-test." (UUID/randomUUID))}}
-                    ig/prep
-                    ig/init)]
-        (try
-          (binding [*obj-store* (::azure/blob-object-store sys)]
-            (f))
-          (finally
-            (ig/halt! sys)))))))
-
 (os-test/def-obj-store-tests ^:azure azure [f]
-  (f *obj-store*))
+  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET & AZURE_TENANT_ID set)? - " config-present?)
+  (when config-present?
+    (let [sys (-> {::azure/blob-object-store {:storage-account storage-account
+                                              :container container
+                                              :prefix (str "xtdb.azure-test." (UUID/randomUUID))}}
+                  ig/prep
+                  ig/init)]
+      (try
+        (binding [*obj-store* (::azure/blob-object-store sys)]
+          (f *obj-store*))
+        (finally
+          (ig/halt! sys))))))
+
+(t/deftest ^:azure test-eventhub-log
+  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET & AZURE_TENANT_ID set)? - " config-present?)
+  (when config-present?
+    (with-open [node (node/start-node {::azure/event-hub-log {:fully-qualified-namespace fully-qualified-namespace
+                                                              :event-hub-name eventhub}})]
+      (xt/submit-tx node [[:put :xt_docs {:xt/id :foo}]])
+      (t/is (= [{:id :foo}]
+               (xt/q node '{:find [id]
+                            :where [($ :xt_docs [{:xt/id id}])]}))))))
