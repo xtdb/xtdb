34b4b7a645def9de037e878e307127582a50a92b
James Henderson <james@jarohen.dev>
2023-06-20 10:50:25 +0100

    ability to dump TPC-H log files

--- modules/datasets/src/main/clojure/xtdb/datasets/tpch.clj
+++ modules/datasets/src/main/clojure/xtdb/datasets/tpch.clj
@@ -1,10 +1,17 @@
 (ns xtdb.datasets.tpch
-  (:require [clojure.string :as str]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [clojure.tools.logging :as log]
-            [xtdb.api :as xt])
+            [cognitect.transit :as t]
+            [xtdb.api :as xt]
+            [xtdb.transit :as xt.t]
+            [xtdb.util :as util])
   (:import clojure.lang.MapEntry
-           [io.airlift.tpch TpchColumn TpchColumnType$Base TpchEntity TpchTable]
-           [java.time LocalDate]))
+           (io.airlift.tpch TpchColumn TpchColumnType$Base TpchEntity TpchTable)
+           (java.io File OutputStream)
+           java.security.MessageDigest
+           (java.time LocalDate)
+           (java.util Arrays)))
 
 ;; 0.05 = 7500 customers, 75000 orders, 299814 lineitems, 10000 part, 40000 partsupp, 500 supplier, 25 nation, 5 region
 
@@ -36,6 +43,13 @@
           TpchColumnType$Base/DATE (fn [^TpchEntity e]
                                      (LocalDate/ofEpochDay (.getDate col e))))))
 
+(def ^:private ^java.security.MessageDigest msg-digest
+  (MessageDigest/getInstance "SHA-256"))
+
+(defn- ->iid ^bytes [^String eid]
+  (-> (.digest msg-digest (.getBytes eid))
+      (Arrays/copyOfRange 0 16)))
+
 (defn- tpch-table->docs [^TpchTable table scale-factor]
   (let [cell-readers (mapv ->cell-reader (.getColumns table))
         table-name (keyword (.getTableName table))
@@ -44,7 +58,7 @@
       (let [doc (into {} (map #(% e)) cell-readers)
             eid (str/join "___" (map doc pk-cols))]
         (-> (assoc doc :xt/id eid)
-            (with-meta {:table table-name}))))))
+            (with-meta {:table table-name, :iid (->iid eid)}))))))
 
 (defn submit-docs! [tx-producer scale-factor]
   (log/debug "Transacting TPC-H tables...")
@@ -96,3 +110,58 @@
                    (log/debug "Transacted" doc-count (.getTableName table))
                    @!last-tx))
                nil)))
+
+(defn dump-tpch-log-files [^File root-dir {:keys [scale-factor tx-size seg-row-limit]
+                                           :or {scale-factor 0.01
+                                                tx-size 200
+                                                seg-row-limit (bit-shift-left 1 20)}}]
+  (let [!os (volatile! nil)]
+    (try
+      (->> (TpchTable/getTables)
+           (transduce (comp (mapcat #(tpch-table->docs % scale-factor))
+                            (map-indexed (fn [row-id doc]
+                                           (let [{:keys [table iid]} (meta doc)]
+                                             {:iid iid, :table table, :row-id row-id, :doc doc})))
+                            (partition-all tx-size)
+                            (map-indexed (fn [tx-id rows]
+                                           {:tx-id tx-id
+                                            :system-time (-> (util/->zdt #inst "2020") (.plusDays tx-id) util/->instant)
+                                            :rows rows})))
+                      (completing
+                       (fn
+                         ([] {:seg-row-count 0})
+                         ([acc tx]
+                          (let [{:keys [^long seg-row-count wtr]}
+                                (loop [{:keys [^long seg-row-count] :as acc} acc]
+                                  (cond
+                                    (>= seg-row-count seg-row-limit) (do
+                                                                       (some-> @!os util/close)
+                                                                       (vreset! !os nil)
+                                                                       (recur {:seg-row-count 0}))
+
+                                    (nil? @!os) (let [{first-tx-id :tx-id, [{first-row-id :row-id}] :rows} tx
+                                                      os (io/output-stream
+                                                          (doto (io/file root-dir "input" (format "seg-r%s-tx%s.transit.json"
+                                                                                                  (util/->lex-hex-string first-row-id)
+                                                                                                  (util/->lex-hex-string first-tx-id)))
+                                                            (io/make-parents)))]
+                                                  (vreset! !os os)
+                                                  (recur {:seg-row-count 0, :wtr (t/writer os :json {:handlers xt.t/tj-write-handlers}) }))
+                                    :else acc))]
+
+                            (when (Thread/interrupted)
+                              (throw (InterruptedException.)))
+
+                            (t/write wtr tx)
+                            (.write ^OutputStream @!os (int \newline))
+
+                            {:seg-row-count (+ seg-row-count (count (:rows tx))), :wtr wtr}))))))
+
+      nil
+
+      (finally
+        (some-> @!os util/close)))))
+
+(comment
+  (dump-tpch-log-files (io/file "/home/james/tmp/idx-poc/tpch-01")
+                       {:scale-factor 0.01}))
