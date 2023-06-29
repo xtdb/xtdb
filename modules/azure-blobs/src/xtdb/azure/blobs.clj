9180cf34f2eb48e027d32c991ddb29f349284949
Daniel Mason <dan@juxt.pro>
2023-06-29 17:08:21 +0100

    Add azure blobs checkpoint store implementation + test.

--- modules/azure-blobs/src/xtdb/azure/blobs.clj
+++ modules/azure-blobs/src/xtdb/azure/blobs.clj
@@ -1,46 +1,59 @@
 (ns xtdb.azure.blobs
-  (:require [clojure.spec.alpha :as s]
+  (:require [clojure.edn :as edn]
+            [clojure.spec.alpha :as s]
             [clojure.string :as string]
             [juxt.clojars-mirrors.nippy.v3v1v1.taoensso.nippy :as nippy]
+            [xtdb.checkpoint :as cp]
+            [xtdb.api :as xt]
             [xtdb.db :as db]
             [xtdb.io :as xio]
             [xtdb.system :as sys]
-            [xtdb.document-store :as ds])
+            [xtdb.document-store :as ds]
+            [clojure.java.io :as io])
   (:import com.azure.core.util.BinaryData
+           java.io.File
+           java.nio.file.Path
            [com.azure.storage.blob BlobServiceClientBuilder]
            [com.azure.identity DefaultAzureCredentialBuilder]
            [com.azure.storage.blob.models BlobStorageException ListBlobsOptions BlobItem]
            [com.azure.storage.blob BlobContainerClient]))
 
-(defn- get-blob [{:keys [^BlobContainerClient blob-container-client]} blob-name]
+(defn get-blob [{:keys [^BlobContainerClient blob-container-client prefix]} blob-name]
   (try
-    (-> (.getBlobClient blob-container-client blob-name)
-        (.downloadContent)
-        (.toBytes))
+    (-> (.getBlobClient blob-container-client (str prefix blob-name))
+        (.downloadContent))
     (catch BlobStorageException e
       (if (= 404 (.getStatusCode e))
         nil
         (throw e)))))
 
-(defn- put-blob 
-  ([opts blob-name blob-bytes]
-   (put-blob opts blob-name blob-bytes false))
-  ([{:keys [^BlobContainerClient blob-container-client]} blob-name blob-bytes overwrite?]
+(defn get-blob-to-file [{:keys [^BlobContainerClient blob-container-client prefix]} blob-name to-file]
+  (try
+    (-> (.getBlobClient blob-container-client (str prefix blob-name))
+        (.downloadToFile to-file))
+    (catch BlobStorageException e
+      (if (= 404 (.getStatusCode e))
+        nil
+        (throw e)))))
+
+(defn put-blob
+  ([this blob-name data]
+   (put-blob this blob-name data false))
+  ([{:keys [^BlobContainerClient blob-container-client prefix]} blob-name ^BinaryData data overwrite?]
    (try
-     (-> (.getBlobClient blob-container-client blob-name)
-         (.upload (BinaryData/fromBytes blob-bytes) overwrite?))
+     (-> (.getBlobClient blob-container-client (str prefix blob-name))
+         (.upload data overwrite?))
      (catch BlobStorageException e
        (if (= 409 (.getStatusCode e))
          nil
          (throw e))))))
 
-(defn- delete-blob [^BlobContainerClient blob-container-client blob-name]
-  (-> (.getBlobClient blob-container-client blob-name)
+(defn delete-blob [{:keys [^BlobContainerClient blob-container-client prefix]} blob-name]
+  (-> (.getBlobClient blob-container-client (str prefix blob-name))
       (.deleteIfExists)))
 
-(defn- list-blobs [^BlobContainerClient blob-container-client prefix obj-prefix]
-  (let [list-blob-opts (cond-> (ListBlobsOptions.)
-                         obj-prefix (.setPrefix obj-prefix))]
+(defn list-blobs [{:keys [^BlobContainerClient blob-container-client prefix]} obj-prefix]
+  (let [list-blob-opts (.setPrefix (ListBlobsOptions.) (str prefix obj-prefix))]
     (->> (.listBlobs blob-container-client list-blob-opts nil)
          (.iterator)
          (iterator-seq)
@@ -51,19 +64,88 @@
   (submit-docs [this docs]
     (->> (for [[id doc] docs]
            (future
-             ;; Put blob - key set to prefix + id, blob is nippy compressed document, overwrite set to true
-             (put-blob this (str prefix id) (nippy/freeze doc) true)))
+             ;; Put blob - key set to id, blob is nippy compressed document 
+             ;; converted to BinaryData, overwrite? set to true
+             (put-blob this
+                       id
+                       (BinaryData/fromBytes (nippy/freeze doc))
+                       true)))
          vec
          (run! deref)))
 
   (fetch-docs [this docs]
     (xio/with-nippy-thaw-all
       (reduce
-       #(if-let [doc (get-blob this (str prefix %2))]
-          (assoc %1 %2 (nippy/thaw doc))
+       #(if-let [doc (get-blob this %2)]
+          (assoc %1 %2 (nippy/thaw (.toBytes doc)))
           %1)
        {}
        docs))))
+(defrecord AzureBlobsCheckpointStore [^BlobContainerClient blob-container-client prefix]
+  cp/CheckpointStore
+  (available-checkpoints [this {::cp/keys [cp-format]}]
+    (->> (list-blobs this "metadata-")
+         (sort-by (fn [path]
+                    (let [[_ tx-id checkpoint-at] (re-matches #"metadata-checkpoint-(\d+)-(.+).edn" path)]
+                      [(Long/parseLong tx-id) checkpoint-at]))
+                  #(compare %2 %1))
+         (keep (fn [cp-metadata-path]
+                 (let [resp (some-> (get-blob this cp-metadata-path)
+                                    (.toString)
+                                    (edn/read-string))]
+                   (when (= (::cp/cp-format resp) cp-format)
+                     resp))))))
+
+  (download-checkpoint [this {::keys [azure-dir] :as checkpoint} dir]
+    (when-not (empty? (.listFiles ^File dir))
+      (throw (IllegalArgumentException. (str "non-empty checkpoint restore dir: " dir))))
+
+    (let [azure-paths (list-blobs this azure-dir)
+          blob-files (->> azure-paths
+                          (mapv (fn [azure-path]
+                                  (let [output-file (io/file dir (subs azure-path (count azure-dir)))
+                                        _ (io/make-parents output-file)
+                                        blob-properties (get-blob-to-file this azure-path (str output-file))]
+                                    (when (.exists output-file)
+                                      [azure-path blob-properties]))))
+                          (into {}))]
+
+      (when-not (= (set (keys blob-files)) (set azure-paths))
+        (xio/delete-dir dir)
+        (throw (ex-info "incomplete checkpoint restore" {:expected azure-paths
+                                                         :actual (keys blob-files)})))
+      checkpoint))
+
+  (upload-checkpoint [this dir {:keys [tx cp-at ::cp/cp-format]}]
+    (let [dir-path (.toPath ^File dir)
+          azure-dir (format "checkpoint-%s-%s" (::xt/tx-id tx) (xio/format-rfc3339-date cp-at))]
+      
+      (->> (file-seq dir)
+           (mapv (fn [^File file]
+                   (when (.isFile file)
+                     (let [file-path (.toPath file)
+                           blob-name (str azure-dir "/" (.relativize dir-path (.toPath file)))]
+                       (put-blob this blob-name (BinaryData/fromFile file-path)))))))
+
+      (let [cp {::cp/cp-format cp-format,
+                :tx tx
+                ::azure-dir (str azure-dir "/")
+                ::cp/checkpoint-at cp-at}]
+        (put-blob this
+                  (str "metadata-" azure-dir ".edn")
+                  (BinaryData/fromString (pr-str cp)))
+        
+        cp)))
+
+  (cleanup-checkpoint [this {:keys [tx cp-at]}]
+    (let [azure-dir (format "checkpoint-%s-%s" (::xt/tx-id tx) (xio/format-rfc3339-date cp-at))]
+
+      ;; Delete EDN file first
+      (delete-blob this (str "metadata-" azure-dir ".edn"))
+
+      ;; List & delete directory contents
+      (let [files (list-blobs this azure-dir)]
+        (mapv #(delete-blob this %) files)))))
 
 (s/def ::prefix (s/and ::sys/string
                        (s/conformer (fn [prefix]
@@ -93,3 +175,20 @@
             :document-cache document-cache
             :document-store
             (->AzureBlobsDocumentStore blob-client prefix)))))
+
+(defn ->cp-store {::sys/args {:storage-account {:required? true
+                                                :spec ::sys/string
+                                                :doc "Azure Storage Account Name"}
+                              :container {:required? true,
+                                          :spec ::sys/string
+                                          :doc "Azure Blob Storage Container"}
+                              :prefix {:required? false
+                                       :spec ::prefix
+                                       :doc "A string to prefix all of your files with (ie, if 'foo' is provided all xtdb files will be located under a 'foo' directory)"}}}
+  [{:keys [storage-account container prefix]}]
+  (let [blob-service-client (cond-> (-> (BlobServiceClientBuilder.)
+                                        (.endpoint (str "https://" storage-account ".blob.core.windows.net"))
+                                        (.credential (.build (DefaultAzureCredentialBuilder.)))
+                                        (.buildClient)))
+        blob-client (.getBlobContainerClient blob-service-client container)]
+    (->AzureBlobsCheckpointStore blob-client prefix)))
--- modules/azure-blobs/test/xtdb/azure/blobs_test.clj
+++ modules/azure-blobs/test/xtdb/azure/blobs_test.clj
@@ -1,12 +1,19 @@
 (ns xtdb.azure.blobs-test
-  (:require [clojure.test :as t]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.test :as t]
             [clojure.tools.logging :as log]
+            [xtdb.api :as xt]
             [xtdb.azure.blobs :as azb]
+            [xtdb.checkpoint :as cp]
             [xtdb.codec :as c]
             [xtdb.db :as db]
+            [xtdb.fixtures :as fix]
+            [xtdb.fixtures.checkpoint-store :as fix.cp-store]
             [xtdb.fixtures.document-store :as fix.ds]
             [xtdb.system :as sys])
-   (:import java.util.UUID))
+   (:import java.util.UUID
+            java.util.Date))
 
 (def storage-account "xtdbazureobjectstoretest")
 (def container "xtdb-test")
@@ -62,3 +69,74 @@
 
           (t/is (= {uuid-key uuid-document}
                    (db/fetch-docs doc-store [uuid-key]))))))))
+
+(t/deftest test-checkpoint-store
+  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID & AZURE_TENANT_ID set)? - " config-present?)
+  (when config-present?
+    (with-open [sys (-> (sys/prep-system {:store {:xtdb/module `azb/->cp-store
+                                                  :storage-account storage-account
+                                                  :container container
+                                                  :prefix (str "test-checkpoint-" (UUID/randomUUID))}})
+                        (sys/start-system))]
+      (fix.cp-store/test-checkpoint-store (:store sys)))))
+
+(t/deftest test-checkpoint-store-cleanup
+  (with-open [sys (-> (sys/prep-system {:store {:xtdb/module `azb/->cp-store
+                                                :storage-account storage-account
+                                                :container container
+                                                :prefix (str "test-checkpoint-" (UUID/randomUUID))}})
+                      (sys/start-system))]
+    (fix/with-tmp-dirs #{dir}
+      (let [cp-at (Date.)
+            cp-store (:store sys)
+            ;; create file for upload
+            _ (spit (io/file dir "hello.txt") "Hello world")
+            {:keys [::azb/azure-dir]} (cp/upload-checkpoint cp-store dir {::cp/cp-format ::foo-cp-format
+                                                                          :tx {::xt/tx-id 1}
+                                                                          :cp-at cp-at})
+            metadata-file (string/replace (str "metadata-" azure-dir) #"/" ".edn")]
+
+        (t/testing "call to upload-checkpoint creates expected folder & checkpoint metadata file for the checkpoint"
+          (let [blob-set (set (azb/list-blobs cp-store nil))]
+            (t/is (= 2 (count blob-set)))
+            (t/is (contains? blob-set metadata-file))
+            (t/is (contains? blob-set (str azure-dir "hello.txt")))))
+
+        (t/testing "call to `cleanup-checkpoints` entirely removes an uploaded checkpoint and metadata"
+          (cp/cleanup-checkpoint cp-store {:tx {::xt/tx-id 1}
+                                           :cp-at cp-at})
+          (t/is (empty? (azb/list-blobs cp-store nil))))))))
+
+(t/deftest test-checkpoint-store-failed-cleanup
+  (with-open [sys (-> (sys/prep-system {:store {:xtdb/module `azb/->cp-store
+                                                :storage-account storage-account
+                                                :container container
+                                                :prefix (str "test-checkpoint-" (UUID/randomUUID))}})
+                      (sys/start-system))]
+    (fix/with-tmp-dirs #{dir}
+      (let [cp-at (Date.)
+            cp-store (:store sys)
+            ;; create file for upload
+            _ (spit (io/file dir "hello.txt") "Hello world")
+            {:keys [::azb/azure-dir]} (cp/upload-checkpoint cp-store dir {::cp/cp-format ::foo-cp-format
+                                                                          :tx {::xt/tx-id 1}
+                                                                          :cp-at cp-at})
+            metadata-file (string/replace (str "metadata-" azure-dir) #"/" ".edn")]
+
+        (t/testing "call to upload-checkpoint creates expected folder & checkpoint metadata file for the checkpoint"
+          (let [blob-set (set (azb/list-blobs cp-store nil))]
+            (t/is (= 2 (count blob-set)))
+            (t/is (contains? blob-set metadata-file))
+            (t/is (contains? blob-set (str azure-dir "hello.txt")))))
+
+        (t/testing "error in `cleanup-checkpoints` after deleting checkpoint metadata file still leads to checkpoint not being available"
+          (with-redefs [azb/list-blobs (fn [_ _] (throw (Exception. "Test Exception")))]
+            (t/is (thrown-with-msg? Exception
+                                    #"Test Exception"
+                                    (cp/cleanup-checkpoint cp-store {:tx {::xt/tx-id 1}
+                                                                     :cp-at cp-at}))))
+          ;; Only directory should be available - checkpoint metadata file should have been deleted
+          (t/is (= [(str azure-dir "hello.txt")]
+                   (azb/list-blobs cp-store nil)))
+          ;; Should not be able to fetch checkpoint as checkpoint metadata file is gone
+          (t/is (empty? (cp/available-checkpoints cp-store ::foo-cp-format))))))))
