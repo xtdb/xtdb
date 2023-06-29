8b2168b2e99805d4f2b3d509cfdce400f57df30c
Daniel Mason <dan@juxt.pro>
2023-06-29 16:31:54 +0100

    Add explicit test against UUID containing document, issue 2602.

--- modules/azure-blobs/test/xtdb/azure/blobs_test.clj
+++ modules/azure-blobs/test/xtdb/azure/blobs_test.clj
@@ -1,9 +1,12 @@
 (ns xtdb.azure.blobs-test
-  (:require [xtdb.azure.blobs :as azb]
-            [clojure.test :as t]
+  (:require [clojure.test :as t]
             [clojure.tools.logging :as log]
+            [xtdb.azure.blobs :as azb]
+            [xtdb.codec :as c]
+            [xtdb.db :as db]
             [xtdb.fixtures.document-store :as fix.ds]
-            [xtdb.system :as sys]))
+            [xtdb.system :as sys])
+   (:import java.util.UUID))
 
 (def storage-account "xtdbazureobjectstoretest")
 (def container "xtdb-test")
@@ -21,3 +24,30 @@
                         (sys/start-system))]
 
       (fix.ds/test-doc-store (::azb/document-store sys)))))
+
+(t/deftest test-blobs-issue-2602
+  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID & AZURE_TENANT_ID set)? - " config-present?)
+  (when config-present?
+    (with-open [sys (-> (sys/prep-system {::azb/document-store
+                                          {:storage-account storage-account
+                                           :container container}})
+                        (sys/start-system))]
+      (let [doc-store (::azb/document-store sys)
+            keyword-document {:xt/id :test-entity
+                              :name "test entity with keyword"}
+            keyword-key (c/new-id keyword-document)
+            uuid-document {:xt/id (UUID/randomUUID)
+                           :name "test entity with UUID"}
+            uuid-key (c/new-id uuid-document)]
+
+        (t/testing "can insert and fetch keyword id document"
+          (db/submit-docs doc-store {keyword-key keyword-document})
+
+          (t/is (= {keyword-key keyword-document}
+                   (db/fetch-docs doc-store [keyword-key]))))
+
+        (t/testing "can insert and fetch UUID document"
+          (db/submit-docs doc-store {uuid-key uuid-document})
+
+          (t/is (= {uuid-key uuid-document}
+                   (db/fetch-docs doc-store [uuid-key]))))))))
