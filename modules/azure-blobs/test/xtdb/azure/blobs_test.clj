6537b297ba4d083b03f8c2e133392b6130d8b146
Dan Stone <wotbrew@gmail.com>
2023-08-31 14:47:30 +0100

    Fix storage test failures (#2745)
    * Ensure azure blob tests only run if config set
    
    * Add nil check before .exists  call in google cloud storage fixture
    
    * Increase resource_class of circle runner and memory available to tests

--- modules/azure-blobs/test/xtdb/azure/blobs_test.clj
+++ modules/azure-blobs/test/xtdb/azure/blobs_test.clj
@@ -22,63 +22,61 @@
                                  (System/getenv "AZURE_TENANT_ID")
                                  (System/getenv "AZURE_SUBSCRIPTION_ID"))))
 
-(t/deftest test-blobs-doc-store
-  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID & AZURE_TENANT_ID set)? - " config-present?)
-  (when config-present?
-    (with-open [sys (-> (sys/prep-system {::azb/document-store
-                                          {:storage-account storage-account
-                                           :container container}})
-                        (sys/start-system))]
+(t/use-fixtures :once
+  (fn [f]
+    (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID & AZURE_TENANT_ID set)? - " config-present?)
+    (when config-present?
+      (f))))
 
-      (fix.ds/test-doc-store (::azb/document-store sys)))))
+(t/deftest test-blobs-doc-store
+  (with-open [sys (-> (sys/prep-system {::azb/document-store
+                                        {:storage-account storage-account
+                                         :container container}})
+                      (sys/start-system))]
+
+    (fix.ds/test-doc-store (::azb/document-store sys))))
 
 (t/deftest test-blobs-doc-store-with-prefix
-  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID & AZURE_TENANT_ID set)? - " config-present?)
-  (when config-present?
-    (with-open [sys (-> (sys/prep-system {::azb/document-store
-                                          {:storage-account storage-account
-                                           :container container
-                                           :prefix (str "test-prefix-" (UUID/randomUUID))}})
-                        (sys/start-system))]
+  (with-open [sys (-> (sys/prep-system {::azb/document-store
+                                        {:storage-account storage-account
+                                         :container container
+                                         :prefix (str "test-prefix-" (UUID/randomUUID))}})
+                      (sys/start-system))]
 
-      (fix.ds/test-doc-store (::azb/document-store sys)))))
+    (fix.ds/test-doc-store (::azb/document-store sys))))
 
 (t/deftest test-blobs-issue-2602
-  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID & AZURE_TENANT_ID set)? - " config-present?)
-  (when config-present?
-    (with-open [sys (-> (sys/prep-system {::azb/document-store
-                                          {:storage-account storage-account
-                                           :container container}})
-                        (sys/start-system))]
-      (let [doc-store (::azb/document-store sys)
-            keyword-document {:xt/id :test-entity
-                              :name "test entity with keyword"}
-            keyword-key (c/new-id keyword-document)
-            uuid-document {:xt/id (UUID/randomUUID)
-                           :name "test entity with UUID"}
-            uuid-key (c/new-id uuid-document)]
+  (with-open [sys (-> (sys/prep-system {::azb/document-store
+                                        {:storage-account storage-account
+                                         :container container}})
+                      (sys/start-system))]
+    (let [doc-store (::azb/document-store sys)
+          keyword-document {:xt/id :test-entity
+                            :name "test entity with keyword"}
+          keyword-key (c/new-id keyword-document)
+          uuid-document {:xt/id (UUID/randomUUID)
+                         :name "test entity with UUID"}
+          uuid-key (c/new-id uuid-document)]
 
-        (t/testing "can insert and fetch keyword id document"
-          (db/submit-docs doc-store {keyword-key keyword-document})
+      (t/testing "can insert and fetch keyword id document"
+        (db/submit-docs doc-store {keyword-key keyword-document})
 
-          (t/is (= {keyword-key keyword-document}
-                   (db/fetch-docs doc-store [keyword-key]))))
+        (t/is (= {keyword-key keyword-document}
+                 (db/fetch-docs doc-store [keyword-key]))))
 
-        (t/testing "can insert and fetch UUID document"
-          (db/submit-docs doc-store {uuid-key uuid-document})
+      (t/testing "can insert and fetch UUID document"
+        (db/submit-docs doc-store {uuid-key uuid-document})
 
-          (t/is (= {uuid-key uuid-document}
-                   (db/fetch-docs doc-store [uuid-key]))))))))
+        (t/is (= {uuid-key uuid-document}
+                 (db/fetch-docs doc-store [uuid-key])))))))
 
 (t/deftest test-checkpoint-store
-  (log/info "Azure config present (AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID & AZURE_TENANT_ID set)? - " config-present?)
-  (when config-present?
-    (with-open [sys (-> (sys/prep-system {:store {:xtdb/module `azb/->cp-store
-                                                  :storage-account storage-account
-                                                  :container container
-                                                  :prefix (str "test-checkpoint-" (UUID/randomUUID))}})
-                        (sys/start-system))]
-      (fix.cp-store/test-checkpoint-store (:store sys)))))
+  (with-open [sys (-> (sys/prep-system {:store {:xtdb/module `azb/->cp-store
+                                                :storage-account storage-account
+                                                :container container
+                                                :prefix (str "test-checkpoint-" (UUID/randomUUID))}})
+                      (sys/start-system))]
+    (fix.cp-store/test-checkpoint-store (:store sys))))
 
 (t/deftest test-checkpoint-store-cleanup
   (with-open [sys (-> (sys/prep-system {:store {:xtdb/module `azb/->cp-store
