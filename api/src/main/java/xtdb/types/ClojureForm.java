d2ae6e543ec8ca598f907db56bdc1903199a4d7c
James Henderson <james@jarohen.dev>
2023-10-04 17:36:12 +0100

    Revert "Override `equals` and `hashCode` for x.t.ClojureForm" - provided by default by `record`
    This reverts commit 1f4ff9bb7675862842a6431dc699dae1dd1af5df.

--- api/src/main/java/xtdb/types/ClojureForm.java
+++ api/src/main/java/xtdb/types/ClojureForm.java
@@ -1,29 +1,4 @@
 package xtdb.types;
 
-import java.util.Objects;
-
 public record ClojureForm(Object form) {
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (other == null) {
-            return false;
-        }
-        if (!(other instanceof ClojureForm)) {
-            return false;
-        }
-        ClojureForm cljForm = (ClojureForm) other;
-        if (cljForm.form.equals(this.form)) {
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-        public int hashCode() {
-        return Objects.hash(form);
-    }
 }
