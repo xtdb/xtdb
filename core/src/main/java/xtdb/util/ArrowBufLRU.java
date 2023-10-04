3c4a8720cdcf60f7193c30fc07dcec80067f54ce
Dan Stone <wotbrew@gmail.com>
2023-10-04 09:55:34 +0100

    Simplify ArrowBufLRU

--- core/src/main/java/xtdb/util/ArrowBufLRU.java
+++ core/src/main/java/xtdb/util/ArrowBufLRU.java
@@ -4,39 +4,33 @@ import org.apache.arrow.memory.ArrowBuf;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.function.BiPredicate;
 
-public class ArrowBufLRU<K, ArrowBuf> extends LinkedHashMap<K, ArrowBuf> {
+public class ArrowBufLRU<K> extends LinkedHashMap<K, ArrowBuf> {
     private static final long serialVersionUID = 1L;
 
     private long byteSize;
-    private final BiPredicate<Map<K, ArrowBuf>, Entry<K, ArrowBuf>> removeEldestEntryFn;
+    private final int maxSize;
+    private final long maxByteSize;
 
     @Override
     public ArrowBuf put(K key, ArrowBuf buf) {
-        byteSize += ((org.apache.arrow.memory.ArrowBuf) buf).capacity();
-        return super.put(key, (ArrowBuf) buf);
+        byteSize += buf.capacity();
+        return super.put(key, buf);
     }
 
     public ArrowBufLRU(int initialSize, int maxSize, long maxByteSize) {
         super(initialSize, 0.75f, true);
         this.byteSize = 0;
-        this.removeEldestEntryFn = new BiPredicate<Map<K, ArrowBuf>, Entry<K, ArrowBuf>>() {
-            @Override
-            public boolean test(Map<K, ArrowBuf> m, Entry<K, ArrowBuf> entry) {
-                if(maxSize < m.size() || maxByteSize < byteSize){
-                    ((org.apache.arrow.memory.ArrowBuf) entry.getValue()).close();
-                    return  true;
-                }
-                return false;
-            }
-        };
+        this.maxSize = maxSize;
+        this.maxByteSize = maxByteSize;
     }
 
     public boolean removeEldestEntry(Map.Entry<K, ArrowBuf> entry) {
-        var res = this.removeEldestEntryFn.test(this, entry);
-        if (res) byteSize -= ((org.apache.arrow.memory.ArrowBuf) entry.getValue()).capacity();
-        return res;
+        if(maxSize < size() || maxByteSize < byteSize){
+            byteSize -= entry.getValue().capacity();
+            entry.getValue().close();
+            return true;
+        }
+        return false;
     }
 }
