8569bd91556193eb7a7d598c32fcc56e6740d56e
Dan Stone <wotbrew@gmail.com>
2023-10-09 11:10:41 +0100

    Indexer uses new :bytes-per-chunk option
    This helps limit memory usage when row widths are unpredictable
    
    Progress toward #2816

--- core/src/main/java/xtdb/util/RowCounter.java
+++ core/src/main/java/xtdb/util/RowCounter.java
@@ -1,8 +1,15 @@
 package xtdb.util;
 
 public class RowCounter {
+    /**
+     * An approximation of the chunk-size overhead added for a single row, regardless of contents.
+     * This number is a parameter to the deterministic indexing, so it is not something we can change.
+     */
+    private static final long ROW_BYTE_COUNT_APPROX_OVERHEAD = 48;
+
     private long chunkIdx;
     private long chunkRowCount;
+    private long chunkApproxByteCount;
 
     public RowCounter(long chunkIdx) {
         this.chunkIdx = chunkIdx;
@@ -11,11 +18,14 @@ public class RowCounter {
     public void nextChunk() {
         chunkIdx += chunkRowCount;
         chunkRowCount = 0;
+        chunkApproxByteCount = 0;
     }
 
     public void addRows(int rowCount) {
         chunkRowCount += rowCount;
+        chunkApproxByteCount += ROW_BYTE_COUNT_APPROX_OVERHEAD * rowCount;
     }
+    public void addTxBytes(int txByteCount) { chunkApproxByteCount += txByteCount; }
 
     public long getChunkRowCount() {
         return chunkRowCount;
@@ -23,5 +33,9 @@ public class RowCounter {
 
     public long getChunkIdx() {
         return chunkIdx;
+    }
+
+    public long getChunkByteCount() {
+        return chunkApproxByteCount;
     }
 }
