11f43d52557e10e2351a7795f27b08789cbd320a
James Henderson <james@jarohen.dev>
2023-10-19 10:43:11 +0100

    bump CIDER

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ allprojects {
                 implementation("org.clojure", "clojure", "1.11.1")
 
                 testRuntimeOnly("dev.clojurephant", "jovial", "0.4.1")
-                nrepl("cider", "cider-nrepl", "0.30.0")
+                nrepl("cider", "cider-nrepl", "0.40.0")
             }
 
             clojure {
