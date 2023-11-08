dada604cbfd642d1bb6665b17ee7c5318e733e30
James Henderson <james@jarohen.dev>
2023-11-08 14:36:04 +0000

    compactor stores polygons rather than just log valid-from/valid-to

--- core/src/main/clojure/xtdb/compactor.clj
+++ core/src/main/clojure/xtdb/compactor.clj
@@ -1,6 +1,7 @@
 (ns xtdb.compactor
   (:require [clojure.tools.logging :as log]
             [juxt.clojars-mirrors.integrant.core :as ig]
+            [xtdb.bitemporal :as bitemp]
             xtdb.buffer-pool
             xtdb.object-store
             [xtdb.trie :as trie]
@@ -15,9 +16,13 @@
            [org.apache.arrow.memory.util ArrowBufPointer]
            org.apache.arrow.vector.types.pojo.Field
            org.apache.arrow.vector.VectorSchemaRoot
+           xtdb.bitemporal.Polygon
            xtdb.IBufferPool
            (xtdb.trie EventRowPointer IDataRel LiveHashTrie)
-           xtdb.vector.IRowCopier))
+           xtdb.util.TemporalBounds
+           xtdb.vector.IRelationWriter
+           xtdb.vector.IRowCopier
+           xtdb.vector.RelationReader))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (definterface ICompactor
@@ -31,13 +36,63 @@
                                    types/field->col-type))
                              (apply types/merge-col-types))))
 
+(defn- ->polygon-writer [^IRelationWriter data-wtr]
+  (let [valid-times-wtr (.colWriter data-wtr "xt$valid_times")
+        valid-time-wtr (.listElementWriter valid-times-wtr)
+        sys-time-ceils-wtr (.colWriter data-wtr "xt$system_time_ceilings")
+        sys-time-ceil-wtr (.listElementWriter sys-time-ceils-wtr)]
+
+    (fn write-polygon! [^Polygon polygon]
+      (if polygon
+        (let [range-count (.getValidTimeRangeCount polygon)]
+          (.startList valid-times-wtr)
+          (.startList sys-time-ceils-wtr)
+          (dotimes [i range-count]
+            (.writeLong valid-time-wtr (.getValidFrom polygon i))
+            (.writeLong sys-time-ceil-wtr (.getSystemTo polygon i)))
+
+          (.writeLong valid-time-wtr (.getValidTo polygon (dec range-count)))
+
+          (.endList sys-time-ceils-wtr)
+          (.endList valid-times-wtr))
+
+        (do
+          (.writeNull valid-times-wtr)
+          (.writeNull sys-time-ceils-wtr))))))
+
+(defn- ->reader->copier [^IRelationWriter data-wtr]
+  (let [iid-wtr (.colWriter data-wtr "xt$iid")
+        sf-wtr (.colWriter data-wtr "xt$system_from")
+        calculate-polygon (bitemp/polygon-calculator (TemporalBounds.))
+        write-polygon! (->polygon-writer data-wtr)
+        op-wtr (.colWriter data-wtr "op")]
+    (fn reader->copier [^RelationReader data-rdr, ^EventRowPointer ev-ptr]
+      (let [iid-copier (-> (.readerForName data-rdr "xt$iid") (.rowCopier iid-wtr))
+            sf-copier (-> (.readerForName data-rdr "xt$system_from") (.rowCopier sf-wtr))
+            op-copier (-> (.readerForName data-rdr "op") (.rowCopier op-wtr))]
+        (reify IRowCopier
+          (copyRow [_ ev-idx]
+            (.startRow data-wtr)
+            (let [pos (.copyRow iid-copier ev-idx)]
+              (.copyRow sf-copier ev-idx)
+
+              (doto (calculate-polygon ev-ptr)
+                (write-polygon!))
+
+              (.copyRow op-copier ev-idx)
+              (.endRow data-wtr)
+
+              pos)))))))
+
 (defn merge-tries! [^BufferAllocator allocator, ^IBufferPool buffer-pool
                     tries data-rels table-path trie-key]
   (let [data-rel-schema (->log-data-rel-schema data-rels)]
     (util/with-open [trie-wtr (trie/open-trie-writer allocator buffer-pool data-rel-schema table-path trie-key)
                      data-root (VectorSchemaRoot/create data-rel-schema allocator)]
       (let [data-wtr (vw/root->writer data-root)
+            reader->copier (->reader->copier data-wtr)
             is-valid-ptr (ArrowBufPointer.)]
+
         (letfn [(merge-nodes! [path [mn-tag mn-arg]]
                   (case mn-tag
                     :branch (.writeBranch trie-wtr (int-array mn-arg))
@@ -45,10 +100,10 @@
                     :leaf (let [data-rdrs (trie/load-data-pages data-rels mn-arg)
                                 merge-q (PriorityQueue. (Comparator/comparing (util/->jfn :ev-ptr) (EventRowPointer/comparator)))]
 
-                            (doseq [data-rdr data-rdrs
+                            (doseq [^RelationReader data-rdr data-rdrs
                                     :when data-rdr
                                     :let [ev-ptr (EventRowPointer. data-rdr (byte-array 0))
-                                          row-copier (.rowCopier data-wtr data-rdr)]]
+                                          row-copier (reader->copier data-rdr ev-ptr)]]
                               (when (.isValid ev-ptr is-valid-ptr path)
                                 (.add merge-q {:ev-ptr ev-ptr, :row-copier row-copier})))
 
@@ -57,7 +112,9 @@
                                             (.build))]
 
                               (if-let [{:keys [^EventRowPointer ev-ptr, ^IRowCopier row-copier] :as q-obj} (.poll merge-q)]
-                                (let [pos (.copyRow row-copier (.getIndex ev-ptr))]
+                                (let [ev-idx (.getIndex ev-ptr)
+                                      pos (.copyRow row-copier ev-idx)]
+
                                   (.nextIndex ev-ptr)
                                   (when (.isValid ev-ptr is-valid-ptr path)
                                     (.add merge-q q-obj))
