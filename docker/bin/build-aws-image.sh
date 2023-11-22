144eb963042454e9c4e1b3fee288e314f91b998e
Daniel Mason <dan@juxt.pro>
2023-11-22 11:20:57 +0000

    Add AWS dockerfile with config and build script.

---
+++ docker/bin/build-aws-image.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+(
+    cd $(dirname $0)/..
+
+    if [ "$1" == "--clean" ] || ! [ -e build/libs/xtdb-standalone.jar ]; then
+        ../gradlew :docker:aws:shadowJar
+    fi
+
+    sha=$(git rev-parse --short HEAD)
+
+    echo Building Docker image ...
+    docker build -t ghcr.io/xtdb/xtdb-aws-ea:latest --build-arg GIT_SHA="$sha" --build-arg XTDB_VERSION="${XTDB_VERSION:-dev-SNAPSHOT}" --output type=docker aws
+    echo Done
+)
