371210000b26c3b94783efeefc891c6aae42af63
James Henderson <james@jarohen.dev>
2023-11-26 11:51:09 +0000

    supports sets in unwind, fixes #2915

--- core/src/main/clojure/xtdb/operator/unwind.clj
+++ core/src/main/clojure/xtdb/operator/unwind.clj
@@ -10,9 +10,10 @@
            (java.util.stream IntStream)
            (org.apache.arrow.memory BufferAllocator)
            (org.apache.arrow.vector IntVector)
-           [org.apache.arrow.vector.types.pojo ArrowType$List ArrowType$FixedSizeList ArrowType$Union Field]
+           [org.apache.arrow.vector.types.pojo ArrowType$FixedSizeList ArrowType$List Field]
            (xtdb ICursor)
-           (xtdb.vector RelationReader IVectorReader IVectorWriter)))
+           (xtdb.vector IRowCopier IVectorReader IVectorWriter RelationReader)
+           xtdb.vector.extensions.SetType))
 
 (s/def ::ordinality-column ::lp/column)
 
@@ -35,49 +36,63 @@
       (while (and (.tryAdvance in-cursor
                                (reify Consumer
                                  (accept [_ in-rel]
-                                   (let [^RelationReader in-rel in-rel
-                                         out-cols (LinkedList.)
-                                         vec-rdr (.readerForName in-rel from-column-name)
-                                         list-rdr (cond-> vec-rdr
-                                                    (instance? ArrowType$Union (.getType (.getField vec-rdr))) (.legReader :list))
-                                         el-rdr (some-> list-rdr .listElementReader)
-                                         idxs (IntStream/builder)
+                                   (with-open [out-vec (.createVector to-field allocator)]
+                                     (let [out-writer (vw/->writer out-vec)
+                                           out-cols (LinkedList.)
 
-                                         ordinal-vec (when ordinality-column
-                                                       (IntVector. ordinality-column allocator))
+                                           ^RelationReader in-rel in-rel
+                                           vec-rdr (.readerForName in-rel from-column-name)
+                                           vec-type (.getType (.getField vec-rdr))
 
-                                         _ (when ordinal-vec
-                                             (.add out-cols (vr/vec->reader ordinal-vec)))
+                                           rdrs+copiers
+                                           (case vec-type
+                                             (#xt.arrow/type :list, #xt.arrow/type :set)
+                                             [[vec-rdr (-> (.listElementReader vec-rdr)
+                                                           (.rowCopier out-writer))]]
 
-                                         ^IVectorWriter ordinal-wtr (some-> ordinal-vec vw/->writer)]
+                                             #xt.arrow/type :union
+                                             (concat (when-let [list-rdr (.legReader vec-rdr :list)]
+                                                       [[list-rdr (-> (.rowCopier (.listElementReader list-rdr) out-writer))]])
+                                                     (when-let [set-rdr (.legReader vec-rdr :set)]
+                                                       [[set-rdr (-> (.rowCopier (.listElementReader set-rdr) out-writer))]]))
 
-                                     (try
-                                       (with-open [out-vec (.createVector to-field allocator)]
-                                         (let [out-writer (vw/->writer out-vec)
-                                               el-copier (.rowCopier el-rdr out-writer)]
-                                           (dotimes [n (.valueCount vec-rdr)]
-                                             (when-not (.isNull list-rdr n)
-                                               (let [len (.getListCount list-rdr n)
-                                                     start-pos (.getListStartIndex list-rdr n)]
+                                             nil)
+
+                                           idxs (IntStream/builder)
+
+                                           ordinal-vec (when ordinality-column
+                                                         (IntVector. ordinality-column allocator))
+
+                                           _ (when ordinal-vec
+                                               (.add out-cols (vr/vec->reader ordinal-vec)))
+
+                                           ^IVectorWriter ordinal-wtr (some-> ordinal-vec vw/->writer)]
+
+                                       (try
+                                         (dotimes [n (.valueCount vec-rdr)]
+                                           (doseq [[^IVectorReader coll-rdr, ^IRowCopier el-copier] rdrs+copiers]
+                                             (when (and coll-rdr (not (.isNull coll-rdr n)))
+                                               (let [len (.getListCount coll-rdr n)
+                                                     start-pos (.getListStartIndex coll-rdr n)]
                                                  (dotimes [el-idx len]
                                                    (.copyRow el-copier (+ start-pos el-idx))
                                                    (.add idxs n)
-                                                   (some-> ordinal-wtr (.writeInt (inc el-idx)))))))
+                                                   (some-> ordinal-wtr (.writeInt (inc el-idx))))))))
 
-                                           (let [idxs (.toArray (.build idxs))]
-                                             (when (pos? (alength idxs))
-                                               (some-> ordinal-vec (.setValueCount (alength idxs)))
+                                         (let [idxs (.toArray (.build idxs))]
+                                           (when (pos? (alength idxs))
+                                             (some-> ordinal-vec (.setValueCount (alength idxs)))
 
-                                               (doseq [^IVectorReader in-col in-rel]
-                                                 (when (= from-column-name (.getName in-col))
-                                                   (.add out-cols (vw/vec-wtr->rdr out-writer)))
+                                             (doseq [^IVectorReader in-col in-rel]
+                                               (when (= from-column-name (.getName in-col))
+                                                 (.add out-cols (vw/vec-wtr->rdr out-writer)))
 
-                                                 (.add out-cols (.select in-col idxs)))
+                                               (.add out-cols (.select in-col idxs)))
 
-                                               (.accept c (vr/rel-reader out-cols (alength idxs)))
-                                               (aset advanced? 0 true)))))
-                                       (finally
-                                         (util/try-close ordinal-vec)))))))
+                                             (.accept c (vr/rel-reader out-cols (alength idxs)))
+                                             (aset advanced? 0 true)))
+                                         (finally
+                                           (util/try-close ordinal-vec))))))))
 
                   (not (aget advanced? 0))))
       (aget advanced? 0)))
@@ -100,16 +115,18 @@
     (lp/unary-expr (lp/emit-expr relation op-args)
       (fn [fields]
         (let [unwind-field (->> (get fields from-col)
-                                   types/flatten-union-field
-                                   (keep (fn [^Field field]
-                                           (condp = (class (.getType field))
-                                             ArrowType$List (first (.getChildren field))
-                                             ArrowType$FixedSizeList (first (.getChildren field))
-                                             (types/col-type->field :null))))
-                                   (apply types/merge-fields))]
+                                types/flatten-union-field
+                                (keep (fn [^Field field]
+                                        (condp instance? (.getType field)
+                                          ArrowType$List (first (.getChildren field))
+                                          SetType (first (.getChildren field))
+                                          ArrowType$FixedSizeList (first (.getChildren field))
+                                          (types/col-type->field :null))))
+                                (apply types/merge-fields))]
+
           {:fields (-> fields
-                          (assoc to-col unwind-field)
-                          (cond-> ordinality-column (assoc ordinality-column (types/col-type->field :i32))))
+                       (assoc to-col unwind-field)
+                       (cond-> ordinality-column (assoc ordinality-column (types/col-type->field :i32))))
            :->cursor (fn [{:keys [allocator]} in-cursor]
                        (UnwindCursor. allocator in-cursor
                                       (str from-col) (types/field-with-name unwind-field (str to-col))
