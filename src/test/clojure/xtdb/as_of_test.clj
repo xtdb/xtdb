1dda7f497cd25df83963849ed040752734f0b4c9
James Henderson <james@jarohen.dev>
2023-12-04 11:29:56 +0000

    as-of-test -> XTQL

--- src/test/clojure/xtdb/as_of_test.clj
+++ src/test/clojure/xtdb/as_of_test.clj
@@ -8,34 +8,28 @@
 (t/use-fixtures :each tu/with-node)
 
 (t/deftest test-as-of-tx
-  (let [tx1 (xt/submit-tx tu/*node* [(xt/put :xt_docs {:xt/id :my-doc, :last-updated "tx1"})])]
-    (xt/submit-tx tu/*node* [(xt/put :xt_docs {:xt/id :my-doc, :last-updated "tx2"})])
+  (let [tx1 (xt/submit-tx tu/*node* [(xt/put :docs {:xt/id :my-doc, :last-updated "tx1"})])]
+    (xt/submit-tx tu/*node* [(xt/put :docs {:xt/id :my-doc, :last-updated "tx2"})])
 
     (t/is (= #{{:last-updated "tx1"} {:last-updated "tx2"}}
-             (set (tu/query-ra '[:scan {:table xt_docs} [last_updated]]
+             (set (tu/query-ra '[:scan {:table docs} [last_updated]]
                                {:node tu/*node* :default-all-valid-time? true}))))
 
     (t/is (= #{{:last-updated "tx2"}}
-             (set (xt/q tu/*node*
-                        '{:find [last-updated]
-                          :where [(match :xt_docs {:xt/id e})
-                                  [e :last-updated last-updated]]}))))
+             (set (xt/q tu/*node* '(from :docs [last-updated])))))
 
     (t/testing "at tx1"
       (t/is (= #{{:last-updated "tx1"}}
-               (set (tu/query-ra '[:scan {:table xt_docs} [last_updated]]
+               (set (tu/query-ra '[:scan {:table docs} [last_updated]]
                                  {:node tu/*node*, :basis {:tx tx1}}))))
 
       (t/is (= #{{:last-updated "tx1"}}
-               (set (xt/q tu/*node*
-                          '{:find [last-updated]
-                            :where [(match :xt_docs {:xt/id e})
-                                    [e :last-updated last-updated]]}
+               (set (xt/q tu/*node* '(from :docs [last-updated])
                           {:basis {:tx tx1}})))))))
 
 (t/deftest test-app-time
-  (let [{:keys [system-time]} (xt/submit-tx tu/*node* [(xt/put :xt_docs {:xt/id :doc, :version 1})
-                                                       (-> (xt/put :xt_docs {:xt/id :doc-with-app-time})
+  (let [{:keys [system-time]} (xt/submit-tx tu/*node* [(xt/put :docs {:xt/id :doc, :version 1})
+                                                       (-> (xt/put :docs {:xt/id :doc-with-app-time})
                                                            (xt/starting-from #inst "2021"))])
         system-time (time/->zdt system-time)]
 
@@ -49,7 +43,7 @@
                                   :xt/valid-to nil
                                   :xt/system-from system-time
                                   :xt/system-to nil}}
-             (->> (tu/query-ra '[:scan {:table xt_docs}
+             (->> (tu/query-ra '[:scan {:table docs}
                                  [xt$id
                                   xt$valid_from xt$valid_to
                                   xt$system_from xt$system_to]]
@@ -57,10 +51,10 @@
                   (into {} (map (juxt :xt/id identity))))))))
 
 (t/deftest test-system-time
-  (let [tx1 (xt/submit-tx tu/*node* [(xt/put :xt_docs {:xt/id :doc, :version 0})])
+  (let [tx1 (xt/submit-tx tu/*node* [(xt/put :docs {:xt/id :doc, :version 0})])
         tt1 (time/->zdt (:system-time tx1))
 
-        tx2 (xt/submit-tx tu/*node* [(xt/put :xt_docs {:xt/id :doc, :version 1})])
+        tx2 (xt/submit-tx tu/*node* [(xt/put :docs {:xt/id :doc, :version 1})])
         tt2 (time/->zdt (:system-time tx2))
 
         original-v0-doc {:xt/id :doc, :version 0
@@ -82,40 +76,38 @@
                 :xt/system-to nil}]
 
     (t/is (= #{original-v0-doc v1-doc}
-             (set (tu/query-ra '[:scan {:table xt_docs}
-                                 [xt$id version
-                                  xt$valid_from xt$valid_to
-                                  xt$system_from xt$system_to]]
-                               {:node tu/*node*, :default-all-valid-time? true})))
+             (set (xt/q tu/*node*
+                        '(from :docs {:bind [xt/id version
+                                             xt/valid-from xt/valid-to xt/system-from xt/system-to]
+                                      :for-valid-time :all-time}))))
           "all app-time")
 
     (t/is (= #{original-v0-doc replaced-v0-doc v1-doc}
-             (set (tu/query-ra '[:scan {:table xt_docs, :for-system-time :all-time}
-                                 [xt$id version
-                                  xt$valid_from xt$valid_to
-                                  xt$system_from xt$system_to]]
-                               {:node tu/*node*
-                                :default-all-valid-time? true})))
+             (set (xt/q tu/*node*
+                        '(from :docs {:bind [xt/id version
+                                             xt/valid-from xt/valid-to xt/system-from xt/system-to]
+                                      :for-valid-time :all-time
+                                      :for-system-time :all-time}))))
           "all app, all sys")))
 
 (t/deftest test-erase
   (letfn [(all-time-docs []
-            (->> (tu/query-ra '[:scan {:table xt_docs, :for-valid-time :all-time, :for-system-time :all-time}
-                                [xt$id xt$valid_from xt$valid_to xt$system_from xt$system_to]]
-                              {:node tu/*node*
-                               :default-all-valid-time? true})
+            (->> (xt/q tu/*node*
+                       '(from :docs {:bind [xt/id xt/valid-from xt/valid-to xt/system-from xt/system-to]
+                                     :for-valid-time :all-time
+                                     :for-system-time :all-time}))
                  (map :xt/id)
                  frequencies))]
 
-    (xt/submit-tx tu/*node* [(xt/put :xt_docs {:xt/id :doc, :version 0})
-                             (xt/put :xt_docs {:xt/id :other-doc, :version 0})])
+    (xt/submit-tx tu/*node* [(xt/put :docs {:xt/id :doc, :version 0})
+                             (xt/put :docs {:xt/id :other-doc, :version 0})])
 
-    (xt/submit-tx tu/*node* [(xt/put :xt_docs {:xt/id :doc, :version 1})])
+    (xt/submit-tx tu/*node* [(xt/put :docs {:xt/id :doc, :version 1})])
 
     (t/is (= {:doc 3, :other-doc 1} (all-time-docs))
           "documents present before erase")
 
-    (xt/submit-tx tu/*node* [(xt/erase :xt_docs :doc)])
+    (xt/submit-tx tu/*node* [(xt/erase :docs :doc)])
 
     (t/is (= {:other-doc 1} (all-time-docs))
           "documents removed after erase")))
