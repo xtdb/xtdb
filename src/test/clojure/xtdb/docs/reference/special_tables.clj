aab0faf6966d16b6f48b2da41ea9e0ad02ad758e
Oliver Marshall <olivershawmarshall@gmail.com>
2023-12-12 15:05:36 +0000

    docs: add page for special tables

---
+++ src/test/clojure/xtdb/docs/reference/special_tables.clj
@@ -0,0 +1,40 @@
+(ns xtdb.docs.reference.special-tables
+  (:require [clojure.test :as t :refer [deftest]]
+            [xtdb.api :as xt]
+            [xtdb.test-util :as tu]))
+
+(t/use-fixtures :each tu/with-node)
+
+(deftest test-examples
+  (let [*node* tu/*node*]
+    ;; TODO: Remove when this is fixed:
+    ;;       https://github.com/xtdb/xtdb/issues/3035
+    (xt/submit-tx *node*
+      [(xt/put :users {:xt/id :not-john})])
+
+    ;; tag::try-update[]
+    (xt/submit-tx *node*
+      [(xt/assert-exists '(from :users [{:xt/id :john}]))
+       (xt/update-table :users '{:bind {:xt/id :john :age age}
+                                 :set {:age (inc age)}})])
+    ;; end::try-update[]
+
+    (t/is (= ;; tag::query-result[]
+             [{:xt/id 0,
+               :xt/committed? true,
+               :xt/error nil}
+              {:xt/id 1,
+               :xt/committed? false,
+               :xt/error {:xtdb.error/error-type :runtime-error,
+                          :xtdb.error/error-key :xtdb/assert-failed,
+                          :xtdb.error/message "Precondition failed: assert-exists",
+                          :row-count 0}}]
+             ;; end::query-result[]
+
+             ;; tag::query-txs[]
+             (->> (xt/q *node*
+                    '(-> (from :xt/txs [xt/id xt/committed? xt/error])
+                         (order-by xt/id)))
+                  (map #(update % :xt/error ex-data)))
+             ;; end::query-txs[]
+             ,))))
