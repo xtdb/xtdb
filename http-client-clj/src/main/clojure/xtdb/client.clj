582a6b70845e2571737c2cea92eabac35a74bb4a
James Henderson <james@jarohen.dev>
2023-12-12 17:31:49 +0000

    add docstrings to `xtdb.node` and `xtdb.client`, #2967

--- http-client-clj/src/main/clojure/xtdb/client.clj
+++ http-client-clj/src/main/clojure/xtdb/client.clj
@@ -1,5 +1,9 @@
 (ns xtdb.client
   (:require [xtdb.client.impl :as impl]))
 
-(defn start-client ^java.lang.AutoCloseable [url]
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn start-client
+  "Starts a client that connects to a remote XTDB node"
+
+  ^java.lang.AutoCloseable [url]
   (impl/start-client url))
