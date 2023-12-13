acb4fa40321f1d8997f1afc5e3de8e86bbdc0f0b
James Henderson <james@jarohen.dev>
2023-12-13 12:49:51 +0000

    equals + hashCode in *Specs

--- api/src/main/java/xtdb/query/ArgSpec.java
+++ api/src/main/java/xtdb/query/ArgSpec.java
@@ -1,5 +1,7 @@
 package xtdb.query;
 
+import java.util.Objects;
+
 public final class ArgSpec {
     public final String attr;
     public final Expr expr;
@@ -16,5 +18,18 @@ public final class ArgSpec {
 
     public static ArgSpec of(String attr, Expr expr) {
         return new ArgSpec(attr, expr);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArgSpec argSpec = (ArgSpec) o;
+        return Objects.equals(attr, argSpec.attr) && Objects.equals(expr, argSpec.expr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attr, expr);
     }
 }
--- api/src/main/java/xtdb/query/ColSpec.java
+++ api/src/main/java/xtdb/query/ColSpec.java
@@ -1,5 +1,7 @@
 package xtdb.query;
 
+import java.util.Objects;
+
 public final class ColSpec {
     public final String attr;
     public final Expr expr;
@@ -16,5 +18,18 @@ public final class ColSpec {
 
     public static ColSpec of(String attr, Expr expr) {
         return new ColSpec(attr, expr);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ColSpec colSpec = (ColSpec) o;
+        return Objects.equals(attr, colSpec.attr) && Objects.equals(expr, colSpec.expr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attr, expr);
     }
 }
