f2c32c9d011908d48ad0f42c7cb8dc494fb258e8
Oliver Marshall <olivershawmarshall@gmail.com>
2023-12-19 17:33:40 +0000

    docs: fix special_tables test

--- src/test/clojure/xtdb/docs/reference/special_tables.clj
+++ src/test/clojure/xtdb/docs/reference/special_tables.clj
@@ -8,33 +8,31 @@
 (deftest test-examples
   (let [*node* tu/*node*]
     ;; TODO: Remove when this is fixed:
-    ;;       https://github.com/xtdb/xtdb/issues/3035
+    ;;       https://github.com/xtdb/xtdb/issues/3061
     (xt/submit-tx *node*
       [(xt/put :users {:xt/id :not-john})])
 
     ;; tag::try-update[]
     (xt/submit-tx *node*
       [(xt/assert-exists '(from :users [{:xt/id :john}]))
-       (xt/update-table :users '{:bind {:xt/id :john :age age}
+       (xt/update-table :users '{:bind [{:xt/id :john :age age}]
                                  :set {:age (inc age)}})])
     ;; end::try-update[]
 
     (t/is (= ;; tag::query-result[]
-             [{:xt/id 0,
-               :xt/committed? true,
-               :xt/error nil}
-              {:xt/id 1,
-               :xt/committed? false,
-               :xt/error {:xtdb.error/error-type :runtime-error,
-                          :xtdb.error/error-key :xtdb/assert-failed,
-                          :xtdb.error/message "Precondition failed: assert-exists",
-                          :row-count 0}}]
+             {:xt/id 1,
+              :xt/committed? false,
+              :xt/error {:xtdb.error/error-type :runtime-error,
+                         :xtdb.error/error-key :xtdb/assert-failed,
+                         :xtdb.error/message "Precondition failed: assert-exists",
+                         :row-count 0}}
              ;; end::query-result[]
 
              ;; tag::query-txs[]
-             (->> (xt/q *node*
-                    '(-> (from :xt/txs [xt/id xt/committed? xt/error])
-                         (order-by xt/id)))
-                  (map #(update % :xt/error ex-data)))
+             (-> (xt/q *node*
+                   '(-> (from :xt/txs [xt/id xt/committed? xt/error])
+                        (order-by xt/id)))
+                 last
+                 (update :xt/error ex-data))
              ;; end::query-txs[]
              ,))))
