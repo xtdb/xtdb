f58037956cee35c98bcd6153e9648dfc9d99ed17
James Henderson <james@jarohen.dev>
2024-01-02 16:33:35 +0000

    migrate IKeyFn -> Kotlin

---
+++ api/src/main/kotlin/xtdb/IKeyFn.kt
@@ -0,0 +1,48 @@
+package xtdb
+
+import clojure.lang.Keyword
+import xtdb.IKeyFn
+import xtdb.util.NormalForm
+import java.util.*
+import kotlin.String
+import kotlin.collections.HashMap
+import kotlin.invoke
+
+fun interface IKeyFn<V> {
+    fun denormalize(key: String): V
+
+    companion object {
+        private fun clojureFormString(s: String) =
+            s.replace('_', '-')
+                .replace('$', '.')
+
+        @JvmStatic
+        @JvmName("keyword")
+        fun keyword(f: IKeyFn<String>) = IKeyFn { k -> Keyword.intern(f.denormalize(k)) }
+
+        @JvmStatic
+        @JvmName("cached")
+        fun <V : Any> cached(f: IKeyFn<V>): IKeyFn<V> {
+            val cache = HashMap<String, V>()
+            return IKeyFn { k: String -> cache.computeIfAbsent(k) { key -> f.denormalize(key) } }
+        }
+
+        @JvmField
+        val CLOJURE: IKeyFn<String> = IKeyFn { s ->
+            val i = s.lastIndexOf('$')
+            if (i < 0) {
+                clojureFormString(s)
+            } else {
+                String.format("%s/%s", clojureFormString(s.substring(0, i)), clojureFormString(s.substring(i + 1)))
+            }
+        }
+
+        // TODO the inner hyphen to underscore is not strictly necessary on the way out
+        @JvmField
+        val SQL: IKeyFn<String> = IKeyFn { s -> NormalForm.normalForm(s) }
+
+        // TODO the inner hyphen to underscore is not strictly necessary on the way out
+        @JvmField
+        val SNAKE_CASE: IKeyFn<String> = IKeyFn { s -> NormalForm.snakeCase(s) }
+    }
+}
