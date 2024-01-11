996e14f621c8bf4bc3c9e071bf4221d1f45285d5
Oliver Marshall <olivershawmarshall@gmail.com>
2024-01-11 14:37:38 +0000

    refactor(lint): use a common set of functions and predicates

--- api/src/main/resources/clj-kondo.exports/com.xtdb/xtdb-api/hooks/xtql.clj
+++ api/src/main/resources/clj-kondo.exports/com.xtdb/xtdb-api/hooks/xtql.clj
@@ -19,27 +19,59 @@
     'where
     'with})
 
-(defmulti lint-unify-clause (fn [node] (-> node api/sexpr first)))
-(defmulti lint-source-op (fn [node] (-> node api/sexpr first)))
-(defmulti lint-tail-op (fn [node] (-> node api/sexpr first)))
+(defn node-map? [node]
+  (contains? #{:map :namespaced-map}
+             (:tag node)))
+
+(defn node-namespaced-map? [node]
+  (= :namespaced-map (:tag node)))
+
+(defn map-children [node]
+  (->> (if (node-namespaced-map? node)
+          (-> node :children first)
+          node)
+       :children
+       (partition-all 2)))
+
+(defn node-vector? [node]
+  (= :vector (:tag node)))
+
+(defn node-list? [node]
+  (= :list (:tag node)))
+
+(defn node-symbol? [node]
+  (symbol? (:value node)))
+
+(defn node-symbol [node]
+  (:value node))
+
+(defn node-keyword? [node]
+  (keyword? (:k node)))
+
+(defn node-keyword [node]
+  (:k node))
+
+(defn node-op [node]
+  (-> node :children first))
+
+(defmulti lint-unify-clause #(-> % node-op node-symbol))
+(defmulti lint-source-op #(-> % node-op node-symbol))
+(defmulti lint-tail-op #(-> % node-op node-symbol))
 
 ;; TODO: Lint more unify clauses
 (defmethod lint-unify-clause :default [node]
-  (let [sexpr (api/sexpr node)
-        op (first sexpr)]
-    (when-not (unify-clause? op)
-      (api/reg-finding!
-        (assoc (some-> node :children first meta)
-               :message "unrecognized unify clause"
-               :type :xtql/unrecognized-operation)))))
+  (when-not (unify-clause? (-> node node-op node-symbol))
+    (api/reg-finding!
+      (assoc (some-> node :children first meta)
+             :message "unrecognized unify clause"
+             :type :xtql/unrecognized-operation))))
 
 (defmethod lint-unify-clause 'from [node]
   (lint-source-op node))
 
 ;; TODO: Lint more source ops
 (defmethod lint-source-op :default [node]
-  (let [sexpr (api/sexpr node)
-        op (first sexpr)]
+  (let [op (-> node node-op node-symbol)]
     (if (tail-op? op)
       (api/reg-finding!
         (assoc (some-> node :children first meta)
@@ -52,37 +84,32 @@
                  :type :xtql/unrecognized-operation))))))
 
 (defn lint-bind [node]
-  (let [sexpr (api/sexpr node)]
-    (cond
-      (symbol? sexpr)
-      (when (= \$ (first (str sexpr)))
-        (api/reg-finding!
-          ;; TODO: Make own type, should really be a warning
-          (assoc (meta node)
-                 :message "unexpected parameter in binding"
-                 :type :xtql/unrecognized-parameter)))
-
-      (map? sexpr)
-      (doseq [[k _v] (->> (if (= (:tag node) :namespaced-map)
-                            (-> node :children first)
-                            node)
-                          :children
-                          (partition-all 2))]
-        (when-not (keyword? (api/sexpr k))
-          (api/reg-finding!
-            (assoc (meta k)
-                   :message "all keys in binding maps must be keywords"
-                   :type :xtql/type-mismatch))))
-
-      :else
+  (cond
+    (node-symbol? node)
+    (when (= \$ (-> node node-symbol str first))
       (api/reg-finding!
+        ;; TODO: Make own type, should really be a warning
         (assoc (meta node)
-               :message "expected a symbol or map"
-               :type :xtql/type-mismatch)))))
+               :message "unexpected parameter in binding"
+               :type :xtql/unrecognized-parameter)))
+
+    (node-map? node)
+    (doseq [[k _v] (map-children node)]
+      (when-not (node-keyword? k)
+        (api/reg-finding!
+          (assoc (meta k)
+                 :message "all keys in binding maps must be keywords"
+                 :type :xtql/type-mismatch))))
+
+    :else
+    (api/reg-finding!
+      (assoc (meta node)
+             :message "expected a symbol or map"
+             :type :xtql/type-mismatch))))
 
 (defmethod lint-source-op 'from [node]
   (let [[_ table opts] (some-> node :children)]
-    (when-not (keyword? (api/sexpr table))
+    (when-not (node-keyword? table)
       (api/reg-finding!
         (assoc (meta table)
                :message "expected 'table' to be a keyword"
@@ -90,25 +117,25 @@
     (case (:tag opts)
       :vector (->> opts :children (run! lint-bind))
       :map 
-      (let [kvs (some->> opts :children (partition-all 2))
-            ks (map first kvs)]
-        (when-not (contains? (->> ks
-                                  (map api/sexpr)
-                                  (filter keyword?)
-                                  (into #{}))
-                             :bind)
+      (let [kvs (map-children opts)
+            ks (->> kvs
+                    (map first)
+                    (map api/sexpr)
+                    (filter keyword?)
+                    (into #{}))]
+        (when-not (contains? ks :bind)
           (api/reg-finding!
             (assoc (meta opts)
                    :message "Missing :bind parameter"
                    :type :xtql/missing-parameter)))
-        (doseq [[k v] (some->> opts :children (partition-all 2))]
-          (when-not (keyword? (api/sexpr k))
+        (doseq [[k v] kvs]
+          (when-not (node-keyword? k)
             (api/reg-finding!
               (assoc (meta k)
                      :message "All keys in 'opts' must be keywords"
                      :type :xtql/type-mismatch)))
-          (case (api/sexpr k)
-            :bind (if (= (:tag v) :vector)
+          (case (node-keyword k)
+            :bind (if (node-vector? v)
                     (->> v :children (run! lint-bind))
                     (api/reg-finding!
                       (assoc (meta opts)
@@ -118,6 +145,7 @@
             :for-valid-time nil
             ;; TODO
             :for-system-time nil
+            ; else
             (api/reg-finding!
               (assoc (meta k)
                      :message "unrecognized parameter"
@@ -129,14 +157,14 @@
 
 (defmethod lint-source-op 'unify [node]
   (let [clauses (some-> node :children rest)]
-    (doseq [bad-op (remove #(= (:tag %) :list)  clauses)]
+    (doseq [bad-op (remove node-list? clauses)]
       (api/reg-finding!
         (assoc (meta bad-op)
                :message "all operations in a unify must be lists"
                :type :xtql/type-mismatch)))
     (when (= (count clauses) 1)
       (let [clause (first clauses)
-            clause-op (-> clause api/sexpr first)
+            clause-op (-> clause node-op node-symbol)
             unify-node (some-> node :children first)]
         (case clause-op
           from (api/reg-finding!
@@ -144,7 +172,6 @@
                         :message "redundant unify"
                         :type :xtql/redundant-unify))
           rel (do
-                (println clause-op)
                 (api/reg-finding!
                   (assoc (meta unify-node)
                          :message "redundant unify"
@@ -155,13 +182,13 @@
       ;; What cases?
       ;; Single source op, in which case warn
       ;; Single unify clause that isn't source op
-    (run! lint-unify-clause
-          (filter #(= (:tag %) :list) clauses))))
+    (->> clauses
+         (filter node-list?)
+         (run! lint-unify-clause))))
 
 ;; TODO: Lint more tail ops
 (defmethod lint-tail-op :default [node]
-  (let [sexpr (api/sexpr node)
-        op (first sexpr)]
+  (let [op (-> node node-op node-symbol)]
     (if (source-op? op)
       (api/reg-finding!
         (assoc (some-> node :children first meta)
@@ -175,7 +202,7 @@
 
 (defn lint-pipeline [node]
   (let [ops (some-> node :children rest)]
-    (doseq [bad-op (remove #(= (:tag %) :list) ops)]
+    (doseq [bad-op (remove node-list? ops)]
       (api/reg-finding!
         (assoc (meta bad-op)
                :message "all operations in a pipeline must be lists"
@@ -186,11 +213,11 @@
                :message "redundant pipeline"
                :type :xtql/redundant-pipeline)))
     (let [first-op (first ops)]
-      (when (= (:tag first-op) :list)
+      (when (node-list? first-op)
         (lint-source-op (first ops))))
     (run! lint-tail-op (->> ops
                             (drop 1)
-                            (filter #(= (:tag %) :list))))))
+                            (filter node-list?)))))
 
 (defn lint-query [node]
   (let [quoted-query (api/sexpr node)]
