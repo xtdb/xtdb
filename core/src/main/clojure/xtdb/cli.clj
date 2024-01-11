4d4a829e2a2f1f23b3d3614ef1d62d2a6268d3c9
Daniel Mason <dan@juxt.pro>
2024-01-11 10:32:33 +0000

    Separate out config loading code from CLI namespace, update usages.

--- core/src/main/clojure/xtdb/cli.clj
+++ core/src/main/clojure/xtdb/cli.clj
@@ -1,82 +1,31 @@
 (ns ^:no-doc xtdb.cli
-  (:require [clojure.data.json :as json]
-            [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [clojure.string :as str]
+  (:require [clojure.java.io :as io]
             [clojure.tools.cli :as cli]
-            [clojure.tools.logging :as log]
-            [clojure.walk :as walk]
-            [xtdb.error :as err]
+            [clojure.tools.logging :as log] 
+            [xtdb.config :as config] 
             [xtdb.node :as xtn]
-            [xtdb.util :as util]
-            [juxt.clojars-mirrors.integrant.core :as ig])
-  (:import java.io.File
-           java.net.URL
-           java.util.Map))
+            [xtdb.util :as util])
+  (:import java.io.File))
 
 (defn- if-it-exists [^File f]
   (when (.exists f)
     f))
 
-(defn- file-extension [^File f]
-  (second (re-find #"\.(.+?)$" (.getName f))))
-
-(defn read-env-var [env-var]
-  (System/getenv (str env-var)))
-
-(defn edn-read-string [edn-string]
-  (ig/read-string {:readers {'env read-env-var}}
-                  edn-string))
-
-(defn json-read-string [json-string]
-  (walk/postwalk
-   (fn [item]
-     (let [env-key (keyword "@env")]
-       (cond
-         (env-key item) (read-env-var (env-key item))
-         :else item)))
-   (json/read-str json-string :key-fn keyword)))
-
 (def cli-options
   [["-f" "--file CONFIG_FILE" "Config file to load XTDB options from - EDN, JSON"
     :parse-fn io/file
     :validate [if-it-exists "Config file doesn't exist"
-               #(contains? #{"edn" "json"} (file-extension %)) "Config file must be .edn or .json"]]
+               #(contains? #{"edn" "json"} (util/file-extension %)) "Config file must be .edn or .json"]]
 
    ["-e" "--edn EDN" "Options as EDN."
     :default nil
-    :parse-fn edn-read-string]
+    :parse-fn config/edn-read-string]
 
    ["-j" "--json JSON" "Options as JSON."
     :default nil
-    :parse-fn json-read-string]
+    :parse-fn config/json-read-string]
 
    ["-h" "--help"]])
-
-(defprotocol OptsSource
-  (load-opts [src]))
-
-(alter-meta! #'load-opts assoc :private true)
-
-(defn- read-opts [src file-name]
-  (cond
-    (str/ends-with? file-name ".json") (json-read-string (slurp src))
-    (str/ends-with? file-name ".edn") (edn-read-string (slurp src))
-    :else (throw (err/illegal-arg :unsupported-options-type
-                                  {::err/message (format "Unsupported options type: '%s'" file-name)}))))
-
-(extend-protocol OptsSource
-  Map
-  (load-opts [src] src)
-
-  File
-  (load-opts [src] (read-opts src (.getName src)))
-
-  URL
-  (load-opts [src] (read-opts src (.getFile src)))
-
-  nil
-  (load-opts [_] nil))
 
 (defn parse-args [args]
   (let [{:keys [options errors summary]} (cli/parse-opts args cli-options)]
@@ -86,16 +35,15 @@
       (:help options) {::help summary}
 
       :else (let [{:keys [file edn json]} options]
-              {::node-opts (->> [(or file
-                                     (some-> (io/file "xtdb.edn") if-it-exists)
-                                     (some-> (io/file "xtdb.json") if-it-exists)
-                                     (io/resource "xtdb.edn")
-                                     (io/resource "xtdb.json"))
-
-                                 json
-                                 edn]
-                                (map load-opts)
-                                (apply merge-with merge))}))))
+              {::node-opts (let [config-file (or file
+                                                 (some-> (io/file "xtdb.edn") if-it-exists)
+                                                 (some-> (io/file "xtdb.json") if-it-exists)
+                                                 (io/resource "xtdb.edn")
+                                                 (io/resource "xtdb.json"))
+                                 config-from-file (some-> config-file config/file->config-opts)]
+                             ;; TODO: Do we really want to merge config like this? Ie, if file provided/xtdb.edn exists, we merge in config map from --edn?
+                             ;; Seems like somewhat confusing behaviour. Kept from previous code here but worth a think.
+                             (apply merge-with merge [config-from-file json edn]))}))))
 
 (defn- shutdown-hook-promise []
   (let [main-thread (Thread/currentThread)
---
+++ core/src/main/clojure/xtdb/config.clj
@@ -0,0 +1,39 @@
+(ns xtdb.config
+  (:require [clojure.data.json :as json] 
+            [clojure.walk :as walk]
+            [juxt.clojars-mirrors.integrant.core :as ig] 
+            [xtdb.error :as err]
+            [xtdb.util :as util])
+  (:import java.io.File))
+
+(defn read-env-var [env-var]
+  (System/getenv (str env-var)))
+
+(defn edn-read-string [edn-string]
+  (ig/read-string {:readers {'env read-env-var}}
+                  edn-string))
+
+(defn json-read-string [json-string]
+  (walk/postwalk
+   (fn [item]
+     (let [env-key (keyword "@env")]
+       (cond
+         (env-key item) (read-env-var (env-key item))
+         :else item)))
+   (json/read-str json-string :key-fn keyword)))
+
+
+(defn- read-opts-from-file [^File f]
+  (let [file-extension (util/file-extension f)]
+    (cond
+      (= file-extension "json") (json-read-string (slurp f))
+      (= file-extension "edn") (edn-read-string (slurp f))
+      :else (throw (err/illegal-arg :unsupported-options-type
+                                    {::err/message (format "Unsupported type for options file: '%s'" file-extension)})))))
+
+(defn file->config-opts
+  [^File f]
+  (if (.exists f)
+    (read-opts-from-file f)
+    (throw (err/illegal-arg :opts-file-not-found
+                            {::err/message (format "File not found: '%s'" (.getName f))}))))
