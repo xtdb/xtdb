f8d26d9f457b485b76e2292533327fbba196e784
Daniel Mason <dan@juxt.pro>
2024-01-17 10:47:33 +0000

    Add and use KafkaLogFactory for configuring kafka log.

--- modules/kafka/build.gradle.kts
+++ modules/kafka/build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `java-library`
     id("dev.clojurephant.clojure")
     `maven-publish`
     signing
+    kotlin("jvm")
 }
 
 publishing {
@@ -21,4 +24,6 @@ dependencies {
     api(project(":core"))
 
     api("org.apache.kafka", "kafka-clients", "3.1.0")
+
+    api(kotlin("stdlib-jdk8"))
 }
