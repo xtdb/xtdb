7dea6fdc96117dc08e8969c3c5388a5602421801
Daniel Mason <dan@juxt.pro>
2024-01-17 15:47:59 +0000

    Add & use EventHubLogFactory - note, event hub log currently failing.

--- src/test/clojure/xtdb/azure_test.clj
+++ src/test/clojure/xtdb/azure_test.clj
@@ -2,8 +2,7 @@
   (:require [clojure.java.shell :as sh]
             [clojure.set :as set]
             [clojure.test :as t]
-            [clojure.tools.logging :as log]
-            [juxt.clojars-mirrors.integrant.core :as ig] 
+            [clojure.tools.logging :as log] 
             [xtdb.api :as xt]
             [xtdb.azure :as azure]
             [xtdb.datasets.tpch :as tpch]
@@ -17,14 +16,16 @@
            [java.nio ByteBuffer]
            [java.nio.file Path]
            [java.time Duration]
-           [xtdb.api.storage ObjectStore]
+           [java.util UUID]
            [xtdb.api AzureObjectStoreFactory]
+           [xtdb.api.log Log]
+           [xtdb.api.storage ObjectStore]
            [xtdb.multipart IMultipartUpload SupportsMultipart]))
 
 (def resource-group-name "azure-modules-test")
 (def storage-account "xtdbteststorageaccount")
 (def container "xtdb-test-object-store")
-(def eventhub-namespace "xtdb-test-storage-account-eventbus")
+(def eventhub-namespace "xtdb-test-eventhub")
 (def servicebus-namespace "xtdb-test-storage-account-eventbus")
 (def servicebus-topic-name "xtdb-test-storage-bus-topic")
 (def config-present? (some? (and (System/getenv "AZURE_CLIENT_ID")
@@ -63,15 +64,15 @@
                     (.prefix (util/->path (str prefix))))]
     (azure/open-object-store factory)))
 
-(t/deftest ^:azure put-delete-test 
+(t/deftest ^:azure put-delete-test
   (with-open [os (object-store (random-uuid))]
     (os-test/test-put-delete os)))
 
-(t/deftest ^:azure range-test 
+(t/deftest ^:azure range-test
   (with-open [os (object-store (random-uuid))]
     (os-test/test-range os)))
 
-(t/deftest ^:azure list-test 
+(t/deftest ^:azure list-test
   (with-open [os (object-store (random-uuid))]
     (os-test/test-list-objects os)))
 
@@ -108,16 +109,35 @@
       (t/is (= (mapv util/->path ["alan" "alice"])
                (.listObjects ^ObjectStore os-2))))))
 
-;; Currently not testing this - will need to setup the event hub namespace and config to run
+;; Currently not testing this regularly:
+;; Need to setup the event hub namespace `xtdb-test-eventhub` and configure to run.
+;; Ensure your credentials have eventhub permissions for the eventhub namespace 
+;; As this costs a set price per month, we test it occasionally when changes are made to it
+;; We do not currently have a set of log tests, so we just submit + query a document
+;; TODO: Investigate this, currently failing to submit
 ;; (t/deftest ^:azure test-eventhub-log
-;;   (with-open [node (xtn/start-node {::azure/event-hub-log {:namespace eventhub-namespace
-;;                                                             :resource-group-name resource-group-name
-;;                                                             :event-hub-name (str "xtdb.azure-test-hub." (UUID/randomUUID))
-;;                                                             :create-event-hub? true
-;;                                                             :retention-period-in-days 1}})]
-;;     (xt/submit-tx node [(xt/put :xt_docs {:xt/id :foo}])])
-;;     (t/is (= [{:id :foo}]
-;;              (xt/q node '(from :xt_docs [{:xt/id id}])))))
+;;   (let [event-hub-name (str "xtdb.azure-test-hub." (UUID/randomUUID))]
+;;     (t/testing "creating a new eventhub with create-event-hub, sending a message"
+;;       (with-open [node (xtn/start-node {:log [:azure-event-hub {:namespace eventhub-namespace
+;;                                                                 :resource-group-name resource-group-name
+;;                                                                 :event-hub-name event-hub-name
+;;                                                                 :create-event-hub? true
+;;                                                                 :max-wait-time "PT1S"
+;;                                                                 :poll-sleep-duration "PT1S"
+;;                                                                 :retention-period-in-days 1}]})]
+;;         (t/testing "EventHubLog successfully created"
+;;           (let [log (get-in node [:system :xtdb/log])]
+;;             (t/is (instance? Log log))))
+        
+;;         (xt/submit-tx node [(xt/put :xt_docs {:xt/id :foo})])
+;;         (t/is (= [{:id :foo}] (xt/q node '(from :xt_docs [{:xt/id id}]))))))
+    
+;;     (t/testing "connecting to previously created event-hub"
+;;       (with-open [node (xtn/start-node {:log [:azure-event-hub {:namespace eventhub-namespace
+;;                                                                 :event-hub-name event-hub-name
+;;                                                                 :max-wait-time "PT1S"
+;;                                                                 :poll-sleep-duration "PT1S"}]})]
+;;         (t/is (= [{:id :foo}] (xt/q node '(from :xt_docs [{:xt/id id}]))))))))
 
 (defn list-filenames [^BlobContainerClient blob-container-client ^Path prefix ^ListBlobsOptions list-opts]
   (->> (.listBlobs blob-container-client list-opts nil)
@@ -138,7 +158,7 @@
                                                    (.setRetrieveUncommittedBlobs true))))]
     (set/difference all-blobs comitted-blobs)))
 
-(t/deftest ^:azure multipart-start-and-cancel 
+(t/deftest ^:azure multipart-start-and-cancel
   (with-open [os (object-store (random-uuid))]
     (let [blob-container-client (:blob-container-client os)
           prefix (:prefix os)]
@@ -155,7 +175,7 @@
             (let [uncomitted-blobs (fetch-uncomitted-blobs blob-container-client prefix)]
               (= #{} uncomitted-blobs))))))))
 
-(t/deftest ^:azure multipart-put-test 
+(t/deftest ^:azure multipart-put-test
   (with-open [os (object-store (random-uuid))]
     (let [blob-container-client (:blob-container-client os)
           prefix (:prefix os)
@@ -180,7 +200,6 @@
         (let [^ByteBuffer uploaded-buffer @(.getObject ^ObjectStore os (util/->path "test-multi-put"))]
           (t/testing "capacity should be equal to total of 2 parts"
             (t/is (= (* 2 part-size) (.capacity uploaded-buffer)))))))))
-
 
 (t/deftest ^:azure node-level-test
   (util/with-tmp-dirs #{local-disk-cache}
