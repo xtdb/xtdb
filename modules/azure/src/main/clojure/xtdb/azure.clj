8b257a8695a8c0188516a498f153bb10a07f0f4e
Daniel Mason <dan@juxt.pro>
2024-01-17 15:47:59 +0000

    Add & use EventHubLogFactory - note, event hub log currently failing.

--- modules/azure/src/main/clojure/xtdb/azure.clj
+++ modules/azure/src/main/clojure/xtdb/azure.clj
@@ -1,12 +1,11 @@
 (ns xtdb.azure
-  (:require [clojure.spec.alpha :as s]
-            [clojure.tools.logging :as log]
-            [juxt.clojars-mirrors.integrant.core :as ig]
+  (:require [clojure.tools.logging :as log] 
             [xtdb.azure.file-watch :as azure-file-watch]
             [xtdb.azure.log :as tx-log]
             [xtdb.azure.object-store :as os]
             [xtdb.buffer-pool :as bp]
             [xtdb.log :as xtdb-log]
+            [xtdb.node :as xtn]
             [xtdb.time :as time]
             [xtdb.util :as util])
   (:import [com.azure.core.credential TokenCredential]
@@ -18,8 +17,9 @@
            [com.azure.resourcemanager.eventhubs.models EventHub EventHub$Definition EventHubs]
            [com.azure.storage.blob BlobServiceClientBuilder] 
            [java.util.concurrent ConcurrentSkipListSet]
-           [xtdb.api.storage ObjectStore]
-           [xtdb.api AzureObjectStoreFactory]))
+           [xtdb.api AzureObjectStoreFactory XtdbSubmitClient$Config] 
+           [xtdb.api.log AzureEventHubLogFactory]
+           [xtdb.api.storage ObjectStore]))
 
 (defmethod bp/->object-store-factory ::object-store [_ {:keys [storage-account container servicebus-namespace servicebus-topic-name prefix]}]
   (cond-> (AzureObjectStoreFactory. storage-account container servicebus-namespace servicebus-topic-name) 
@@ -56,32 +56,22 @@
                                file-name-cache
                                file-list-watcher)))
 
-(s/def ::resource-group-name string?)
-(s/def ::namespace string?)
-(s/def ::event-hub-name string?)
-(s/def ::create-event-hub? boolean?)
-(s/def ::retention-period-in-days number?)
-(s/def ::max-wait-time ::time/duration)
-(s/def ::poll-sleep-duration ::time/duration)
+(defmethod xtn/apply-config! ::event-hub-log
+  [^XtdbSubmitClient$Config config _ {:keys [namespace event-hub-name max-wait-time poll-sleep-duration
+                                             create-event-hub? retention-period-in-days resource-group-name]}]
+  (doto config
+    (.setTxLog (cond-> (AzureEventHubLogFactory. namespace event-hub-name)
+                 max-wait-time (.maxWaitTime (time/->duration max-wait-time))
+                 poll-sleep-duration (.pollSleepDuration (time/->duration poll-sleep-duration))
+                 create-event-hub? (.autoCreateEventHub create-event-hub?)
+                 retention-period-in-days (.retentionPeriodInDays retention-period-in-days)
+                 resource-group-name (.resourceGroupName resource-group-name)))))
 
-(derive ::event-hub-log :xtdb/log)
-
-(defmethod ig/prep-key ::event-hub-log [_ opts]
-  (-> (merge {:create-event-hub? false
-              :max-wait-time "PT1S"
-              :poll-sleep-duration "PT1S"
-              :retention-period-in-days 7} opts)
-      (util/maybe-update :max-wait-time time/->duration)))
-
-(defmethod ig/pre-init-spec ::event-hub-log [_]
-  (s/keys :req-un [::namespace ::event-hub-name ::max-wait-time ::create-event-hub? ::retention-period-in-days ::poll-sleep-duration]
-          :opt-un [::resource-group-name]))
-
-(defn resource-group-present? [{:keys [resource-group-name]}]
+(defn resource-group-present? [resource-group-name]
   (when-not resource-group-name
     (throw (IllegalArgumentException. "Must provide :resource-group-name when creating an eventhub automatically."))))
 
-(defn create-event-hub-if-not-exists [^TokenCredential azure-credential {:keys [resource-group-name namespace event-hub-name retention-period-in-days]}]
+(defn create-event-hub-if-not-exists [^TokenCredential azure-credential resource-group-name namespace event-hub-name retention-period-in-days]
   (let [event-hub-manager (EventHubsManager/authenticate azure-credential (AzureProfile. (AzureEnvironment/AZURE)))
         ^EventHubs event-hubs (.eventHubs event-hub-manager)
         event-hub-exists? (some
@@ -99,9 +89,17 @@
         (log/error "Error when creating event hub - " (.getMessage e))
         (throw e)))))
 
-(defmethod ig/init-key ::event-hub-log [_ {:keys [create-event-hub? namespace event-hub-name max-wait-time poll-sleep-duration] :as opts}]
-  (let [credential (.build (DefaultAzureCredentialBuilder.))
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn open-log [^AzureEventHubLogFactory factory]
+  (let [namespace (.getNamespace factory)
+        event-hub-name (.getEventHubName factory)
+        max-wait-time (.getMaxWaitTime factory)
+        poll-sleep-duration (.getPollSleepDuration factory)
+        create-event-hub? (.getAutoCreateEventHub factory)
+        retention-period-in-days (.getRetentionPeriodInDays factory)
+        resource-group-name (.getResourceGroupName factory) 
         fully-qualified-namespace (format "%s.servicebus.windows.net" namespace)
+        credential (.build (DefaultAzureCredentialBuilder.))
         event-hub-client-builder (-> (EventHubClientBuilder.)
                                      (.consumerGroup "$DEFAULT")
                                      (.credential credential)
@@ -110,14 +108,11 @@
         subscriber-handler (xtdb-log/->notifying-subscriber-handler nil)]
 
     (when create-event-hub?
-      (resource-group-present? opts)
-      (create-event-hub-if-not-exists credential opts))
+      (resource-group-present? resource-group-name)
+      (create-event-hub-if-not-exists credential resource-group-name namespace event-hub-name retention-period-in-days))
 
     (tx-log/->EventHubLog subscriber-handler
                           (.buildAsyncProducerClient event-hub-client-builder)
                           (.buildConsumerClient event-hub-client-builder)
                           max-wait-time
                           poll-sleep-duration)))
-
-(defmethod ig/halt-key! ::event-hub-log [_ log]
-  (util/try-close log))
