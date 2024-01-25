62f26f89e5678e57a8c6f01118df66153da6bd45
Daniel Mason <dan@juxt.pro>
2024-01-25 11:27:09 +0000

    Update kafka reference + add examples to KDocs.

--- modules/kafka/src/main/kotlin/xtdb/api/log/KafkaLogFactory.kt
+++ modules/kafka/src/main/kotlin/xtdb/api/log/KafkaLogFactory.kt
@@ -14,6 +14,35 @@ import java.time.Duration
 import java.time.InstantSource
 import java.util.concurrent.CompletableFuture
 
+/**
+ * Used to set configuration options for Kafka as an XTDB Transaction Log.
+ *
+ * For more info on setting up the necessary infrastructure to be able to use Kafka as an XTDB Transaction Log, see the
+ * section on infrastructure within our [Kafka Module Reference](https://docs.xtdb.com/components/tx-log/kafka.html).
+ *
+ * Example usage, as part of a node config:
+ * ```kotlin
+ * Xtdb.Config(
+ *    log = KafkaLogFactory(
+ *              bootstrapServers = "localhost:9092",
+ *              topicName = "xtdb_topic",
+ *              autoCreateTopic = true,
+ *              replicationFactor = 1,
+ *              pollDuration = Duration.ofSeconds(1)
+ *          ),
+ *    ...
+ * )
+ * ```
+ *
+ * @property bootstrapServers A comma-separated list of host:port pairs to use for establishing the initial connection to the Kafka cluster.
+ * @property topicName Name of the Kafka topic to use for the transaction log.
+ * @property autoCreateTopic Whether to automatically create the topic, if it does not already exist.
+ * @property replicationFactor The [replication factor](https://kafka.apache.org/documentation/#replication.factor) of the transaction log topic (if it is automatically created by XTDB).
+ * @property pollDuration The maximum amount of time to block waiting for records to be returned by the Kafka consumer.
+ * @property topicConfig A map of [topic configuration options](https://kafka.apache.org/documentation/#topicconfigs) to use when creating the transaction log topic (if it is automatically created by XTDB).
+ * @property propertiesMap A map of Kafka connection properties, supplied directly to the Kafka client.
+ * @property propertiesFile Path to a Java properties file containing Kafka connection properties, supplied directly to the Kafka client.
+ */
 @Serializable
 @SerialName("!Kafka")
 data class KafkaLogFactory @JvmOverloads constructor(
