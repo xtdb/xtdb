b17809af25f78c7538fd058e34dd14c701daab4a
Finn Völkel <finn.volkel@gmail.com>
2024-02-05 13:36:09 +0100

    Bump Cloudformation stuff

--- modules/bench/bin/run-cloudformation.sh
+++ modules/bench/bin/run-cloudformation.sh
@@ -2,13 +2,38 @@
 
 set -xe
 
+REGION="eu-west-1"
+STACK_NAME="xtdb2-bench"
+
+echo "Checking if stack exists ..."
 (
     cd $(dirname $0)/../
 
-    aws cloudformation update-stack \
-        --region eu-west-1 \
-        --capabilities CAPABILITY_IAM \
-        --template-body file://$(pwd)/cloudformation/bench.yml \
-        --stack-name core2-bench \
-        --tags Key=juxt:team,Value=xtdb-core
+    if ! aws cloudformation describe-stacks --region $REGION --stack-name $STACK_NAME ; then
+
+        echo -e "\nStack does not exist, creating ..."
+        aws cloudformation create-stack \
+            --region $REGION \
+            --capabilities CAPABILITY_IAM \
+            --template-body file://$(pwd)/cloudformation/bench.yml \
+            --stack-name $STACK_NAME \
+            --tags Key=juxt:team,Value=xtdb-core
+
+
+        echo "Waiting for stack to be created ..."
+        aws cloudformation wait stack-create-complete \
+            --region $REGION \
+            --stack-name $STACK_NAME
+
+    else
+
+        echo "\nUpdating existing stack ..."
+        aws cloudformation update-stack \
+            --region $REGION \
+            --capabilities CAPABILITY_IAM \
+            --template-body file://$(pwd)/cloudformation/bench.yml \
+            --stack-name $STACK_NAME \
+            --tags Key=juxt:team,Value=xtdb-core
+
+    fi
 )
