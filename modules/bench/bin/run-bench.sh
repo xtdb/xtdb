cef57fb9c7bcf9e879c33f3c56cce2499662a99b
Finn Völkel <finn.volkel@gmail.com>
2024-02-05 17:08:33 +0100

    bin/run-bench.sh updates

--- modules/bench/bin/run-bench.sh
+++ modules/bench/bin/run-bench.sh
@@ -3,20 +3,14 @@
 set -e
 
 TASKDEF_ARN=$(aws cloudformation describe-stack-resource \
-                  --stack-name core2-bench \
+                  --stack-name xtdb2-bench \
                   --logical-resource-id BenchTask \
                   --query 'StackResourceDetail.PhysicalResourceId' \
                   --output text)
 
-VPC_STACK_ID=$(aws cloudformation describe-stack-resource \
-                   --stack-name xtdb-cloud \
-                   --logical-resource-id VPCStack \
-                   --query 'StackResourceDetail.PhysicalResourceId' \
-                   --output text)
-
 SUBNET_ID=$(aws cloudformation describe-stack-resource \
-                --stack-name $VPC_STACK_ID \
-                --logical-resource-id PublicSubnetOne \
+                --stack-name xtdb2-bench \
+                --logical-resource-id PublicSubnet1 \
                 --query 'StackResourceDetail.PhysicalResourceId' \
                 --output text)
 
@@ -28,7 +22,7 @@ COMMAND='['
 if [[ "$#" -eq 0 ]]; then echo "No benchmark passed."; exit 1; fi
 
 case $1 in
-    tpch|ts-devices|multinode-tpch|watdiv)
+    tpch|ts-devices)
         COMMAND+='"'"xtdb.bench.$1"'"'
         shift 1;;
     *) echo "Unknown benchmark passed: $1"; exit 1;;
@@ -37,7 +31,7 @@ esac
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --scale-factor|--size|--sleep-ms)
+        --scale-factor|--size)
             COMMAND+=", \"$1\", \"$2\""
             shift 2;;
         --count)
@@ -53,10 +47,10 @@ set -x
 
 aws ecs run-task \
     --task-definition "$TASKDEF_ARN" \
-    --cluster core2-bench \
+    --cluster xtdb2-bench \
     --launch-type FARGATE \
     --count "$COUNT" \
     --network-configuration "awsvpcConfiguration={subnets=[$SUBNET_ID],assignPublicIp=\"ENABLED\"}" \
-    --overrides '{"containerOverrides": [{"name": "core2-bench", "command": '"$COMMAND"'}]}' \
+    --overrides '{"containerOverrides": [{"name": "bench", "command": '"$COMMAND"'}]}' \
     --output json \
     | jq .failures
