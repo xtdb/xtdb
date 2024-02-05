02a072e352d107838b44fbc76789f281e0ad9e1f
Finn Völkel <finn.volkel@gmail.com>
2024-02-05 12:19:39 +0100

    Login + push-docker update

--- modules/bench/bin/ecr-login.sh
+++ modules/bench/bin/ecr-login.sh
@@ -4,7 +4,6 @@ set -xe
 
 ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 ECR_URL="$ACCOUNT.dkr.ecr.eu-west-1.amazonaws.com"
-TAG="$ECR_URL/core2-bench:latest"
 
 aws ecr get-login-password \
     --region eu-west-1 \
--- modules/bench/bin/push-docker.sh
+++ modules/bench/bin/push-docker.sh
@@ -4,8 +4,8 @@ set -xe
 
 ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 ECR_URL="$ACCOUNT.dkr.ecr.eu-west-1.amazonaws.com"
-TAG="$ECR_URL/core2-bench:latest"
+TAG="$ECR_URL/xtdb2-bench:latest"
 
-docker tag ghcr.io/xtdb/core2-bench:latest $TAG
+docker tag juxt.xtdb2/bench:latest $TAG
 
 docker push $TAG
