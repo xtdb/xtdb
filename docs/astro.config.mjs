0f751ea4a9adf9cec1bb1dcc7d0bfb691777ad3a
Jeremy Taylor <jdt@juxt.pro>
2024-03-04 22:51:34 +0000

    docs - misc checkpoint

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -45,9 +45,15 @@ export default defineConfig({
 
         {
           label: 'Tutorials',
-          collapsed: true,
+//          collapsed: true,
           items: [
-            { label: 'Learn XTQL Today, with Clojure', link: '/tutorials/learn-xtql-today-with-clojure' },
+  	    { label: 'SQL over HTTP', link: '/tutorials/sql-over-http' },
+	    { label: 'XTQL',
+	      collapsed: true,
+	      items: [
+                { label: 'Learn XTQL Today, with Clojure', link: '/tutorials/learn-xtql-today-with-clojure' },
+		],
+            },
           ],
         },
 
@@ -55,45 +61,20 @@ export default defineConfig({
           label: 'Guides',
           collapsed: true,
           items: [
- 	    { label: 'SQL over HTTP', link: '/guides/sql-over-http' },
-
-            { label: 'Setting up a cluster on AWS', link: '/guides/starting-with-aws' },
-            { label: 'XTQL Walkthrough', link: '/guides/xtql-walkthrough' },
+	    { label: 'Deployment',
+	      collapsed: true,
+	      items: [
+	        { label: 'Setting up a cluster on AWS', link: '/guides/starting-with-aws' },
+		],
+            },
+	    { label: 'XTQL',
+	      collapsed: true,	    
+	      items: [
+            
+                { label: 'XTQL Walkthrough', link: '/guides/xtql-walkthrough' },
+	      ]
+	     },
           ],
-        },
-
-        {
-          label: 'Configuration',
-          collapsed: true,
-          items: [
-            { label: 'Overview', link: '/config' },
-
-            {
-              label: 'Transaction Log',
-              items: [
-                { label: 'Overview', link: '/config/tx-log' },
-                { label: 'Kafka', link: '/config/tx-log/kafka' },
-              ],
-            },
-
-            {
-              label: 'Storage',
-              items: [
-                { label: 'Overview', link: '/config/storage' },
-                { label: 'AWS S3', link: '/config/storage/s3' },
-                { label: 'Azure Blob Storage', link: '/config/storage/azure' },
-                { label: 'Google Cloud Storage', link: '/config/storage/google-cloud' }
-              ]
-            },
-
-            {
-              label: 'Optional Modules',
-              items: [
-                { label: 'Overview', link: '/config/modules' },
-                { label: 'HTTP Server', link: '/config/modules/http-server' }
-              ]
-            },
-          ]
         },
 
         {
@@ -101,24 +82,10 @@ export default defineConfig({
           collapsed: true,
           items: [
             { label: 'Overview', link: '/reference/main' },
-            {
-              label: 'XTQL',
-              collapsed: true,
-              items: [
-                { label: 'What is XTQL?', link: '/intro/what-is-xtql' },     
-                { label: 'Transactions', link: '/reference/main/xtql/txs' },
-                { label: 'Queries', link: '/reference/main/xtql/queries' },
-              ]
-            },
 
-            {
-              label: 'SQL',
-              collapsed: true,
-              items: [
-                { label: 'Transactions', link: '/reference/main/sql/txs' },
-                { label: 'Queries', link: '/reference/main/sql/queries' },
-              ]
-            },
+            { label: 'SQL Transactions', link: '/reference/main/sql/txs' },
+	    
+            { label: 'SQL Queries', link: '/reference/main/sql/queries' },
 
             { label: 'Data Types', link: '/reference/main/data-types' },
 
@@ -135,6 +102,51 @@ export default defineConfig({
                 { label: 'Other functions', link: '/reference/main/stdlib/other'}
               ]
             },
+
+	    {
+              label: 'XTQL',
+              collapsed: true,
+              items: [
+                { label: 'What is XTQL?', link: '/intro/what-is-xtql' },     
+                { label: 'Transactions', link: '/reference/main/xtql/txs' },
+                { label: 'Queries', link: '/reference/main/xtql/queries' },
+              ]
+            },
+
+	    {
+	      label: 'Advanced Configuration',
+	      collapsed: true,
+	      items: [
+		{ label: 'Overview', link: '/config' },
+
+		{
+		  label: 'Transaction Log',
+		  items: [
+		    { label: 'Overview', link: '/config/tx-log' },
+		    { label: 'Kafka', link: '/config/tx-log/kafka' },
+		  ],
+		},
+
+		{
+		  label: 'Storage',
+		  items: [
+		    { label: 'Overview', link: '/config/storage' },
+		    { label: 'AWS S3', link: '/config/storage/s3' },
+		    { label: 'Azure Blob Storage', link: '/config/storage/azure' },
+		    { label: 'Google Cloud Storage', link: '/config/storage/google-cloud' }
+		  ]
+		},
+
+		{
+		  label: 'Optional Modules',
+		  items: [
+		    { label: 'Overview', link: '/config/modules' },
+		    { label: 'HTTP Server', link: '/config/modules/http-server' }
+		  ]
+		},
+	      ]
+	    },
+
           ]
         },
 
@@ -143,9 +155,36 @@ export default defineConfig({
           collapsed: true,
           items: [
             { label: 'Overview', link: '/drivers' },
+	    {
+	      label: 'HTTP',
+	      items: [
+	        { label: 'Getting started', link:'/drivers/http/getting-started'},
+                { label: 'HTTP (OpenAPI) ↗', link: '/drivers/http/openapi/index.html', attrs: { target: '_blank' } },
+		]
+            },
+
+            {
+              label: 'Java',
+              collapsed: true,	      
+              items: [
+                { label: 'Getting started', link: '/drivers/java/getting-started' },
+                // TODO broken atm
+                // { label: 'Javadoc ↗', link: '/drivers/java/javadoc/index.html', attrs: {target: '_blank'} }
+              ]
+            },
+
+            {
+              label: 'Kotlin',
+              collapsed: true,	      
+              items: [
+                { label: 'Getting started', link: '/drivers/kotlin/getting-started' },
+                { label: 'KDoc ↗', link: '/drivers/kotlin/kdoc/index.html', attrs: { target: '_blank' } }
+              ]
+            },
 
             {
               label: 'Clojure',
+              collapsed: true,	      
               items: [
                 { label: 'Getting started', link: '/drivers/clojure/getting-started' },
                 { label: 'Configuration cookbook', link: '/drivers/clojure/configuration' },
@@ -153,25 +192,7 @@ export default defineConfig({
                 { label: 'Codox ↗', link: '/drivers/clojure/codox/index.html', attrs: { target: '_blank' } }
               ]
             },
-
-            {
-              label: 'Kotlin',
-              items: [
-                { label: 'Getting started', link: '/drivers/kotlin/getting-started' },
-                { label: 'KDoc ↗', link: '/drivers/kotlin/kdoc/index.html', attrs: { target: '_blank' } }
-              ]
-            },
-
-            { label: 'HTTP (OpenAPI) ↗', link: '/drivers/http/openapi/index.html', attrs: { target: '_blank' } },
-
-            {
-              label: 'Java',
-              items: [
-                { label: 'Getting started', link: '/drivers/java/getting-started' },
-                // TODO broken atm
-                // { label: 'Javadoc ↗', link: '/drivers/java/javadoc/index.html', attrs: {target: '_blank'} }
-              ]
-            },
+	    
           ]
         },
       ],
