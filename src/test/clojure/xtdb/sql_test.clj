1b10122f71d5cf84276c7de3f1f6ab94e9a52d19
Daniel Mason <dan@juxt.pro>
2024-03-27 14:45:51 +0000

    Add table/schema privilege function parsing, always return true.

--- src/test/clojure/xtdb/sql_test.clj
+++ src/test/clojure/xtdb/sql_test.clj
@@ -1893,3 +1893,39 @@
   
   (t/is (= [{:xt/column-1 "public"}]
            (xt/q tu/*node* "SELECT current_schema FROM (VALUES 1) AS x"))))
+
+(t/deftest test-postgres-access-control-functions
+  ;; These current functions should always should return true
+
+  (t/testing "has table privilege"
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT has_table_privilege('xtdb','docs', 'select') FROM (VALUES 1) AS x")))
+
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT has_table_privilege('docs', 'select') FROM (VALUES 1) AS x")))
+
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT pg_catalog.has_table_privilege('docs', 'select') FROM (VALUES 1) AS x"))))
+
+  (t/testing "has schema privilege"
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT has_schema_privilege('xtdb', 'public', 'select') FROM (VALUES 1) AS x")))
+
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT has_schema_privilege('public', 'select') FROM (VALUES 1) AS x")))
+
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT pg_catalog.has_schema_privilege('public', 'select') FROM (VALUES 1) AS x"))))
+
+  (t/testing "current_user passed in"
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT pg_catalog.has_table_privilege(current_user, 'docs', 'select') FROM (VALUES 1) AS x")))
+    
+    (t/is (= [{:xt/column-1 true}]
+             (xt/q tu/*node* "SELECT pg_catalog.has_schema_privilege(current_user, 'public', 'select') FROM (VALUES 1) AS x"))))
+
+  (t/testing "example query"
+    (xt/submit-tx tu/*node* [[:put-docs :docs {:xt/id 1 :x 3}]])
+
+    (t/is (= [{:x 3}]
+             (xt/q tu/*node* "SELECT docs.x FROM docs WHERE has_table_privilege('docs', 'select') ")))))
