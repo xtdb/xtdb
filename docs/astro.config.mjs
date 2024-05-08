2c8184cb6958e1d010dccccc17cedfa8f8256df1
Oliver Marshall <olivershawmarshall@gmail.com>
2024-05-08 15:39:53 +0100

    docs: refactor swup setup into Head component override

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -269,7 +269,6 @@ export default defineConfig({
             components: {
                 TableOfContents: './src/components/table-of-contents.astro',
                 MobileTableOfContents: './src/components/mobile-table-of-contents.astro',
-                MobileMenuToggle: './src/components/mobile-menu-toggle.astro',
                 Head: './src/components/Head.astro',
             },
         }),
