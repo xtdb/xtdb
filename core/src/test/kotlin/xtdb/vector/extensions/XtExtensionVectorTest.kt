cdae0a7cd64c1a383c29c064580d856acc3466f2
Finn Völkel <finn.volkel@gmail.com>
2024-05-23 12:11:00 +0200

    test ExtensionVector TransferPair

---
+++ core/src/test/kotlin/xtdb/vector/extensions/XtExtensionVectorTest.kt
@@ -0,0 +1,55 @@
+package xtdb.vector.extensions
+
+import org.apache.arrow.memory.BufferAllocator
+import org.apache.arrow.memory.RootAllocator
+import org.apache.arrow.vector.FieldVector
+import org.apache.arrow.vector.types.pojo.Field
+import org.apache.arrow.vector.types.pojo.FieldType
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import xtdb.vector.ValueVectorReader.from
+import xtdb.vector.toList
+import xtdb.vector.writerFor
+import java.util.*
+
+class XtExtensionVectorTest {
+    private lateinit var al: BufferAllocator
+
+    @BeforeEach
+    fun setUp() {
+        al = RootAllocator()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        al.close()
+    }
+
+    @Test
+    fun `test ExtensionVector TransferPair` () {
+        val uuidField = Field("uuid", FieldType.notNullable(UuidType), emptyList())
+        UuidType.getNewVector("uuid", FieldType.nullable(UuidType), al).use {  uuidVector ->
+            val writer = writerFor(uuidVector)
+            val uuid1 = UUID.randomUUID()
+            val uuid2 = UUID.randomUUID()
+
+            writer.writeObject(uuid1)
+            writer.writeObject(uuid2)
+            writer.syncValueCount()
+
+            val newVector = uuidVector
+                .getTransferPair(uuidField, al)
+                .also { it.transfer() }
+                .to as FieldVector
+
+            assertEquals(uuidField, newVector.field)
+            assertEquals(2, newVector.valueCount)
+            assertEquals(listOf(uuid1, uuid2), from(newVector).toList())
+
+            uuidVector.close()
+            newVector.close()
+        }
+    }
+}
