bf66c91b85253c93579f2db7c6681b2963cdd3ee
James Henderson <james@jarohen.dev>
2024-05-24 12:24:12 +0100

    TPC-H Q19: manually extract common predicate out of all OR branches - not an optimisation we currently make

--- src/test/resources/xtdb/sql/tpch/q19.sql
+++ src/test/resources/xtdb/sql/tpch/q19.sql
@@ -2,10 +2,10 @@ SELECT SUM(l.l_extendedprice * (1 - l.l_discount)) AS revenue
 FROM
   lineitem AS l,
   part AS p
-WHERE
-  (
-    p.p_partkey = l.l_partkey
-    AND p.p_brand = 'Brand#12'
+WHERE p.p_partkey = l.l_partkey
+  AND
+  ((
+    p.p_brand = 'Brand#12'
     AND p.p_container IN ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
     AND l.l_quantity >= 1 AND l.l_quantity <= 1 + 10
     AND p.p_size BETWEEN 1 AND 5
@@ -14,8 +14,7 @@ WHERE
   )
   OR
   (
-    p.p_partkey = l.l_partkey
-    AND p.p_brand = 'Brand#23'
+    p.p_brand = 'Brand#23'
     AND p.p_container IN ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
     AND l.l_quantity >= 10 AND l.l_quantity <= 10 + 10
     AND p.p_size BETWEEN 1 AND 10
@@ -24,11 +23,10 @@ WHERE
   )
   OR
   (
-    p.p_partkey = l.l_partkey
-    AND p.p_brand = 'Brand#34'
+    p.p_brand = 'Brand#34'
     AND p.p_container IN ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
     AND l.l_quantity >= 20 AND l.l_quantity <= 20 + 10
     AND p.p_size BETWEEN 1 AND 15
     AND l.l_shipmode IN ('AIR', 'AIR REG')
     AND l.l_shipinstruct = 'DELIVER IN PERSON'
-  )
+  ))
