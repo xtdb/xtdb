a9bb888fcc9d88eaf2ba4f2d41d7ee97e097fbc8
Daniel Mason <dan@juxt.pro>
2024-06-11 14:48:48 +0100

    Better handling of exceptions in slack.

--- cloud-benchmark/src/main/clojure/xtdb/run_auctionmark/main.clj
+++ cloud-benchmark/src/main/clojure/xtdb/run_auctionmark/main.clj
@@ -1,13 +1,14 @@
 (ns xtdb.run-auctionmark.main
   (:require [cheshire.core :as json]
             [clj-http.client :as http]
+            [clojure.java.io :as io]
             [clojure.stacktrace :as st]
             [clojure.tools.logging :as log]
             [xtdb.bench :as b]
             [xtdb.bench.auctionmark :as am]
             [xtdb.bench.measurement :as bm]
-            [xtdb.node :as xtn]
-            [clojure.java.io :as io]))
+            [xtdb.node :as xtn]) 
+  (:import [clojure.lang ExceptionInfo]))
 
 (def run-duration (or (System/getenv "AUCTIONMARK_DURATION") "PT24H"))
 (def scale-factor (or (some-> (System/getenv "AUCTIONMARK_SCALE_FACTOR") (Float/parseFloat)) 0.1))
@@ -25,19 +26,29 @@
       (some-> (System/getenv "SLACK_WEBHOOK_URL")
               (not-empty))))
 
-(defn send-error-to-slack [error]
+;; If ex-info contains `system` suggests integrant runtime error 
+;; - get the cause instead, as that's more useful to us
+;; See https://github.com/weavejester/integrant/blob/master/src/integrant/core.cljc#L270
+(defn ex-info-handle-integrant-runtime-error [ex]
+  (let [{:keys [system]} (ex-data ex)]
+    (if system (ex-cause ex) ex)))
+
+(defn send-error-to-slack [e]
   (when slack-url
-    (let [error-message (format
-                         ":x: Error thrown within Auctionmark: `%s`! \n\n*Stack Trace:*\n ```%s```"
+    (let [error (if (instance? ExceptionInfo e)
+                  (ex-info-handle-integrant-runtime-error e)
+                  e)
+          error-message (format
+                         ":x: Error thrown within Auctionmark: *%s*! \n\n*Stack Trace:*\n ```%s```"
                          (.getMessage error) 
-                         (with-out-str (st/print-stack-trace error 15)))]
+                         (with-out-str (st/print-cause-trace error 15)))]
       (http/post slack-url {:headers {"Content-Type" "application/json"}
                             :body (json/generate-string {:text error-message})}))))
 
 (defn send-success-to-slack [{:keys [duration scale-factor load-phase]}]
   (when slack-url
     (let [success-message (format
-                           ":white_check_mark: Auctionmark successfully ran for `%s`! \n\n*Scale Factor*: `%s` \n*Load-Phase*: `%s`"
+                           ":white_check_mark: Auctionmark successfully ran for *%s*! \n\n*Scale Factor*: `%s` \n*Load-Phase*: `%s`"
                            duration scale-factor load-phase) ]
       (http/post slack-url {:headers {"Content-Type" "application/json"}
                             :body (json/generate-string {:text success-message})}))))
