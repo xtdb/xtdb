3315e29c87f8983ed91e0b8d5170df44d21fcc16
James Henderson <james@jarohen.dev>
2024-06-21 11:22:49 +0100

    combining 'XTQL Overview' and 'XTQL Walkthrough' into one page, and moving it to tutorials (user aim is to learn, not to end up with something concrete)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -58,6 +58,8 @@ export default defineConfig({
                                 { label: '4) Changing an immutable database', link: '/tutorials/immutability-walkthrough/part-4' },
                             ],
                         },
+
+                        { label: 'Introducing XTQL', link: '/tutorials/introducing-xtql' },
                     ],
                 },
 
@@ -85,9 +87,6 @@ export default defineConfig({
                     collapsed: true,
                     items: [
                         { label: 'Setting up a cluster on AWS', link: '/guides/starting-with-aws' },
-                        { label: 'XTQL Overview', link: '/intro/what-is-xtql' },
-                        { label: 'XTQL Walkthrough', link: '/guides/xtql-walkthrough' },
-
                     ],
                 },
 
