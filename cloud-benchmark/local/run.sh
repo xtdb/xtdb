b566f2e08206bccbe3239c051f942f9bf0304ee2
Daniel Mason <dan@juxt.pro>
2024-06-28 14:30:31 +0100

    Updates to `cloud-benchmark:local` to profile runs.

--- cloud-benchmark/local/run.sh
+++ cloud-benchmark/local/run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# Create Docker volume
+docker volume create xtdb-am-data
+
 set -e
 (
   echo Running Docker image ...
-  docker run -it --env-file local-vars.env -p 10001:10001 --rm xtdb-local-auctionmark:latest
+  docker run -it --env-file local-vars.env -p 10001:10001 --memory=4096m --rm -v xtdb-am-data:/var/lib/xtdb/ xtdb-local-auctionmark:latest
 )
 
