xtdb/xtdb

commit a65bc3923364d4e55bfff9d74441856e58b0df28

... and also "NOT <period> CONTAINS <date-time>" in the SQL planner when period-end == date-time

core/src/main/clojure/xtdb/sql/plan.clj
@@ -1270,7 +1270,7 @@
   (visitPeriodContainsPredicate [this ctx]
     (let [p1 (-> (.periodPredicand ctx) (.accept this))
           p2 (-> (.periodOrPointInTimePredicand ctx) (.accept this))]
-      (list 'and (list '<= (:from p1) (:from p2)) (list '>= (:to p1) (:to p2)))))
+      (list 'and (list '<= (:from p1) (:from p2)) (list '> (:to p1) (:to p2)))))
 
   (visitPeriodPrecedesPredicate [this ctx]
     (let [p1 (-> (.periodPredicand ctx 0) (.accept this))

src/test/clojure/xtdb/sql_test.clj
@@ -1833,3 +1833,6 @@ VALUES
                {:value 530, :xt/valid-from (zdt 10), :xt/valid-to (zdt 15)}
                {:value 580, :xt/valid-from (zdt 27), :xt/valid-to (zdt 30)}}
              (q)))))
+
+(t/deftest test-contains-period-datetime
+  (t/is (= [{:contains false}] (xt/q tu/*node* "SELECT PERIOD(TIMESTAMP '2024-01-01T00:00:00', TIMESTAMP '2024-01-02T00:00:00') CONTAINS TIMESTAMP '2024-01-02T00:00:00' AS `contains`"))))

commit d974d5c15f6ab904a749c26aa9196a4e93ec47e5

fix bug in SQL 'CONTAINS' planner introduced in a65bc392, suspect I missed a test fail because plan caching

core/src/main/antlr/xtdb/antlr/Sql.g4
@@ -237,7 +237,8 @@ exprPrimary
     // period predicates
     | periodPredicand 'OVERLAPS' periodPredicand # PeriodOverlapsPredicate
     | periodPredicand 'EQUALS' periodPredicand # PeriodEqualsPredicate
-    | periodPredicand 'CONTAINS' periodOrPointInTimePredicand # PeriodContainsPredicate
+    | periodPredicand 'CONTAINS' periodPredicand # PeriodContainsPeriodPredicate
+    | periodPredicand 'CONTAINS' pointInTimePredicand # PeriodContainsPointPredicate
     | periodPredicand 'PRECEDES' periodPredicand # PeriodPrecedesPredicate
     | periodPredicand 'SUCCEEDS' periodPredicand # PeriodSucceedsPredicate
     | periodPredicand 'IMMEDIATELY' 'PRECEDES' periodPredicand # PeriodImmediatelyPrecedesPredicate
@@ -687,7 +688,6 @@ periodColumnName : 'VALID_TIME' | 'SYSTEM_TIME' ;
 periodStartValue : expr ;
 periodEndValue : expr ;
 
-periodOrPointInTimePredicand : periodPredicand | pointInTimePredicand ;
 pointInTimePredicand : expr ;
 /// §8.21 <search condition>
 

core/src/main/clojure/xtdb/sql/plan.clj
@@ -1267,10 +1267,17 @@
           p2 (-> (.periodPredicand ctx 1) (.accept this))]
       (list 'and (list '= (:from p1) (:from p2)) (list '= (:to p1) (:to p2)))))
 
-  (visitPeriodContainsPredicate [this ctx]
-    (let [p1 (-> (.periodPredicand ctx) (.accept this))
-          p2 (-> (.periodOrPointInTimePredicand ctx) (.accept this))]
-      (list 'and (list '<= (:from p1) (:from p2)) (list '> (:to p1) (:to p2)))))
+  (visitPeriodContainsPeriodPredicate [this ctx]
+    (let [p1 (-> (.periodPredicand ctx 0) (.accept this))
+          p2 (-> (.periodPredicand ctx 1) (.accept this))]
+      (list 'and (list '<= (:from p1) (:from p2)) (list '>= (:to p1) (:to p2)))))
+
+  (visitPeriodContainsPointPredicate [this ctx]
+    (let [period (-> (.periodPredicand ctx) (.accept this))
+          pit (-> (.pointInTimePredicand ctx) (.accept this))]
+      ;; TODO this currently duplicates the expr, but emitting a `let`
+      ;; probably won't get optimised into scan preds
+      (list 'and (list '<= (:from period) pit) (list '> (:to period) pit))))
 
   (visitPeriodPrecedesPredicate [this ctx]
     (let [p1 (-> (.periodPredicand ctx 0) (.accept this))
@@ -1306,11 +1313,7 @@
           ev (some-> (.periodEndValue ctx) (.expr) (.accept this))]
       {:from sv :to ev}))
 
-  (visitPeriodOrPointInTimePredicand [this ctx] (.accept (.getChild ctx 0) this))
-
-  (visitPointInTimePredicand [this ctx]
-    (let [pit (-> (.expr ctx) (.accept this))]
-      {:from pit :to pit}))
+  (visitPointInTimePredicand [this ctx] (.accept (.expr ctx) this))
 
   (visitHasTablePrivilegePredicate [_ _] true)
   (visitHasSchemaPrivilegePredicate [_ _] true)

src/test/clojure/xtdb/sql/expr_test.clj
@@ -878,7 +878,6 @@
     (t/is (= [{:itvl #xt/interval-mdn ["P0D" "PT-2H-0.001S"]}]
              (xt/q tu/*node* "SELECT AGE(TIMESTAMP '2023-07-01T12:00:30.499+02:00[Europe/Berlin]', TIMESTAMP '2023-07-01T12:00:30.500') as itvl")))))
 
-
 (t/deftest test-period-predicates
   (t/are [expected sql] (= expected (plan-expr-with-foo sql))
     '(and (<= f/xt$valid_from #xt.time/zoned-date-time "2000-01-01T00:00Z")
@@ -886,7 +885,7 @@
     "foo.VALID_TIME CONTAINS PERIOD (TIMESTAMP '2000-01-01 00:00:00+00:00', TIMESTAMP '2001-01-01 00:00:00+00:00')"
 
     '(and (<= f/xt$valid_from #xt.time/zoned-date-time "2000-01-01T00:00Z")
-          (>= f/xt$valid_to #xt.time/zoned-date-time "2000-01-01T00:00Z"))
+          (> f/xt$valid_to #xt.time/zoned-date-time "2000-01-01T00:00Z"))
     "foo.VALID_TIME CONTAINS TIMESTAMP '2000-01-01 00:00:00+00:00'"
 
     ;; also testing all period-predicate permutations
@@ -921,20 +920,19 @@
 (t/deftest test-period-predicates-point-in-time
   (t/are [expected sql] (= expected (plan-expr-with-foo sql))
 
-    '(and (<= f/xt$valid_from f/a) (>= f/xt$valid_to f/a))
+    '(and (<= f/xt$valid_from f/a) (> f/xt$valid_to f/a))
     "foo.valid_time CONTAINS foo.a"
 
     '(and
       (<= f/xt$valid_from #xt.time/zoned-date-time "2010-01-01T11:10:11Z")
-      (>= f/xt$valid_to #xt.time/zoned-date-time "2010-01-01T11:10:11Z"))
+      (> f/xt$valid_to #xt.time/zoned-date-time "2010-01-01T11:10:11Z"))
     "foo.valid_time CONTAINS TIMESTAMP '2010-01-01T11:10:11Z'"
 
     '(and (<= f/xt$valid_from f/a) (>= f/xt$valid_to f/a))
     "foo.valid_time CONTAINS PERIOD(foo.a, foo.a)"
 
-    '(and (<= f/xt$valid_from f/xt$system_from) (>= f/xt$valid_to f/xt$system_from))
+    '(and (<= f/xt$valid_from f/xt$system_from) (> f/xt$valid_to f/xt$system_from))
     "foo.valid_time CONTAINS foo.xt$system_from"))
-
 
 (t/deftest test-coalesce
   (t/testing "planning"