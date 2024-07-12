b795b4a35e4f5062cd06c8c7a2ccc4e4bfe9a5c4
Finn Völkel <finn.volkel@gmail.com>
2024-07-12 09:52:15 +0200

    cloud-benchmark config tweaks

--- cloud-benchmark/local/build.sh
+++ cloud-benchmark/local/build.sh
@@ -7,6 +7,6 @@ set -e
     sha=$(git rev-parse --short HEAD)
 
     echo Building Docker image ...
-    docker build -t xtdb-local-auctionmark:latest --build-arg GIT_SHA="$sha" --build-arg XTDB_VERSION="${XTDB_VERSION:-dev-SNAPSHOT}" --output type=docker .
+    docker build -t xtdb-local-auctionmark:latest --build-arg GIT_SHA="$sha" --build-arg XTDB_VERSION="${XTDB_VERSION:-dev-SNAPSHOT}" .
     echo Done
 )
--- cloud-benchmark/local/run.sh
+++ cloud-benchmark/local/run.sh
@@ -6,6 +6,5 @@ docker volume create xtdb-am-data
 set -e
 (
   echo Running Docker image ...
-  docker run -it --env-file local-vars.env -p 10001:10001 --memory=4096m --rm -v xtdb-am-data:/var/lib/xtdb/ xtdb-local-auctionmark:latest
+  docker run -it --env-file local-vars.env -p 10001:10001 -p 8080:8080 --memory=4096m --rm -v xtdb-am-data:/var/lib/xtdb/ -v .:/external/dir xtdb-local-auctionmark:latest
 )
-
