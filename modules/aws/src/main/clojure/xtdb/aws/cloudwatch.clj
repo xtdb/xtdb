73f712f66e4515b4ecee83a9c12abe89565735ff
Finn Völkel <finn.volkel@gmail.com>
2024-07-17 16:58:46 +0200

    Type hint Xtdb$Config in aws.cloudwatch

--- modules/aws/src/main/clojure/xtdb/aws/cloudwatch.clj
+++ modules/aws/src/main/clojure/xtdb/aws/cloudwatch.clj
@@ -1,9 +1,10 @@
 (ns xtdb.aws.cloudwatch
   (:require [xtdb.node :as xtn])
   (:import (software.amazon.awssdk.services.cloudwatch CloudWatchAsyncClient)
+           (xtdb.api Xtdb$Config)
            (xtdb.aws CloudWatchMetrics$Factory)))
 
-(defmethod xtn/apply-config! ::metrics [config _ {:keys [namespace client]
-                                                  :or {namespace "xtdb.metrics"
-                                                       client (CloudWatchAsyncClient/create)}}]
+(defmethod xtn/apply-config! ::metrics [^Xtdb$Config config _ {:keys [namespace client]
+                                                               :or {namespace "xtdb.metrics"
+                                                                    client (CloudWatchAsyncClient/create)}}]
   (.setMetrics config (CloudWatchMetrics$Factory. namespace client)))
