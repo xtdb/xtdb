fef5db89e0eca24df3b0756d18035d606a64434b
Finn Völkel <finn.volkel@gmail.com>
2024-07-23 17:01:37 +0200

    Make TemporalBounds human-readable

--- core/src/main/kotlin/xtdb/util/TemporalBounds.kt
+++ core/src/main/kotlin/xtdb/util/TemporalBounds.kt
@@ -1,18 +1,19 @@
 package xtdb.util
 
+import java.time.Instant
 import kotlin.math.max
 import kotlin.math.min
 
-class TemporalBounds {
+data class TemporalBounds(
+    val validFrom: TemporalColumn = TemporalColumn(),
+    val validTo: TemporalColumn = TemporalColumn(),
+    val systemFrom: TemporalColumn = TemporalColumn(),
+    val systemTo: TemporalColumn = TemporalColumn()) {
 
-    val validFrom: TemporalColumn = TemporalColumn()
-    val validTo: TemporalColumn = TemporalColumn()
-    val systemFrom: TemporalColumn = TemporalColumn()
-    val systemTo: TemporalColumn = TemporalColumn()
 
-    class TemporalColumn {
-        var lower: Long = Long.MIN_VALUE
-        var upper: Long = Long.MAX_VALUE
+    class TemporalColumn (
+        var lower: Long = Long.MIN_VALUE,
+        var upper: Long = Long.MAX_VALUE) {
 
         fun lt(operand: Long) = apply { upper = min(operand - 1, upper) }
         fun lte(operand: Long) = apply { upper = min(operand, upper) }
@@ -20,6 +21,12 @@ class TemporalBounds {
         fun gte(operand: Long) = apply { lower = max(operand, lower) }
 
         fun inRange(operand: Long) = operand in lower..upper
+
+        override fun toString(): String {
+            val l = Instant.ofEpochMilli(lower / 1000)
+            val u = Instant.ofEpochMilli(upper / 1000)
+            return "TemporalColumn(lower=$l, upper=$u)"
+        }
     }
 
     fun inRange(validFrom: Long, validTo: Long, systemFrom: Long, systemTo: Long) =
