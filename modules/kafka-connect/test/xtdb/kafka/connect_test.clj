34f8f2c55589c03ca36d9dd9d670a32f42a70904
Oliver Marshall <olivershawmarshall@gmail.com>
2024-08-01 16:33:27 +0100

    test(kafka-sink): add basic config test

--- modules/kafka-connect/test/xtdb/kafka/connect_test.clj
+++ modules/kafka-connect/test/xtdb/kafka/connect_test.clj
@@ -31,28 +31,42 @@
              XtdbSinkConnector/ID_MODE_CONFIG "record_key"}))
 
 (t/deftest e2e-test
-  (let [sink (partial kc/submit-sink-records *node*)
-        props (->config {XtdbSinkConnector/URL_CONFIG "url"
-                         XtdbSinkConnector/ID_MODE_CONFIG "record_value"
-                         XtdbSinkConnector/ID_FIELD_CONFIG "my-id-field"
-                         XtdbSinkConnector/VALID_FROM_FIELD_CONFIG "my-valid-from-field"
-                         XtdbSinkConnector/VALID_TO_FIELD_CONFIG "my-valid-to-field"
-                         XtdbSinkConnector/TABLE_NAME_FORMAT_CONFIG "pre_${topic}_post"})]
-    (sink props [(->sink-record
-                   {:topic "foo"
-                    :key-schema nil
-                    :key-value nil
-                    :value-schema nil
-                    :value-value {:my-id-field 1
-                                  :my-valid-from-field #inst "2021-01-01T00:00:00Z"
-                                  :my-valid-to-field #inst "2021-01-02T00:00:00Z"
-                                  :value {:a 1}}})])
-    (t/is
-      (= (xt/q *node* "SELECT foo.*, _valid_from, _valid_to FROM pre_foo_post FOR VALID_TIME ALL AS foo")
-         [{:xt/id 1
-           :my-id-field 1
-           :xt/valid-from #xt.time/zoned-date-time "2021-01-01T00:00:00Z[UTC]"
-           :my-valid-from-field #xt.time/zoned-date-time "2021-01-01T00:00:00Z[UTC]"
-           :xt/valid-to #xt.time/zoned-date-time "2021-01-02T00:00:00Z[UTC]"
-           :my-valid-to-field #xt.time/zoned-date-time "2021-01-02T00:00:00Z[UTC]"
-           :value {:a 1}}]))))
+  (let [sink (partial kc/submit-sink-records *node*)]
+    (t/testing "basic record_key"
+      (let [props (->config {XtdbSinkConnector/URL_CONFIG "url"
+                             XtdbSinkConnector/ID_MODE_CONFIG "record_key"})]
+        (sink props [(->sink-record
+                       {:topic "foo"
+                        :key-schema nil
+                        :key-value 1
+                        :value-schema nil
+                        :value-value {:value {:a 1}}})])
+        (t/is
+          (= (xt/q *node* "SELECT * FROM foo")
+             [{:xt/id 1
+               :value {:a 1}}]))))
+    (t/testing "full config"
+      (let [props (->config {XtdbSinkConnector/URL_CONFIG "url"
+                             XtdbSinkConnector/ID_MODE_CONFIG "record_value"
+                             XtdbSinkConnector/ID_FIELD_CONFIG "my-id-field"
+                             XtdbSinkConnector/VALID_FROM_FIELD_CONFIG "my-valid-from-field"
+                             XtdbSinkConnector/VALID_TO_FIELD_CONFIG "my-valid-to-field"
+                             XtdbSinkConnector/TABLE_NAME_FORMAT_CONFIG "pre_${topic}_post"})]
+        (sink props [(->sink-record
+                       {:topic "foo"
+                        :key-schema nil
+                        :key-value nil
+                        :value-schema nil
+                        :value-value {:my-id-field 1
+                                      :my-valid-from-field #inst "2021-01-01T00:00:00Z"
+                                      :my-valid-to-field #inst "2021-01-02T00:00:00Z"
+                                      :value {:a 1}}})])
+        (t/is
+          (= (xt/q *node* "SELECT foo.*, _valid_from, _valid_to FROM pre_foo_post FOR VALID_TIME ALL AS foo")
+             [{:xt/id 1
+               :my-id-field 1
+               :xt/valid-from #xt.time/zoned-date-time "2021-01-01T00:00:00Z[UTC]"
+               :my-valid-from-field #xt.time/zoned-date-time "2021-01-01T00:00:00Z[UTC]"
+               :xt/valid-to #xt.time/zoned-date-time "2021-01-02T00:00:00Z[UTC]"
+               :my-valid-to-field #xt.time/zoned-date-time "2021-01-02T00:00:00Z[UTC]"
+               :value {:a 1}}]))))))
