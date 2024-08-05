9c589eb46487a3c4c231f0d85d12a0e70d087f2a
Ernesto Garcia <egg@juxt.pro>
2024-08-05 13:45:52 +0200

    Added passing test for ANY/SOME aggregation functions going to be renamed.

--- src/test/clojure/xtdb/sql_test.clj
+++ src/test/clojure/xtdb/sql_test.clj
@@ -757,6 +757,11 @@
                         "SELECT SUM(heads) AS sum_heads, MIN(heads) AS min_heads, MAX(heads) AS max_heads, COUNT(heads) AS count_heads FROM docs")))
           "various aggs")
 
+    (t/is (= #{{:any-medusa true, :some-medusa true, :every-medusa false}}
+             (set (xt/q tu/*node*
+                    "SELECT ANY(_id = 'medusa') AS any_medusa, SOME(_id = 'medusa') AS some_medusa, EVERY(_id = 'medusa') AS every_medusa FROM docs")))
+      "bool aggs")
+
     (t/is (= #{{:heads 1, :count-heads 3}}
              (set (xt/q tu/*node*
                         "SELECT heads, COUNT(heads) AS count_heads FROM docs HAVING COUNT(heads) > 1")))
