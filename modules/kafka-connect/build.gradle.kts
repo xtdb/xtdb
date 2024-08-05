e057a9282aeda41f0424ac945af8ed9f981873a3
Oliver Marshall <olivershawmarshall@gmail.com>
2024-08-05 12:56:50 +0100

    feat: downgrade jvm version to 17 for kafka-connect, api, & http-client-jvm

--- modules/kafka-connect/build.gradle.kts
+++ modules/kafka-connect/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("com.github.johnrengelman.shadow")
 }
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
 dependencies {
     api(project(":xtdb-api"))
