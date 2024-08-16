b01677e3b2615ff67ed7c049d82f6ac6c7146d76
Daniel Mason <dan@juxt.pro>
2024-08-16 10:08:36 +0100

    Switch Azure auctionmark benchmark to use a single node hosted on AKS  (#3602)
    * Switch to use AKS for single node benchmark
    * Updates to terraform & kubernetes setup.
    - Pull out kubernetes to separate files for easier management.
    - Update README with instructions for setup.
    - Remove eventhub config, switch to local txLog (for now).
    
    ---------
    
    Co-authored-by: Tim Greene <tim.g.greene@gmail.com>

--- cloud-benchmark/azure/terraform/main.tf
+++ cloud-benchmark/azure/terraform/main.tf
@@ -28,14 +28,6 @@ resource "azurerm_storage_container" "cloud_benchmark" {
   container_access_type = "private"
 }
 
-# Fileshare Configuration
-
-resource "azurerm_storage_share" "cloud_benchmark" {
-  name                 = "cloudbenchmarkshare"
-  storage_account_name = azurerm_storage_account.cloud_benchmark.name
-  quota                = 50
-}
-
 # Service Bus Setup
 resource "azurerm_eventgrid_system_topic" "cloud_benchmark" {
   name                = "cloud-benchmark-system-topic"
@@ -65,24 +57,6 @@ resource "azurerm_eventgrid_system_topic_event_subscription" "cloud_benchmark" {
   resource_group_name           = azurerm_resource_group.cloud_benchmark.name
   event_delivery_schema         = "EventGridSchema"
   service_bus_topic_endpoint_id = azurerm_servicebus_topic.cloud_benchmark.id
-}
-
-# Event Hub Configuration
-
-resource "azurerm_eventhub_namespace" "cloud_benchmark" {
-  name                = "cloud-benchmark-eventhub-namespace"
-  location            = azurerm_resource_group.cloud_benchmark.location
-  resource_group_name = azurerm_resource_group.cloud_benchmark.name
-  sku                 = "Standard"
-  capacity            = 1
-}
-
-resource "azurerm_eventhub" "cloud_benchmark" {
-  name                = "cloud-benchmark-eventhub-topic-${var.eventhub_topic_suffix}"
-  namespace_name      = azurerm_eventhub_namespace.cloud_benchmark.name
-  resource_group_name = azurerm_resource_group.cloud_benchmark.name
-  partition_count     = 1
-  message_retention   = 7
 }
 
 # Metrics Config
@@ -131,19 +105,6 @@ resource "azurerm_role_assignment" "cloud_benchmark_servicebus_contributor" {
   scope                = azurerm_servicebus_namespace.cloud_benchmark.id
 }
 
-resource "azurerm_role_assignment" "cloud_benchmark_eventhub_send" {
-  principal_id         = azurerm_user_assigned_identity.cloud_benchmark.principal_id
-  role_definition_name = "Azure Event Hubs Data Sender"
-  scope                = azurerm_eventhub.cloud_benchmark.id
-}
-
-resource "azurerm_role_assignment" "cloud_benchmark_eventhub_receive" {
-  principal_id         = azurerm_user_assigned_identity.cloud_benchmark.principal_id
-  role_definition_name = "Azure Event Hubs Data Receiver"
-  scope                = azurerm_eventhub.cloud_benchmark.id
-}
-
-# Container App Config
 resource "azurerm_container_registry" "acr" {
   name                = "cloudbenchmarkregistry"
   resource_group_name = azurerm_resource_group.cloud_benchmark.name
@@ -152,29 +113,23 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-resource "azurerm_container_app_environment" "cloud_benchmark" {
-  name                       = "cloud-benchmark-container-app-environment"
-  location                   = azurerm_resource_group.cloud_benchmark.location
-  resource_group_name        = azurerm_resource_group.cloud_benchmark.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.cloud_benchmark.id
-}
+## Kubernetes Cluster
+resource "azurerm_kubernetes_cluster" "cloud_benchmark" {
+  name                = "cloud-benchmark-cluster"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.cloud_benchmark.name
+  dns_prefix          = "cloud-benchmark-cluster"
 
-## Fileshare Storage
-resource "azurerm_container_app_environment_storage" "cloud_benchmark" {
-  name                         = "app-persistent-storage"
-  container_app_environment_id = azurerm_container_app_environment.cloud_benchmark.id
-  account_name                 = azurerm_storage_account.cloud_benchmark.name
-  share_name                   = azurerm_storage_share.cloud_benchmark.name
-  access_key                   = azurerm_storage_account.cloud_benchmark.primary_access_key
-  access_mode                  = "ReadWrite"
-}
-
-resource "azurerm_container_app" "cloud_benchmark_single_node" {
-  count                        = var.run_single_node ? 1 : 0
-  name                         = "cloud-benchmark-single-node"
-  resource_group_name          = azurerm_resource_group.cloud_benchmark.name
-  container_app_environment_id = azurerm_container_app_environment.cloud_benchmark.id
-  revision_mode                = "Single"
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+    upgrade_settings {
+      drain_timeout_in_minutes      = 0
+      max_surge                     = "10%"
+      node_soak_duration_in_minutes = 0
+    }
+  }
 
   identity {
     type = "UserAssigned"
@@ -183,108 +138,42 @@ resource "azurerm_container_app" "cloud_benchmark_single_node" {
     ]
   }
 
-  registry {
-    server   = "cloudbenchmarkregistry.azurecr.io"
-    identity = azurerm_user_assigned_identity.cloud_benchmark.id
-  }
-
-
-  template {
-    max_replicas = 1
-    min_replicas = 1
-
-    container {
-      image = "cloudbenchmarkregistry.azurecr.io/xtdb-azure-bench:latest"
-      name  = "cloud-benchmark"
-
-      cpu    = 2
-      memory = "4Gi"
-
-      env {
-        name  = "AUCTIONMARK_DURATION"
-        value = var.auctionmark_duration
-      }
-
-      env {
-        name  = "AUCTIONMARK_SCALE_FACTOR"
-        value = var.auctionmark_scale_factor
-      }
-
-      env {
-        name  = "AUCTIONMARK_LOAD_PHASE"
-        value = true
-      }
-
-      env {
-        name  = "AUCTIONMARK_LOAD_PHASE_ONLY"
-        value = false
-      }
-
-      env {
-        name  = "XTDB_LOCAL_DISK_CACHE"
-        value = "/var/lib/xtdb/disk-cache/cache-1"
-      }
-
-      env {
-        name  = "XTDB_AZURE_USER_MANAGED_IDENTITY_CLIENT_ID"
-        value = azurerm_user_assigned_identity.cloud_benchmark.client_id
-      }
-
-      env {
-        name  = "XTDB_AZURE_STORAGE_ACCOUNT"
-        value = azurerm_storage_account.cloud_benchmark.name
-      }
-
-      env {
-        name  = "XTDB_AZURE_STORAGE_CONTAINER"
-        value = azurerm_storage_container.cloud_benchmark.name
-      }
-
-      env {
-        name  = "XTDB_AZURE_SERVICE_BUS_NAMESPACE"
-        value = azurerm_servicebus_namespace.cloud_benchmark.name
-      }
-
-      env {
-        name  = "XTDB_AZURE_SERVICE_BUS_TOPIC_NAME"
-        value = azurerm_servicebus_topic.cloud_benchmark.name
-      }
-
-      env {
-        name  = "XTDB_AZURE_EVENTHUB_NAMESPACE"
-        value = azurerm_eventhub_namespace.cloud_benchmark.name
-      }
-
-      env {
-        name  = "XTDB_AZURE_EVENTHUB_NAME"
-        value = azurerm_eventhub.cloud_benchmark.name
-      }
-
-      env {
-        name  = "CLOUD_PLATFORM_NAME"
-        value = "Azure"
-      }
-
-      # env {
-      #   name = "SLACK_WEBHOOK_URL"
-      #   value = var.slack_webhook_url
-      # }
-
-      volume_mounts {
-        name = "app-persistent-storage"
-        path = "/var/lib/xtdb"
-      }
-    }
-
-    volume {
-      name         = "app-persistent-storage"
-      storage_name = azurerm_container_app_environment_storage.cloud_benchmark.name
-      storage_type = "AzureFile"
-    }
-  }
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
 }
 
-# TODO: Multi node config could probably be added here - taking inspiration 
-# from the above. Generally speaking, similar to how I'd handle in kubernetes:
-# - InitContainer that runs first which ONLY runs load phase (ie, AUCTIONMARK_LOAD_PHASE_ONLY = true)
-# - Can run a number of parallel containers that point to different local disk cache paths
+resource "azurerm_role_assignment" "cloud_benchmark_cluster_role" {
+  principal_id                     = azurerm_kubernetes_cluster.cloud_benchmark.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_federated_identity_credential" "cloud_benchmark" {
+  name                = "cloud-benchmark-worker"
+  resource_group_name = azurerm_resource_group.cloud_benchmark.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = azurerm_kubernetes_cluster.cloud_benchmark.oidc_issuer_url
+  parent_id           = azurerm_user_assigned_identity.cloud_benchmark.id
+  subject             = "system:serviceaccount:${var.kubernetes_namespace}:${var.kubernetes_service_account_name}"
+}
+
+output "user_managed_identity_client_id" {
+  value = azurerm_user_assigned_identity.cloud_benchmark.client_id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.cloud_benchmark.name
+}
+
+output "storage_account_container" {
+  value = azurerm_storage_container.cloud_benchmark.name
+}
+
+output "service_bus_namespace" {
+  value = azurerm_servicebus_namespace.cloud_benchmark.name
+}
+
+output "service_bus_topic" {
+  value = azurerm_servicebus_topic.cloud_benchmark.name
+}
