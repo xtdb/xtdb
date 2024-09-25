6b6d44d3143085b4856964ced5a26941e87f974e
Matt Butler <mattb-w@hotmail.co.uk>
2024-09-25 10:29:19 +0100

    Add pgwire support and psycopg testing to python harness

--- lang/test-harness/src/test/clojure/drivers/python_test.clj
+++ lang/test-harness/src/test/clojure/drivers/python_test.clj
@@ -1,7 +1,8 @@
 (ns drivers.python-test
   (:require [clojure.test :refer [deftest use-fixtures is]]
             [test-harness.test-utils :as tu]
-            [clojure.java.shell :refer [sh]]))
+            [clojure.java.shell :refer [sh]]
+            [xtdb.node :as xtn]))
 
 (def project-root (str @tu/root-path "/lang/python/"))
 (def report-path (str project-root "/build/test-results/test/TEST-python-test.xml"))
@@ -12,13 +13,21 @@
     (println (:out out)))
   (f))
 
+(def ^:dynamic ^long *pg-port* -1)
+
+(defn- with-node [f]
+  (with-open [node (xtn/start-node {:pgwire-server {:port 0}})]
+    (binding [*pg-port* (.getPgPort node)]
+      (f))))
+
 (use-fixtures :once poetry-install)
-(use-fixtures :each tu/with-system)
+(use-fixtures :each with-node)
 
 (deftest python-test
   (let [out (sh "poetry"
                 "run" "pytest"
                 (str "--junitxml=" report-path)
-                :dir project-root)]
+                :dir project-root
+                :env (into {"PG_PORT" *pg-port*} (System/getenv)))]
     (is (= 0 (:exit out))
         (:out out))))
