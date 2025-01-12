f3e85144e2bc36d2f8946966d13b81f767ae2608
James Henderson <james@jarohen.dev>
2025-01-12 19:15:04 +0000

    bump to Kotlin 2 (2.1.0, specifically)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.9.22"
-        kotlin("plugin.serialization") version "1.9.22"
+        kotlin("jvm") version "2.1.0"
+        kotlin("plugin.serialization") version "2.1.0"
         id("org.jetbrains.dokka") version "1.9.10"
     }
 }
