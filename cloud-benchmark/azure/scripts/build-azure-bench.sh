bd80002b5ea319c653db9f6ee506ba3f27249a84
Daniel Mason <dan@juxt.pro>
2025-01-31 11:13:30 +0000

    Use `docker buildx` in azure `build-azure-bench` script.

--- cloud-benchmark/azure/scripts/build-azure-bench.sh
+++ cloud-benchmark/azure/scripts/build-azure-bench.sh
@@ -7,6 +7,7 @@ set -e
     sha=$(git rev-parse --short HEAD)
 
     echo Building Docker image ...
-    docker build -t xtdb-azure-bench:latest --platform=linux/amd64 --build-arg GIT_SHA="$sha" --build-arg XTDB_VERSION="${XTDB_VERSION:-dev-SNAPSHOT}" .
+    docker buildx build --platform linux/arm64/v8,linux/amd64 --push -t cloudbenchmarkregistry.azurecr.io/xtdb-azure-bench:latest --platform=linux/amd64 --build-arg GIT_SHA="$sha" --build-arg XTDB_VERSION="${XTDB_VERSION:-dev-SNAPSHOT}" .
     echo Done
 )
+
