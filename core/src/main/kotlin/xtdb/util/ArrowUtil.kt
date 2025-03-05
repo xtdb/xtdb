832ee707480ca29a74e2180553eeca596e7de72e
James Henderson <james@jarohen.dev>
2025-03-05 21:03:57 +0000

    simplify openSlice

--- core/src/main/kotlin/xtdb/util/ArrowUtil.kt
+++ core/src/main/kotlin/xtdb/util/ArrowUtil.kt
@@ -3,7 +3,6 @@ package xtdb.util
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
 import org.apache.arrow.memory.BufferAllocator
-import org.apache.arrow.vector.BaseFixedWidthVector
 import org.apache.arrow.vector.ValueVector
 import org.apache.arrow.vector.complex.ListVector
 import org.apache.arrow.vector.complex.UnionVector
@@ -20,16 +19,10 @@ internal fun BufferAllocator.registerMetrics(meterRegistry: MeterRegistry) = app
 fun ValueVector.openSlice(offset: Int = 0, len: Int = valueCount): ValueVector =
     when {
         // see #3088
-        this is ListVector && offset == 0 && len == 0 ->
-            ListVector.empty(name, allocator)
+        this is ListVector && len == 0 -> ListVector.empty(name, allocator)
 
-        this is UnionVector && offset == 0 && len == 0 ->
-            UnionVector.empty(name, allocator)
-                .also { it.initializeChildrenFromFields(field.children) }
+        this is UnionVector && len == 0 ->
+            UnionVector.empty(name, allocator).also { it.initializeChildrenFromFields(field.children) }
 
-        // doesn't preserve nullability otherwise
-        this is BaseFixedWidthVector ->
-            getTransferPair(field, allocator).also { it.splitAndTransfer(offset, len) }.to
-
-        else -> getTransferPair(allocator).also { it.splitAndTransfer(offset, len) }.to
+        else -> getTransferPair(field, allocator).also { it.splitAndTransfer(offset, len) }.to
     }
