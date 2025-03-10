53418dd111b24c1c2e0748aecf9e385f756ee161
James Henderson <james@jarohen.dev>
2025-03-10 12:14:48 +0000

    refactor: inline PageMerge within SegmentMerge

--- core/src/main/kotlin/xtdb/compactor/SegmentMerge.kt
+++ core/src/main/kotlin/xtdb/compactor/SegmentMerge.kt
@@ -2,11 +2,14 @@ package xtdb.compactor
 
 import com.carrotsearch.hppc.ByteArrayList
 import org.apache.arrow.memory.BufferAllocator
+import org.apache.arrow.memory.util.ArrowBufPointer
 import org.apache.arrow.vector.types.pojo.Schema
 import xtdb.arrow.Relation
-import xtdb.trie.ISegment
+import xtdb.arrow.RelationReader
+import xtdb.arrow.RowCopier
+import xtdb.bitemporal.PolygonCalculator
+import xtdb.trie.*
 import xtdb.trie.Trie.dataRelSchema
-import xtdb.trie.toMergePlan
 import xtdb.types.Fields.mergeFields
 import xtdb.types.withName
 import xtdb.util.closeOnCatch
@@ -29,6 +32,83 @@ class SegmentMerge(private val al: BufferAllocator) {
                 val len = min(size, pagePath.size)
                 Arrays.equals(this, 0, len, pagePath, 0, len)
             }
+    }
+
+    private class CopierFactory(private val dataRel: Relation) {
+        private val iidWtr = dataRel["_iid"]!!
+        private val sfWtr = dataRel["_system_from"]!!
+        private val vfWtr = dataRel["_valid_from"]!!
+        private val vtWtr = dataRel["_valid_to"]!!
+        private val opWtr = dataRel["op"]!!
+
+        fun rowCopier(dataReader: RelationReader): RowCopier {
+            val iidCopier = dataReader["_iid"]!!.rowCopier(iidWtr)
+            val sfCopier = dataReader["_system_from"]!!.rowCopier(sfWtr)
+            val vfCopier = dataReader["_valid_from"]!!.rowCopier(vfWtr)
+            val vtCopier = dataReader["_valid_to"]!!.rowCopier(vtWtr)
+            val opCopier = dataReader["op"]!!.rowCopier(opWtr)
+
+            return RowCopier {
+                val pos = iidCopier.copyRow(it)
+
+                sfCopier.copyRow(it)
+                vfCopier.copyRow(it)
+                vtCopier.copyRow(it)
+                opCopier.copyRow(it)
+                dataRel.endRow()
+
+                pos
+            }
+        }
+    }
+
+    private class PageMerge(dataRel: Relation, private val pathFilter: ByteArray?) {
+
+        private data class QueueElem(val evPtr: EventRowPointer, val rowCopier: RowCopier)
+
+        @Suppress("UNCHECKED_CAST")
+        private fun <L : HashTrie.Node<*>> MergePlanNode<L>.loadDataPage(): RelationReader? =
+            segment.dataRel?.loadPage(node as L)
+
+        private val copierFactory = CopierFactory(dataRel)
+
+        fun mergePages(task: MergePlanTask) {
+            val polygonCalculator = PolygonCalculator()
+            val isValidPtr = ArrowBufPointer()
+
+            val mpNodes = task.mpNodes
+            var path = task.path
+            val dataReaders = mpNodes.map { it.loadDataPage() }
+            val mergeQueue = PriorityQueue(Comparator.comparing(QueueElem::evPtr, EventRowPointer.comparator()))
+            path = if (pathFilter == null || path.size > pathFilter.size) path else pathFilter
+
+            for (dataReader in dataReaders) {
+                if (dataReader == null) continue
+                val evPtr = EventRowPointer.XtArrow(dataReader, path)
+                val rowCopier = copierFactory.rowCopier(dataReader)
+                if (evPtr.isValid(isValidPtr, path)) {
+                    mergeQueue.add(QueueElem(evPtr, rowCopier))
+                }
+            }
+
+            var seenErase = false
+
+            while (true) {
+                val elem = mergeQueue.poll() ?: break
+                val (evPtr, rowCopier) = elem
+                if (polygonCalculator.calculate(evPtr) != null) {
+                    rowCopier.copyRow(evPtr.index)
+                } else {
+                    if (!seenErase) rowCopier.copyRow(evPtr.index)
+                    seenErase = true
+                }
+
+                evPtr.nextIndex()
+                if (evPtr.isValid(isValidPtr, path)) {
+                    mergeQueue.add(elem)
+                }
+            }
+        }
     }
 
     internal fun List<ISegment<*>>.mergeTo(ch: WritableByteChannel, pathFilter: ByteArray?): List<PageTree.Leaf> {
