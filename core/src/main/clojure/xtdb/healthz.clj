2c41bdeb5ece347b1bcc99c5d9a4f385695c06b1
James Henderson <james@jarohen.dev>
2025-03-14 19:39:40 +0000

    using latestCompletedOffset to determine node catchup so that non-txs are included (otherwise node might never reach 'started'), #4273

--- core/src/main/clojure/xtdb/healthz.clj
+++ core/src/main/clojure/xtdb/healthz.clj
@@ -1,20 +1,20 @@
 (ns xtdb.healthz
   (:require [clojure.tools.logging :as log]
             [integrant.core :as ig]
-            [reitit.http :as http] 
-            [reitit.http.interceptors.exception :as ri.exception] 
+            [reitit.http :as http]
+            [reitit.http.interceptors.exception :as ri.exception]
             [reitit.interceptor.sieppari :as r.sieppari]
             [reitit.ring :as r.ring]
             [ring.adapter.jetty9 :as j]
             [xtdb.api :as xt]
             [xtdb.node :as xtn]
-            [xtdb.protocols :as xtp]
             [xtdb.util :as util])
   (:import io.micrometer.core.instrument.composite.CompositeMeterRegistry
            (io.micrometer.prometheusmetrics PrometheusConfig PrometheusMeterRegistry)
            [java.lang AutoCloseable]
            org.eclipse.jetty.server.Server
            (xtdb.api Xtdb$Config)
+           (xtdb.api.log Log)
            (xtdb.api.metrics HealthzConfig)
            xtdb.api.Xtdb$Config
            xtdb.indexer.LogProcessor))
@@ -30,13 +30,16 @@
                                      :body (.scrape prometheus-registry)})}]
 
                 ["/healthz/started" {:name :started
-                                     :get (fn [{:keys [node, ^long initial-target-tx-id]}]
-                                            (let [^long lc-tx-id (:tx-id (xtp/latest-completed-tx node) -1)]
-                                              (if (< lc-tx-id initial-target-tx-id)
-                                                {:status 503,
-                                                 :body (format "Catching up - at: %d, target: %d" lc-tx-id initial-target-tx-id)}
-                                            
-                                                {:status 200, :body "Started."})))}]
+                                     :get (fn [{:keys [^long initial-target-offset, ^LogProcessor log-processor]}]
+                                            (let [lco (.getLatestCompletedOffset log-processor)]
+                                              (into {:headers {"X-XTDB-Target-Offset" (str initial-target-offset)
+                                                               "X-XTDB-Current-Offset" (str lco)}}
+                                                    (if (< lco initial-target-offset)
+                                                      {:status 503,
+                                                       :body (format "Catching up - at: %d, target: %d" lco initial-target-offset)}
+
+                                                      {:status 200,
+                                                       :body "Started."}))))}]
 
                 ["/healthz/alive" {:name :alive
                                    :get (fn [{:keys [log-processor]}]
@@ -69,14 +72,15 @@
 (defmethod ig/prep-key :xtdb/healthz [_ ^HealthzConfig config]
   {:port (.getPort config) 
    :metrics-registry (ig/ref :xtdb.metrics/registry)
+   :log (ig/ref :xtdb/log)
    :log-processor (ig/ref :xtdb.log/processor)
    :node (ig/ref :xtdb/node)})
 
-(defmethod ig/init-key :xtdb/healthz [_ {:keys [node, ^long port, ^CompositeMeterRegistry metrics-registry, log-processor]}]
+(defmethod ig/init-key :xtdb/healthz [_ {:keys [node, ^long port, ^CompositeMeterRegistry metrics-registry, ^Log log, log-processor]}]
   (let [prometheus-registry (PrometheusMeterRegistry. PrometheusConfig/DEFAULT)
         ^Server server (-> (handler {:prometheus-registry prometheus-registry
                                      :log-processor log-processor
-                                     :initial-target-tx-id (xtp/latest-submitted-tx-id node)
+                                     :initial-target-offset (.getLatestSubmittedOffset log)
                                      :node node})
                            (j/run-jetty {:port port, :async? true, :join? false}))]
     (.add metrics-registry prometheus-registry) 
--- src/test/clojure/xtdb/healthz_test.clj
+++ src/test/clojure/xtdb/healthz_test.clj
@@ -1,10 +1,10 @@
 (ns xtdb.healthz-test
-  (:require
-   [clj-http.client :as clj-http]
-   [clojure.test :as t]
-   [xtdb.healthz :as healthz]
-   [xtdb.test-util :as tu]
-   [xtdb.util :as util]))
+  (:require [clj-http.client :as clj-http]
+            [clojure.test :as t]
+            [xtdb.api :as xt]
+            [xtdb.healthz :as healthz]
+            [xtdb.test-util :as tu]
+            [xtdb.util :as util]))
 
 (defn ->healthz-url [port endpoint]
   (format "http://localhost:%s/healthz/%s" port endpoint))
@@ -17,15 +17,53 @@
         (t/testing "started endpoint"
           (let [resp (clj-http/get (->healthz-url port "started"))]
             (t/is (= 200 (:status resp)))
+            (t/is (= {"X-XTDB-Target-Offset" "-1", "X-XTDB-Current-Offset" "-1"}
+                     (-> (:headers resp)
+                         (select-keys ["X-XTDB-Target-Offset" "X-XTDB-Current-Offset"]))))
             (t/is (= "Started." (:body resp)))))
+
         (t/testing "alive endpoint"
           (let [resp (clj-http/get (->healthz-url port "alive"))]
             (t/is (= 200 (:status resp)))
             (t/is (= "Alive." (:body resp)))))
+
         (t/testing "ready endpoint"
           (let [resp (clj-http/get (->healthz-url port "ready"))]
             (t/is (= 200 (:status resp)))
             (t/is (= "Ready." (:body resp)))))))))
+
+(t/deftest test-started-catchup-4273
+  (util/with-tmp-dirs #{local-path}
+    (let [port (tu/free-port)]
+      (with-open [node (tu/->local-node {:node-dir local-path
+                                         :compactor-threads 0})]
+        (xt/execute-tx node [[:put-docs :docs {:xt/id 1, :foo 1}]])
+        (tu/finish-block! node))
+
+      (with-open [_node (tu/->local-node {:node-dir local-path
+                                          :healthz-port port
+                                          :compactor-threads 0})]
+        (t/testing "started endpoint"
+          (let [timeout-at-ms (+ (System/currentTimeMillis) 1000)]
+            (loop []
+              (when (< timeout-at-ms (System/currentTimeMillis))
+                (throw (ex-info "timed out" {})))
+
+              (let [resp (clj-http/get (->healthz-url port "started")
+                                       {:throw-exceptions false})]
+
+                (case (long (:status resp))
+                  503 (do
+                        (t/is (= {"X-XTDB-Target-Offset" "2093"}
+                                 (-> (:headers resp)
+                                     (select-keys ["X-XTDB-Target-Offset"]))))
+                        (Thread/sleep 250)
+                        (recur))
+                  200 (do
+                        (t/is (= {"X-XTDB-Target-Offset" "2093", "X-XTDB-Current-Offset" "2093"}
+                                 (-> (:headers resp)
+                                     (select-keys ["X-XTDB-Target-Offset" "X-XTDB-Current-Offset"]))))
+                        (t/is (= "Started." (:body resp)))))))))))))
 
 (t/deftest test-indexer-error
   (util/with-tmp-dirs #{local-path}
