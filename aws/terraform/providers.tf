bb8be9f30fc761cd9d5449a4b0774bb3e93c6a07
Daniel Mason <dan@juxt.pro>
2025-03-14 14:22:26 +0000

    Add terraform and helm config for AWS. (#4247)
    * Add AWS Terraform Templates.
    * Add AWS Helm Charts.
    * Add AWS to helm workflow.
    * AWS doc updates.
    * Update gitignore - ignore tf files.

---
+++ aws/terraform/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">=1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0, < 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
