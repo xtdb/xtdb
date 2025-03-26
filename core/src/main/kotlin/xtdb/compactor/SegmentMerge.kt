83ab2fea352edc287e47ffbf1b7caa5b97b23d73
Finn Völkel <finn.volkel@gmail.com>
2025-03-26 17:11:19 +0100

    Bitemporal resolution for events in the same transaction when compacting L0 -> L1, #4303

--- core/src/main/kotlin/xtdb/compactor/SegmentMerge.kt
+++ core/src/main/kotlin/xtdb/compactor/SegmentMerge.kt
@@ -20,6 +20,8 @@ import java.nio.file.Path
 import java.time.LocalDate
 import java.util.*
 import java.util.function.Predicate
+import kotlin.Long.Companion.MAX_VALUE
+import kotlin.Long.Companion.MIN_VALUE
 import kotlin.io.path.deleteExisting
 import kotlin.math.min
 import kotlin.Long.Companion.MAX_VALUE as MAX_LONG
@@ -76,48 +78,120 @@ internal class SegmentMerge(private val al: BufferAllocator) : AutoCloseable {
 
     private val outWriters = OutWriters(al)
 
-    private fun MergePlanTask.merge(outWriter: OutWriter, pathFilter: ByteArray?) {
+    private fun resolveSameSystemTimeEvents(dataReader: RelationReader, path: ByteArray) : RelationReader {
+        val isValidPtr = ArrowBufPointer()
+        val startIidPtr = ArrowBufPointer()
+        val curIidPtr = ArrowBufPointer()
+        var curSystemFrom : Long
+
+        val relWriter = Relation(al, dataReader.schema)
+        val iidVec = dataReader["_iid"].rowCopier(relWriter["_iid"])
+        val sysFromVec= dataReader["_system_from"].rowCopier(relWriter["_system_from"])
+        val validFromVec = relWriter["_valid_from"]
+        val validToVec = relWriter["_valid_to"]
+        val opCopier = dataReader["op"].rowCopier(relWriter["op"])
+
+        val evPtr = EventRowPointer(dataReader, path)
+        val polygonCalculator = PolygonCalculator()
+
+        var seenErase = false
+
+        while (evPtr.isValid(isValidPtr, path)) {
+            evPtr.getIidPointer(startIidPtr)
+            curSystemFrom = evPtr.systemFrom
+
+            while(evPtr.isValid(isValidPtr, path) && evPtr.systemFrom == curSystemFrom && startIidPtr == evPtr.getIidPointer(curIidPtr)) {
+
+                when (val polygon = polygonCalculator.calculate(evPtr)) {
+                    // here we are only taking care of an erase that happens within the same transaction
+                    null -> {
+                        if (!seenErase) {
+                            iidVec.copyRow(evPtr.index)
+                            sysFromVec.copyRow(evPtr.index)
+                            validFromVec.writeLong(MIN_VALUE)
+                            validToVec.writeLong(MAX_VALUE)
+                            opCopier.copyRow(evPtr.index)
+                            relWriter.endRow()
+                        }
+                        seenErase = true
+                    }
+                    else -> {
+                        repeat(polygon.validTimeRangeCount) { i ->
+                            if (polygon.getSystemTo(i) > curSystemFrom) {
+                                iidVec.copyRow(evPtr.index)
+                                sysFromVec.copyRow(evPtr.index)
+                                validFromVec.writeLong(polygon.getValidFrom(i))
+                                validToVec.writeLong(polygon.getValidTo(i))
+                                opCopier.copyRow(evPtr.index)
+                                relWriter.endRow()
+                            }
+                        }
+                    }
+                }
+                evPtr.nextIndex()
+            }
+            polygonCalculator.reset()
+            seenErase = false
+        }
+        return relWriter
+    }
+
+    private fun MergePlanTask.merge(outWriter: OutWriter, pathFilter: ByteArray?, bitemporalResolution: Boolean = false) {
         val isValidPtr = ArrowBufPointer()
 
         val mpNodes = mpNodes
         val path = path.let { if (pathFilter == null || it.size > pathFilter.size) it else pathFilter }
         val mergeQueue = PriorityQueue(Comparator.comparing(QueueElem::evPtr, EventRowPointer.comparator()))
 
-        for (dataReader in mpNodes.mapNotNull { it.loadDataPage() }) {
-            val evPtr = EventRowPointer(dataReader, path)
-            val rowCopier = outWriter.rowCopier(dataReader)
+        val dataReaders = mutableListOf<RelationReader>()
 
-            if (evPtr.isValid(isValidPtr, path))
-                mergeQueue.add(QueueElem(evPtr, rowCopier))
-        }
-
-        var seenErase = false
-        val iidPtr = ArrowBufPointer()
-
-        val polygonCalculator = PolygonCalculator()
-
-        while (true) {
-            val elem = mergeQueue.poll() ?: break
-            val (evPtr, rowCopier) = elem
-
-            if (polygonCalculator.currentIidPtr != evPtr.getIidPointer(iidPtr)) seenErase = false
-
-            when (val polygon = polygonCalculator.calculate(evPtr)) {
-                null -> {
-                    if (!seenErase) rowCopier.copyRow(MAX_LONG, evPtr.index)
-                    seenErase = true
+        try {
+            for (dataReader in mpNodes.mapNotNull { it.loadDataPage() }) {
+                val dataReader = dataReader.run {
+                    if (bitemporalResolution) resolveSameSystemTimeEvents(this, path).also { dataReaders.add(it) }
+                    else this
                 }
+                val evPtr = EventRowPointer(dataReader , path)
+                val rowCopier = outWriter.rowCopier(dataReader)
 
-                else -> rowCopier.copyRow(polygon.recency, evPtr.index)
+                if (evPtr.isValid(isValidPtr, path))
+                    mergeQueue.add(QueueElem(evPtr, rowCopier))
             }
 
-            evPtr.nextIndex()
+            var seenErase = false
+            val iidPtr = ArrowBufPointer()
 
-            if (evPtr.isValid(isValidPtr, path))
-                mergeQueue.add(elem)
+            val polygonCalculator = PolygonCalculator()
+
+            while (true) {
+                val elem = mergeQueue.poll() ?: break
+                val (evPtr, rowCopier) = elem
+
+                if (polygonCalculator.currentIidPtr != evPtr.getIidPointer(iidPtr)) seenErase = false
+
+                when (val polygon = polygonCalculator.calculate(evPtr)) {
+                    null -> {
+                        if (!seenErase) rowCopier.copyRow(MAX_LONG, evPtr.index)
+                        seenErase = true
+                    }
+
+                    else ->  rowCopier.copyRow(polygon.recency, evPtr.index)
+                }
+
+                evPtr.nextIndex()
+
+                if (evPtr.isValid(isValidPtr, path))
+                    mergeQueue.add(elem)
+            }
+
+            if (bitemporalResolution) dataReaders.forEach { it.close() }
+
+            outWriter.endPage(ByteArrayList.from(*this.path))
+
+        } catch (e: Throwable) {
+            if (bitemporalResolution) dataReaders.forEach { it.close() }
+            throw e
         }
-
-        outWriter.endPage(ByteArrayList.from(*this.path))
     }
 
     sealed interface RecencyPartitioning {
@@ -135,16 +209,16 @@ internal class SegmentMerge(private val al: BufferAllocator) : AutoCloseable {
     ): Results {
         val schema = logDataRelSchema(segments.map { it.dataRel!!.schema })
 
-        val outWriter = when(recencyPartitioning) {
-            RecencyPartitioning.Partition -> outWriters.PartitionedOutWriter(schema, recencyPartition)
-            is RecencyPartitioning.Preserve -> outWriters.OutRel(schema, recency = recencyPartitioning.recency)
+        val (outWriter, bitemporalResolution)= when(recencyPartitioning) {
+            RecencyPartitioning.Partition -> outWriters.PartitionedOutWriter(schema, recencyPartition) to true
+            is RecencyPartitioning.Preserve -> outWriters.OutRel(schema, recency = recencyPartitioning.recency) to false
         }
 
         return outWriter.use {
             for (task in segments.toMergePlan(pathFilter?.toPathPredicate())) {
                 if (Thread.interrupted()) throw InterruptedException()
 
-                task.merge(it, pathFilter)
+                task.merge(it, pathFilter, bitemporalResolution)
             }
 
             it.end()
