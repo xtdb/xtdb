91ce1b11557d2af5b3327f72470b4dbe7811f400
Finn Völkel <finn.volkel@gmail.com>
2025-04-21 14:42:57 +0200

    Roundtripping keywords in next.jdbc, adding new oid for keywords

--- jdbc/src/main/clojure/xtdb/next/jdbc.clj
+++ jdbc/src/main/clojure/xtdb/next/jdbc.clj
@@ -98,6 +98,9 @@
 (defmethod <-pg-obj "jsonb" [^PGobject obj]
   (JsonSerde/decode (.getValue obj)))
 
+(defmethod <-pg-obj "keyword" [^PGobject obj]
+  (keyword (.getValue obj)))
+
 (defmethod <-pg-obj :default [^PGobject obj]
   obj)
 
