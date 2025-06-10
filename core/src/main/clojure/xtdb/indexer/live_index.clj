a03ad2394bef2b0153c911f480143aa187ed1895
Finn Völkel <finn.volkel@gmail.com>
2025-06-10 12:48:13 +0200

    Remove fast-tripping of new tries in live-index

--- core/src/main/clojure/xtdb/indexer/live_index.clj
+++ core/src/main/clojure/xtdb/indexer/live_index.clj
@@ -175,11 +175,9 @@
                                    (util/rethrowing-cause))]
             (let [added-tries (for [[table-name {:keys [trie-key data-file-size trie-metadata state]}] table-metadata]
                                 (trie/->trie-details table-name trie-key data-file-size trie-metadata state))]
-              ;; TODO remove this if once we need as-of-system-time
+              (.appendMessage log (Log$Message$TriesAdded. Storage/VERSION added-tries))
               (doseq [^TrieDetails added-trie added-tries]
-                (.addTries trie-cat (.getTableName added-trie) [added-trie]))
-              (.appendMessage log (Log$Message$TriesAdded. Storage/VERSION added-tries)))
-
+                (.addTries trie-cat (.getTableName added-trie) [added-trie] (.getSystemTime latest-completed-tx))))
             (let [all-tables (set (concat (keys table-metadata) (.getAllTableNames block-cat)))
                   table->all-tries (->> all-tables
                                         (map (fn [table-name]
