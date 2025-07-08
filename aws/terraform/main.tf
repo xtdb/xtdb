e1d5d3e3ba6a11d35be940ba946efb679f3630ec
Tim Greene <tim.g.greene@gmail.com>
2025-07-08 11:19:02 +0100

    Add k8s policies to iam role

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -122,10 +122,14 @@ module "xtdb_eks" {
     bastion = {
       principal_arn     = aws_iam_role.bastion.arn
       type             = "STANDARD"
-      kubernetes_groups = ["system:masters"]
-      kubernetes_username = "bastion-admin"
       policy_associations = {
         admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+        clusteradmin = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
             type = "cluster"
