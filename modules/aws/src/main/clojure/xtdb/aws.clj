89764a16cdea999fe63ec8c79b9de9cdc9445461
James Henderson <james@jarohen.dev>
2025-08-06 12:17:54 +0100

    add `path-style-access-enabled?` config to EDN S3 config

--- modules/aws/src/main/clojure/xtdb/aws.clj
+++ modules/aws/src/main/clojure/xtdb/aws.clj
@@ -4,9 +4,11 @@
   (:import [xtdb.aws S3]
            [xtdb.aws.s3 S3Configurator]))
 
-(defmethod bp/->object-store-factory ::s3 [_ {:keys [bucket ^S3Configurator configurator prefix credentials endpoint]}]
+(defmethod bp/->object-store-factory ::s3 [_ {:keys [bucket ^S3Configurator configurator prefix credentials endpoint
+                                                     path-style-access-enabled?]}]
   (cond-> (S3/s3 bucket)
     configurator (.s3Configurator configurator)
     prefix (.prefix (util/->path prefix))
     credentials (.credentials (:access-key credentials) (:secret-key credentials))
-    endpoint (.endpoint endpoint)))
+    endpoint (.endpoint endpoint)
+    (some? path-style-access-enabled?) (.pathStyleAccessEnabled (boolean path-style-access-enabled?))))
