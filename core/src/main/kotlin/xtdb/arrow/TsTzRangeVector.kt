497517487cef43f77fbbb5487d5ac8ea052af9c5
James Henderson <james@jarohen.dev>
2025-09-18 17:42:40 +0100

    ensuring xt-arrow TsTzRangeVector returns nulls from getObject when from/to are min/max-long respectively

--- core/src/main/kotlin/xtdb/arrow/TsTzRangeVector.kt
+++ core/src/main/kotlin/xtdb/arrow/TsTzRangeVector.kt
@@ -6,26 +6,32 @@ import org.apache.arrow.vector.types.pojo.ArrowType.Timestamp
 import org.apache.arrow.vector.types.pojo.FieldType
 import xtdb.api.query.IKeyFn
 import xtdb.arrow.metadata.MetadataFlavour
+import xtdb.time.microsAsInstant
 import xtdb.types.ZonedDateTimeRange
 import xtdb.vector.extensions.TsTzRangeType
+import java.time.ZoneId
 import java.time.ZonedDateTime
 
 class TsTzRangeVector(override val inner: FixedSizeListVector) : ExtensionVector(), MetadataFlavour.Presence {
+    companion object {
+        private fun Long.toZdt(): ZonedDateTime = microsAsInstant.atZone(ZoneId.of("UTC"))
+    }
+
     override val type = TsTzRangeType
 
     override fun getObject0(idx: Int, keyFn: IKeyFn<*>) =
-        inner.listElements.let {
+        inner.listElements.let { elVec ->
             ZonedDateTimeRange(
-                it.getObject(idx * 2) as ZonedDateTime?,
-                it.getObject(idx * 2 + 1) as? ZonedDateTime?
+                elVec.getLong(idx * 2).takeUnless { it == Long.MIN_VALUE }?.toZdt(),
+                elVec.getLong(idx * 2 + 1).takeUnless { it == Long.MAX_VALUE }?.toZdt()
             )
         }
 
     override fun writeObject0(value: Any) = when (value) {
         is ZonedDateTimeRange -> {
-            inner.getListElements(FieldType.notNullable(Timestamp(MICROSECOND, "UTC"))).let {
-                if (value.from != null) it.writeObject(value.from) else it.writeLong(Long.MIN_VALUE)
-                if (value.to != null) it.writeObject(value.to) else it.writeLong(Long.MAX_VALUE)
+            inner.getListElements(FieldType.notNullable(Timestamp(MICROSECOND, "UTC"))).let { elVec ->
+                if (value.from != null) elVec.writeObject(value.from) else elVec.writeLong(Long.MIN_VALUE)
+                if (value.to != null) elVec.writeObject(value.to) else elVec.writeLong(Long.MAX_VALUE)
             }
             inner.endList()
         }
