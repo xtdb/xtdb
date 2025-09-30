71744fa4fb8f2e2652ff694b46e695654b3e215f
Finn Völkel <finn.volkel@gmail.com>
2025-09-30 12:46:33 +0200

    Uncomment testcase for #4693

--- src/test/clojure/xtdb/vector_test.clj
+++ src/test/clojure/xtdb/vector_test.clj
@@ -19,13 +19,13 @@
              (xt/q tu/*node* "SELECT * FROM docs ORDER BY _id")))))
 
 (t/deftest nested-null-in-live-index-4693
-  #_ ; Upstream issue: https://github.com/apache/arrow-java/issues/830
+  ;; Upstream issue: https://github.com/apache/arrow-java/issues/830
   (binding [c/*ignore-signal-block?* true]
     (xt/execute-tx tu/*node* [[:put-docs :docs {:a {:d nil}, :xt/id 0}]])
 
     (xt/execute-tx tu/*node* [[:put-docs :docs {:xt/id 1}]])
 
-    (t/is (= [{:xt/id 0}] (xt/q tu/*node* "SELECT * FROM docs ORDER BY _id")))))
+    (t/is (= [{:xt/id 0, :a {}} {:xt/id 1}] (xt/q tu/*node* "SELECT * FROM docs ORDER BY _id")))))
 
 (t/deftest set-field-mismatch-4690
   (binding [c/*ignore-signal-block?* true]
