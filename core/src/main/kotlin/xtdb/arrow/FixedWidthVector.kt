xtdb/xtdb

commit 31d276f493ff8bd1b46b3167e5f748d11866e193

don't need to get a heap byte array out for FWV.hashcode, hasher can take a byte-buf

core/src/main/kotlin/xtdb/arrow/FixedWidthVector.kt
@@ -153,7 +153,7 @@ sealed class FixedWidthVector : Vector() {
     override val metadataFlavours get() = listOf(this as MetadataFlavour)
 
     override fun hashCode0(idx: Int, hasher: Hasher) =
-        hasher.hash(getByteArray(idx))
+        hasher.hash(getBytes(idx))
 
     override fun rowCopier0(src: VectorReader): RowCopier {
         nullable = nullable || src.nullable

commit c15d8a87f87621c948fa98c7a3e0ed3505ca045d

fix getBytes abstract-method-error introduced in 31d276f4

core/src/main/kotlin/xtdb/arrow/FixedWidthVector.kt
@@ -153,7 +153,7 @@ sealed class FixedWidthVector : Vector() {
     override val metadataFlavours get() = listOf(this as MetadataFlavour)
 
     override fun hashCode0(idx: Int, hasher: Hasher) =
-        hasher.hash(getBytes(idx))
+        hasher.hash(getBytes0(idx))
 
     override fun rowCopier0(src: VectorReader): RowCopier {
         nullable = nullable || src.nullable