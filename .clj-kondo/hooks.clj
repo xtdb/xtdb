c366ec4daf569ff089090eaf78947bc66e5fbfa0
Tim Greene <tim.g.greene@gmail.com>
2025-11-03 11:18:20 +0000

    Add clj-kondo config for with-tmp-dirs

--- .clj-kondo/hooks.clj
+++ .clj-kondo/hooks.clj
@@ -14,3 +14,18 @@
                  (list (kondo/token-node 'quote) nms))
 
                 (kondo/vector-node (list min-arity max-arity))))})
+
+(defn with-tmp-dirs [{{[_with-tmp-dirs binding-set & body] :children} :node}]
+  (let [syms (some-> binding-set kondo/sexpr seq)
+        body (or (seq body) [(kondo/token-node nil)])]
+    (if (seq syms)
+      (let [binding-nodes (mapcat (fn [sym]
+                                    [(kondo/token-node sym)
+                                     (kondo/token-node nil)])
+                                  syms)]
+        {:node (kondo/list-node
+                (concat [(kondo/token-node 'let)
+                         (kondo/vector-node binding-nodes)]
+                        body))})
+      {:node (kondo/list-node
+              (cons (kondo/token-node 'do) body))})))
