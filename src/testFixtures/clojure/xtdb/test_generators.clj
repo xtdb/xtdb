1d2a390081ebec7ffe5fdd8cf8ead54bb32cbd04
Daniel Mason <dan@juxt.pro>
2026-01-05 14:26:18 +0000

    test: update float generation to not generate infinite/NaN.

--- src/testFixtures/clojure/xtdb/test_generators.clj
+++ src/testFixtures/clojure/xtdb/test_generators.clj
@@ -21,7 +21,7 @@
 (def i16-gen (gen/let [v (gen/choose Short/MIN_VALUE Short/MAX_VALUE)] (short v)))
 (def i32-gen (gen/let [v (gen/choose Integer/MIN_VALUE Integer/MAX_VALUE)] (int v)))
 (def i64-gen (gen/choose Long/MIN_VALUE Long/MAX_VALUE))
-(def f64-gen gen/double)
+(def f64-gen (gen/double* {:infinite? false :NaN? false}))
 (def decimal-gen (gen/let [^double v (gen/double* {:infinite? false :NaN? false})]
                    (BigDecimal/valueOf v)))
 
