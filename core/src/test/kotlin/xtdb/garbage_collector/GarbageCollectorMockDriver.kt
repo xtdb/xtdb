9e0d36061f67b11c0767405b73cdf351e8cf1a1a
Daniel Mason <dan@juxt.pro>
2026-01-06 14:17:24 +0000

    test: Add tracking of deletedPaths/deletedTrieKeys to GC mock driver.

--- core/src/test/kotlin/xtdb/garbage_collector/GarbageCollectorMockDriver.kt
+++ core/src/test/kotlin/xtdb/garbage_collector/GarbageCollectorMockDriver.kt
@@ -13,21 +13,29 @@ private val LOGGER = GarbageCollectorMockDriver::class.logger
 class GarbageCollectorMockDriver() : GarbageCollector.Driver.Factory {
     var nextSystemId = 0
 
+    // Track all deleted paths and trie keys across all systems
+    val deletedPaths = mutableListOf<Path>()
+    val deletedTrieKeys = mutableMapOf<TableRef, MutableSet<TrieKey>>()
+
     override fun create(db: IDatabase) = ForDatabase(db, nextSystemId++)
 
-    class ForDatabase(val db: IDatabase, val systemId: Int) : GarbageCollector.Driver {
+    inner class ForDatabase(val db: IDatabase, val systemId: Int) : GarbageCollector.Driver {
         private val bufferPool = db.bufferPool
         private val trieCatalog = db.trieCatalog
 
         override suspend fun deletePath(path: Path) {
             yield()
             LOGGER.debug("systemId=$systemId Deleting path $path")
+            synchronized(deletedPaths) { deletedPaths.add(path) }
             bufferPool.deleteIfExists(path)
         }
 
         override suspend fun deleteTries(tableName: TableRef, trieKeys: Set<TrieKey>) {
             yield()
             LOGGER.debug("systemId=$systemId Removing ${trieKeys.size} tries from catalog for table $tableName: $trieKeys")
+            synchronized(deletedTrieKeys) {
+                deletedTrieKeys.getOrPut(tableName) { mutableSetOf() }.addAll(trieKeys)
+            }
             trieCatalog.deleteTries(tableName, trieKeys)
         }
 
