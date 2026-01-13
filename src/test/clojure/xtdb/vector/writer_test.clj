ed8a008bdc33e1c4a453e4b22c85c9afe4f36f06
James Henderson <james@jarohen.dev>
2026-01-13 22:15:59 +0000

    tidy: use getField in writer_test to check the returned field

--- src/test/clojure/xtdb/vector/writer_test.clj
+++ src/test/clojure/xtdb/vector/writer_test.clj
@@ -1,210 +1,209 @@
 (ns xtdb.vector.writer-test
   (:require [clojure.test :as t :refer [deftest]]
             [xtdb.test-util :as tu])
-  (:import (org.apache.arrow.vector.types Types$MinorType)
-           (xtdb.arrow DenseUnionVector ListVector StructVector Vector)))
+  (:import (xtdb.arrow DenseUnionVector ListVector StructVector Vector)))
 
 (t/use-fixtures :each tu/with-allocator tu/with-node)
 
 (deftest adding-legs-to-dense-union
   (with-open [duv (DenseUnionVector. tu/*allocator* "my-duv")]
-    (t/is (= #xt/type [:union :i64]
+    (t/is (= #xt/field {"my-duv" [:union :i64]}
              (-> duv
                  (doto (.writeObject 42))
-                 (.getType)))))
+                 (.getField)))))
 
   (with-open [duv (DenseUnionVector. tu/*allocator* "my-duv")]
     (let [my-list-wtr (.vectorFor duv "list" #xt.arrow/type :list false)
           my-set-wtr (.vectorFor duv "set" #xt.arrow/type :set false)]
 
-      (t/is (= #xt/type [:union [:list :null] [:set :null]]
-               (.getType duv))
+      (t/is (= #xt/field {"my-duv" [:union [:list :null] [:set :null]]}
+               (.getField duv))
             "vectorFor creates lists/sets with uninitialized data vectors")
 
       (.getListElements my-list-wtr #xt.arrow/type :i64 false)
 
       (.getListElements my-set-wtr #xt.arrow/type :f64 false)
 
-      (t/is (= #xt/type [:union [:list :i64] [:set :f64]]
-               (.getType duv)))
+      (t/is (= #xt/field {"my-duv" [:union [:list :i64] [:set :f64]]}
+               (.getField duv)))
 
       (.getListElements my-list-wtr #xt.arrow/type :f64 false)
 
-      (t/is (= #xt/type [:union [:list [:union :i64 :f64]] [:set :f64]]
-               (.getType duv)))))
+      (t/is (= #xt/field {"my-duv" [:union [:list [:union :i64 :f64]] [:set :f64]]}
+               (.getField duv)))))
 
   (with-open [duv (DenseUnionVector. tu/*allocator* "my-duv")]
     (let [my-struct-wtr (.vectorFor duv "struct" #xt.arrow/type :struct false)]
-      (t/is (= #xt/type [:union :struct]
-               (.getType duv)))
+      (t/is (= #xt/field {"my-duv" [:union :struct]}
+               (.getField duv)))
 
       (let [a-wtr (.vectorFor my-struct-wtr "a" #xt.arrow/type :union false)]
-        (t/is (= #xt/type [:union [:struct {"a" :union}]]
-                 (.getType duv)))
+        (t/is (= #xt/field {"my-duv" [:union [:struct {"a" :union}]]}
+                 (.getField duv)))
 
-        (.vectorFor a-wtr "i64" (.getType Types$MinorType/BIGINT) false)
+        (.vectorFor a-wtr "i64" #xt.arrow/type :i64 false)
 
-        (t/is (= #xt/type [:union [:struct {"a" [:union :i64]}]]
-                 (.getType duv)))
+        (t/is (= #xt/field {"my-duv" [:union [:struct {"a" [:union :i64]}]]}
+                 (.getField duv)))
 
         (-> (.vectorFor my-struct-wtr "b" #xt.arrow/type :union false)
-            (.vectorFor "f64" (.getType Types$MinorType/FLOAT8) false))
+            (.vectorFor "f64" #xt.arrow/type :f64 false))
 
-        (.vectorFor a-wtr "utf8" (.getType Types$MinorType/VARCHAR) false)
+        (.vectorFor a-wtr "utf8" #xt.arrow/type :utf8 false)
 
-        (t/is (= #xt/type [:union
-                           {"struct" [:struct {"a" [:union {"i64" :i64, "utf8" :utf8}],
-                                               "b" [:union {"f64" :f64}]}]}]
-                 (.getType duv)))))))
+        (t/is (= #xt/field {"my-duv" [:union
+                                      {"struct" [:struct {"a" [:union {"i64" :i64, "utf8" :utf8}],
+                                                          "b" [:union {"f64" :f64}]}]}]}
+                 (.getField duv)))))))
 
 (deftest list-writer-data-vec-transition
   (with-open [list-vec (ListVector. tu/*allocator* "my-list" false)]
     (t/testing "null vector initially"
-      (t/is (= #xt/type [:list [:? :null]]
-               (.getType list-vec)))
+      (t/is (= #xt/field {"my-list" [:list [:? :null]]}
+               (.getField list-vec)))
 
-      (t/is (= #xt/type [:list [:? :null]]
+      (t/is (= #xt/field {"my-list" [:list [:? :null]]}
                (-> list-vec
                    (doto (.getListElements))
-                   (.getType)))
+                   (.getField)))
             "call getListElements initializes it with a null vector")
 
-      (t/is (= #xt/type [:list :i64]
+      (t/is (= #xt/field {"my-list" [:list :i64]}
                (-> list-vec
                    (doto (.getListElements #xt.arrow/type :i64 false))
-                   (.getType)))
+                   (.getField)))
             "asking for an i64 with an empty data vec swaps for a mono")))
 
   (with-open [list-vec (ListVector. tu/*allocator* "my-list" false)]
-    (t/is (= #xt/type [:list :i64]
+    (t/is (= #xt/field {"my-list" [:list :i64]}
              (-> list-vec
                  (doto (.getListElements #xt.arrow/type :i64 false))
                  (doto (.getListElements #xt.arrow/type :i64 false))
-                 (.getType)))
+                 (.getField)))
           "explicit monomorphic :i64 requested")
 
-    (t/is (= #xt/type :i64
+    (t/is (= #xt/field {"$data$" :i64}
              (-> list-vec
                  (.getListElements)
-                 (.getType)))
+                 (.getField)))
           "nested field correct")
 
-    (t/is (= #xt/type [:list [:union :i64 :f64]]
+    (t/is (= #xt/field {"my-list" [:list [:union :i64 :f64]]}
              (-> list-vec
                  (doto (.getListElements #xt.arrow/type :f64 false))
-                 (.getType)))
+                 (.getField)))
           "asking for an f64 promotes"))
 
   (with-open [list-vec (Vector/open tu/*allocator* #xt/field {"my-list" [:list :i64]})]
     (t/testing "already initialized arrow vector"
-      (t/is (= #xt/type [:list :i64]
-               (.getType list-vec)))
+      (t/is (= #xt/field {"my-list" [:list :i64]}
+               (.getField list-vec)))
 
-      (t/is (= #xt/type :i64
+      (t/is (= #xt/field {"$data$" :i64}
                (-> list-vec
                    (.getListElements)
-                   (.getType)))
+                   (.getField)))
             "call listElementWriter honours the underlying vector")
 
-      (t/is (= #xt/type :i64
+      (t/is (= #xt/field {"$data$" :i64}
                (-> list-vec
                    (.getListElements #xt.arrow/type :i64 false)
-                   (.getType)))
+                   (.getField)))
             "can ask for :i64")
 
-      (t/is (= #xt/type [:list [:union :i64 :f64]]
+      (t/is (= #xt/field {"my-list" [:list [:union :i64 :f64]]}
                (-> list-vec
                    (doto (.getListElements #xt.arrow/type :f64 false))
-                   (.getType)))
+                   (.getField)))
             "asking for f64 promotes"))))
 
 (deftest adding-nested-struct-fields-dynamically
   (with-open [struct-vec (StructVector. tu/*allocator* "my-struct" false)]
-    (t/is (= #xt/type :struct
-             (.getType struct-vec)))
+    (t/is (= #xt/field {"my-struct" :struct}
+             (.getField struct-vec)))
 
-    (t/is (= #xt/type [:? :null]
+    (t/is (= #xt/field {"foo" [:? :null]}
              (-> struct-vec
                  (.vectorFor "foo" #xt.arrow/type :null true)
-                 (.getType)))
+                 (.getField)))
           "call to vectorFor with a field-type creates the key")
 
-    (t/is (= #xt/type [:struct {"foo" :f64}]
+    (t/is (= #xt/field {"my-struct" [:struct {"foo" :f64}]}
              (-> struct-vec
                  (doto (.vectorFor "foo" #xt.arrow/type :f64 false))
-                 (.getType)))
+                 (.getField)))
           "new type promotes the struct key")
 
-    (t/is (= #xt/type [:struct {"foo" :f64, "bar" :i64}]
+    (t/is (= #xt/field {"my-struct" [:struct {"foo" :f64, "bar" :i64}]}
              (-> struct-vec
                  (doto (.vectorFor "bar" #xt.arrow/type :i64 false))
                  (doto (.vectorFor "bar" #xt.arrow/type :i64 false))
-                 (.getType))))
+                 (.getField))))
 
-    (t/is (= #xt/type :i64
+    (t/is (= #xt/field {"bar" :i64}
              (-> struct-vec
                  (.vectorFor "bar" #xt.arrow/type :i64 false)
-                 (.getType))))
+                 (.getField))))
 
-    (t/is (= #xt/type :i64
+    (t/is (= #xt/field {"bar" :i64}
              (-> struct-vec
                  (.vectorFor "bar")
-                 (.getType))))
+                 (.getField))))
 
-    (t/is (= #xt/type [:struct {"foo" :f64, "bar" [:union :i64 :f64]}]
+    (t/is (= #xt/field {"my-struct" [:struct {"foo" :f64, "bar" [:union :i64 :f64]}]}
              (-> struct-vec
                  (doto (.vectorFor "bar" #xt.arrow/type :f64 false))
-                 (.getType)))))
+                 (.getField)))))
 
   (with-open [struct-vec (Vector/open tu/*allocator* #xt/field {"my-struct" [:struct {"baz" :i64}]})]
-    (t/is (= #xt/type [:struct {"baz" :i64}]
-             (.getType struct-vec)))
+    (t/is (= #xt/field {"my-struct" [:struct {"baz" :i64}]}
+             (.getField struct-vec)))
 
-    (t/is (= #xt/type :i64
+    (t/is (= #xt/field {"baz" :i64}
              (-> struct-vec
                  (.vectorFor "baz")
-                 (.getType)))
+                 (.getField)))
           "vectorFor should return the writer for the existing field")
 
-    (t/is (= #xt/type :i64
+    (t/is (= #xt/field {"baz" :i64}
              (-> struct-vec
                  (.vectorFor "baz" #xt.arrow/type :i64 false)
-                 (.getType)))
+                 (.getField)))
           "call to vectorFor with correct field should not throw")
 
-    (t/is (= #xt/type [:struct {"baz" [:union :i64 :f64]}]
+    (t/is (= #xt/field {"my-struct" [:struct {"baz" [:union :i64 :f64]}]}
              (-> struct-vec
                  (doto (.vectorFor "baz" #xt.arrow/type :f64 false))
-                 (.getType)))
+                 (.getField)))
           "vectorFor promotes non pre-existing union type")))
 
 (deftest rel-writer-dynamic-testing
   (with-open [rel (tu/open-rel)]
-    (t/is (= #xt/type :union
+    (t/is (= #xt/field {"my-union" :union}
              (-> rel
                  (.vectorFor "my-union" #xt.arrow/type :union false)
-                 (.getType))))
+                 (.getField))))
 
-    (t/is (= #xt/type :union
+    (t/is (= #xt/field {"my-union" :union}
              (-> rel
                  (.vectorFor "my-union" #xt.arrow/type :union false)
-                 (.getType))))
+                 (.getField))))
 
-    (t/is (= #xt/type [:union :i64]
+    (t/is (= #xt/field {"my-union" [:union :i64]}
              (-> rel
                  (.vectorFor "my-union" #xt.arrow/type :i64 false)
-                 (.getType))))
+                 (.getField))))
 
-    (t/is (= #xt/type :i64
+    (t/is (= #xt/field {"my-i64" :i64}
              (-> rel
                  (doto (.vectorFor "my-i64" #xt.arrow/type :i64 false))
                  (.vectorFor "my-i64" #xt.arrow/type :i64 false)
-                 (.getType))))
+                 (.getField))))
 
-    (t/is (= #xt/type [:union :i64 :f64]
+    (t/is (= #xt/field {"my-i64" [:union :i64 :f64]}
              (-> rel
                  (.vectorFor "my-i64" #xt.arrow/type :f64 false)
-                 (.getType)))))
+                 (.getField)))))
 
   (with-open [rel (tu/open-rel)]
     (let [int-wtr (.vectorFor rel "my-int" #xt.arrow/type :i64 false)
